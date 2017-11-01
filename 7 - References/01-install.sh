13647054c7be31238746951f1a48f06dc2ff5b77
Admin9705 <user@dunn.cloud>
2017-11-01 18:24:20 -0400

    Update 01-install.sh

--- 7 - References/01-install.sh
+++ 7 - References/01-install.sh
@@ -1,28 +1,6 @@
-############## Ask About SSH Install
-
-echo -n "Do you want to install SSH on your Server (y/n)? "
-old_stty_cfg=$(stty -g)
-stty raw -echo
-answer=$( while ! head -c 1 | grep -i '[ny]' ;do true ;done )
-stty $old_stty_cfg
-if echo "$answer" | grep -iq "^y" ;then
-    echo Yes;
-    yes | apt-get install openssh-server
-    clear
-    echo Installed OpenSSH-Server
-    echo 
-    echo To utilize SSH, use Port 22 with  IP Address; hostname -I;
-    echo
-else
-    echo No
-    clear
-    echo Not Installed - OpenSSH-Server
-    echo 
-fi
-
 ################### Install Required Programs
 
-echo -n "Do you want to install all Required Programs (y/n)? "
+echo -n "Do you agree to install "The Awesome Plex Server (y/n)? "
 old_stty_cfg=$(stty -g)
 stty raw -echo
 answer=$( while ! head -c 1 | grep -i '[ny]' ;do true ;done )
@@ -45,8 +23,30 @@ if echo "$answer" | grep -iq "^y" ;then
 else
     echo No
     clear
-    echo Program Stopped - Required programs are needed
+    echo Install Aborted! You failed to agree to the install
     exit
+    echo 
+fi
+
+############## Ask About SSH Install
+
+echo -n "Do you want to install SSH on your Server (y/n)? "
+old_stty_cfg=$(stty -g)
+stty raw -echo
+answer=$( while ! head -c 1 | grep -i '[ny]' ;do true ;done )
+stty $old_stty_cfg
+if echo "$answer" | grep -iq "^y" ;then
+    echo Yes;
+    yes | apt-get install openssh-server
+    clear
+    echo Installed OpenSSH-Server
+    echo 
+    echo To utilize SSH, use Port 22 with  IP Address; hostname -I;
+    echo
+else
+    echo No
+    clear
+    echo Not Installed - OpenSSH-Server
     echo 
 fi
 
