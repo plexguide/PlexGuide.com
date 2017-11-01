0f1517c04e178b905e8bc162372abf1a85c97076
Admin9705 <user@dunn.cloud>
2017-11-01 19:07:17 -0400

    Update 01-install.sh

--- 7 - References/01-install.sh
+++ 7 - References/01-install.sh
@@ -76,28 +76,5 @@ else
     echo 
 fi
 
-################# Install Plex
-
-echo -n "Do you want to Plex? (y/n)? "
-old_stty_cfg=$(stty -g)
-stty raw -echo
-answer=$( while ! head -c 1 | grep -i '[ny]' ;do true ;done )
-stty $old_stty_cfg
-if echo "$answer" | grep -iq "^y" ;then
-    echo Yes;
-    echo deb https://downloads.plex.tv/repo/deb ./public main | sudo tee /etc/apt/sources.list.d/plexmediaserver.list
-    curl https://downloads.plex.tv/plex-keys/PlexSign.key | sudo apt-key add -
-    yes | apt-get update    
-    yes | apt-get install plexmediaserver
-    echo Installed NetData
-    echo 
-    echo To utilize NetData, use Port 19999 with IP Address; hostname -I;
-    echo
-else
-    echo No
-    clear
-    echo Not Installed - Plex Media Server
-    echo 
-fi
- 
+bash plex.sh
 bash sabnzbd.sh
