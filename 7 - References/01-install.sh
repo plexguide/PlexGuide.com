713ebbeab559eb622a55cdd6113e35722c814e20
Admin9705 <user@dunn.cloud>
2017-11-01 18:43:41 -0400

    Update 01-install.sh

--- 7 - References/01-install.sh
+++ 7 - References/01-install.sh
@@ -1,32 +1,4 @@
-################### Install Required Programs
 
-echo -n "Do you Agree to Install the Awesome Plex Server (y/n)? "
-old_stty_cfg=$(stty -g)
-stty raw -echo
-answer=$( while ! head -c 1 | grep -i '[ny]' ;do true ;done )
-stty $old_stty_cfg
-if echo "$answer" | grep -iq "^y" ;then
-    echo Yes;
-    yes | apt-get update
-    yes | apt-get install nano
-    yes | apt-get install fuse
-    yes | apt-get install man-db
-    yes | apt-get install screen
-    yes | apt-get install unzip
-    yes | apt-get install python
-    yes | apt-get install software-properties-common    
-    clear
-    echo Installed Required Programs
-    echo 
-    echo Installed update, nano, fuse, man-db, screen, unzip, python, curl
-    echo
-else
-    echo No
-    clear
-    echo Install Aborted - You failed to agree to the install
-    exit
-    echo 
-fi
 
 ############## Ask About SSH Install
 
