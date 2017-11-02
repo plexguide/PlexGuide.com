a35af2184cee25569213adf5087837238083310f
Admin9705 <user@dunn.cloud>
2017-11-01 20:55:05 -0400

    Update 01-install.sh

--- 7 - References/01-install.sh
+++ 7 - References/01-install.sh
@@ -13,6 +13,7 @@ do
     case $opt in
         "Mass Install")
             bash mass.sh
+            bass ssh.sh  
             ;;
         "Install Individual Programs")
             echo "Not Enabled Yet"
