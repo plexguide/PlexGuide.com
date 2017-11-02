751765c16a96353a96ff369d8be637c4023d4a9e
Admin9705 <user@dunn.cloud>
2017-11-01 21:22:18 -0400

    Update 01-install.sh

--- 7 - References/01-install.sh
+++ 7 - References/01-install.sh
@@ -1,32 +1,76 @@
 #!/bin/bash
+# A menu driven shell script sample template 
+## ----------------------------------
+# Step #1: Define variables
+# ----------------------------------
+EDITOR=vim
+PASSWD=/etc/passwd
+RED='\033[0;41;30m'
+STD='\033[0;0;39m'
+ 
+# ----------------------------------
+# Step #2: User defined function
+# ----------------------------------
+pause(){
+  read -p "Press [Enter] key to continue..." fackEnterKey
+}
 
-clear
-echo "Welcome to the Awesome Plex Server ~ PlexGuide.com"
-echo
-echo "Warning: Only Select Mass Install for a Clean Server!"
-echo
-
-PS3='Please Make a Choice: '
-options=("Mass Install" "Install Individual Programs" "Update Individual Programs" "Quit")
-select opt in "${options[@]}"
-do
-    case $opt in
-        "Mass Install")
+one(){
+	echo "one() called"
+        pause
+}
+ 
+# do something in two()
+two(){
+	echo "two() called"
+        pause
+}
+ 
+# function to display menus
+show_menus() {
+	clear
+	echo "~~~~~~~~~~~~~~~~~~~~~"	
+	echo " M A I N - M E N U"
+	echo "~~~~~~~~~~~~~~~~~~~~~"
+	echo "1. Set Terminal"
+	echo "2. Reset Terminal"
+	echo "3. Exit"
+}
+# read input from the keyboard and take a action
+# invoke the one() when the user select 1 from the menu option.
+# invoke the two() when the user select 2 from the menu option.
+# Exit when user the user select 3 form the menu option.
+read_options(){
+	local choice
+	read -p "Enter choice [ 1 - 3] " choice
+	case $choice in
+		1) 
             bash mass.sh
             bash ssh.sh  
             bash netdata.sh
             bash plex.sh
             bash sabnzbd.sh
-            ;;
-        "Install Individual Programs")
-            echo "Not Enabled Yet"
-            ;;
-        "Update Individual Programs")
-            echo "Not Enabled Yet"
-            ;;
-        "Quit")
-            break
-            ;;
-        *) echo invalid option;;
-    esac
+        ;;
+		2) two ;;
+		3) exit 0;;
+		*) echo -e "${RED}Error...${STD}" && sleep 2
+	esac
+}
+ 
+# ----------------------------------------------
+# Step #3: Trap CTRL+C, CTRL+Z and quit singles
+# ----------------------------------------------
+trap '' SIGINT SIGQUIT SIGTSTP
+ 
+# -----------------------------------
+# Step #4: Main logic - infinite loop
+# ------------------------------------
+while true
+do
+ 
+	show_menus
+	read_options
 done
+
+
+            
