794f744dcf1e2496bb4b4b7e7ee28d4f76d160e5
Admin9705 <user@dunn.cloud>
2017-11-02 06:35:04 -0400

    Update docker.sh

--- 7 - References/docker.sh
+++ 7 - References/docker.sh
@@ -1,5 +1,17 @@
 #!/bin/bash
 
-curl -sSL https://get.docker.com | sh
-sudo apt install docker-compose
-sudo docker-compose -f docker-compose.yml up -d
+echo -n "Do you want to install Docker | PlexPy | Mumimux | Portainer (y/n)? "
+old_stty_cfg=$(stty -g)
+stty raw -echo
+answer=$( while ! head -c 1 | grep -i '[ny]' ;do true ;done )
+stty $old_stty_cfg
+if echo "$answer" | grep -iq "^y" ;then
+   curl -sSL https://get.docker.com | sh
+   sudo apt install docker-compose
+   sudo docker-compose -f docker-compose.yml up -d
+else
+    echo No
+    clear
+    echo Not Installed - Docker | PlexPy | Mumimux | Portainer
+    echo 
+fi
