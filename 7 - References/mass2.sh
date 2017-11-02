26bcfc333aff66c693b2fe5fa7e3b601120f4238
Admin9705 <user@dunn.cloud>
2017-11-01 21:47:27 -0400

    Create mass2.sh

---
+++ 7 - References/mass2.sh
@@ -0,0 +1,15 @@
+####### Install Required Files
+
+    yes | apt-get update
+    yes | apt-get install nano
+    yes | apt-get install fuse
+    yes | apt-get install man-db
+    yes | apt-get install screen
+    yes | apt-get install unzip
+    yes | apt-get install python
+    yes | apt-get install software-properties-common    
+    clear
+    echo Installed Required Programs
+    echo 
+    echo Installed update, nano, fuse, man-db, screen, unzip, python, curl
+    echo
