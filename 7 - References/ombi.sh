b599b8ae957a78aded3a0b7167a35dd7f2a61bcb
Admin9705 <user@dunn.cloud>
2017-11-02 19:24:49 -0400

    Create ombi.sh

---
+++ 7 - References/ombi.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+echo -n "Do you want to install Ombi v3 (Required Docker Install)? "
+old_stty_cfg=$(stty -g)
+stty raw -echo
+answer=$( while ! head -c 1 | grep -i '[ny]' ;do true ;done )
+stty $old_stty_cfg
+if echo "$answer" | grep -iq "^y" ;then
+   sudo docker-compose -f ombi.yml up -d
+else
+    echo No
+    clear
+    echo Not Installed Ombi v3
+    echo 
+fi
