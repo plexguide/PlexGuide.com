5bb8808a9d3065396328223f6502b30b30edb0c8
Admin9705 <user@dunn.cloud>
2017-11-02 21:26:17 -0400

    Create radarr.sh

---
+++ 7 - References/radarr.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+echo -n "Do you want to install Radarr? (y/n)"
+old_stty_cfg=$(stty -g)
+stty raw -echo
+answer=$( while ! head -c 1 | grep -i '[ny]' ;do true ;done )
+stty $old_stty_cfg
+if echo "$answer" | grep -iq "^y" ;then
+   apt update && sudo install libmono-cil-dev curl mediainfo
+   cd /opt
+   wget $( curl -s https://api.github.com/repos/Radarr/Radarr/releases | grep linux.tar.gz | grep browser_download_url | head -1 | cut -d \" -f 4 )
+   tar -xvzf Radarr.develop.*.linux.tar.gz
+   mv radarr.service /etc/systemd/system/
+   systemctl daemon-reload
+   systemctl enable radarr.service
+   systemctl start radarr.service
+else
+    echo No
+    clear
+    echo Not Installed - Radarr
+fi 
