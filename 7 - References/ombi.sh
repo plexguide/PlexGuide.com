64b85cb89ee186aa5c53a60fd93adbbd75bcefb0
Admin9705 <user@dunn.cloud>
2017-11-02 20:02:15 -0400

    Update ombi.sh

--- 7 - References/ombi.sh
+++ 7 - References/ombi.sh
@@ -6,7 +6,11 @@ stty raw -echo
 answer=$( while ! head -c 1 | grep -i '[ny]' ;do true ;done )
 stty $old_stty_cfg
 if echo "$answer" | grep -iq "^y" ;then
-   sudo docker-compose -f ombi.yml up -d
+   sudo apt-get install libunwind8
+   sudo mkdir /opt/Ombi && cd /opt/Ombi
+   sudo wget https://ci.appveyor.com/api/buildjobs/78fxna1vxsmei7wn/artifacts/linux.tar.gz
+   sudo tar -xzf linux.tar.gz
+   sudo chmod 755 Ombi
 else
     echo No
     clear
