44dfd8d4fcce1faec8efd2acbde04e70fd364d68
Admin9705 <user@dunn.cloud>
2017-11-02 21:54:06 -0400

    Update radarr.sh

--- 7 - References/radarr.sh
+++ 7 - References/radarr.sh
@@ -9,8 +9,8 @@ if echo "$answer" | grep -iq "^y" ;then
    apt update && sudo install libmono-cil-dev curl mediainfo
    wget $( curl -s https://api.github.com/repos/Radarr/Radarr/releases | grep linux.tar.gz | grep browser_download_url | head -1 | cut -d \" -f 4 )
    tar -xvzf Radarr.develop.*.linux.tar.gz
-   mv Radarr /opt
-   mv radarr.service /etc/systemd/system/
+   mv Radarr /opt 
+   mv radarr.service /etc/systemd/system/ 2>dev/null
    systemctl daemon-reload
    systemctl enable radarr.service
    systemctl start radarr.service
