9d79eed80f1f7a920c3cd4d3af4e37424dee3f2f
Admin9705 <user@dunn.cloud>
2017-11-02 21:38:46 -0400

    Update radarr.sh

--- 7 - References/radarr.sh
+++ 7 - References/radarr.sh
@@ -7,9 +7,9 @@ answer=$( while ! head -c 1 | grep -i '[ny]' ;do true ;done )
 stty $old_stty_cfg
 if echo "$answer" | grep -iq "^y" ;then
    apt update && sudo install libmono-cil-dev curl mediainfo
-   cd /opt
    wget $( curl -s https://api.github.com/repos/Radarr/Radarr/releases | grep linux.tar.gz | grep browser_download_url | head -1 | cut -d \" -f 4 )
    tar -xvzf Radarr.develop.*.linux.tar.gz
+   mv Radarr /opt
    mv radarr.service /etc/systemd/system/
    systemctl daemon-reload
    systemctl enable radarr.service
