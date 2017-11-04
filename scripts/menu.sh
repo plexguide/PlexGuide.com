10d97972778a6ae70a56bdcab0d7f6a9844640e8
Admin0705 <user@dunn.cloud>
2017-11-04 16:58:41 -0400

    update

--- scripts/menu.sh
+++ scripts/menu.sh
@@ -29,7 +29,7 @@ two(){
 # function to display menus
 show_menus() {
 	clear
-	echo Welcome to the Awesome Plex Server - PlexGuide.com
+	echo Welcome to the PlexGuide Installer v3.5 | 0001
 	echo
 	echo "~~~~~~~~~~~~~~~~~~~~~"	
 	echo " M A I N - M E N U"
