c8d69092af985e895d89da9ffd462c59342ce103
Admin0705 <user@dunn.cloud>
2017-11-04 12:11:56 -0400

    fix

--- scripts/radarr.sh
+++ scripts/radarr.sh
@@ -7,6 +7,7 @@ answer=$( while ! head -c 1 | grep -i '[ny]' ;do true ;done )
 stty $old_stty_cfg
 if echo "$answer" | grep -iq "^y" ;then
    apt update
+   sudo apt-get install curl
    install libmono-cil-dev curl mediainfo
    wget $( curl -s https://api.github.com/repos/Radarr/Radarr/releases | grep linux.tar.gz | grep browser_download_url | head -1 | cut -d \" -f 4 )
    tar -xvzf Radarr.develop.*.linux.tar.gz
