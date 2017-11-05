b211bd79141405d90ccae7b457596dcfd035dce3
Admin0705 <user@dunn.cloud>
2017-11-05 09:13:34 -0500

    ombi fixes

--- scripts/ombi.sh
+++ scripts/ombi.sh
@@ -8,7 +8,7 @@ if echo "$answer" | grep -iq "^y" ;then
     echo Yes;
     sudo docker-compose -f ombi.yml up -d
     clear
-    echo Installed Ombi - Use Port 5000 with IP Address; hostname -I;
+    echo Installed Ombi - Use Port 3579 with IP Address; hostname -I;
     echo
 else
     echo No
