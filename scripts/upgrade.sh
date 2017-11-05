ab3515343756c04c778e482768def09a1f5314bb
gforce75 <user@dunn.cloud>
2017-11-05 14:58:52 -0500

    test

--- scripts/upgrade.sh
+++
@@ -1,3 +0,0 @@
-mv upgrade2.sh /tmp
-cd /tmp
-bash /tmp/upgrade2.sh
--- scripts/upgrade2.sh
+++
@@ -1,3 +0,0 @@
-sudo rm -r /opt/plexg* 2>/dev/nu*
-git clone https://github.com/Admin9705/PlexGuide.com-The-Awesome-Plex-Server.git /opt/plexguide
-bash /opt/plexg*/ins*
