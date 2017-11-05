ef708b45077a8f3d0194c68689f9f86860453a26
Admin9705 <user@dunn.cloud>
2017-11-05 14:45:35 -0500

    Update plexdrive4.sh

--- scripts/plexdrive4.sh
+++ scripts/plexdrive4.sh
@@ -27,7 +27,19 @@ if echo "$answer" | grep -iq "^y" ;then
     sudo systemctl daemon-reload
     sudo systemctl enable mongod
     sudo systemctl start mongod
-    
+    clear
+    echo "Important for WINDOWS Users: When you have to copy and paste"
+    echo "from terminal, USE CTRL + Insert.  DO NOT use CTRL +C or right "
+    echo "click on terminal.  If you mess it up, select OPTION 3 on the main"
+    echo "menu and start with Option [1] again"
+    echo
+    echo "Again, when you see the BLUE Letters moving until it nearly stops"
+    echo "leave this window terminal open.  When it's complete, close it and"
+    echo "come back to the same menu and select Option [2] about the plexdrive"
+    echo "service. If you forget, you'll never have content at /mnt/plexdrive4"
+    echo "until you do so!"
+    clear
+    bash continue.sh
     cd /tmp
     wget https://github.com/dweidenfeld/plexdrive/releases/download/4.0.0/plexdrive-linux-amd64
     mv plexdrive-linux-amd64 plexdrive4
@@ -37,7 +49,7 @@ if echo "$answer" | grep -iq "^y" ;then
     chmod 755 /usr/bin/plexdrive4
     mkdir /mnt/plexdrive4 2>dev/null
     chmod 755 /mnt/plexdrive4
-    screen plexdrive4 --uid=0 --gid=0 -o allow_other -v 2 --refresh-interval=1m /mnt/plexdrive4
+    plexdrive4 --uid=0 --gid=0 -o allow_other -v 2 --refresh-interval=1m /mnt/plexdrive4
     clear
     cd /opt/plexguide/scripts
     echo Installed PlexDrive; hostname -I;
