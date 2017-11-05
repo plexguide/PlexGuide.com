cabd5b16c95c8cefb7d30a5545bb201ad726d7dd
Admin0705 <user@dunn.cloud>
2017-11-05 07:53:34 -0500

    update docker-compose

--- scripts/docker.sh
+++ scripts/docker.sh
@@ -6,8 +6,8 @@ stty raw -echo
 answer=$( while ! head -c 1 | grep -i '[ny]' ;do true ;done )
 stty $old_stty_cfg
 if echo "$answer" | grep -iq "^y" ;then
-   curl -sSL https://get.docker.com | sh
-   sudo apt install docker-compose
+   sudo curl -L https://github.com/docker/compose/releases/download/1.17.0/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
+   sudo chmod +x /usr/local/bin/docker-compose
    sudo docker-compose -f docker-compose.yml up -d
 else
     echo No
