4d7f4ede3a8638488da7c038b8e0ae36cb0216ad
Admin0705 <user@dunn.cloud>
2017-11-05 12:45:01 -0500

    rclone

--- scripts/rclone-un.sh
+++ scripts/rclone-un.sh
@@ -1,7 +1,10 @@
+## Making the directories and setting the permissions
 mkdir /mnt/rclone-union
 mkdir /mnt/rclone-move
 chmod 755 /mnt/rclone-move
 chmod 755 /mnt/rclone-union
+
+## Installing rclone
 cd /tmp
 curl -O https://downloads.rclone.org/rclone-current-linux-amd64.zip
 unzip rclone-current-linux-amd64.zip
@@ -13,6 +16,24 @@ mkdir -p /usr/local/share/man/man1
 cp rclone.1 /usr/local/share/man/man1/
 mandb
 cd .. && sudo rm -r rclone*
+
+## Making rclone directory
 mkdir /mnt/rclone
 chmod 755 /mnt/rclone
-schown root /mnt/rclone
+chown root /mnt/rclone
+
+## Warning
+clear
+echo "Warning: You are going to make two rclone directories.  Please go"
+echo "to http://rclone.plexguide.com for instructions"
+
+echo "Quick Instructions - Write Down or goto website"
+echo "[N] New Remote, [9] Google, Enter Info, Verify, Ok, and continue"
+echo "[N] New Remote, [11] Local, Type /mnt/rclone-move, Ok, and quit"
+echo
+bash continue.sh
+rclone config
+
+## Copying the config from the local folder to the root folder
+sudo mkdir /root/.config && sudo mkdir /root/.config/rclone 2>dev/null
+sudo cp ~/.config/rclone/rclone.conf /root/.config/rclone 2>dev/null
