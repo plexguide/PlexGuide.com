15aa4bb89c049d901fa93ce128fe95eebca15221
Admin0705 <user@dunn.cloud>
2017-11-04 22:16:52 -0400

    updating plexdrive4

--- scripts/plexdrive4.sh
+++ scripts/plexdrive4.sh
@@ -17,6 +17,23 @@ if echo "$answer" | grep -iq "^y" ;then
     echo
     read -n 1 -s -r -p "Press any key to continue"
     clear
+    apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 0C49F3730359A14518585931BC711F9BA15703C6
+    echo "deb [ arch=amd64,arm64 ] http://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/3.4 multiverse" | tee /etc/apt/sources.list.d/mongodb-org-3.4.list
+    apt-get update
+    apt-get install -y mongodb-org
+    systemctl daemon-reload
+    systemctl enable mongod
+    systemctl start mongod
+    cd /tmp
+    wget https://github.com/dweidenfeld/plexdrive/releases/download/4.0.0/plexdrive-linux-amd64
+    mv plexdrive-linux-amd64 plexdrive4
+    mv plexdrive4 /usr/bin/
+    cd /usr/bin/
+    chown root:root /usr/bin/plexdrive4
+    chmod 755 /usr/bin/plexdrive4
+    mkdir /mnt/plexdrive4 && chmod 755 /mnt/plexdrive4
+    screen plexdrive4 --uid=0 --gid=0 -o allow_other -v 2 --refresh-interval=1m /mnt/plexdrive4
+    clear
     echo Installed PlexDrive; hostname -I;
     echo
 else
