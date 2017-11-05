69eccd7f2d1cf6b56249004aa61767b11797dbec
Admin0705 <user@dunn.cloud>
2017-11-05 07:58:51 -0500

    readded docker
    mistake removed

--- scripts/docker.sh
+++ scripts/docker.sh
@@ -6,9 +6,10 @@ stty raw -echo
 answer=$( while ! head -c 1 | grep -i '[ny]' ;do true ;done )
 stty $old_stty_cfg
 if echo "$answer" | grep -iq "^y" ;then
-   sudo curl -L https://github.com/docker/compose/releases/download/1.17.0/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
-   sudo chmod +x /usr/local/bin/docker-compose
-   sudo docker-compose -f docker-compose.yml up -d
+   curl -sSL https://get.docker.com | sh
+   curl -L https://github.com/docker/compose/releases/download/1.17.0/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
+   chmod +x /usr/local/bin/docker-compose
+   docker-compose -f docker-compose.yml up -d
 else
     echo No
     clear
