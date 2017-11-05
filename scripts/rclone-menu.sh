5258dfbb08140e836871b9d025b719c08fde7416
Admin0705 <user@dunn.cloud>
2017-11-05 10:57:55 -0500

    rclone start

--- scripts/rclone-menu.sh
+++ scripts/rclone-menu.sh
@@ -29,24 +29,15 @@ two(){
 # function to display menus
 show_menus() {
 	clear
-	echo "Welcome to the PlexGuide.com Installer v3.5 - 5 NOV II"
+	echo "Note: Install only one version of Rclone"
+	echo "This is NOT WORKING YET - Only TESTING"
 	echo
-	echo "Note: It will always prompt you for installs on each program"
-	echo 
-	echo "Google Drive Only: If you are mounting your google drive,"
-	echo "you must install part 1 first (plexdrive4). http://plexdrive.plexguide.com"
-	echo
-	echo "Non-Google Drive setup, skip part 1"
-    echo	
-	echo "~~~~~~~~~~~~~~~~~~~~~"	
-	echo " M A I N - M E N U"
-	echo "~~~~~~~~~~~~~~~~~~~~~"
-	echo "1. Install: PlexDrive 4"
-	echo "2. Install: RClone (Not Operational Yet)"
-	echo "2. Install: New Server Setup"
-	echo "3. Install: Programs Individually"
-	echo "4. Update : PlexGuide Program"
-	echo "5. Exit"
+	echo "~~~~~~~~~~~~~~~~~~~~"	
+	echo "    R-C-L-O-N-E"
+	echo "~~~~~~~~~~~~~~~~~~~~"
+	echo "1. Install: RClone - Unencrypted"
+	echo "2. Install: RClone - Encrypted"
+	echo "3. Exit"
 }
 # read input from the keyboard and take a action
 # invoke the one() when the user select 1 from the menu option.
@@ -54,37 +45,24 @@ show_menus() {
 # Exit when user the user select 3 form the menu option.
 read_options(){
 	local choice
-	read -p "Enter choice [ 1 - 6 ] " choice
+	read -p "Enter choice [ 1 - 3] " choice
 	case $choice in
 		1)
 		cd /opt/plexguide/scripts/
-		bash plexmenu.sh
+		bash plexdrive4.sh
 		;;
 		2)
         cd /opt/plexguide/scripts/
-        bash rclone-menu.sh
+        bash plexdrive-service-move.sh
         ;;
-		3)
-        cd /opt/plexguide/scripts/
-        bash mass.sh
-        ;;
-		4) 
-        cd /opt/plexguide/scripts/
-		bash dep2.sh
-		bash individual-menu.sh
+		3) 
+        rm -r /root/.plexdrive
+        echo "Tokens Removed"
+        echo
+        read -n 1 -s -r -p "Press any key to continue"
+        clear
 		;;
-		5) 
-        cd /opt/plexguide/scripts/
-        bash upgrade.sh
-        clear
-        echo You are required to restart the program. 
-        echo Restart the program afterwards by typing:  plexguide
-        echo 
-        read -n 1 -s -r -p "Press any key to continue "
-        clear
-        echo Remember, restart by typing:  plexguide
-        exit 0;;
-        6)
+        4)
         clear
         echo Remember, restart by typing:  plexguide
         exit 0;;
