3b5e57bdd2a3a172e912ca2e28163a9a49c09770
Admin0705 <user@dunn.cloud>
2017-11-04 22:03:09 -0400

    updates

--- scripts/plexdrive4.sh
+++ scripts/plexdrive4.sh
@@ -7,7 +7,10 @@ stty $old_stty_cfg
 if echo "$answer" | grep -iq "^y" ;then
     echo Yes;
     clear
-    echo "Very Important - Once PlexDrive is running (blue letters), make sure to press CTRL+C. Whatever you do, do not shut down the computer until PlexDrive4 completed the scan.  You should see all your folders under /mnt/plexdrive4.  Again, press CTRL + C once it's running the scan to continue onward."
+    echo "Very Important - Once PlexDrive is running (blue letters), make sure to press CTRL+C." 
+    echo
+    echo "Whatever you do, do not shut down the computer until PlexDrive4 completed the scan.  You should see all your folders under /mnt/plexdrive4.  Again, press CTRL + C once it's running the scan to continue onward."
+    echo
     read -n 1 -s -r -p "Press any key to continue"
     clear
     echo Installed PlexDrive; hostname -I;
