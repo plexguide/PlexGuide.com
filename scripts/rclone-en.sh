d7ff42b4beff8250d582d006a9df3b1d9355bd32
Deiteq <fayly80@gmail.com>
2017-11-06 22:54:28 +0100

    Update rclone-en.sh
    Commented out lines 67 & 68 as there's issue with /dev/null

--- scripts/rclone-en.sh
+++ scripts/rclone-en.sh
@@ -64,8 +64,8 @@ bash rclone-config.sh
 
 ## Copying the config from the local folder to the root folder
 ## rm -r /root/.config/rclone
-mkdir /root/.config && sudo mkdir /root/.config/rclone /dev/null 2>&1 &
-cp ~/.config/rclone/rclone.conf /root/.config/rclone /dev/null 2>&1 &
+## mkdir /root/.config && sudo mkdir /root/.config/rclone /dev/null 2>&1 &
+## cp ~/.config/rclone/rclone.conf /root/.config/rclone /dev/null 2>&1 &
 
 ## Replace Fuse by removing the # from user_allow_toerh
 rm -r /etc/fuse.conf
