6f193af7d84981c592648286f40c4aa9df1780cb
gforce75 <user@dunn.cloud>
2017-11-05 20:40:10 -0500

    test

--- scripts/rclone-un.sh
+++ scripts/rclone-un.sh
@@ -1,9 +1,10 @@
-## Must Be root
-if [[ $UID -eq 0 ]]; then
-  echo "You must be ROOT to run the RCLONE Script" 1>&2
-  echo
-  read -n 1 -s -r -p "Press any key to continue"
-  exit 0
+#!/bin/bash
+
+# Must Be Root
+if [ "$(id -u)" != "0" ]; then
+   echo "This script must be run as root" 1>&2
+   read -n 1 -s -r -p "Press any key to continue"
+   exit 1
 fi
 
 ## Making the directories and setting the permissions
