3c983cc1c303676672ab9cc8e40128709523caec
Admin0705 <user@dunn.cloud>
2017-11-09 22:18:47 -0500

    test

--- scripts/docker/plex.sh
+++ scripts/docker/plex.sh
@@ -6,7 +6,7 @@ answer=$( while ! head -c 1 | grep -i '[ny]' ;do true ;done )
 stty $old_stty_cfg
 if echo "$answer" | grep -iq "^y" ;then
     echo Yes;
-    sudo docker-compose -f plex.yml up -d
+    sudo docker-compose -f /opt/plexguide/scripts/docker/plex.yml up -d
     clear
     echo Installed Plex - Use Port 32400 with IP Address; hostname -I;
     echo
