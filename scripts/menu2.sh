6c9b0139e23ab04e2b6f3befd7fdaa95ef591e4b
Admin0705 <user@dunn.cloud>
2017-11-09 19:45:31 -0500

    test

--- scripts/menu2.sh
+++ scripts/menu2.sh
@@ -21,6 +21,14 @@ while true; do
     --backtitle "Version - 9 Nov" \
     --title "PlexGuide.com Installer" \
     --clear \
+      --inputbox "Hi, this is an input dialog box. You can use \n
+this to ask questions that require the user \n
+to input a string as the answer. You can \n
+input strings of length longer than the \n
+width of the input box, in that case, the \n
+input field will be automatically scrolled. \n
+You can use BACKSPACE to correct errors. \n\n
+Try entering your name below:"
     --cancel-label "Exit" \
     --menu "Please select:" $HEIGHT $WIDTH 8 \
     "1" "Install: PlexDrive4" \
