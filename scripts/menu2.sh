e709e46291f82974b215f7d55c921be6cde379cb
Admin0705 <user@dunn.cloud>
2017-11-09 19:47:16 -0500

    test

--- scripts/menu2.sh
+++ scripts/menu2.sh
@@ -15,20 +15,25 @@ display_result() {
     --msgbox "$result" 0 0
 }
 
-while true; do
-  exec 3>&1
-  selection=$(dialog \
-    --backtitle "Version - 9 Nov" \
-    --title "PlexGuide.com Installer" \
-    --clear \
-      --inputbox "Hi, this is an input dialog box. You can use \n
+# Generate the dialog box
+result=$(dialog --title "INPUT BOX" \
+  --clear  \
+  --inputbox "Hi, this is an input dialog box. You can use \n
 this to ask questions that require the user \n
 to input a string as the answer. You can \n
 input strings of length longer than the \n
 width of the input box, in that case, the \n
 input field will be automatically scrolled. \n
 You can use BACKSPACE to correct errors. \n\n
-Try entering your name below:"
+Try entering your name below:" 
+
+while true; do
+  exec 3>&1
+  selection=$(dialog \
+    --backtitle "Version - 9 Nov" \
+    --title "PlexGuide.com Installer" \
+    --clear \
+    --inputbox 
     --cancel-label "Exit" \
     --menu "Please select:" $HEIGHT $WIDTH 8 \
     "1" "Install: PlexDrive4" \
