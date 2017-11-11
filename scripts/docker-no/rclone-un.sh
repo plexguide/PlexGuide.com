ec578d8181e1e8d27df4138502e41c1bc635fe43
Admin0705 <user@dunn.cloud>
2017-11-11 13:35:30 -0500

    test
    test

--- scripts/docker-no/rclone-un.sh
+++ scripts/docker-no/rclone-un.sh
@@ -15,24 +15,32 @@ WARNING: Write this down and follow the order
 EOF
 bash /opt/plexguide/scripts/docker-no/continue.sh
 
-clear
-
 cat << EOF
 Directory 2 (Local Drive)
 WARNING: Write this down and follow the order
 
-* [N]  New Remote 
-* [11] Local 
-* [Type] /mnt/rclone-move (ignore the longfile prompt, just type this)
-* Verify
-* Ok
-* Quit
+N < For New remote
+gdrive < for the name
+9 < For Google Drive (double check the number select incase)
+Enter Your Google ID
+Enter Your Google Secret
+
+Y < for GUI Interface (much easier if using a Graphical Interface)
+N < for headless machine (if using only Terminal)
+
+N < For New remote
+local < for the name
+11 < For a Local Drive
+
+Ignore this part about ... long file names, UNC, and selecting [1])
+>>> Just type this exactly: /mnt/rclone-move and then press [ENTER]
+
+Y < Is asking all is ok?
+Q < to quit
 
 EOF
 
 bash /opt/plexguide/scripts/docker-no/continue.sh
-
-clear
 
 rclone config
 
