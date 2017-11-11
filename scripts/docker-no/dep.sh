c7a6e48f36025a2496f10e0fe0a741acdd6e29f0
Admin0705 <user@dunn.cloud>
2017-11-10 20:43:46 -0500

    test

--- scripts/docker-no/dep.sh
+++ scripts/docker-no/dep.sh
@@ -1,8 +1,4 @@
-####### Agree to Install Plex Server
-
 clear
-echo "Dependicies & Docker are Required to be Installed"
-echo 
 echo -n "Do you Agree to Install the PlexGuide.com Installer (y/n)?"
 old_stty_cfg=$(stty -g)
 stty raw -echo
