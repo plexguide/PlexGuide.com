7ef2446a427c202b36221dbd2fb94367c6a45b86
Admin0705 <user@dunn.cloud>
2017-11-10 20:45:34 -0500

    fix

--- scripts/docker-no/dep.sh
+++ scripts/docker-no/dep.sh
@@ -15,16 +15,17 @@ if echo "$answer" | grep -iq "^y" ;then
     yes | apt-get install python
     yes | apt-get install curl
     yes | apt-get install openssh-server
+    
+    #Prevents this script from running again
     mkdir /var/plexguide 
     touch /var/plexguide/dep.yes
-    # Install Docker
-    #curl -sSL https://get.docker.com | sh
-    #curl -L https://github.com/docker/compose/releases/download/1.17.0/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
-    #chmod +x /usr/local/bin/docker-compose
-    #docker-compose -f /opt/plexguide/scripts/docker/docker-compose.yml up -d
-    clear
-    echo "Installed Required Dependicies"
-    echo
+    
+    #Install Docker
+    curl -sSL https://get.docker.com | sh
+    curl -L https://github.com/docker/compose/releases/download/1.17.0/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
+    chmod +x /usr/local/bin/docker-compose
+    docker-compose -f /opt/plexguide/scripts/docker/docker-compose.yml up -d
+    
 else
     echo No
     clear
