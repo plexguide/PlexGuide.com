d6e8208ef133b693a4e6b7a4572de9630755dc89
gforce75 <user@dunn.cloud>
2017-11-13 08:06:42 -0500

    fix

--- scripts/menu.sh
+++ scripts/menu.sh
@@ -28,7 +28,7 @@ two(){
 
 #check to see if /var/plexguide/dep exists - if not, install dependencies
 
-file="/var/plexguide/dep3.yes"
+file="/var/plexguide/dep4.yes"
 if [ -e "$file" ]
 then
     clear
