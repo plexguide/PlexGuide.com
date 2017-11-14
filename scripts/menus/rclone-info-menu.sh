96ab1540742c64754c6f8c2e013aa8a202ac34a7
gforce75 <user@dunn.cloud>
2017-11-13 21:42:50 -0500

    test

--- scripts/menus/rclone-info-menu.sh
+++ scripts/menus/rclone-info-menu.sh
@@ -59,52 +59,52 @@ read_options(){
 		1)
     clear
     cat << EOF
-    Directory 1 (For Google Drive)
-    WARNING: Write this down and follow the order
+Directory 1 (For Google Drive)
+WARNING: Write this down and follow the order
 
-    N < For New remote
-    gdrive < for the name
-    9 < For Google Drive (double check the number select incase)
-    Enter Your Google ID
-    Enter Your Google Secret
+N < For New remote
+gdrive < for the name
+9 < For Google Drive (double check the number select incase)
+Enter Your Google ID
+Enter Your Google Secret
 
-    Y < for GUI Interface (much easier if using a Graphical Interface)
-    N < for headless machine (if using only Terminal)
+Y < for GUI Interface (much easier if using a Graphical Interface)
+N < for headless machine (if using only Terminal)
 
-    Enter Your Verification Code
+Enter Your Verification Code
 
-    Windows Users: Use CTRL+Insert (for copy) and Shift+Insert (for Paste)
-    Do anything else, you will mess it up
+Windows Users: Use CTRL+Insert (for copy) and Shift+Insert (for Paste)
+Do anything else, you will mess it up
 
-    N < Configure this as a team drive?
-    Y < If asking all is ok?
+N < Configure this as a team drive?
+Y < If asking all is ok?
 
 EOF
     bash /opt/plexguide/scripts/docker-no/continue.sh
 
     cat << EOF
-    Directory 2 (Local Drive)
-    WARNING: Write this down and follow the order
+Directory 2 (Local Drive)
+WARNING: Write this down and follow the order
 
-    N < For New remote
-    local < for the name
-    11 < For a Local Drive
+N < For New remote
+local < for the name
+11 < For a Local Drive
 
-    Ignore this part about ... long file names, UNC, and selecting [1])
-    >>> Just type this exactly: /mnt/rclone-move and then press [ENTER]
+Ignore this part about ... long file names, UNC, and selecting [1])
+>>> Just type this exactly: /mnt/rclone-move and then press [ENTER]
 
-    Y < Is asking all is ok?
-    Q < to quit
+Y < Is asking all is ok?
+Q < to quit
 
 EOF
     bash /opt/plexguide/scripts/docker-no/continue.sh
 
     ##### Final Note for Encrypted
         cat << EOF
-        PlexGuide.com
-        http://unrclone.plexguide.com
+PlexGuide.com
+http://unrclone.plexguide.com
 
-        You can visit this area again.  Visiting the website is highly encouraged!
+You can visit this area again.  Visiting the website is highly encouraged!
 EOF
     bash /opt/plexguide/scripts/docker-no/continue.sh
       ;;
@@ -112,115 +112,112 @@ EOF
 ####### For Encryption Part 1
     clear
     cat << EOF
-    Maintained By - Deiteq
-    Directory 1 (For Google Drive)
-    WARNING: Write this down and follow the order or http://enrclone.plexguide.com
+Maintained By - Deiteq
+Directory 1 (For Google Drive)
+WARNING: Write this down and follow the order or http://enrclone.plexguide.com
 
-    N < For New remote
-    gdrive < for the name
-    9 < For Google Drive (double check the number select incase)
-    Enter Your Google ID
-    Enter Your Google Secret
+N < For New remote
+gdrive < for the name
+9 < For Google Drive (double check the number select incase)
+Enter Your Google ID
+Enter Your Google Secret
 
-    Y < for GUI Interface (much easier if using a Graphical Interface)
-    N < for headless machine (if using only Terminal)
+Y < for GUI Interface (much easier if using a Graphical Interface)
+N < for headless machine (if using only Terminal)
 
-    Enter Your Verification Code
+Enter Your Verification Code
 
-    Windows Users: Use CTRL+Insert (for copy) and Shift+Insert (for Paste)
-    Do anything else, you will mess it up
+Windows Users: Use CTRL+Insert (for copy) and Shift+Insert (for Paste)
+Do anything else, you will mess it up
 
-    N < Configure this as a team drive?
-    Y < If asking all is ok?
-
-EOF
-    bash /opt/plexguide/scripts/docker-no/continue.sh
-
-    ####### For Encryption Part II
-    cat << EOF
-    Maintained By - Deiteq
-    Part I Encryption
-    WARNING: Write this down and follow the order or http://enrclone.plexguide.com
-
-    N < For New remote
-    gcrypt < for the name
-    6 < For Encrypt/Decrypt (double check the number select incase)
-    gdrive:/encrypt (encrypt being the rclone encrypted folder within your gdrive)
-    2 < Encrypt standard
-    Y < type your own password (write it, secure it and do not lose it)
-    Y < type your own salt password (write it, secure it, make different from before)
-    Should see something like this:
-
-    [gcrypt]
-    remote = gdrive:/encrypt
-    filename_encryption = standard
-    password = *** ENCRYPTED ***
-    password2 = *** ENCRYPTED ***
-
-    Y < Is asking all is ok?
+N < Configure this as a team drive?
+Y < If asking all is ok?
 
 EOF
+bash /opt/plexguide/scripts/docker-no/continue.sh
 
-    bash /opt/plexguide/scripts/docker-no/continue.sh
+####### For Encryption Part II
+cat << EOF
+Maintained By - Deiteq
+Part I Encryption
+WARNING: Write this down and follow the order or http://enrclone.plexguide.com
 
-    ####### For Encryption Part III
-    Maintained By - Deiteq
-    cat << EOF
-    Part II Encryption
-    WARNING: Write this down and follow the order or http://enrclone.plexguide.com
+N < For New remote
+gcrypt < for the name
+6 < For Encrypt/Decrypt (double check the number select incase)
+gdrive:/encrypt (encrypt being the rclone encrypted folder within your gdrive)
+2 < Encrypt standard
+Y < type your own password (write it, secure it and do not lose it)
+Y < type your own salt password (write it, secure it, make different from before)
+Should see something like this:
 
-    N < For New remote
-    crypt < for the name
-    6 < For Encrypt/Decrypt (double check the number select incase)
-    /mnt/plexdrive4/encrypt
-    2 < Encrypt standard
-    Y < type your own password (use same as before for gcrypt)
-    Y < type your own salt password (use same as before for gcrypt salt)
-    Should see something like this:
+[gcrypt]
+remote = gdrive:/encrypt
+filename_encryption = standard
+password = *** ENCRYPTED ***
+password2 = *** ENCRYPTED ***
 
-    [crypt]
-    remote = /mnt/plexdrive4/encrypt
-    filename_encryption = standard
-    password = *** ENCRYPTED ***
-    password2 = *** ENCRYPTED ***
+Y < Is asking all is ok?
+EOF
 
-    Y < Is asking all is ok?
+bash /opt/plexguide/scripts/docker-no/continue.sh
+
+####### For Encryption Part III
+Maintained By - Deiteq
+cat << EOF
+Part II Encryption
+WARNING: Write this down and follow the order or http://enrclone.plexguide.com
+
+N < For New remote
+crypt < for the name
+6 < For Encrypt/Decrypt (double check the number select incase)
+/mnt/plexdrive4/encrypt
+2 < Encrypt standard
+Y < type your own password (use same as before for gcrypt)
+Y < type your own salt password (use same as before for gcrypt salt)
+Should see something like this:
+
+[crypt]
+remote = /mnt/plexdrive4/encrypt
+filename_encryption = standard
+password = *** ENCRYPTED ***
+password2 = *** ENCRYPTED ***
+
+Y < Is asking all is ok?
 
 EOF
 
-    bash /opt/plexguide/scripts/docker-no/continue.sh
+bash /opt/plexguide/scripts/docker-no/continue.sh
 
-    ##### For Encryption Part IV
-    cat << EOF
-    Maintained By - Deiteq
-    Local Drive
-    WARNING: Write this down and follow the order or http://enrclone.plexguide.com
+##### For Encryption Part IV
+cat << EOF
+Maintained By - Deiteq
+Local Drive
+WARNING: Write this down and follow the order or http://enrclone.plexguide.com
 
-    N < For New remote
-    local < for the name
-    11 < For a Local Drive
+N < For New remote
+local < for the name
+11 < For a Local Drive
 
-    Ignore this part about ... long file names, UNC, and selecting [1])
-    >>> Just type this exactly: /mnt/rclone-move and then press [ENTER]
+Ignore this part about ... long file names, UNC, and selecting [1])
+>>> Just type this exactly: /mnt/rclone-move and then press [ENTER]
 
-    Y < Is asking all is ok?
-    Q < to quit
+Y < Is asking all is ok?
+Q < to quit
 EOF
 
 bash /opt/plexguide/scripts/docker-no/continue.sh
 
 ##### Final Note for Encrypted
-    cat << EOF
-    Maintained By - Deiteq
-    http://enrclone.plexguide.com
+cat << EOF
+Maintained By - Deiteq
+http://enrclone.plexguide.com
 
-    You can visit this area again.  Visiting the website is highly encouraged!
+You can visit this area again.  Visiting the website is highly encouraged!
 EOF
 bash /opt/plexguide/scripts/docker-no/continue.sh
-
       ;;
     3)
-
 bash /opt/plexguide/scripts/menus/rclone-menu.sh
       ;;
     4)
