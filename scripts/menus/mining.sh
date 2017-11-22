cd2209b5e7275c0e645dcf9e013d6c61ffb67455
Admin0705 <user@dunn.cloud>
2017-11-21 20:42:11 -0500

    update

--- scripts/menus/mining.sh
+++ scripts/menus/mining.sh
@@ -40,7 +40,7 @@ fi
 # function to display menus
 show_menus() {
 clear
-echo "Your IP Address: " hostname -I | hostname -I | awk '{print $1}'
+echo "Your IP Address: " | hostname -I
 cat << EOF
 PlexGuide.com Installer V4 (17.11.21-2) | Written By: Admin9705 & Deiteq
 ASSIST US: Visit http://wiki.plexguide.com - Update & Edit our Wiki
