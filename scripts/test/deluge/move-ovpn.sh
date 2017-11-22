6d8133b25b47423ebe0d8363990c15e277d1f440
Deiteq <fayly80@gmail.com>
2017-11-22 21:39:26 +0000

    edit

--- scripts/test/deluge/move-ovpn.sh
+++ scripts/test/deluge/move-ovpn.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
-mv /opt/appdata/delugevpn/config/Remotes/ca.rsa2048.crt /opt/appdata/delugevpn/config/openvpn/
-mv /opt/appdata/delugevpn/config/Remotes/crl.rsa.2048.pem /opt/appdata/delugevpn/config/openvpn/
+`mv /opt/appdata/delugevpn/config/Remotes/ca.rsa2048.crt /opt/appdata/delugevpn/config/openvpn/`
+`mv /opt/appdata/delugevpn/config/Remotes/crl.rsa.2048.pem /opt/appdata/delugevpn/config/openvpn/`
 
 ## Uncomment remote server that you want to use.
 
-#mv /opt/appdata/delugevpn/config/Open/CA\ Montreal.ovpn /opt/appdata/delugevpn/config/openvpn/
-#mv /opt/appdata/delugevpn/config/Open/CA\ Toronto.ovpn /opt/appdata/delugevpn/config/openvpn/
-#mv /opt/appdata/delugevpn/config/Open/Netherlands.ovpn /opt/appdata/delugevpn/config/openvpn/
-mv /opt/appdata/delugevpn/config/Remotes/Switzerland.ovpn /opt/appdata/delugevpn/config/openvpn/
-#mv /opt/appdata/delugevpn/config/Open/Sweden.ovpn /opt/appdata/delugevpn/config/openvpn/
-#mv /opt/appdata/delugevpn/config/Open/France.ovpn /opt/appdata/delugevpn/config/openvpn/
-#mv /opt/appdata/delugevpn/config/Open/Romania.ovpn /opt/appdata/delugevpn/config/openvpn/
-#mv /opt/appdata/delugevpn/config/Open/Israel.ovpn /opt/appdata/delugevpn/config/openvpn/
+#`mv /opt/appdata/delugevpn/config/Open/CA\ Montreal.ovpn /opt/appdata/delugevpn/config/openvpn/`
+#`mv /opt/appdata/delugevpn/config/Open/CA\ Toronto.ovpn /opt/appdata/delugevpn/config/openvpn/`
+#`mv /opt/appdata/delugevpn/config/Open/Netherlands.ovpn /opt/appdata/delugevpn/config/openvpn/`
+`mv /opt/appdata/delugevpn/config/Remotes/Switzerland.ovpn /opt/appdata/delugevpn/config/openvpn/`
+#`mv /opt/appdata/delugevpn/config/Open/Sweden.ovpn /opt/appdata/delugevpn/config/openvpn/`
+#`mv /opt/appdata/delugevpn/config/Open/France.ovpn /opt/appdata/delugevpn/config/openvpn/`
+#`mv /opt/appdata/delugevpn/config/Open/Romania.ovpn /opt/appdata/delugevpn/config/openvpn/`
+#`mv /opt/appdata/delugevpn/config/Open/Israel.ovpn /opt/appdata/delugevpn/config/openvpn/`
