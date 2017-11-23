103258f0dddb47a0424e6e151e25de7771bb4cc3
Deiteq <fayly80@gmail.com>
2017-11-23 13:15:39 +0000

    commented out bash for move-ovpn.sh as it should get run with bash openvpn-setup.sh at the beginning of this script.

--- scripts/menus/delugevpn-menu.sh
+++ scripts/menus/delugevpn-menu.sh
@@ -120,7 +120,7 @@ read_options(){
         echo ymldisplay DelugeVPN >> /opt/plexguide/tmp.txt
         echo ymlport 8112 >> /opt/plexguide/tmp.txt
         bash /opt/plexguide/scripts/docker-no/program-installer.sh
-        bash /opt/plexguide/scripts/test/deluge/move-ovpn.sh
+      #  bash /opt/plexguide/scripts/test/deluge/move-ovpn.sh
         clear
       else
         echo
