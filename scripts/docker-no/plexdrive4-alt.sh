1f0f34daa803b274c0340c1303c0b9245de84e67
Deiteq <fayly80@gmail.com>
2017-11-24 16:45:47 +0000

    allow_non_empty_mount added for when plexdrive is not running as it should.

---
+++ scripts/docker-no/plexdrive4-alt.sh
@@ -0,0 +1,64 @@
+################# Install Plex
+echo -n "Do you want to install PlexDrive? (y/n)? "
+old_stty_cfg=$(stty -g)
+stty raw -echo
+answer=$( while ! head -c 1 | grep -i '[ny]' ;do true ;done )
+stty $old_stty_cfg
+if echo "$answer" | grep -iq "^y" ;then
+    echo Yes;
+    clear
+
+## Install Mongod Program
+apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 0C49F3730359A14518585931BC711F9BA15703C6
+echo "deb [ arch=amd64,arm64 ] http://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/3.4 multiverse" | tee /etc/apt/sources.list.d/mongodb-org-3.4.list
+apt-get update
+apt-get install -y mongodb-org
+
+## Enable Mongod Service
+systemctl daemon-reload
+systemctl enable mongod
+systemctl start mongod
+clear
+
+## Create the PlexDrive4 Service
+tee "/etc/systemd/system/plexdrive4.service" > /dev/null <<EOF
+## Create the PlexDrive4 Service
+[Unit]
+Description=PlexDrive4 Service
+After=multi-user.target
+
+[Service]
+Type=simple
+ExecStart=/usr/bin/plexdrive4 --uid=0 --gid=0 --fuse-options=allow_other allow_non_empty_mount --refresh-interval=1m /mnt/plexdrive4
+TimeoutStopSec=20
+KillMode=process
+RemainAfterExit=yes
+
+[Install]
+WantedBy=multi-user.target
+EOF
+
+## Enables the PlexDrive Service
+systemctl daemon-reload
+systemctl enable plexdrive4.service
+
+    clear
+    cd /tmp
+    wget https://github.com/dweidenfeld/plexdrive/releases/download/4.0.0/plexdrive-linux-amd64
+    mv plexdrive-linux-amd64 plexdrive4
+    mv plexdrive4 /usr/bin/
+    cd /usr/bin/
+    chown root:root /usr/bin/plexdrive4
+    chmod 755 /usr/bin/plexdrive4
+    clear
+    plexdrive4 --uid=0 --gid=0 -o allow_other allow_non_empty_mount -v 2 --refresh-interval=1m /mnt/plexdrive4
+    clear
+    cd /opt/plexguide/scripts
+    echo Installed PlexDrive; hostname -I;
+    echo
+else
+    echo No
+    clear
+    echo Not Installed - PlexDrive
+    echo
+fi
