fa20cb95f62ee890621439e06efbeb765671b0ba
gforce75 <user@dunn.cloud>
2017-11-30 23:44:02 -0500

    took out mining

--- scripts/menu.sh
+++ scripts/menu.sh
@@ -46,22 +46,22 @@ PlexGuide.com Installer V4 (17.11.30) | Written By: Admin9705 & Deiteq
 ASSIST US: Visit http://wiki.plexguide.com - Update & Edit the Wiki
 
 DONATION ASSISTANCE INFORMATION *************************************
-1. Donation - Disabled (Requires Script Update)
->>> Visit http://hexabot.us to compound 20% Weekly on Your Coins! <<<
+>>> Visit http://hexabot.us to Compound Weekly on Bit & Lite Coins! <<<
+>>> Buy coins to Start investing at http://coin.plexguide.com <<<
 
 GOOGLE DRIVE ********************************************************
-2. RClone   :  Media Syncs to Google Drive
-3. PlexDrive:  Prevent G-Drive Plex Scan Bans
+1. RClone   :  Media Syncs to Google Drive
+2. PlexDrive:  Prevent G-Drive Plex Scan Bans
 
 PROGRAMS ************************************************************
-4. Programs :  Install Plex, Couch, NetData, Radarr, Sonarr & More!
-5. BETA BETA:  Install Working Beta Programs! No Guides, YMLs Work!
+3. Programs :  Install Plex, Couch, NetData, Radarr, Sonarr & More!
+4. BETA BETA:  Install Working Beta Programs! No Guides, YMLs Work!
 
 TOOLS & T-SHOOT *****************************************************
-6. Updates  :  Update PlexGuide for New Features & Fixes
-7. Tools    :  Troubleshoot Problems & Provides Helpful Information
-8. Backup   :  NOT FUNCTIONAL YET - Backup Program Data
-9. Restore  :  NOT FUNCTIONAL YET - Restore Program Data
+5. Updates  :  Update PlexGuide for New Features & Fixes
+6. Tools    :  Troubleshoot Problems & Provides Helpful Information
+7. Backup   :  NOT FUNCTIONAL YET - Backup Program Data
+8. Restore  :  NOT FUNCTIONAL YET - Restore Program Data
 
 EOF
 
@@ -69,38 +69,34 @@ EOF
 
 read_options(){
 	local choice
-	read -p "Enter choice [ 1 - 10 ];  Type [10] to Exit! " choice
+	read -p "Enter choice [ 1 - 9 ];  Type [9] to Exit! " choice
 	case $choice in
-    1)
-        bash /opt/plexguide/scripts/menus/mine/mining.sh
-        clear
-        ;;
-	2)
+	1)
         clear
 	      bash /opt/plexguide/scripts/menus/rclone-info-menu.sh
         ;;
-    3)
+    2)
         clear
         bash /opt/plexguide/scripts/menus/plexdrive-info-menu.sh
         ;;
-	4)
+	3)
         bash /opt/plexguide/scripts/menus/programs.sh
         clear
         ;;
-    5)
+    4)
         bash /opt/plexguide/scripts/menus/programs-beta.sh
         clear
         ;;
-    6)
+    5)
         bash /opt/plexguide/scripts/docker-no/upgrade.sh
         clear
         echo Remember, restart by typing: plexguide
         exit 0;;
-    7)
+    6)
         bash /opt/plexguide/scripts/menus/trouble-menu.sh
         clear
         ;;
-    10)
+    9)
         clear
         echo Remember, restart by typing:  plexguide
         exit 0;;
