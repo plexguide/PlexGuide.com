52eec1f8f62e145995b68c97e9477e0e069f8d92
gforce75 <user@dunn.cloud>
2017-12-01 00:13:50 -0500

    rclone check

--- scripts/startup/rclone-preinstall.sh
+++ scripts/startup/rclone-preinstall.sh
@@ -211,12 +211,10 @@ sudo systemctl daemon-reload
 
 ###################### If Running Unencrypted RClone, Turn This On
 
-file="/var/plexguide/rclone/en"
+file="/var/plexguide/rclone/un"
 if [ -e "$file" ]
 then
-    echo ">>> Running Encrypted RClone"
-    echo ""
-else
+    echo ">>> Running Unencrypted RClone"
     systemctl enable unionfs 1>/dev/null 2>&1
     systemctl enable rclone 1>/dev/null 2>&1
     systemctl enable move 1>/dev/null 2>&1
@@ -229,27 +227,26 @@ else
     systemctl disable move-en 1>/dev/null 2>&1
     systemctl stop unionfs-encrypt 1>/dev/null 2>&1
     systemctl stop rclone-en 1>/dev/null 2>&1
-    systemctl stop move-en 1>/dev/null 2>&1
+    systemctl stop move-en 1>/dev/null 2>&1    
 fi
 
 ##################### If Running Encrypted RClone, Turn This On
-file="/var/plexguide/rclone/un"
+file="/var/plexguide/rclone/en"
 if [ -e "$file" ]
 then
-    echo ">>> Running Unencrypted RClone"
+    echo ">>> Running Encrypted RClone"
     echo ""
-else
-  systemctl disable unionfs 1>/dev/null 2>&1
-  systemctl disable rclone 1>/dev/null 2>&1
-  systemctl disable move 1>/dev/null 2>&1
-  systemctl stop unionfs 1>/dev/null 2>&1
-  systemctl stop rclone 1>/dev/null 2>&1
-  systemctl stop move 1>/dev/null 2>&1
+    systemctl disable unionfs 1>/dev/null 2>&1
+    systemctl disable rclone 1>/dev/null 2>&1
+    systemctl disable move 1>/dev/null 2>&1
+    systemctl stop unionfs 1>/dev/null 2>&1
+    systemctl stop rclone 1>/dev/null 2>&1
+    systemctl stop move 1>/dev/null 2>&1
 
-  systemctl enable unionfs-encrypt 1>/dev/null 2>&1
-  systemctl enable rclone-en 1>/dev/null 2>&1
-  systemctl enable move-en 1>/dev/null 2>&1
-  systemctl start unionfs-encrypt 1>/dev/null 2>&1
-  systemctl start rclone-en 1>/dev/null 2>&1
-  systemctl start move-en 1>/dev/null 2>&1
+    systemctl enable unionfs-encrypt 1>/dev/null 2>&1
+    systemctl enable rclone-en 1>/dev/null 2>&1
+    systemctl enable move-en 1>/dev/null 2>&1
+    systemctl start unionfs-encrypt 1>/dev/null 2>&1
+    systemctl start rclone-en 1>/dev/null 2>&1
+    systemctl start move-en 1>/dev/null 2>&1
 fi
