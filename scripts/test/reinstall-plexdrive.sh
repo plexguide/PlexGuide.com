3aba5d7197187b0418bdd6c07f324d9380323bf6
gforce75 <user@dunn.cloud>
2017-11-30 21:43:50 -0500

    test

--- scripts/test/reinstall-plexdrive.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-## Re-do preinstall of MongoDB for PlexDrive
-
-bash /opt/plexguide/scripts/startup/plexdrive-preinstall.sh
--- scripts/test/reinstall-rclone.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-## Re-do preinstall of RClone
-
-bash /opt/plexguide/scripts/startup/rclone-preinstall.sh
-
-systemctl restart unionfs
-systemctl restart unionfs-encrypt
