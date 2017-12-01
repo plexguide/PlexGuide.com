4207b7720251aeca17f5da687c6ff69eb082c680
Deiteq <fayly80@gmail.com>
2017-12-01 16:46:07 +0000

    text edits

--- scripts/delugevpn/move-ovpn.sh
+++ scripts/delugevpn/move-ovpn.sh
@@ -5,11 +5,11 @@
 
 ## Uncomment remote server that you want to use.
 
-#`mv /opt/appdata/delugevpn/config/Open/CA\ Montreal.ovpn /opt/appdata/delugevpn/config/openvpn/`
-#`mv /opt/appdata/delugevpn/config/Open/CA\ Toronto.ovpn /opt/appdata/delugevpn/config/openvpn/`
-#`mv /opt/appdata/delugevpn/config/Open/Netherlands.ovpn /opt/appdata/delugevpn/config/openvpn/`
+#`mv /opt/appdata/delugevpn/config/Remotes/CA\ Montreal.ovpn /opt/appdata/delugevpn/config/openvpn/`
+#`mv /opt/appdata/delugevpn/config/Remotes/CA\ Toronto.ovpn /opt/appdata/delugevpn/config/openvpn/`
+#`mv /opt/appdata/delugevpn/config/Remotes/Netherlands.ovpn /opt/appdata/delugevpn/config/openvpn/`
 `mv /opt/appdata/delugevpn/config/Remotes/Switzerland.ovpn /opt/appdata/delugevpn/config/openvpn/`
-#`mv /opt/appdata/delugevpn/config/Open/Sweden.ovpn /opt/appdata/delugevpn/config/openvpn/`
-#`mv /opt/appdata/delugevpn/config/Open/France.ovpn /opt/appdata/delugevpn/config/openvpn/`
-#`mv /opt/appdata/delugevpn/config/Open/Romania.ovpn /opt/appdata/delugevpn/config/openvpn/`
-#`mv /opt/appdata/delugevpn/config/Open/Israel.ovpn /opt/appdata/delugevpn/config/openvpn/`
+#`mv /opt/appdata/delugevpn/config/Remotes/Sweden.ovpn /opt/appdata/delugevpn/config/openvpn/`
+#`mv /opt/appdata/delugevpn/config/Remotes/France.ovpn /opt/appdata/delugevpn/config/openvpn/`
+#`mv /opt/appdata/delugevpn/config/Remotes/Romania.ovpn /opt/appdata/delugevpn/config/openvpn/`
+#`mv /opt/appdata/delugevpn/config/Remotes/Israel.ovpn /opt/appdata/delugevpn/config/openvpn/`
