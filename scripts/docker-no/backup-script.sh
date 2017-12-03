d172e0b514be89d553562acbf31d82d4a22bcb3a
Admin0705 <user@dunn.cloud>
2017-12-03 15:03:19 -0500

    test

--- scripts/docker-no/backup-script.sh
+++ scripts/docker-no/backup-script.sh
@@ -11,14 +11,14 @@ if echo "$answer" | grep -iq "^y" ;then
     echo Yes;
     clear
     echo "1. Stopping Your Docker Program"
-    echo "2. Compressing & Zipping Your Backup Request for "$YMLDISPLAY""
+    echo "2. Compressing & Zipping Your Backup Request for "$YMLDISPLAY"" 
     mkdir -p /gdrive/Backup/"$YMLPROGRAM"
-    docker stop "$YMLPROGRAM"
-    sudo -s tar -cvjf /tmp/"$YMLPROGRAM".tar.bz2 /opt/appdata/"$YMLPROGRAM"
+    docker stop "$YMLPROGRAM" 1>/dev/null 2>&1
+    sudo -s tar -cvjf /tmp/"$YMLPROGRAM".tar.bz2 /opt/appdata/"$YMLPROGRAM" 1>/dev/null 2>&1
     echo "3. Copy Files to Your Google Drive"
-    rclone copy /tmp/"$YMLPROGRAM".tar.bz2 gdrive:/Backup/"$YMLPROGRAM" -v --checksum --drive-chunk-size=64M
+    rclone copy /tmp/"$YMLPROGRAM".tar.bz2 gdrive:/Backup/"$YMLPROGRAM" -v --checksum --drive-chunk-size=64M 1>/dev/null 2>&1
     rm /tmp/"$YMLPROGRAM".tar.bz2
-    docker start "$YMLPROGRAM"
+    docker start "$YMLPROGRAM" 1>/dev/null 2>&1
     echo "4. Restarting Your Docker Program"
     echo ""
     echo "Finished - Check Your Google Drive for the Backup Incase!"
