96cf61cd0062b24cfe255dbad602fc17252ba7bb
Admin0705 <user@dunn.cloud>
2017-12-07 19:42:19 -0500

    update

--- scripts/docker-no/upgrade2.sh
+++ scripts/docker-no/upgrade2.sh
@@ -1,3 +1,3 @@
 sudo rm -r /opt/plexg* 2>/dev/nu*
-sudo wget https://github.com/Admin9705/PlexGuide.com-The-Awesome-Plex-Server/archive/Version-5.zip /opt/plexguide
+sudo wget https://github.com/Admin9705/PlexGuide.com-The-Awesome-Plex-Server/archive/Version-5.zip -d /opt/plexguide
 sudo bash /opt/plexg*/sc*/ins*
