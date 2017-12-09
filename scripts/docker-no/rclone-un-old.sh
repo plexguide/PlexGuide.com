367c80c0a9bbefbeb46df88ed8edc4c2268f8cc4
Admin0705 <user@dunn.cloud>
2017-12-09 16:59:44 -0500

    update

--- scripts/docker-no/rclone-un-old.sh
+++ scripts/docker-no/rclone-un-old.sh
@@ -16,88 +16,111 @@ clear
   cd .. && sudo rm -r rclone* 1>/dev/null 2>&1
   cd ~
 
-## Executes RClone Config
-rclone config
+## Supporting Folders
+mkdir -p /home/plexguide/move
+mkdir -p /home/plexguide/gdrive
+mkdir -p /home/plexguide/unionfs
+mkdir -p /opt/appdata/plexguide/scripts
 
-# copy rclone config from sudo user to root, which is the target
-  cp ~/.config/rclone/rclone.conf /root/.config/rclone/
+## Executes RClone Config
+sudo rclone config
 
 ## RClone - Replace Fuse by removing the # from user_allow_other
-  rm -r /etc/fuse.conf  1>/dev/null 2>&1
+rm -r /etc/fuse.conf  1>/dev/null 2>&1
 
 tee "/etc/fuse.conf" > /dev/null <<EOF
   # /etc/fuse.conf - Configuration file for Filesystem in Userspace (FUSE)
+
   # Set the maximum number of FUSE mounts allowed to non-root users.
   # The default is 1000.
   #mount_max = 1000
+
   # Allow non-root users to specify the allow_other or allow_root mount options.
   user_allow_other
 EOF
 
+####################################### RCLONE
+
+## Create the RClone Script
+tee "/opt/appdata/plexguide/scripts/rclone-un.sh" > /dev/null <<EOF
+#!/bin/bash
+rclone --allow-non-empty --allow-other mount gdrive: /home/plexguide/gdrive --bwlimit 8650k --size-only
+EOF
+## chmod 755 /home/plexguide/.config/rclone/rclone.conf
+chmod 755 /opt/appdata/plexguide/scripts/rclone-un.sh
+
 ## Create the RClone Service
-tee "/etc/systemd/system/rclone.service" > /dev/null <<EOF
+tee "/etc/systemd/system/rclone-un.service" > /dev/null <<EOF
 [Unit]
 Description=RClone Daemon
 After=multi-user.target
+
 [Service]
 Type=simple
-User=root
-Group=root
-ExecStart=/usr/bin/rclone --allow-non-empty --allow-other mount gdrive: /mnt/gdrive --bwlimit 8650k --size-only
+User=plexguide
+Group=1000
+ExecStart=/bin/bash /opt/appdata/plexguide/scripts/rclone-un.sh
 TimeoutStopSec=20
 KillMode=process
 RemainAfterExit=yes
+
 [Install]
 WantedBy=multi-user.target
 EOF
+
+######################################## UNIONFS
 
 ## Create the UnionFS Service
 tee "/etc/systemd/system/unionfs.service" > /dev/null <<EOF
 [Unit]
 Description=UnionFS Daemon
 After=multi-user.target
+
 [Service]
 Type=simple
-User=root
-Group=root
-ExecStart=/usr/bin/unionfs -o cow,allow_other,nonempty /mnt/move=RW:/mnt/plexdrive4=RO /mnt/unionfs
+User=plexguide
+Group=1000
+ExecStart=/usr/bin/unionfs -o cow,allow_other,nonempty /home/plexguide/move=RW:/home/plexguide/plexdrive4=RO /home/plexguide/unionfs
 TimeoutStopSec=20
 KillMode=process
 RemainAfterExit=yes
+
 [Install]
 WantedBy=multi-user.target
 EOF
 
+######################################## MOVE SERVICE
 
 ## Create the Move Script
-rm -r /opt/appdata/plexguide/move.sh 1>/dev/null 2>&1
-tee "/opt/appdata/plexguide/move.sh" > /dev/null <<EOF
+rm -r /opt/appdata/plexguide/scripts/move.sh 1>/dev/null 2>&1
+tee "/opt/appdata/plexguide/scripts/move.sh" > /dev/null <<EOF
 #!/bin/bash
 sleep 30
 while true
 do
-# Purpose of sleep starting is so rclone has time to startup and kick in (1HR, you can change)
-# Anything above 9M will result in a google ban if uploading above 9M for 24 hours
-rclone move --bwlimit 9M --tpslimit 4 --max-size 99G --log-level INFO --stats 15s local:/mnt/move gdrive:/
+# Script kicks in every 10 minutes (loop) and aynthing above 9M may result in a Google Upload Ban
+rclone move --bwlimit 9M --tpslimit 4 --max-size 99G --log-level INFO --stats 15s local:/home/plexguide/move gdrive:/
 sleep 600
 done
 EOF
-chmod 755 /opt/appdata/plexguide/move.sh
+chmod 755 /opt/appdata/plexguide/scripts/move.sh
 
 ## Create the Move Service
 tee "/etc/systemd/system/move.service" > /dev/null <<EOF
 [Unit]
 Description=Move Service Daemon
 After=multi-user.target
+
 [Service]
 Type=simple
-User=root
-Group=root
-ExecStart=/bin/bash /opt/appdata/plexguide/move.sh
+User=plexguide
+Group=1000
+ExecStart=/bin/bash /opt/appdata/plexguide/scripts/move.sh
 TimeoutStopSec=20
 KillMode=process
 RemainAfterExit=yes
 Restart=always
+
 [Install]
 WantedBy=multi-user.target
 EOF
@@ -116,11 +139,11 @@ systemctl stop unionfs-encrypt 1>/dev/null 2>&1
 systemctl stop rclone-encrypt 1>/dev/null 2>&1
 
 # ensure that the unencrypted services are on
-systemctl enable rclone 1>/dev/null 2>&1
+systemctl enable rclone-un 1>/dev/null 2>&1
 systemctl enable move 1>/dev/null 2>&1
 systemctl enable unionfs 1>/dev/null 2>&1
 systemctl start unionfs 1>/dev/null 2>&1 
-systemctl start rclone 1>/dev/null 2>&1
+systemctl start rclone-un 1>/dev/null 2>&1
 systemctl start move 1>/dev/null 2>&1
 
 # set variable to remember what version of rclone user installed
@@ -137,12 +160,17 @@ cat << EOF
 NOTE: You installed the unencrypted version for the RClone data transport!
 If you messed anything up, select [2] and run through again.  Also check:
 http://unrclone.plexguide.com and or post on http://reddit.plexguide.com
+
 HOW TO CHECK: In order to check if everything is working, have 1 item at least
 in your google Drive
+
 1. Type: /mnt/gdrive (and then you should see some item from your g-drive there)
 2. Type: /mnt/unionfs (and you should see the same g-drive stuff there)
+
 Verifying that 1 and 2 are important due to this is how your data will sync!
+
 To make it easy, you can also use the CHECKING TOOLS built in!
+
 EOF
 
 bash /opt/plexguide/scripts/docker-no/continue.sh
