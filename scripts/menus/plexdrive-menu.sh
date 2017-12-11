21826a3173a8d8a62af49ad729a51ad9f895df80
Admin0705 <user@dunn.cloud>
2017-12-11 05:15:27 -0500

    menu update

--- scripts/menus/plexdrive-menu.sh
+++ scripts/menus/plexdrive-menu.sh
@@ -73,7 +73,7 @@ EOF
 
 read_options(){
 	local choice
-	read -p "Enter choice [ 1 - 4 ] " choice
+	read -p "Enter choice [ 1 - 5 ]; Select [5] to Exit " choice
 	case $choice in
 		1)
 		      bash /opt/plexguide/scripts/docker-no/plexdrive4.sh
