a13bc51269463b77385635174f2b4809276a4de5
Admin9705 <24727006+Admin9705@users.noreply.github.com>
2017-12-10 19:07:39 -0500

    Update upgrade2.sh

--- scripts/docker-no/upgrade2.sh
+++ scripts/docker-no/upgrade2.sh
@@ -1,3 +1,6 @@
 sudo rm -r /opt/plexg* 2>/dev/nu*
-git clone https://github.com/Admin9705/PlexGuide.com-The-Awesome-Plex-Server.git /opt/plexguide
-bash /opt/plexg*/ins*
+sudo wget https://github.com/Admin9705/PlexGuide.com-The-Awesome-Plex-Server/archive/Version-4.zip -P /tmp
+sudo unzip /tmp/Version-5.zip -d /opt/
+sudo mv /opt/PlexG* /opt/plexguide
+sudo bash /opt/plexg*/sc*/ins*
+sudo rm -r /tmp/Version-4.zip
