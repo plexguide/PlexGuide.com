283e2b34bc91eab163668a2c66312f562faa677d
Admin0705 <user@dunn.cloud>
2017-12-12 07:56:33 -0500

    update

--- scripts/menu2.sh
+++ scripts/menu2.sh
@@ -1,37 +1,4 @@
 #!/bin/bash
-# Main Menu for PlexGuide
-## ----------------------------------
-# Step #1: Define variables
-# ----------------------------------
-EDITOR=vim
-PASSWD=/etc/passwd
-RED='\033[0;41;30m'
-STD='\033[0;0;39m'
-
-# ----------------------------------
-# Step #2: User defined function
-# ----------------------------------
-pause(){
-  read -p "Press [Enter] key to continue..." fackEnterKey
-}
-
-one(){
-	echo "one() called"
-        pause
-}
-
-# do something in two()
-two(){
-	echo "two() called"
-        pause
-}
-
-#actuid=6000 #user1 uid
-#actuid=`id user1 | sed 's/uid=\([0-9]*\).*/\1/g'`
-
-#NOTAU=64
-
-#[ $UID -ne $actuid ] && echo "You are not user: plexguide" && exit $NOTAU || echo "Thank you for logging in as: plexguide"
 
 #check to see if /var/plexguide/dep exists - if not, install dependencies
 bash /opt/plexguide/scripts/docker-no/user.sh
@@ -52,40 +19,68 @@ chown -R plexguide:1000 /opt/plexguide/scripts/docker-no/*
 ##clear screen
 clear
 
-show_menus() {
-cat << EOF
-PlexGuide.com Installer V5 (17.12.12) | Written By: Admin9705 & Deiteq
-Visit http://wiki.plexguide.com <<< You can edit and improve the Wiki!
-
-DONATION *********************************************************
-1. Mining   :  Enable a little CPU to Mine Coins (It Helps Us)
-
-GOOGLE DRIVE *********************************************************
-2. RClone   :  Media Syncs to Google Drive
-3. PlexDrive:  Prevent G-Drive Plex Scan Bans
-
-PROGRAMS *************************************************************
-4. Programs :  Install Plex, Couch, NetData, Radarr, Sonarr & More!
-5. Updates  :  Update PlexGuide for the newest features & bugfixes!
-
-INFO & T-SHOOT *******************************************************
-6. Info View:  View System Information to Assist You
-7. T-Shoot  :  Troubleshoot Problems & Provides Helpful Information
-
-SYSTEM BACKUP & RESTORE - RCLONE MUST BE SETUP ***********************
-8. Backup   :  Backup Docker Program Data to Your Google Drive
-9. Restore  :  Restore Program Data From Your Google Drive
-
-EOF
-
+function contextSwitch {
+    {
+    ctxt1=$(grep ctxt /proc/stat | awk '{print $2}')
+        echo 50
+    sleep 1
+        ctxt2=$(grep ctxt /proc/stat | awk '{print $2}')
+        ctxt=$(($ctxt2 - $ctxt1))
+        result="Number os context switches in the last secound: $ctxt"
+    echo $result > result
+    } | whiptail --gauge "Getting data ..." 6 60 0
 }
 
-read_options(){
-	local choice
-	read -p "Enter choice [ 1 - 10 ];  Type [10] to Exit! " choice
-	case $choice in
-    1)
-        clear
+
+function userKernelMode {
+    {   
+    raw=( $(grep "cpu " /proc/stat) )
+        userfirst=$((${raw[1]} + ${raw[2]}))
+        kernelfirst=${raw[3]}
+    echo 50
+        sleep 1
+    raw=( $(grep "cpu " /proc/stat) )
+        user=$(( $((${raw[1]} + ${raw[2]})) - $userfirst ))
+    echo 90
+        kernel=$(( ${raw[3]} - $kernelfirst ))
+        sum=$(($kernel + $user))
+        result="Percentage of last sekund in usermode: \
+        $((( $user*100)/$sum ))% \
+        \nand in kernelmode: $((($kernel*100)/$sum ))%"
+    echo $result > result
+    echo 100
+    } | whiptail --gauge "Getting data ..." 6 60 0
+} 
+
+function interupts {
+    {
+    ints=$(vmstat 1 2 | tail -1 | awk '{print $11}')
+        result="Number of interupts in the last secound:  $ints"
+    echo 100
+    echo $result > result
+    } | whiptail --gauge "Getting data ..." 6 60 50
+}
+
+while [ 1 ]
+do
+CHOICE=$(
+whiptail --title "PlexGuide.com Installer | By Admin9705 & Deiteq" --menu "Make your choice" 16 100 9 \
+    "1) " "Donation Mining:  Enable a little CPU Power to Mine Coins (Helps Us) ."   \
+    "2) " "RClone         :  ." \
+    "3) " "PlexDrive      :  ." \
+    "4) " "Programs       :  ." \
+    "5) " "Updates        :  ." \
+    "6) " "Info View      :  ." \
+    "7) " "T-Shoot        :  ." \
+    "8) " "Backup         :  ." \
+    "9) " "Restore        :  ." \
+    "10)" "Exit Plexguide"  3>&2 2>&1 1>&3   
+)
+
+result=$(whoami)
+case $CHOICE in
+    "1)")   
+       clear
             file="/var/plexguide/miner.set"
             if [ -e "$file" ]
             then
@@ -99,62 +94,54 @@ read_options(){
             fi
           bash /opt/plexguide/scripts/menus/mine/mining.sh
         ;;
-	2)
+
+    "2)")   
         clear
-	      bash /opt/plexguide/scripts/menus/rclone-menu.sh
+        bash /opt/plexguide/scripts/menus/rclone-menu.sh
         ;;
-    3)
+
+    "3)")   
         clear
         bash /opt/plexguide/scripts/menus/plexdrive-menu.sh
         ;;
-	4)
+
+    "4)")   
         bash /opt/plexguide/scripts/menus/programs.sh
         clear
         ;;
-    5)
+
+    "5)")   
         bash /opt/plexguide/scripts/docker-no/upgrade.sh
         clear
         echo Remember, restart by typing: plexguide
         exit 0;;
-    6)
+
+    "6)")   
         bash /opt/plexguide/scripts/menus/info-menu.sh
         clear
         ;;
-    7)
+
+    "7)")   
         bash /opt/plexguide/scripts/menus/trouble-menu.sh
         clear
         ;;
-    8)
+
+    "8)")   
         bash /opt/plexguide/scripts/menus/backup-menu.sh
         clear
         ;;
-    9)
+
+    "9)")   
         bash /opt/plexguide/scripts/menus/restore-menu.sh
         clear
         ;;
-    99)
-        bash /opt/plexguide/scripts/menus/programs-beta.sh
-        clear
-        ;;
-    10)
+
+    "10)") 
         clear
         echo Remember, restart by typing:  plexguide
-        exit 0;;
-		*) echo -e "${RED}Error...${STD}" && sleep 2
-	esac
-}
-
-# ----------------------------------------------
-# Step #3: Trap CTRL+C, CTRL+Z and quit singles
-# ----------------------------------------------
-trap '' SIGINT SIGQUIT SIGTSTP
-
-# -----------------------------------
-# Step #4: Main logic - infinite loop
-# ------------------------------------
-while true
-do
-
-	show_menus
-	read_options
+        exit
+        ;;
+esac
+whiptail --msgbox "$result" 20 78
 done
+exit
