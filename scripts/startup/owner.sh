bc4c19031829813ce3e8a25d70092bcae002d91b
gforce75 <user@dunn.cloud>
2017-12-11 22:28:39 -0500

    update

--- scripts/startup/owner.sh
+++ scripts/startup/owner.sh
@@ -1,36 +1,36 @@
 #!/bin/bash
 
-chown plexguide:1000 /home/plexguide/
+chown plexguide:1000 /home/plexguide/ 1>/dev/null 2>&1
 
-chown plexguide:1000 /home/plexguide/gdrive
-chown plexguide:1000 /home/plexguide/gdrive/tv
-chown plexguide:1000 /home/plexguide/gdrive/movies
+chown plexguide:1000 /home/plexguide/gdrive 1>/dev/null 2>&1
+chown plexguide:1000 /home/plexguide/gdrive/tv 1>/dev/null 2>&1
+chown plexguide:1000 /home/plexguide/gdrive/movies 1>/dev/null 2>&1
 
-chown plexguide:1000 /home/plexguide/move
-chown plexguide:1000 /home/plexguide/tv
-chown plexguide:1000 /home/plexguide/movies
+chown plexguide:1000 /home/plexguide/move 1>/dev/null 2>&1
+chown plexguide:1000 /home/plexguide/tv 1>/dev/null 2>&1
+chown plexguide:1000 /home/plexguide/movies 1>/dev/null 2>&1
 
-chown plexguide:1000 /home/plexguide/nzbget
-chown plexguide:1000 /home/plexguide/nzbget/completed
-chown plexguide:1000 /home/plexguide/nzbget/completed/movies
-chown plexguide:1000 /home/plexguide/nzbget/completed/tv
-chown plexguide:1000 /home/plexguide/nzbget/incomplete
-chown plexguide:1000 /home/plexguide/nzbget/nzb
-chown plexguide:1000 /home/plexguide/nzbget/queue
-chown plexguide:1000 /home/plexguide/nzbget/tmp
+chown plexguide:1000 /home/plexguide/nzbget 1>/dev/null 2>&1
+chown plexguide:1000 /home/plexguide/nzbget/completed 1>/dev/null 2>&1
+chown plexguide:1000 /home/plexguide/nzbget/completed/movies 1>/dev/null 2>&1
+chown plexguide:1000 /home/plexguide/nzbget/completed/tv 1>/dev/null 2>&1
+chown plexguide:1000 /home/plexguide/nzbget/incomplete 1>/dev/null 2>&1
+chown plexguide:1000 /home/plexguide/nzbget/nzb 1>/dev/null 2>&1
+chown plexguide:1000 /home/plexguide/nzbget/queue 1>/dev/null 2>&1
+chown plexguide:1000 /home/plexguide/nzbget/tmp 1>/dev/null 2>&1
 
-chown plexguide:1000 /home/plexguide/plexdrive4
-chown plexguide:1000 /home/plexguide/plexdrive4/tv
-chown plexguide:1000 /home/plexguide/plexdrive4/movies
+chown plexguide:1000 /home/plexguide/plexdrive4 1>/dev/null 2>&1
+chown plexguide:1000 /home/plexguide/plexdrive4/tv 1>/dev/null 2>&1
+chown plexguide:1000 /home/plexguide/plexdrive4/movies 1>/dev/null 2>&1
 
-chown plexguide:1000 /home/plexguide/sab
-chown plexguide:1000 /home/plexguide/sab/admin
-chown plexguide:1000 /home/plexguide/sab/complete
-chown plexguide:1000 /home/plexguide/sab/complete/tv
-chown plexguide:1000 /home/plexguide/sab/complete/movies
-chown plexguide:1000 /home/plexguide/sab/incomplete
-chown plexguide:1000 /home/plexguide/sab/nzb
+chown plexguide:1000 /home/plexguide/sab 1>/dev/null 2>&1
+chown plexguide:1000 /home/plexguide/sab/admin 1>/dev/null 2>&1
+chown plexguide:1000 /home/plexguide/sab/complete 1>/dev/null 2>&1
+chown plexguide:1000 /home/plexguide/sab/complete/tv 1>/dev/null 2>&1
+chown plexguide:1000 /home/plexguide/sab/complete/movies 1>/dev/null 2>&1
+chown plexguide:1000 /home/plexguide/sab/incomplete 1>/dev/null 2>&1
+chown plexguide:1000 /home/plexguide/sab/nzb 1>/dev/null 2>&1
 
-chown plexguide:1000 /home/plexguide/unionfs
-chown plexguide:1000 /home/plexguide/tv
-chown plexguide:1000 /home/plexguide/movies
+chown plexguide:1000 /home/plexguide/unionfs 1>/dev/null 2>&1
+chown plexguide:1000 /home/plexguide/tv 1>/dev/null 2>&1
+chown plexguide:1000 /home/plexguide/movies 1>/dev/null 2>&1
