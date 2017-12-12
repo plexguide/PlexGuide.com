e79da1f1b0b06a1852e49745b7a7a86ed8e21678
Admin0705 <user@dunn.cloud>
2017-12-12 08:21:03 -0500

    update

--- scripts/menu2.sh
+++ scripts/menu2.sh
@@ -64,7 +64,7 @@ function interupts {
 while [ 1 ]
 do
 CHOICE=$(
-whiptail --title "Operative Systems" --menu "Make your choice" 16 100 10 \
+whiptail --title "Operative Systems" --menu "Make your choice" 16 100 11 \
     "1)" "Mining"   \
     "2)" "RClone"  \
     "3)" "PlexDrive" \
