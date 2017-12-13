414fccacee684f810d9f48adf8787d57687df5fc
gforce75 <user@dunn.cloud>
2017-12-13 07:51:19 -0500

    updates

--- scripts/docker-no/backup-script-plexdrive.sh
+++ scripts/docker-no/backup-script-plexdrive.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
     clear
-    rm -r /mnt/gdrive/Backup/plexdrive 1>/dev/null 2>&1
+    rm -r /home/plexguide/gdrive/Backup/plexdrive 1>/dev/null 2>&1
     echo "1. Compressing & Zipping Your Backup Request for PLEXDRIVE"
-    mkdir -p /gdrive/Backup/plexdrive
-    zip -r /tmp/plexdrive.zip /home/plexdrive/.plexdrive
+    mkdir -p home/plexguide/gdrive/Backup/plexdrive
+    zip -r /tmp/plexdrive.zip /home/plexguide/.plexdrive
     echo "2. Copy Files to Your Google Drive"
     rclone copy /tmp/plexdrive.zip gdrive:/Backup/plexdrive -v --checksum --drive-chunk-size=64M 1>/dev/null 2>&1
     rm /tmp/plexdrive.zip
