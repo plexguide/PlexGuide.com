030968c787c2d35e99e5385b4f76aeabeea86f93
Admin0705 <user@dunn.cloud>
2017-12-12 20:18:55 -0500

    update

--- scripts/menus/restore-menu.sh
+++ scripts/menus/restore-menu.sh
@@ -1,112 +1,128 @@
-#!/bin/bash
-## ----------------------------------
-# Step #1: Define variables
-# ----------------------------------
-EDITOR=vim
-PASSWD=/etc/passwd
-RED='\033[0;41;30m'
-STD='\033[0;0;39m'
+ #!/bin/bash
 
-# ----------------------------------
-# Step #2: User defined function#
-# ----------------------------------
-pause(){
-  read -p "Press [Enter] key to continue..." fackEnterKey
-}
+#check to see if /var/plexguide/dep exists - if not, install dependencies
+bash /opt/plexguide/scripts/docker-no/user.sh
 
-one(){
-	echo "one() called"
-        pause
-}
+file="/var/plexguide/dep19.yes"
+if [ -e "$file" ]
+then
+    clear
+else
+    bash /opt/plexguide/scripts/startup/dep.sh
+    touch /var/plexguide/dep19.yes
+fi
 
-# do something in two()
-two(){
-	echo "two() called"
-        pause
-}
+## ensure folders follow plexguide
+bash /opt/plexguide/scripts/startup/owner.sh
+chown -R plexguide:1000 /opt/plexguide/scripts/docker-no/*
 
-
-# function to display menus
-show_menus() {
+##clear screen
 clear
-cat << EOF
-PlexGuide.com - RESTORE
 
-Data is being unzipped, copied from Google to your /opt/appdata/!
-
-WARNING 1: Restored Data will Restore Current Data!
-WARNING 2: Restoring Plex & Downloading can take awhile! Do Not Close!
-
-1. OMBIv3
-2. NZBGET
-3. PLEX
-4. SABNZBD
-5. SONARR
-6. RADARR
-7. EMBY
-
-EOF
+function contextSwitch {
+    {
+    ctxt1=$(grep ctxt /proc/stat | awk '{print $2}')
+        echo 50
+    sleep 1
+        ctxt2=$(grep ctxt /proc/stat | awk '{print $2}')
+        ctxt=$(($ctxt2 - $ctxt1))
+        result="Number os context switches in the last secound: $ctxt"
+    echo $result > result
+    } | whiptail --gauge "Getting data ..." 6 60 0
 }
-# read input from the keyboard and take a action
-# invoke the one() when the user select 1 from the menu option.
-# invoke the two() when the user select 2 from the menu option.
-# Exit when user the user select 3 form the menu option.
-read_options(){
-	local choice
-	read -p "Enter choice [ 1 - 7 ];  Type [8] to Exit! " choice
-	case $choice in
-     1)
+
+
+function userKernelMode {
+    {   
+    raw=( $(grep "cpu " /proc/stat) )
+        userfirst=$((${raw[1]} + ${raw[2]}))
+        kernelfirst=${raw[3]}
+    echo 50
+        sleep 1
+    raw=( $(grep "cpu " /proc/stat) )
+        user=$(( $((${raw[1]} + ${raw[2]})) - $userfirst ))
+    echo 90
+        kernel=$(( ${raw[3]} - $kernelfirst ))
+        sum=$(($kernel + $user))
+        result="Percentage of last sekund in usermode: \
+        $((( $user*100)/$sum ))% \
+        \nand in kernelmode: $((($kernel*100)/$sum ))%"
+    echo $result > result
+    echo 100
+    } | whiptail --gauge "Getting data ..." 6 60 0
+} 
+
+function interupts {
+    {
+    ints=$(vmstat 1 2 | tail -1 | awk '{print $11}')
+        result="Number of interupts in the last secound:  $ints"
+    echo 100
+    echo $result > result
+    } | whiptail --gauge "Getting data ..." 6 60 50
+}
+
+while [ 1 ]
+do
+CHOICE=$(
+whiptail --title "Restore Menu" --menu "Make your choice" 15 21 8 \
+    "1)" "OMBIv3"   \
+    "2)" "NZBGet"  \
+    "3)" "Plex"  \
+    "4)" "SABNZBD"  \
+    "5)" "Sonarr"  \
+    "6)" "Radarr"  \
+    "7)" "Emby"  \
+    "8)" "Exit  "  3>&2 2>&1 1>&3   
+)
+
+result=$(whoami)
+case $CHOICE in
+    "1)")   
       echo ymlprogram ombiv3 > /opt/plexguide/tmp.txt
       echo ymldisplay OMBIV3 >> /opt/plexguide/tmp.txt
       bash /opt/plexguide/scripts/docker-no/restore-script.sh
-     ;;
-     2)
+        ;;
+
+    "2)")   
       echo ymlprogram nzbget > /opt/plexguide/tmp.txt
       echo ymldisplay NZBGET >> /opt/plexguide/tmp.txt
       bash /opt/plexguide/scripts/docker-no/restore-script.sh
       ;;
-     3)
+
+    "3)")   
       echo ymlprogram plex > /opt/plexguide/tmp.txt
       echo ymldisplay PLEX >> /opt/plexguide/tmp.txt
       bash /opt/plexguide/scripts/docker-no/restore-script.sh
       ;;
-     4)
+
+    "4)")   
       echo ymlprogram sabnzbd > /opt/plexguide/tmp.txt
       echo ymldisplay SABNZBD >> /opt/plexguide/tmp.txt
       bash /opt/plexguide/scripts/docker-no/restore-script.sh
       ;;
-      5)
+
+    "5)")   
       echo ymlprogram sonarr > /opt/plexguide/tmp.txt
       echo ymldisplay SONARR >> /opt/plexguide/tmp.txt
       bash /opt/plexguide/scripts/docker-no/backup-script.sh
       ;;
-     6)
+
+    "6)")   
       echo ymlprogram radarr > /opt/plexguide/tmp.txt
       echo ymldisplay RADARR >> /opt/plexguide/tmp.txt
       bash /opt/plexguide/scripts/docker-no/backup-script.sh
       ;;
-     7)
+
+    "7)")   
       echo ymlprogram emby > /opt/plexguide/tmp.txt
       echo ymldisplay EMBY >> /opt/plexguide/tmp.txt
       bash /opt/plexguide/scripts/docker-no/backup-script.sh
       ;;
-     8)
-    exit 0;;
-		*) echo -e "${RED}Error...${STD}" && sleep 2
-	esac
-}
 
-# ----------------------------------------------
-# Step #3: Trap CTRL+C, CTRL+Z and quit singles
-# ----------------------------------------------
-trap '' SIGINT SIGQUIT SIGTSTP
-
-# -----------------------------------
-# Step #4: Main logic - infinite loop
-# ------------------------------------
-while true
-do
-
-	show_menus
-	read_options
+    "8)") 
+        clear
+        exit 0
+        ;;
+esac
 done
+exit
