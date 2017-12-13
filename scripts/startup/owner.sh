5aa71fe6f47fbed5a1b4ad3cd9b14c82faaa1320
Admin0705 <user@dunn.cloud>
2017-12-13 05:43:14 -0500

    permission updates

--- scripts/startup/owner.sh
+++ scripts/startup/owner.sh
@@ -6,9 +6,17 @@ chown plexguide:1000 /home/plexguide/gdrive 1>/dev/null 2>&1
 chown plexguide:1000 /home/plexguide/gdrive/tv 1>/dev/null 2>&1
 chown plexguide:1000 /home/plexguide/gdrive/movies 1>/dev/null 2>&1
 
+chmod 755 /home/plexguide/gdrive 1>/dev/null 2>&1
+chmod 755 /home/plexguide/gdrive/tv 1>/dev/null 2>&1
+chmod 755 /home/plexguide/gdrive/movies 1>/dev/null 2>&1
+
 chown plexguide:1000 /home/plexguide/move 1>/dev/null 2>&1
-chown plexguide:1000 /home/plexguide/tv 1>/dev/null 2>&1
-chown plexguide:1000 /home/plexguide/movies 1>/dev/null 2>&1
+chown plexguide:1000 /home/plexguide/move/tv 1>/dev/null 2>&1
+chown plexguide:1000 /home/plexguide/move/movies 1>/dev/null 2>&1
+
+chmod 755 /home/plexguide/move 1>/dev/null 2>&1
+chmod 755 /home/plexguide/move/tv 1>/dev/null 2>&1
+chmod 755 /home/plexguide/move/movies 1>/dev/null 2>&1
 
 chown plexguide:1000 /home/plexguide/nzbget 1>/dev/null 2>&1
 chown plexguide:1000 /home/plexguide/nzbget/completed 1>/dev/null 2>&1
@@ -19,9 +27,23 @@ chown plexguide:1000 /home/plexguide/nzbget/nzb 1>/dev/null 2>&1
 chown plexguide:1000 /home/plexguide/nzbget/queue 1>/dev/null 2>&1
 chown plexguide:1000 /home/plexguide/nzbget/tmp 1>/dev/null 2>&1
 
+chmod 755 /home/plexguide/nzbget/completed 1>/dev/null 2>&1
+chmod 755 /home/plexguide/nzbget/completed/movies 1>/dev/null 2>&1
+chmod 755 /home/plexguide/nzbget/completed/tv 1>/dev/null 2>&1
+chmod 755 /home/plexguide/nzbget/incomplete 1>/dev/null 2>&1
+chmod 755 /home/plexguide/nzbget/nzb 1>/dev/null 2>&1
+chmod 755 /home/plexguide/nzbget/queue1>/dev/null 2>&1
+chmod 755 /home/plexguide/nzbget/tmp 1>/dev/null 2>&1
+
 chown plexguide:1000 /home/plexguide/plexdrive4 1>/dev/null 2>&1
 chown plexguide:1000 /home/plexguide/plexdrive4/tv 1>/dev/null 2>&1
 chown plexguide:1000 /home/plexguide/plexdrive4/movies 1>/dev/null 2>&1
+
+chmod 755 /home/plexguide/sab/complete 1>/dev/null 2>&1
+chmod 755 /home/plexguide/sab/complete/tv  1>/dev/null 2>&1
+chmod 755 /home/plexguide/sab/complete/movies  1>/dev/null 2>&1
+chmod 755 /home/plexguide/sab/complete/incomplete  1>/dev/null 2>&1
+chmod 755 /home/plexguide/sab/complete/nzb  1>/dev/null 2>&1
 
 chown plexguide:1000 /home/plexguide/sab 1>/dev/null 2>&1
 chown plexguide:1000 /home/plexguide/sab/admin 1>/dev/null 2>&1
@@ -31,9 +53,19 @@ chown plexguide:1000 /home/plexguide/sab/complete/movies 1>/dev/null 2>&1
 chown plexguide:1000 /home/plexguide/sab/incomplete 1>/dev/null 2>&1
 chown plexguide:1000 /home/plexguide/sab/nzb 1>/dev/null 2>&1
 
+chmod 755 /home/plexguide/sab/complete 1>/dev/null 2>&1
+chmod 755 /home/plexguide/sab/complete/tv  1>/dev/null 2>&1
+chmod 755 /home/plexguide/sab/complete/movies  1>/dev/null 2>&1
+chmod 755 /home/plexguide/sab/complete/incomplete  1>/dev/null 2>&1
+chmod 755 /home/plexguide/sab/complete/nzb  1>/dev/null 2>&1
+
 chown plexguide:1000 /home/plexguide/unionfs 1>/dev/null 2>&1
 chown plexguide:1000 /home/plexguide/tv 1>/dev/null 2>&1
 chown plexguide:1000 /home/plexguide/movies 1>/dev/null 2>&1
 
 chown plexguide:1000 /home/plexguide/.config/* 1>/dev/null 2>&1
 chown plexguide:1000 /home/plexguide/.plexdrive* 1>/dev/null 2>&1
+
+chmod 755 /home/plexguide/.config/* 1>/dev/null 2>&1
+chmod 755 /home/plexguide/.plexdrive*  1>/dev/null 2>&1
+
