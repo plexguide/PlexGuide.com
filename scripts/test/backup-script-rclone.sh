2f9584c61b3f3b7a77095b46d232f9a6909a49bc
Deiteq <fayly80@gmail.com>
2017-12-13 19:30:07 +0000

    text edit for rclone backup

--- scripts/test/backup-script-rclone.sh
+++ scripts/test/backup-script-rclone.sh
@@ -6,7 +6,7 @@
     mkdir -p /home/plexguide/gdrive/Backup/rclone
     zip -r /tmp/rclone.zip /home/plexguide/.config/rclone
     echo "2. Copy Files to Your Google Drive"
-    rclone copy /tmp/plexdrive.zip gdrive:/Backup/rclone -v --checksum --drive-chunk-size=64M 1>/dev/null 2>&1
+    rclone copy /tmp/rclone.zip gdrive:/Backup/rclone -v --checksum --drive-chunk-size=64M 1>/dev/null 2>&1
     rm /tmp/rclone.zip
     echo ""
     echo "Finished - Check Your Google Drive for the Backup Incase!"
