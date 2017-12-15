ed43ed7f07aee1f192fb18eaf188203b873428ff
gforce75 <user@dunn.cloud>
2017-12-14 20:45:03 -0500

    update

--- scripts/docker-no/user.sh
+++ scripts/docker-no/user.sh
@@ -9,28 +9,10 @@ then
 else
 clear
 
-whiptail --title "Important" --msgbox "Welcome to PlexGuide.com! Before you install the program, we will assist
-you in making a user named: plexguide
+whiptail --title "Important" --msgbox "Welcome to the PlexGuide.com Installer.  If you have questions,
+please read the wikis or join our #Slack Channel." 8 78
 
-- You are creating a user name known as (plexguide) with sudo permissions!
-- Do Not Forget Your Password (Use Troubleshoot To Change)
-
-ENCRYPTION USERS WARNING! If using encrypted method, do not upgrade. Create the
-user as required, but when asking for upgrade; select NO. Encryption needs work!" 8 78
-
-
-password=$(whiptail --passwordbox "Create a Password for the User: plexguide" 8 78 --title "password dialog" 3>&1 1>&2 2>&3)
-                                                                        # A trick to swap stdout and stderr.
-# Again, you can pack this inside if, but it seems really long for some 80-col terminal users.
-exitstatus=$?
-if [ $exitstatus = 0 ]; then
-    echo "User selected Ok and entered " $password
-else
-    echo "User selected Cancel."
-fi
-
-echo "(Exit status was $exitstatus)"
-
+password=plexguide123
 useradd -m -s /bin/bash plexguide
 echo -e ""$password"\n"$password"\n" | passwd plexguide
 usermod -aG plexguide
