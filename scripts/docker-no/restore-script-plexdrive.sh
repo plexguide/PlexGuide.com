87d161c5052006b14c078d49c53cf584ee82112b
gforce75 <user@dunn.cloud>
2017-12-16 08:55:23 -0500

    update plexdrive backup

--- scripts/docker-no/restore-script-plexdrive.sh
+++ scripts/docker-no/restore-script-plexdrive.sh
@@ -2,7 +2,6 @@
     clear
     rm -r /opt/appdata/plexdrive 1>/dev/null 2>&1
     echo "1. Creating a directory for PlexDrive"
-    mkdir -p /home/plexdrive
     echo "2. Copying Files From Your Google Drive > Server"
     echo ""
     rclone copy gdrive:/Backup/plexdrive/plexdrive.zip /tmp --checksum --drive-chunk-size=64M -v
