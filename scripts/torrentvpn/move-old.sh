d4ee3ff9aaf0f581c16fad24e9e05ff1f21ee6e1
Deiteq <fayly80@gmail.com>
2017-12-20 17:41:27 +0100

    more edits

--- scripts/torrentvpn/move-old.sh
+++ scripts/torrentvpn/move-old.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-mv -r /opt/appdata/delugevpn/ /opt/appdata/vpn
+mv /opt/appdata/delugevpn/ /opt/appdata/vpn
 mv /opt/appdata/vpn/.deluge-env /opt/appdata/vpn/.vpn-env
+mv /opt/appdata/delugevpn/.deluge-env /opt/appdata/vpn/.vpn-env
 
 ## For moving original files to new locations
