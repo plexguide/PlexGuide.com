5b975fcde7850de627b920728f60c863a3c59c3f
Admin9705 <user@dunn.cloud>
2017-12-20 12:16:56 -0600

    remove plexguide user

--- scripts/docker-no/user.sh
+++ scripts/docker-no/user.sh
@@ -12,11 +12,11 @@ clear
 whiptail --title "Important" --msgbox "Welcome to the PlexGuide.com Installer.  If you have questions,
 please read the Wikis or join our #Discord Channel." 8 78
 
-password=plexguide123
-useradd -m -s /bin/bash plexguide
-echo -e ""$password"\n"$password"\n" | passwd plexguide
-usermod -aG plexguide
-touch /var/plexguide/plexguide.user
+#password=plexguide123
+#useradd -m -s /bin/bash plexguide
+#echo -e ""$password"\n"$password"\n" | passwd plexguide
+#usermod -aG plexguide
+# touch /var/plexguide/plexguide.user
 
 clear
 
