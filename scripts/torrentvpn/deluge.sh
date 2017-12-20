332a167b8aa15cc8774b6116c83797afe7f3f610
Deiteq <fayly80@gmail.com>
2017-12-20 19:39:32 +0100

    more text edits

--- scripts/torrentvpn/deluge.sh
+++ scripts/torrentvpn/deluge.sh
@@ -9,6 +9,11 @@ docker rm -f rtorrentvpn
 echo ymlprogram delugevpn > /opt/plexguide/tmp.txt
 echo ymldisplay DelugeVPN >> /opt/plexguide/tmp.txt
 echo ymlport 8112 >> /opt/plexguide/tmp.txt
+echo
+echo
+echo "      *****  Default password is deluge  *****       "
+echo
+echo
 bash /opt/plexguide/scripts/docker-no/program-installer.sh
 bash /opt/plexguide/scripts/torrentvpn/move-ovpn.sh
 #yes | apt-get install -y python-setuptools 1>/dev/null 2>&1
