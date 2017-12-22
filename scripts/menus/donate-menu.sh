0e0ce3b94b00cc13d4bdd61f4d6c1d60ad79f994
Admin9705 <user@dunn.cloud>
2017-12-21 20:02:44 -0600

    update

--- scripts/menus/donate-menu.sh
+++ scripts/menus/donate-menu.sh
@@ -5,7 +5,12 @@
 clear
 
 # If you cannot understand this, read Bash_Shell_Scripting#if_statements again.
-if (whiptail --title "PlexGuide Donation" --yesno "Do You Wish to Donate Unused CPU Power to Help Us?" 8 78) then
+if (whiptail --title "PlexGuide Donation" --yesno "Do You Wish to Donate Unused CPU Power to Help Us?" 
+   TEST 
+   TEST
+
+
+	8 78) then
     echo "User selected Yes, exit status was $?."
 else
     echo "User selected No, exit status was $?."
