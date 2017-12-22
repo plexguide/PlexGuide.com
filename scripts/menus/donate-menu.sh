fd282cb9d78b77cd364826421705d1388f0add25
Admin9705 <user@dunn.cloud>
2017-12-21 22:39:21 -0600

    update

--- scripts/menus/donate-menu.sh
+++ scripts/menus/donate-menu.sh
@@ -1,15 +1,22 @@
 
  #!/bin/bash
 
-#check to see if /var/plexguide/dep exists - if not, install dependencies
-clear
-
 whiptail --title "Donation Option Info" --msgbox "Would you be kind enough to TURN ON the Donation Option to mine for coins.  The program runs in a separate container and scales against (downward) your programs being the lowest priroity.  The donation option utilizes your UNUSED processing power and will not interfere with Plex or your other programs.  This assists in further development and motivation. This option can be turned off anytime. Questions? Please visit our Wiki." 12 84
-
 
 # If you cannot understand this, read Bash_Shell_Scripting#if_statements again.
 if (whiptail --title "PlexGuide Donation" --yesno "Will you Donate your Unused CPU Power to Assist Us?" 8 78) then
-    echo "User selected Yes, exit status was $?."
+    whiptail --title "Donation Status - Yes" --msgbox "Thank you for helping and support the Team!" 8 84
+    rm -r /var/plexguide/donation* 1>/dev/null 2>&1
+    touch /var/plexguide/donation.yes 1>/dev/null 2>&1
+
+     echo ymlprogram support > /opt/plexguide/tmp.txt
+     echo ymldisplay Support >> /opt/plexguide/tmp.txt
+     echo ymlport 0000 >> /opt/plexguide/tmp.txt
+     echo "Setting up PlexGuide Donations - Thank You"
+     bash /opt/plexguide/scripts/docker-no/program-installer.sh 1>/dev/null 2>&1
+
 else
-    whiptail --title "Donation Status - No" --msgbox "We understand! If you change your mind, please visit or donation menu anytime!" 12 84
+    whiptail --title "Donation Status - No" --msgbox "We understand! If you change your mind, please visit or donation menu anytime!" 8 84
+    rm -r /var/plexguide/donation* 1>/dev/null 2>&1
+    touch /var/plexguide/donation.no 1>/dev/null 2>&1
 fi
