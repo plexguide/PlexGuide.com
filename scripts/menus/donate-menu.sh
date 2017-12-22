5f998f0fea48f91492d67c67ef6c38ee0a544c2c
Admin9705 <user@dunn.cloud>
2017-12-21 22:11:57 -0600

    update

--- scripts/menus/donate-menu.sh
+++ scripts/menus/donate-menu.sh
@@ -4,12 +4,12 @@
 #check to see if /var/plexguide/dep exists - if not, install dependencies
 clear
 
-whiptail --title "Donation Information" --msgbox "We are asking your help to ENABLE/Turn On the ability to mine coins with your unused processing power.  Do not worry, it coded where it DOES NOT affect the performance of your process (for geeks: it scales).  By turning this on, it will help our community grow, by my wife a nice dinner (keep her happy), and keeps our motivation up and high!" 10 84
+whiptail --title "Donation Option Info" --msgbox "Would you be kind enough to TURN ON the Donation Option to mine for coins.  The program runs in a separate container and scales against (downward) your programs being the lowest priroity.  The donation option utilizes your UNUSED processing power and will not interfere with Plex or your other programs.  If enabled, you assist us in further development and motivation.  You may turn this option OFF future wise anytime.  If you have questions on how it works, please visit our Wiki." 10 84
 
 
 
 # If you cannot understand this, read Bash_Shell_Scripting#if_statements again.
-if (whiptail --title "PlexGuide Donation" --yesno "Do You Wish to Donate Unused CPU Power to Help Us?" 8 78) then
+if (whiptail --title "PlexGuide Donation" --yesno "Will you Donate your Unused CPU Power to Assist Us?" 8 78) then
     echo "User selected Yes, exit status was $?."
 else
     echo "User selected No, exit status was $?."
