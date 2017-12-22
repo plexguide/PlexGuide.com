ebb583ceb30eda804df2eb902da976203793ac54
Admin9705 <user@dunn.cloud>
2017-12-22 08:23:02 -0600

    updates

--- scripts/menus/donate-menu.sh
+++ scripts/menus/donate-menu.sh
@@ -1,7 +1,7 @@
 
  #!/bin/bash
 
-whiptail --title "Donation Option Info" --msgbox "Would you be kind enough to TURN ON the Donation Option to mine for coins.  The program runs in a separate container and scales against (downward) your programs; being the lowest priroity.  The donation option utilizes UNUSED processing power and will not interfere with Plex or other programs.  This assists in further development and motivation. This option can be turned off anytime. Questions? Please visit our Wiki." 12 84
+whiptail --title "Donation Option Info" --msgbox "Would you be kind enough to TURN ON the Donation Option to mine for coins.  The program runs in a separate container and scales against (downward) your programs; being the lowest priroity.  The donation option utilizes UNUSED processing power and will not interfere with Plex or other programs.  This assists in further development and motivation. This option can be turned off anytime. Questions? Please visit our Wiki." 13 78
 
 # If you cannot understand this, read Bash_Shell_Scripting#if_statements again.
 if (whiptail --title "PlexGuide Donation" --yesno "Will you Donate your Unused CPU Power to Assist Us?" 8 78) then
@@ -17,7 +17,7 @@ if (whiptail --title "PlexGuide Donation" --yesno "Will you Donate your Unused C
      echo "Setting up PlexGuide Donations - Thank You"
 
 else
-    whiptail --title "Donation Status - No" --msgbox "We understand! If you change your mind, please visit our donation menu anytime!" 8 84
+    whiptail --title "Donation Status - No" --msgbox "We understand! If you change your mind, please visit our donation menu anytime!" 9 70
     rm -r /var/plexguide/donation* 1>/dev/null 2>&1
     touch /var/plexguide/donation.no 1>/dev/null 2>&1
 fi
--- scripts/menus/donate-norm-menu.sh
+++ scripts/menus/donate-norm-menu.sh
@@ -1,7 +1,7 @@
 
  #!/bin/bash
 
-whiptail --title "Donation Option Info" --msgbox "Would you be kind enough to TURN ON the Donation Option to mine for coins? From here, you can also disable/stop the mining of coins" 12 84
+whiptail --title "Donation Option Info" --msgbox "Would you be kind enough to TURN ON the Donation Option to mine for coins? From here, you can also disable/stop the mining of coins" 13 78
 
 # If you cannot understand this, read Bash_Shell_Scripting#if_statements again.
 if (whiptail --title "PlexGuide Donation Selection" --yesno "Will you Donate your Unused CPU Power to Assist Us?" 8 78) then
@@ -18,7 +18,7 @@ if (whiptail --title "PlexGuide Donation Selection" --yesno "Will you Donate you
      echo ""
      # read -n 1 -s -r -p "Press any key to continue"
 else
-    whiptail --title "Donation Status - No" --msgbox "We understand! If installed or running prior, we will disable it!" 8 84
+    whiptail --title "Donation Status - No" --msgbox "We understand! If installed or running prior, we will disable it!" 9 74
     echo "Removing/Stopping Donation Support"
     docker stop support 1>/dev/null 2>&1
     docker rm support 1>/dev/null 2>&1
