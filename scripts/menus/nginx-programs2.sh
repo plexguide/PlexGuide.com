39e7713aeb3ecafc0634f78064b6da74dd837185
Deiteq <fayly80@gmail.com>
2017-12-23 12:10:47 +0100

    testing emby, delugevpn & rtorrentvpn for nginx changed wordpress port to 8081

---
+++ scripts/menus/nginx-programs2.sh
@@ -0,0 +1,186 @@
+ #!/bin/bash
+
+clear
+
+function contextSwitch {
+    {
+    ctxt1=$(grep ctxt /proc/stat | awk '{print $2}')
+        echo 50
+    sleep 1
+        ctxt2=$(grep ctxt /proc/stat | awk '{print $2}')
+        ctxt=$(($ctxt2 - $ctxt1))
+        result="Number os context switches in the last secound: $ctxt"
+    echo $result > result
+    } | whiptail --gauge "Getting data ..." 6 60 0
+}
+
+
+function userKernelMode {
+    {
+    raw=( $(grep "cpu " /proc/stat) )
+        userfirst=$((${raw[1]} + ${raw[2]}))
+        kernelfirst=${raw[3]}
+    echo 50
+        sleep 1
+    raw=( $(grep "cpu " /proc/stat) )
+        user=$(( $((${raw[1]} + ${raw[2]})) - $userfirst ))
+    echo 90
+        kernel=$(( ${raw[3]} - $kernelfirst ))
+        sum=$(($kernel + $user))
+        result="Percentage of last sekund in usermode: \
+        $((( $user*100)/$sum ))% \
+        \nand in kernelmode: $((($kernel*100)/$sum ))%"
+    echo $result > result
+    echo 100
+    } | whiptail --gauge "Getting data ..." 6 60 0
+}
+
+function interupts {
+    {
+    ints=$(vmstat 1 2 | tail -1 | awk '{print $11}')
+        result="Number of interupts in the last secound:  $ints"
+    echo 100
+    echo $result > result
+    } | whiptail --gauge "Getting data ..." 6 60 50
+}
+
+while [ 1 ]
+do
+CHOICE=$(
+whiptail --title "Install Menu" --menu "Make your choice" 22 35 15 \
+    "1)" "NGINX - Required First"  \
+    "2)" "Not Ready - Plex"   \
+    "3)" "Testing - Emby"  \
+    "4)" "Netdata"  \
+    "5)" "Not Ready - NZBGET"  \
+    "6)" "Not Ready - Muximux"  \
+    "7)" "Not Ready - OMBIv3"  \
+    "8)" "Not Ready - Organizr"  \
+    "9)" "Not Ready - PlexPy"  \
+    "10)" "Radarr"  \
+    "11)" "SABNZBD"  \
+    "12)" "Sonarr"  \
+    "13)" "Not Ready - Wordpress"  \
+    "14)" "Apache"  \
+    "15)" "Testing - DelugeVPN"  \
+    "16)" "Testing - rTorrentVPN"  \
+    "17)" "Exit  "  3>&2 2>&1 1>&3
+)
+
+result=$(whoami)
+case $CHOICE in
+    "1)")
+     echo ymlprogram nginx-proxy > /opt/plexguide/tmp.txt
+     echo ymldisplay NGINX Proxy >> /opt/plexguide/tmp.txt
+     echo ymlport 8989 >> /opt/plexguide/tmp.txt
+     bash /opt/plexguide/scripts/docker-no/program-installer.sh
+     ;;
+
+    "2)")
+     clear
+     bash /opt/plexguide/scripts/menus/plexsub-menu.sh
+     ;;
+
+    "3)")
+      echo ymlprogram nginx-emby > /opt/plexguide/tmp.txt
+      echo ymldisplay NGINX-Emby >> /opt/plexguide/tmp.txt
+      echo ymlport 8096 >> /opt/plexguide/tmp.txt
+      bash /opt/plexguide/scripts/docker-no/program-installer.sh
+      ;;
+
+    "4)")
+     echo ymlprogram nginx-netdata > /opt/plexguide/tmp.txt
+     echo ymldisplay NGINX NetData >> /opt/plexguide/tmp.txt
+     echo ymlport 19999 >> /opt/plexguide/tmp.txt
+     bash /opt/plexguide/scripts/docker-no/program-installer.sh
+     ;;
+
+    "5)")
+     echo ymlprogram nzbget > /opt/plexguide/tmp.txt
+     echo ymldisplay NZBGET >> /opt/plexguide/tmp.txt
+     echo ymlport 6789 >> /opt/plexguide/tmp.txt
+     bash /opt/plexguide/scripts/docker-no/program-installer.sh
+     ;;
+
+    "6)")
+     echo ymlprogram muximux > /opt/plexguide/tmp.txt
+     echo ymldisplay Muximux >> /opt/plexguide/tmp.txt
+     echo ymlport 8015 >> /opt/plexguide/tmp.txt
+     bash /opt/plexguide/scripts/docker-no/program-installer.sh
+     ;;
+
+    "7)")
+     echo ymlprogram ombi > /opt/plexguide/tmp.txt
+     echo ymldisplay Ombi >> /opt/plexguide/tmp.txt
+     echo ymlport 3579 >> /opt/plexguide/tmp.txt
+     bash /opt/plexguide/scripts/docker-no/program-installer.sh
+     ;;
+
+    "8)")
+     echo ymlprogram organizr > /opt/plexguide/tmp.txt
+     echo ymldisplay Organizr >> /opt/plexguide/tmp.txt
+     echo ymlport 8020 >> /opt/plexguide/tmp.txt
+     bash /opt/plexguide/scripts/docker-no/program-installer.sh
+     ;;
+
+    "9)")
+     echo ymlprogram plexpy > /opt/plexguide/tmp.txt
+     echo ymldisplay PlexPY >> /opt/plexguide/tmp.txt
+     echo ymlport 8181 >> /opt/plexguide/tmp.txt
+     bash /opt/plexguide/scripts/docker-no/program-installer.sh
+     ;;
+
+    "10)")
+     echo ymlprogram nginx-radarr > /opt/plexguide/tmp.txt
+     echo ymldisplay NGINX Radarr >> /opt/plexguide/tmp.txt
+     echo ymlport 7878 >> /opt/plexguide/tmp.txt
+     bash /opt/plexguide/scripts/docker-no/program-installer.sh
+     ;;
+
+    "11)")
+     clear
+     bash /opt/plexguide/scripts/menus/sabsub-nginx-menu.sh
+     ;;
+
+    "12)")
+     echo ymlprogram nginx-sonarr > /opt/plexguide/tmp.txt
+     echo ymldisplay NGINX Sonarr >> /opt/plexguide/tmp.txt
+     echo ymlport 8989 >> /opt/plexguide/tmp.txt
+     bash /opt/plexguide/scripts/docker-no/program-installer.sh
+     ;;
+
+    "13)")
+     echo ymlprogram wordpress > /opt/plexguide/tmp.txt
+     echo ymldisplay Wordpress >> /opt/plexguide/tmp.txt
+     echo ymlport 8081 >> /opt/plexguide/tmp.txt
+     bash /opt/plexguide/scripts/docker-no/program-installer.sh
+     ;;
+
+    "14)")
+     echo ymlprogram nginx-apache > /opt/plexguide/tmp.txt
+     echo ymldisplay NGINX Apache >> /opt/plexguide/tmp.txt
+     echo ymlport 80 >> /opt/plexguide/tmp.txt
+     bash /opt/plexguide/scripts/docker-no/program-installer.sh
+      ;;
+
+    "15)")
+     echo ymlprogram nginx-delugevpn > /opt/plexguide/tmp.txt
+     echo ymldisplay NGINX DelugeVPN >> /opt/plexguide/tmp.txt
+     echo ymlport 8112 >> /opt/plexguide/tmp.txt
+     bash /opt/plexguide/scripts/docker-no/program-installer.sh
+      ;;
+
+    "16)")
+     echo ymlprogram nginx-rtorrentvpn > /opt/plexguide/tmp.txt
+     echo ymldisplay NGINX rTorrentVPN >> /opt/plexguide/tmp.txt
+     echo ymlport 3000 >> /opt/plexguide/tmp.txt
+     bash /opt/plexguide/scripts/docker-no/program-installer.sh
+      ;;
+
+    "17)")
+        clear
+        exit 0
+        ;;
+esac
+done
+exit
