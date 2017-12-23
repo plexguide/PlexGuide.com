1e57b516204d3141d6c9b5013b37984972c8a333
Admin0705 <user@dunn.cloud>
2017-12-22 20:25:10 -0600

    update info menu

--- scripts/menus/info-menu.sh
+++ scripts/menus/info-menu.sh
@@ -1,74 +1,79 @@
-#!/bin/bash
-# A menu driven shell script sample template
-## ----------------------------------
-# Step #1: Define variables
-# ----------------------------------
-EDITOR=vim
-PASSWD=/etc/passwd
-RED='\033[0;41;30m'
-STD='\033[0;0;39m'
-
-# ----------------------------------
-# Step #2: User defined function
-# ----------------------------------
-pause(){
-  read -p "Press [Enter] key to continue..." fackEnterKey
-}
-
-one(){
-	echo "one() called"
-        pause
-}
-
-# do something in two()
-two(){
-	echo "two() called"
-        pause
-}
-
-# function to display menus
-show_menus() {
+ #!/bin/bash
 
 clear
-cat << EOF
-Tools & Troubleshooting 101 Menu
 
-Basic Checks ***********************************************
-1. HD Space      :  View HD Stats
-2. Containers    :  View Space Containers Are Using
-3. PlexDrive     :  Veryify that PlexDrive is Working
-
-Uncrypted Tests ********************************************
-4. RClone Test   :  Verify Google Drive is Mounted
-5. UN-SYNC (Move):  View status of the Unencrypted SYNC
-
-UnionFS Test ***********************************************
-6. UnionFS    :  Verify UnionFS is Operational
-
-Encrypted Tests ********************************************
-7. EN-RClone     :  Verify Encrypted Google Drive is Mounted
-8. EN-SYNC (Move):  View status of the Encrypted SYNC
-
-EOF
+function contextSwitch {
+    {
+    ctxt1=$(grep ctxt /proc/stat | awk '{print $2}')
+        echo 50
+    sleep 1
+        ctxt2=$(grep ctxt /proc/stat | awk '{print $2}')
+        ctxt=$(($ctxt2 - $ctxt1))
+        result="Number os context switches in the last secound: $ctxt"
+    echo $result > result
+    } | whiptail --gauge "Getting data ..." 6 60 0
 }
 
-read_options(){
-	local choice
-	read -p "Enter choice [ 1 - 9 ];  Type [9] to Exit! " choice
-	case $choice in
-    1)
+
+function userKernelMode {
+    {
+    raw=( $(grep "cpu " /proc/stat) )
+        userfirst=$((${raw[1]} + ${raw[2]}))
+        kernelfirst=${raw[3]}
+    echo 50
+        sleep 1
+    raw=( $(grep "cpu " /proc/stat) )
+        user=$(( $((${raw[1]} + ${raw[2]})) - $userfirst ))
+    echo 90
+        kernel=$(( ${raw[3]} - $kernelfirst ))
+        sum=$(($kernel + $user))
+        result="Percentage of last sekund in usermode: \
+        $((( $user*100)/$sum ))% \
+        \nand in kernelmode: $((($kernel*100)/$sum ))%"
+    echo $result > result
+    echo 100
+    } | whiptail --gauge "Getting data ..." 6 60 0
+}
+
+function interupts {
+    {
+    ints=$(vmstat 1 2 | tail -1 | awk '{print $11}')
+        result="Number of interupts in the last secound:  $ints"
+    echo 100
+    echo $result > result
+    } | whiptail --gauge "Getting data ..." 6 60 50
+}
+
+while [ 1 ]
+do
+CHOICE=$(
+whiptail --title "Information Menu" --menu "Make your choice" 14 34 7 \
+    "1)" "View HardDrive Space"   \
+    "2)" "View Container Sizes"  \
+    "3)" "Verify PlexDrive"  \
+    "4)" "Verify UnionFS"  \
+    "5)" "Verify UN-RClone"  \
+    "6)" "Verify EN-RClone"  \
+    "7)" "Exit  "  3>&2 2>&1 1>&3
+)
+
+result=$(whoami)
+case $CHOICE in
+    "1)")
       clear
       df -h --total
       echo ""
       read -n 1 -s -r -p "Press any key to continue "
       ;;
-      2)
+
+    "2)")
         clear
         docker ps -s
         echo ""
         read -n 1 -s -r -p "Press any key to continue "
         ;;
-  	3)
+
+    "3)")
       clear
       ls /mnt/plexdrive4
       echo
@@ -79,39 +84,8 @@ read_options(){
       read -n 1 -s -r -p "Press any key to continue "
       clear
       ;;
-  	4)
-      touch /mnnt/gdrive/gdrivetest.txt
-      clear
-      ls /mnt/gdrive
-  		echo
-      echo "*** RClone: Your Google Drive - If empty, that's not good ***"
-      echo "Note 1: You should at least see gdrivetest.txt"
-      echo
-      read -n 1 -s -r -p "Press any key to continue "
-      clear
-  		;;
-      5)
-        ## create log file if does not exist
-        if [ -e "/opt/plexguide/move.log" ]
-        then
-          echo "Log exists"
-        else
-          touch /opt/plexguide/move.log
-        fi
-        ## obtains move.service info and puts into a log to displayed to the user
-    		clear
-    		systemctl status move > /opt/plexguide/move.log
-        cat /opt/plexguide/move.log
-    		echo
-        echo "*** View the Log ***"
-        echo "Remember, there is a sleep function of 30 minutes after done"
-        echo "If you have tons of stuff downloaded, you should see some activity"
-        echo
-        read -n 1 -s -r -p "Press any key to continue "
-        clear
-    		;;
 
-    6)
+    "4)")
       touch /mnt/move/uniontest.txt
       clear
       ls /mnt/unionfs
@@ -123,7 +97,20 @@ read_options(){
       read -n 1 -s -r -p "Press any key to continue "
       clear
       ;;
-        7)
+
+    "5)")
+      touch /mnnt/gdrive/gdrivetest.txt
+      clear
+      ls /mnt/gdrive
+      echo
+      echo "*** RClone: Your Google Drive - If empty, that's not good ***"
+      echo "Note 1: You should at least see gdrivetest.txt"
+      echo
+      read -n 1 -s -r -p "Press any key to continue "
+      clear
+        ;;
+
+    "6)")
         touch /mnt/.gcrypt/gdrivetest.txt
         clear
         ls /mnt/.gcrypt
@@ -134,44 +121,12 @@ read_options(){
         read -n 1 -s -r -p "Press any key to continue "
         clear
         ;;
-        8)
-          ## create log file if does not exist
-          if [ -e "/opt/plexguide/move-en.log" ]
-          then
-            echo "Log exists"
-          else
-            touch /opt/plexguide/move-en.log
-          fi
 
-          ## obtains move.service info and puts into a log to displayed to the user
-          clear
-          systemctl status move-en > /opt/plexguide/move-en.log
-          cat /opt/plexguide/move-en.log
-          echo
-          echo "*** View the Log ***"
-          echo "Remember, there is a sleep function of 30 minutes after done"
-          echo "If you have tons of stuff downloaded, you should see some activity"
-          echo
-          read -n 1 -s -r -p "Press any key to continue "
-          clear
-          ;;
-        9)
-      exit 0;;
-		*) echo -e "${RED}Error...${STD}" && sleep 2
-	esac
-}
 
-# ----------------------------------------------
-# Step #3: Trap CTRL+C, CTRL+Z and quit singles
-# ----------------------------------------------
-trap '' SIGINT SIGQUIT SIGTSTP
-
-# -----------------------------------
-# Step #4: Main logic - infinite loop
-# ------------------------------------
-while true
-do
-
-	show_menus
-	read_options
+     "7)")
+      clear
+      exit 0
+      ;;
+esac
 done
+exit
