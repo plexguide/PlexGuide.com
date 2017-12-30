c8198290e141cde01e3ca864781b1d9996477a29
Admin0705 <user@dunn.cloud>
2017-12-30 16:00:42 -0600

    update

--- scripts/menus/uninstaller-main.sh
+++ scripts/menus/uninstaller-main.sh
@@ -1,12 +1,16 @@
 
  #!/bin/bash
 
-whiptail --title "Uninstaller Information" --msgbox "The UnInstaller will remove all servers, nuke file directories, nuke accumlated files, uninstall docker, remove all containers; but will prompt you if you want to keep your program (APPDATA)." 13 76
+whiptail --title "Uninstaller Information" --msgbox "The UnInstaller will remove all services, nuke file directories and accumlated files, uninstall docker, and remove all containers; but will prompt you if you want to keep your program (APPDATA)." 13 76
 
 # If you cannot understand this, read Bash_Shell_Scripting#if_statements again.
-if (whiptail --title "UnInstaller Selection" --yesno "Do you want to Uninstall PlexGuide?" 8 76) then
+if (whiptail --title "UnInstaller Selection" --yesno "Do you WANT TO STOP THE UNINSTALL & BACKOUT!?" 8 76) then
     
-    whiptail --title "UnInstall Notice" --msgbox "May The Force Be With You! Uninstalling PlexGuide!" 8 76
+    whiptail --title "No Uninstall" --msgbox "Nothing has been uninstalled" 8 76
+    clear
+    read -n 1 -s -r -p "Press any key to continue"
+else
+    whiptail --title "Uninstalling PlexGuide" --msgbox "May The Force Be With You! Uninstalling PlexGuide!" 9 76
      ansible-playbook /opt/plexguide/ansible/plexguide.yml --tags unservices
      echo ""
      echo "Services Uninstalled"
@@ -16,8 +20,4 @@ if (whiptail --title "UnInstaller Selection" --yesno "Do you want to Uninstall P
      echo ""
      read -n 1 -s -r -p "Press any key to continue"
      clear
-else
-    whiptail --title "No Uninstall" --msgbox "Nothing has been uninstalled" 9 76
-    clear
-    read -n 1 -s -r -p "Press any key to continue"
 fi
