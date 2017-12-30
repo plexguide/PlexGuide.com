035cde5afa874bfb27f9c8af5b1235520e310957
Konstantin Rusenkov <nb3slayer@gmail.com>
2017-12-31 00:13:20 +0100

    Update

--- scripts/menus/bench-menu.sh
+++ scripts/menus/bench-menu.sh
@@ -114,7 +114,7 @@ case $CHOICE in
 
       wget -O speedtest-cli https://raw.githubusercontent.com/sivel/speedtest-cli/master/speedtest.py; chmod +x speedtest-cli
       echo
-      speedtest-cli
+      bash speedtest-cli
 
     else
       echo No
