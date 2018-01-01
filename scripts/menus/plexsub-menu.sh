6c53bccefffbc9fb6ede7642b9fb35967567b439
Konstantin Rusenkov <nb3slayer@gmail.com>
2018-01-01 23:42:52 +0100

    Touch Ups.

--- scripts/menus/plexsub-menu.sh
+++ scripts/menus/plexsub-menu.sh
@@ -4,7 +4,7 @@
 #check to see if /var/plexguide/dep exists - if not, install dependencies
 clear
 
-whiptail --title "Plex Information" --msgbox "If installing Plex on your OWN LOCAL Network, visit http//ip:32400/web to complete the install. If installing Plex on a REMOTE SERVER, visit the wiki at http://wiki.plexguide.com on utilizing SSH to complete your setup. Visit http://ipv4:32400/web afterwards!" 13 76
+whiptail --title "Plex Information" --msgbox "If installing Plex on your OWN LOCAL Network, visit http//ip:32400/web to complete the install. If installing Plex on a REMOTE SERVER have your Plex Claim Token ready by heading over to https://plex.tv/claim , and remember that the token is valid only for 4 minutes at a time! If for some reason the claim process should not work, head over to http://wiki.plexguide.com and use a manual method!" 18 78
 
 function contextSwitch {
     {
@@ -71,7 +71,7 @@ case $CHOICE in
     ansible-playbook /opt/plexguide/ansible/plexguide.yml --tags plex-beta
         echo ""
     read -n 1 -s -r -p "Press any key to continue"
-    whiptail --title "Installing Plex Beta" --msgbox "The Beta Version Of Plex Has Been Installed! Do Not Forget To Read The Wiki!" 8 82
+    whiptail --title "Installing Plex Beta" --msgbox "The Beta Version Of Plex Has Been Installed! Do Not Forget To Read The Wiki!" 8 80
     ;;
 
     "3)")
