bb8105859e5a51cb64cc0e6534d6be399eb8c999
gforce75 <user@dunn.cloud>
2018-01-05 18:41:02 -0500

    update

--- scripts/docker-no/random.sh
+++ scripts/docker-no/random.sh
@@ -3,20 +3,16 @@
 MAXCOUNT=1
 count=1
 
-echo
-echo "$MAXCOUNT random numbers:"
-echo "-----------------"
 while [ "$count" -le $MAXCOUNT ]      # Generate 10 ($MAXCOUNT) random integers.
 do
   number=$RANDOM
   echo $number
   let "count += 1"  # Increment count.
 done
-echo "-----------------"
 
 # If you need a random int within a certain range, use the 'modulo' operator.
 # This returns the remainder of a division operation.
-RANGE=2
+RANGE=3
 #$number is the endstate
 echo
 
