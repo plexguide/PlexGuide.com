f15d4f467c74d7ab70bed7d02bddd99fe3eb097b
gforce75 <user@dunn.cloud>
2018-01-05 18:46:40 -0500

    update

--- scripts/docker-no/random.sh
+++ scripts/docker-no/random.sh
@@ -14,6 +14,12 @@ RANGE=4
 number=$RANDOM
 let "number %= $RANGE"
 
+if [ "$number" -eq "0" ]
+then
+   echo "0"
+fi
+
+
 if [ "$number" -eq "1" ]
 then
    echo "1"
@@ -28,6 +34,12 @@ if [ "$number" -eq "3" ]
 then
    echo "3"
 fi
+
+if [ "$number" -eq "4" ]
+then
+   echo "4"
+fi
+
 
 echo
 exit 0
