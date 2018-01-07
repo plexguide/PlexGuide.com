013c8ceeffc1b688ce291d28c74c1d9d41a7d4bf
Admin0705 <user@dunn.cloud>
2018-01-07 13:59:17 -0500

    update

--- scripts/menu.sh
+++ scripts/menu.sh
@@ -7,7 +7,7 @@ bash /opt/plexguide/scripts/docker-no/user.sh
 cp /root/.config/rclone/rclone.conf ~/.config/rclone/rclone.conf 1>/dev/null 2>&1
 
 
-file="/var/plexguide/dep29.yes"
+file="/var/plexguide/dep30.yes"
 if [ -e "$file" ]
 then
    touch /var/plexguide/message.no
@@ -20,48 +20,6 @@ fi
   bash /opt/plexguide/scripts/docker-no/random.sh
 
 ## ensure folders follow plexguide
-
-function contextSwitch {
-   {
-   ctxt1=$(grep ctxt /proc/stat | awk '{print $2}')
-       echo 50
-   sleep 1
-       ctxt2=$(grep ctxt /proc/stat | awk '{print $2}')
-       ctxt=$(($ctxt2 - $ctxt1))
-       result="Number os context switches in the last secound: $ctxt"
-   echo $result > result
-   } | whiptail --gauge "Getting data ..." 6 60 0
-}
-
-
-function userKernelMode {
-   {
-   raw=( $(grep "cpu " /proc/stat) )
-       userfirst=$((${raw[1]} + ${raw[2]}))
-       kernelfirst=${raw[3]}
-   echo 50
-       sleep 1
-   raw=( $(grep "cpu " /proc/stat) )
-       user=$(( $((${raw[1]} + ${raw[2]})) - $userfirst ))
-   echo 90
-       kernel=$(( ${raw[3]} - $kernelfirst ))
-       sum=$(($kernel + $user))
-       result="Percentage of last sekund in usermode: \
-       $((( $user*100)/$sum ))% \
-       \nand in kernelmode: $((($kernel*100)/$sum ))%"
-   echo $result > result
-   echo 100
-   } | whiptail --gauge "Getting data ..." 6 60 0
-}
-
-function interupts {
-   {
-   ints=$(vmstat 1 2 | tail -1 | awk '{print $11}')
-       result="Number of interupts in the last secound:  $ints"
-   echo 100
-   echo $result > result
-   } | whiptail --gauge "Getting data ..." 6 60 50
-}
 
 while [ 1 ]
 do
