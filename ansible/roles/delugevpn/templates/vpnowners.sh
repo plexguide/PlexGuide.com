a1340b0c63fc1f8b64b4789cefe94d192ebdcc47
Deiteq <fayly80@gmail.com>
2018-01-07 14:03:19 +0100

    created delugevpn directory

---
+++ ansible/roles/delugevpn/templates/vpnowners.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+############################################################### TorrentVPN
+
+# Create required directories
+
+mkdir -p /mnt/torrentvpn 1>/dev/null 2>&1
+mkdir -p /mnt/torrentvpn/temp 1>/dev/null 2>&1
+mkdir -p /mnt/torrentvpn/downloaded 1>/dev/null 2>&1
+mkdir -p /mnt/torrentvpn/.torrent 1>/dev/null 2>&1
+mkdir -p /mnt/torrentvpn/.watched 1>/dev/null 2>&1
+
+chown 1000:1000 /mnt/torrentvpn 1>/dev/null 2>&1
+chown 1000:1000 /mnt/torrentvpn/temp 1>/dev/null 2>&1
+chown 1000:1000 /mnt/torrentvpn/downloaded 1>/dev/null 2>&1
+chown 1000:1000 /mnt/torrentvpn/.torrent 1>/dev/null 2>&1
+chown 1000:1000 /mnt/torrentvpn/.watched 1>/dev/null 2>&1
+
+chmod 777 /mnt/torrentvpn 1>/dev/null 2>&1
+chmod 777 /mnt/torrentvpn/temp 1>/dev/null 2>&1
+chmod 777 /mnt/torrentvpn/downloaded 1>/dev/null 2>&1
+chmod 777 /mnt/torrentvpn/.torrent 1>/dev/null 2>&1
+chmod 777 /mnt/torrentvpn/.watched 1>/dev/null 2>&1
