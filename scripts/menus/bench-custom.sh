85decd1909a8a72ef339734e7185fea1bba884dc
Konstantin Rusenkov <nb3slayer@gmail.com>
2018-01-09 15:18:51 +0100

    test

--- scripts/menus/bench-custom.sh
+++ scripts/menus/bench-custom.sh
@@ -1,11 +1,17 @@
 #!/bin/bash
 
-whiptail --title "Check list example" --checklist \
-"Choose user's permissions" 20 78 4 \
-"NET_OUTBOUND" "Allow connections to other hosts" ON \
-"NET_INBOUND" "Allow connections from other hosts" OFF \
-"LOCAL_MOUNT" "Allow mounting of local devices" OFF \
-"REMOTE_MOUNT" "Allow mounting of remote devices" OFF
+BENCH=$(whiptail --title "Choose Benchmark Options" --checklist "Choose:" 20 30 15 \
+  "John" "" off \
+  "Glen" "" off \
+  "Adam" "" off \
+  "Becky" "" off \
+  "Cheryl" "" off \
+  "Michelle" "" off \
+  3>&1 1>&2 2>&3)
+
+echo "You chose $BENCH"
 
 read -n 1 -s -r -p "Press any key to continue "
 clear
+
+exit
