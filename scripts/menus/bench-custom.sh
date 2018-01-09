8051ddd6a0a580265dc393791194a5b3a444ea1e
Konstantin Rusenkov <nb3slayer@gmail.com>
2018-01-09 18:44:32 +0100

    test

--- scripts/menus/bench-custom.sh
+++ scripts/menus/bench-custom.sh
@@ -14,29 +14,29 @@ BENCH=$(whiptail --title "Choose Benchmark Options" --checklist -- "Choose:" 20 
   -about off \
   3>&1 1>&2 2>&3)
 
-  echo "You chose the following options: $BENCH"
+echo "You chose the following options: $BENCH"
+echo
+
+echo "Do you want to run CUSTOM benchmark and information? (y/n)? "
+old_stty_cfg=$(stty -g)
+stty raw -echo
+answer=$( while ! head -c 1 | grep -i '[ny]' ;do true ;done )
+stty $old_stty_cfg
+if echo "$answer" | grep -iq "^y" ;then
+  echo Yes;
+
+  curl -LsO raw.githubusercontent.com/sayem314/serverreview-benchmark/master/bench.sh; chmod +x bench.sh
   echo
+  bash ./bench.sh $BENCH
 
-  echo "Do you want to run CUSTOM benchmark and information? (y/n)? "
-  old_stty_cfg=$(stty -g)
-  stty raw -echo
-  answer=$( while ! head -c 1 | grep -i '[ny]' ;do true ;done )
-  stty $old_stty_cfg
-  if echo "$answer" | grep -iq "^y" ;then
-    echo Yes;
-
-    curl -LsO raw.githubusercontent.com/sayem314/serverreview-benchmark/master/bench.sh; chmod +x bench.sh
-    echo
-    bash ./bench.sh $BENCH
-
-  else
-    echo No
-    clear
-    echo "Did not run benchmarks and information"
-    echo
-  fi
-
-  read -n 1 -s -r -p "Press any key to continue "
+else
+  echo No
   clear
+  echo "Did not run benchmarks and information"
+  echo
+fi
+
+read -n 1 -s -r -p "Press any key to continue "
+clear
 
 exit
