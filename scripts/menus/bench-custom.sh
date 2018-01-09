84f7e4b799f0d5d5cfde7449d206930baff1266c
Konstantin Rusenkov <nb3slayer@gmail.com>
2018-01-09 18:09:39 +0100

    test

--- scripts/menus/bench-custom.sh
+++ scripts/menus/bench-custom.sh
@@ -2,16 +2,16 @@
 
 BENCH=$(whiptail --title "Choose Benchmark Options" --checklist "Choose:" 20 30 15 \
   "${BENCH[-info]}" "System Information" off \
-  "-io" "System I/O Test" off \
-  "-cdn" "CDN Test Download (200MB)" off \
-  "-northamerica" "North-American Server Test Download (800MB)" off \
-  "-europe" "European Server Test Download (900MB)" off \
-  "-asia" "Asian Server Test Download (400MB)" off \
-  "-b" "System Information, CDN Download Test & I/O Test" off \
-  "-speed" "Network Speedtest Using speedtest-cli" off \
-  "-share" "Share Your Results With Others!" off \
-  "-help" "Help Menu. Do NOT Execute This With Other Options!" off \
-  "-about" "Show About-Info. Do NOT Execute This With Other Options!" off \
+  "${BENCH[-io]}" "System I/O Test" off \
+  "${BENCH[-cdn]}" "CDN Test Download (200MB)" off \
+  "${BENCH[-northamerica]}" "North-American Server Test Download (800MB)" off \
+  "${BENCH[-europe]}" "European Server Test Download (900MB)" off \
+  "${BENCH[-asia]}" "Asian Server Test Download (400MB)" off \
+  "${BENCH[-b]}" "System Information, CDN Download Test & I/O Test" off \
+  "${BENCH[-speed]}" "Network Speedtest Using speedtest-cli" off \
+  "${BENCH[-share]}" "Share Your Results With Others!" off \
+  "${BENCH[-help]}" "Help Menu. Do NOT Execute This With Other Options!" off \
+  "${BENCH[-about]}" "Show About-Info. Do NOT Execute This With Other Options!" off \
   3>&1 1>&2 2>&3)
 
   echo "You chose the following options: $BENCH"
