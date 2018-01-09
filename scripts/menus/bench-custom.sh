fa78173745ce74afb652b6801d9ba2f812db9b25
Konstantin Rusenkov <nb3slayer@gmail.com>
2018-01-09 15:24:22 +0100

    update to succesfull test

--- scripts/menus/bench-custom.sh
+++ scripts/menus/bench-custom.sh
@@ -1,12 +1,12 @@
 #! /bin/bash
 
 BENCH=$(whiptail --title "Choose Benchmark Options" --checklist "Choose:" 20 30 15 \
-  "John" "" off \
-  "Glen" "" off \
-  "Adam" "" off \
-  "Becky" "" off \
-  "Cheryl" "" off \
-  "Michelle" "" off \
+  "John" "test" off \
+  "Glen" "test1" off \
+  "Adam" "test2" off \
+  "Becky" "test3" off \
+  "Cheryl" "test4" off \
+  "Michelle" "test5" off \
   3>&1 1>&2 2>&3)
 
 echo "You chose $BENCH"
