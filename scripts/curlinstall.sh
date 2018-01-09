444ddffe0cedd51c3ac10c16ebb132fbd0b4b0f8
gforce75 <user@dunn.cloud>
2018-01-08 22:03:25 -0500

    update

---
+++ scripts/curlinstall.sh
@@ -0,0 +1,8 @@
+sudo apt-get update -y && sudo apt-get upgrade -y && sudo apt-get full-upgrade -y
+sudo apt-get install git -y && sudo apt-get install whiptail -y
+sudo git clone https://github.com/Admin9705/PlexGuide.com-The-Awesome-Plex-Server.git /opt/plexguide
+sudo bash /opt/plexg*/sc*/ins*
+
+clear
+echo "To deploy this Script anytime, type: plexguide
+echo ""
