567818a6132bc0fc468db82667a9541e6dfd6f32
Konstantin Rusenkov <nb3slayer@gmail.com>
2018-01-09 17:29:29 +0100

    test

--- scripts/menus/bench-custom.sh
+++ scripts/menus/bench-custom.sh
@@ -1,11 +1,7 @@
 #! /bin/bash
 
-curl -LsO raw.githubusercontent.com/sayem314/serverreview-benchmark/master/bench.sh; chmod +x bench.sh
-echo
-./bench.sh $BENCH
-
 BENCH=$(whiptail --title "Choose Benchmark Options" --checklist "Choose:" 20 30 15 \
-"--info" "System Information" off \
+  "-info" "System Information" off \
 #  "io" "System I/O Test" off \
 #  "cdn" "CDN Test Download (200MB)" off \
 #  "northamerica" "North-American Server Test Download (800MB)" off \
@@ -21,25 +17,25 @@ BENCH=$(whiptail --title "Choose Benchmark Options" --checklist "Choose:" 20 30 
   echo "You chose the following options: $BENCH"
   echo
 
-#  echo "Do you want to run CUSTOM benchmark and information? (y/n)? "
-#  old_stty_cfg=$(stty -g)
-#  stty raw -echo
-#  answer=$( while ! head -c 1 | grep -i '[ny]' ;do true ;done )
-#  stty $old_stty_cfg
-#  if echo "$answer" | grep -iq "^y" ;then
-#    echo Yes;
+  echo "Do you want to run CUSTOM benchmark and information? (y/n)? "
+  old_stty_cfg=$(stty -g)
+  stty raw -echo
+  answer=$( while ! head -c 1 | grep -i '[ny]' ;do true ;done )
+  stty $old_stty_cfg
+  if echo "$answer" | grep -iq "^y" ;then
+    echo Yes;
 
 #    curl -LsO raw.githubusercontent.com/thecreatorzone/plexguide-bench/master/bench.sh; chmod +x bench.sh
-#    curl -LsO raw.githubusercontent.com/sayem314/serverreview-benchmark/master/bench.sh; chmod +x bench.sh
-#    echo
-#    ./bench.sh $BENCH
+    curl -LsO raw.githubusercontent.com/sayem314/serverreview-benchmark/master/bench.sh; chmod +x bench.sh
+    echo
+    ./bench.sh $BENCH
 
-#  else
-#    echo No
-#    clear
-#    echo "Did not run benchmarks and information"
-#    echo
-#  fi
+  else
+    echo No
+    clear
+    echo "Did not run benchmarks and information"
+    echo
+  fi
 
   read -n 1 -s -r -p "Press any key to continue "
   clear
