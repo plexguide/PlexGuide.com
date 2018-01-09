7af09b11fdf32f3e876b053d13250647e533214b
Konstantin Rusenkov <nb3slayer@gmail.com>
2018-01-09 16:44:46 +0100

    test

--- scripts/menus/bench-custom.sh
+++ scripts/menus/bench-custom.sh
@@ -2,17 +2,17 @@
 
 
 BENCH=$(whiptail --title "Choose Benchmark Options" --checklist "Choose:" 20 30 15 \
-  "info" "System Information" off \
-  "io" "System I/O Test" off \
-  "cdn" "CDN Test Download (200MB)" off \
-  "northamerica" "North-American Server Test Download (800MB)" off \
-  "europe" "European Server Test Download (900MB)" off \
-  "asia" "Asian Server Test Download (400MB)" off \
-  "b" "System Information, CDN Download Test & I/O Test" off \
-  "speed" "Network Speedtest Using speedtest-cli" off \
-  "share" "Share Your Results With Others!" off \
-  "help" "Help Menu. Do NOT Execute This With Other Options!" off \
-  "about" "Show About-Info. Do NOT Execute This With Other Options!" off \
+  '-info' "System Information" off \
+#  "io" "System I/O Test" off \
+#  "cdn" "CDN Test Download (200MB)" off \
+#  "northamerica" "North-American Server Test Download (800MB)" off \
+#  "europe" "European Server Test Download (900MB)" off \
+#  "asia" "Asian Server Test Download (400MB)" off \
+#  "b" "System Information, CDN Download Test & I/O Test" off \
+#  "speed" "Network Speedtest Using speedtest-cli" off \
+#  "share" "Share Your Results With Others!" off \
+#  "help" "Help Menu. Do NOT Execute This With Other Options!" off \
+#  "about" "Show About-Info. Do NOT Execute This With Other Options!" off \
   3>&1 1>&2 2>&3)
 
   echo "You chose the following options: $BENCH"
@@ -26,9 +26,10 @@ BENCH=$(whiptail --title "Choose Benchmark Options" --checklist "Choose:" 20 30 
   if echo "$answer" | grep -iq "^y" ;then
     echo Yes;
 
-    curl -LsO raw.githubusercontent.com/thecreatorzone/plexguide-bench/master/bench.sh; chmod +x bench.sh
+#    curl -LsO raw.githubusercontent.com/thecreatorzone/plexguide-bench/master/bench.sh; chmod +x bench.sh
+    curl -LsO raw.githubusercontent.com/sayem314/serverreview-benchmark/master/bench.sh; chmod +x bench.sh
     echo
-    bash ./bench.sh $BENCH
+    ./bench.sh $BENCH
 
   else
     echo No
