2d2a243ad175f0d0c00abba8627fbcd491cd3a9d
Konstantin Rusenkov <nb3slayer@gmail.com>
2018-01-09 15:52:39 +0100

    test

--- scripts/menus/bench-custom.sh
+++ scripts/menus/bench-custom.sh
@@ -1,17 +1,18 @@
 #! /bin/bash
+INFO=$"-info"
 
 BENCH=$(whiptail --title "Choose Benchmark Options" --checklist "Choose:" 20 30 15 \
-  "-info" "System Information" off \
-  "-io" "System I/O Test" off \
-  "-cdn" "CDN Test Download (200MB)" off \
-  "-northamerica" "North-American Server Test Download (800MB)" off \
-  "-europe" "European Server Test Download (900MB)" off \
-  "-asia" "Asian Server Test Download (400MB)" off \
-  "-b" "System Information, CDN Download Test & I/O Test" off \
-  "-speed" "Network Speedtest Using speedtest-cli" off \
-  "share" "Share Your Results With Others!" off \
-  "-help" "Help Menu. Do NOT Execute This With Other Options!" off \
-  "-about" "Show About-Info. Do NOT Execute This With Other Options!" off \
+  "$INFO" "System Information" off \
+#  "-io" "System I/O Test" off \
+#  "-cdn" "CDN Test Download (200MB)" off \
+#  "-northamerica" "North-American Server Test Download (800MB)" off \
+#  "-europe" "European Server Test Download (900MB)" off \
+#  "-asia" "Asian Server Test Download (400MB)" off \
+#  "-b" "System Information, CDN Download Test & I/O Test" off \
+#  "-speed" "Network Speedtest Using speedtest-cli" off \
+#  "share" "Share Your Results With Others!" off \
+#  "-help" "Help Menu. Do NOT Execute This With Other Options!" off \
+#  "-about" "Show About-Info. Do NOT Execute This With Other Options!" off \
   3>&1 1>&2 2>&3)
 
   echo "You chose the following options: $BENCH"
