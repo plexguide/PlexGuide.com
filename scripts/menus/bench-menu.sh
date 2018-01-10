28e41c4fb2c8fed12b8b48f4ff177c03bc6ef14a
gforce75 <user@dunn.cloud>
2018-01-10 06:47:37 -0500

    menu code cleanup

--- scripts/menus/bench-menu.sh
+++ scripts/menus/bench-menu.sh
@@ -2,48 +2,6 @@
 
 clear
 
-function contextSwitch {
-   {
-   ctxt1=$(grep ctxt /proc/stat | awk '{print $2}')
-       echo 50
-   sleep 1
-       ctxt2=$(grep ctxt /proc/stat | awk '{print $2}')
-       ctxt=$(($ctxt2 - $ctxt1))
-       result="Number os context switches in the last secound: $ctxt"
-   echo $result > result
-   } | whiptail --gauge "Getting data ..." 6 60 0
-}
-
-
-function userKernelMode {
-   {
-   raw=( $(grep "cpu " /proc/stat) )
-       userfirst=$((${raw[1]} + ${raw[2]}))
-       kernelfirst=${raw[3]}
-   echo 50
-       sleep 1
-   raw=( $(grep "cpu " /proc/stat) )
-       user=$(( $((${raw[1]} + ${raw[2]})) - $userfirst ))
-   echo 90
-       kernel=$(( ${raw[3]} - $kernelfirst ))
-       sum=$(($kernel + $user))
-       result="Percentage of last sekund in usermode: \
-       $((( $user*100)/$sum ))% \
-       \nand in kernelmode: $((($kernel*100)/$sum ))%"
-   echo $result > result
-   echo 100
-   } | whiptail --gauge "Getting data ..." 6 60 0
-}
-
-function interupts {
-   {
-   ints=$(vmstat 1 2 | tail -1 | awk '{print $11}')
-       result="Number of interupts in the last secound:  $ints"
-   echo 100
-   echo $result > result
-   } | whiptail --gauge "Getting data ..." 6 60 50
-}
-
 while [ 1 ]
 do
 CHOICE=$(
--- scripts/menus/plexsub-menu.sh
+++ scripts/menus/plexsub-menu.sh
@@ -6,48 +6,6 @@ clear
 
 whiptail --title "Plex Information" --msgbox "If installing Plex on your OWN LOCAL Network, visit http//ip:32400/web to complete the install. If installing Plex on a REMOTE SERVER have your Plex Claim Token ready by heading over to https://plex.tv/claim , and remember that the claim token is valid only for 4 minutes at a time! If, for some reason, the claim process should not work, head over to http://wiki.plexguide.com and use a manual method!" 18 78
 
-function contextSwitch {
-    {
-    ctxt1=$(grep ctxt /proc/stat | awk '{print $2}')
-        echo 50
-    sleep 1
-        ctxt2=$(grep ctxt /proc/stat | awk '{print $2}')
-        ctxt=$(($ctxt2 - $ctxt1))
-        result="Number os context switches in the last secound: $ctxt"
-    echo $result > result
-    } | whiptail --gauge "Getting data ..." 6 60 0
-}
-
-
-function userKernelMode {
-    {
-    raw=( $(grep "cpu " /proc/stat) )
-        userfirst=$((${raw[1]} + ${raw[2]}))
-        kernelfirst=${raw[3]}
-    echo 50
-        sleep 1
-    raw=( $(grep "cpu " /proc/stat) )
-        user=$(( $((${raw[1]} + ${raw[2]})) - $userfirst ))
-    echo 90
-        kernel=$(( ${raw[3]} - $kernelfirst ))
-        sum=$(($kernel + $user))
-        result="Percentage of last sekund in usermode: \
-        $((( $user*100)/$sum ))% \
-        \nand in kernelmode: $((($kernel*100)/$sum ))%"
-    echo $result > result
-    echo 100
-    } | whiptail --gauge "Getting data ..." 6 60 0
-}
-
-function interupts {
-    {
-    ints=$(vmstat 1 2 | tail -1 | awk '{print $11}')
-        result="Number of interupts in the last secound:  $ints"
-    echo 100
-    echo $result > result
-    } | whiptail --gauge "Getting data ..." 6 60 50
-}
-
 while [ 1 ]
 do
 CHOICE=$(
