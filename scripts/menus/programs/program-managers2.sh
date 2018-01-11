f02ac7d9f20dd3cc31371db1198510e3b81f690d
Deiteq <fayly80@gmail.com>
2018-01-11 16:54:31 +0100

    further testing

--- scripts/menus/programs/program-managers2.sh
+++ scripts/menus/programs/program-managers2.sh
@@ -1,8 +1,13 @@
  #!/bin/bash
 
-# point to variable file for ipv4 and domain.com
+## point to variable file for ipv4 and domain.com
+# This takes .yml file and converts it to bash readable format
+sed -e 's/:[^:\/\/]/="/g;s/$/"/g;s/ *=/=/g' /opt/appdata/plexguide/var2.yml > /opt/appdata/plexguide/var2.sh
 
-ipv4="/opt/appdata/plexguide/var2.yml"
+#ipv4="/opt/appdata/plexguide/var2.yml"
+
+# This replaces $ipv4 with local ip
+ipv4=`hostname -I | awk '{print $1}'`
 
 clear
 
@@ -20,21 +25,21 @@ result=$(whoami)
 case $CHOICE in
     "1)")
     ansible-playbook /opt/plexguide/ansible/plexguide.yml --tags radarr
-    echo "Radarr: http://{{ipv4}}:7878 | For Reverse Proxy radarr.{{domain}}"
+    echo "Radarr: http://$ipv4:7878 | For Reverse Proxy radarr.$domain"
     echo ""
     read -n 1 -s -r -p "Press any key to continue "
      ;;
 
     "2)")
     ansible-playbook /opt/plexguide/ansible/plexguide.yml --tags sonarr
-    echo "Sonarr: http://{{ipv4}}:8989 | For Revese Proxy sonarr.{{domain}}"
+    echo "Sonarr: http://$ipv4:8989 | For Revese Proxy sonarr.$domain"
     echo ""
     read -n 1 -s -r -p "Press any key to continue "
     ;;
 
     "3)")
     ansible-playbook /opt/plexguide/ansible/plexguide.yml --tags medusa
-    echo "MEDUSA: http://{{ipv4}}:8081 | For Reverse Proxy medusa.{{domain}}"
+    echo "MEDUSA: http://$ipv4:8081 | For Reverse Proxy medusa.$domain"
     echo ""
     read -n 1 -s -r -p "Press any key to continue "
     ;;
