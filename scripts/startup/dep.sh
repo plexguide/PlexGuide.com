d5ebd44cf8feb0d65eb9345d38f9a430ae434962
Admin0705 <user@dunn.cloud>
2018-01-10 21:56:58 -0500

    touchup

--- scripts/startup/dep.sh
+++ scripts/startup/dep.sh
@@ -26,7 +26,7 @@ if (whiptail --title "PlexGuide Installer/Upgrader" --yesno "Do You Agree to Ins
     pip install docker 1>/dev/null 2>&1
     echo "4. Installing Ansible Playbook & Supporting Components (Please Wait)"
     yes | apt-get update 1>/dev/null 2>&1
-    echo "5. Installing Dependicies - Please Wait"
+    echo "5. Installing Dependicies & Docker - Please Wait"
     echo
     ansible-playbook /opt/plexguide/ansible/docker.yml
     ansible-playbook /opt/plexguide/ansible/config.yml
@@ -52,22 +52,12 @@ echo "8. Pre-Installing PlexDrive & Services (Please Wait)"
 #Installing MongoDB for PlexDrive
   bash /opt/plexguide/scripts/startup/plexdrive-preinstall.sh 1>/dev/null 2>&1
 
-  echo "9. Installing Docker & Docker Compose (Please Standby)"
-
-# Install Docker and Docker Composer / Checks to see if is installed also
-
-  #curl -sSL https://get.docker.com | sh 1>/dev/null 2>&1
-  #curl -L https://github.com/docker/compose/releases/download/1.18.0/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose 1>/dev/null 2>&1
-  #chmod +x /usr/local/bin/docker-compose 1>/dev/null 2>&1
-  ansible-playbook /opt/plexguide/ansible/plexguide.yml --tags docker
-  #pip install docker 1>/dev/null 2>&1
-
   echo "10. Installing Portainer & Reverse Proxy (Please Wait)"
 
 # Installs Portainer
   ansible-playbook /opt/plexguide/ansible/plexguide.yml --tags portainer
 # Instlls Reverse Prox
-  ansible-playbook /opt/plexguide/ansible/test.yml --tags nginx
+  ansible-playbook /opt/plexguide/ansible/plexguide.yml --tags nginx
 # Remove NGINX if it exists
 
 ############################################# Install a Post-Docker Fix ###################### START
