46eec437b1e61c9859f4a1b77ef12863ef16fb2d
Admin0705 <user@dunn.cloud>
2018-01-10 22:33:21 -0500

    fix

--- scripts/startup/fix/001.sh
+++ scripts/startup/fix/001.sh
@@ -14,8 +14,8 @@ rm /var/plexguide/donation*
 rm /var/plexguide/dep*
 touch /var/plexguide/version-5.27
 whiptail --title "New Containers" --msgbox "Remember to Rebuild Your Containers" 9 66
-    exit
 else
     whiptail --title "No Upgrade" --msgbox "Your System Will Not Upgrade! Do not select or do anything" 9 66
+    sudo touch /var/plexguide/exit.yes
     exit
 fi
