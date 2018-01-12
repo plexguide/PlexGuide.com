81adf1bd795a1ce83e6e4920b56745e29cb49033
gforce75 <user@dunn.cloud>
2018-01-12 11:56:14 -0500

    fix

--- scripts/startup/fix/002.sh
+++ scripts/startup/fix/002.sh
@@ -8,7 +8,7 @@ echo "Stopping All containers"
 docker stop $(docker ps -a -q)
 ################
 ## Moving Nginx-Proxy Certs
-mv /opt/nginx-proxy /opt/appdata/nginx-proxy
+mv /opt/nginx-proxy /opt/appdata
 ## Remake the LetsEncrypt
 ansible-playbook /opt/plexguide/ansible/plexguide.yml --tags nginx
 ################ Version Updates for Patch
