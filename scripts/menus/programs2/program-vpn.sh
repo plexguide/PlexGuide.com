433c7233ac0a65576508e9e73e9e92a79227b045
Deiteq <fayly80@gmail.com>
2018-01-12 18:01:18 +0100

    test

--- scripts/menus/programs2/program-vpn.sh
+++ scripts/menus/programs2/program-vpn.sh
@@ -34,8 +34,8 @@ case $CHOICE in
 
      "3)")
 #     bash ansible-playbook /opt/plexguide/ansible/config2.yml
-#     bash  sed -e 's/:[^:\/\/]/="/g;s/$/"/g;s/ *=/=/g' /opt/appdata/plexguide/var2.yml > /opt/appdata/plexguide/var3.sh
-     bash sudo ansible-playbook /opt/plexguide/ansible/test3.yml
+     bash  sed -e 's/:[^:\/\/]/="/g;s/$/"/g;s/ *=/=/g' /opt/appdata/plexguide/var2.yml > /opt/appdata/plexguide/var3.sh
+#     bash sudo ansible-playbook /opt/plexguide/ansible/test3.yml
      read -n 1 -s -r -p "Press any key to continue "
       ;;
 
