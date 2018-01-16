80b105a41f4432947c0096de950f666763af651e
Deiteq <fayly80@gmail.com>
2018-01-16 16:09:37 +0100

    swapped proxy https for subdomain http in menus

--- scripts/menus/programs/program-vpn.sh
+++ scripts/menus/programs/program-vpn.sh
@@ -34,7 +34,7 @@ case $CHOICE in
      "2)")
       ansible-playbook /opt/plexguide/ansible/vpn.yml --tags rtorrentvpn
       echo "RTorrentVPN: http://$ipv4:3000"
-      #echo "For NGINX Proxy https://rtorrentvpn.$domain"
+      echo "For Subdomain http://rtorrentvpn.$domain"
       echo "For Domain http://$domain:3000"
       echo ""
       echo "Please set your own username & password!"
@@ -45,7 +45,7 @@ case $CHOICE in
      "3)")
       ansible-playbook /opt/plexguide/ansible/vpn.yml --tags delugevpn
       echo "DelugeVPN: http://$ipv4:8112"
-      #echo "For NGINX Proxy https://delugevpn.$domain"
+      echo "For Subdomain http://delugevpn.$domain"
       echo "For Domain http://$domain:8112"
       echo ""
       echo "Default password: deluge"
