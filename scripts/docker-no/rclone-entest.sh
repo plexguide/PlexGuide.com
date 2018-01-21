30f81f2eeb24266c8f904cd56544354727dcae4b
Deiteq <fayly80@gmail.com>
2018-01-21 18:08:47 +0100

    test

--- scripts/docker-no/rclone-entest.sh
+++ scripts/docker-no/rclone-entest.sh
@@ -5,7 +5,7 @@ clear
 ## Supporting Folders
 mkdir -p /mnt/.gcrypt
 mkdir -p /mnt/encrypt
-
+echo 1
 ## Installing RClone
   cd /tmp
   curl -O https://downloads.rclone.org/rclone-current-linux-amd64.zip 1>/dev/null 2>&1
@@ -19,11 +19,11 @@ mkdir -p /mnt/encrypt
   mandb 1>/dev/null 2>&1
   cd .. && sudo rm -r rclone* 1>/dev/null 2>&1
   cd ~
-
+echo 2
 ############################################# RCLONE
 ## Executes RClone Config
 rclone config
-
+echo 3
 # allows others to access fuse
 tee "/etc/fuse.conf" > /dev/null <<EOF
 # /etc/fuse.conf - Configuration file for Filesystem in Userspace (FUSE)
@@ -33,22 +33,22 @@ tee "/etc/fuse.conf" > /dev/null <<EOF
 # Allow non-root users to specify the allow_other or allow_root mount options.
 user_allow_other
 EOF
-
+echo 4
 mkdir -p /root/.config/rclone/ 1>/dev/null 2>&1
-
+echo 5
 ## Copying to /mnt incase
 cp ~/.config/rclone/rclone.conf /root/.config/rclone/ 1>/dev/null 2>&1
-
+echo 6
 ansible-playbook /opt/plexguide/ansible/plexguide.yml --tags rclone_en
-
+echo 7
 ## RClone Script
 chmod 755 /opt/appdata/plexguide/rclone.sh
 chmod 755 /opt/appdata/plexguide/rclone-en.sh
 chmod 755 /opt/appdata/plexguide/rclone-encrypt.sh
-
+echo 8
 ## Create the Move Script
 chmod 755 /opt/appdata/plexguide/move-en.sh
-
+echo 9
 ###### Ensure Changes Are Reflected
 #sudo systemctl daemon-reload
 
@@ -59,7 +59,7 @@ systemctl disable unionfs 1>/dev/null 2>&1
 systemctl stop unionfs 1>/dev/null 2>&1
 systemctl stop rclone 1>/dev/null 2>&1
 systemctl stop move 1>/dev/null 2>&1
-
+echo 10
 # ensure that the encrypted services are on
 systemctl enable rclone 1>/dev/null 2>&1
 systemctl enable rclone-en 1>/dev/null 2>&1
@@ -71,7 +71,7 @@ systemctl start rclone-en 1>/dev/null 2>&1
 systemctl start move-en 1>/dev/null 2>&1
 systemctl start unionfs-encrypt 1>/dev/null 2>&1
 systemctl start rclone-encrypt 1>/dev/null 2>&1
-
+echo 11
 systemctl restart rclone 1>/dev/null 2>&1
 systemctl restart rclone-en 1>/dev/null 2>&1
 systemctl restart rclone-encrypt 1>/dev/null 2>&1
@@ -79,6 +79,6 @@ systemctl restart rclone-encrypt 1>/dev/null 2>&1
 mkdir -p /var/plexguide/rclone 1>/dev/null 2>&1
 touch /var/plexguide/rclone/en 1>/dev/null 2>&1
 rm -r /var/plexguide/rclone/un 1>/dev/null 2>&1
-
+echo 12
 # pauses
 bash /opt/plexguide/scripts/docker-no/continue.sh
