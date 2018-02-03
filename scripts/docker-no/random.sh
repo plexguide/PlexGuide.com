6e1c9c7851d6ad57428206f0fac59148814d1305
Admin0705 <user@dunn.cloud>
2018-02-03 16:43:26 -0500

    update

--- scripts/docker-no/random.sh
+++ scripts/docker-no/random.sh
@@ -17,12 +17,12 @@ let "number %= $RANGE"
 
 if [ "$number" -eq "0" ]
 then
-   whiptail --title "Startup Message" --msgbox "Visit http://binance.plexguide.com to purchase crypto coins. Purchasing Ripple, Stellar, and Tron are the way to go. Any sign ups will help support future website server costs and possible prizes! I truly use the site myself!" 11 66
+   whiptail --title "Startup Message" --msgbox "Did you know that you can type - sudo pgupdate - from the terminal prompt and plexguide will update itself?" 11 66
 fi
 
 if [ "$number" -eq "1" ]
 then
-   whiptail --title "Startup Message" --msgbox "Did you know you can edit our wiki pages? We truly rely on your help on providing screenshots or fixing mistakes to make the process better!" 10 66
+   whiptail --title "Startup Message" --msgbox "See issues with our Wiki Pages? Please comment in the forum and we will udpdate! Once an offical wiki plugin is built, we'll add it!" 10 66
 fi
 
 if [ "$number" -eq "2" ]
@@ -42,7 +42,7 @@ fi
 
 if [ "$number" -eq "5" ]
 then
-   whiptail --title "Startup Message" --msgbox "Visit http://hitbtc.plexguide.com to puchase more crypto! Purchasing bytecoins which are now exploding! Good to trade currency without ID requirements. Signups help pay for some costs; but I personally use!" 11 66
+   whiptail --title "Startup Message" --msgbox "Development cost are about $150 a month! If you can donate on the front page, it will help a-ton!" 11 66
 fi
 
 echo
