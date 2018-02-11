e5e209e4cc34c39c193b6fe2e6b93b5026a50a4e
Admin0705 <user@dunn.cloud>
2018-02-11 15:31:42 -0500

    update couch

--- scripts/menus/programs/program-managers.sh
+++ scripts/menus/programs/program-managers.sh
@@ -11,18 +11,29 @@ clear
 while [ 1 ]
 do
 CHOICE=$(
-whiptail --title "Program Categories" --menu "Make your choice" 12 25 5 \
-    "1)" "Lidarr"   \
-    "2)" "Medusa"   \
-    "3)" "Sonarr"   \
-    "4)" "Radarr"   \
-	"5)" "Mylar"    \
-    "6)" "Exit  "  3>&2 2>&1 1>&3
+whiptail --title "Program Categories" --menu "Make your choice" 12 27 7 \
+    "1)" "Couchpotato" \
+    "2)" "Lidarr"   \
+    "3)" "Medusa"   \
+    "4)" "Sonarr"   \
+    "5)" "Radarr"   \
+	  "6)" "Mylar"    \
+    "7)" "Exit  "  3>&2 2>&1 1>&3
 )
 
 result=$(whoami)
-case $CHOICE in
+case $CHOICE 
+
     "1)")
+    ansible-playbook /opt/plexguide/ansible/plexguide.yml --tags couchpotato
+    echo "Lidarr: http://$ipv4:5050"
+    echo "For Subdomain http://couchpotato.$domain"
+    echo "For Domain http://$domain:5050"
+    echo ""
+    read -n 1 -s -r -p "Press any key to continue "
+     ;;
+
+    "2)")
     ansible-playbook /opt/plexguide/ansible/plexguide.yml --tags lidarr
     echo "Lidarr: http://$ipv4:8686"
     echo "For Subdomain http://lidarr.$domain"
@@ -31,7 +42,7 @@ case $CHOICE in
     read -n 1 -s -r -p "Press any key to continue "
      ;;
 
-    "2)")
+    "3)")
     ansible-playbook /opt/plexguide/ansible/plexguide.yml --tags medusa
     echo "MEDUSA: http://$ipv4:8081"
     echo "For Subdomain http://medusa.$domain"
@@ -40,7 +51,7 @@ case $CHOICE in
     read -n 1 -s -r -p "Press any key to continue "
     ;;
 
-  "3)")
+  "4)")
    ansible-playbook /opt/plexguide/ansible/plexguide.yml --tags sonarr
    echo "Sonarr: http://$ipv4:8989"
    echo "For Subdomain http://sonarr.$domain"
@@ -49,7 +60,7 @@ case $CHOICE in
    read -n 1 -s -r -p "Press any key to continue "
    ;;
 
-    "4)")
+    "5)")
     ansible-playbook /opt/plexguide/ansible/plexguide.yml --tags radarr
     echo "Radarr: http://$ipv4:7878"
     echo "For Subdomain http://radarr.$domain"
@@ -58,7 +69,7 @@ case $CHOICE in
     read -n 1 -s -r -p "Press any key to continue "
      ;;
 	 
-	"5)")
+	"6)")
     ansible-playbook /opt/plexguide/ansible/plexguide.yml --tags mylar
     echo "Mylar: http://$ipv4:8090"
     echo "For Subdomain http://mylar.$domain"
@@ -67,7 +78,7 @@ case $CHOICE in
     read -n 1 -s -r -p "Press any key to continue "
      ;;
 
-     "6)")
+     "7)")
       clear
       exit 0
       ;;
