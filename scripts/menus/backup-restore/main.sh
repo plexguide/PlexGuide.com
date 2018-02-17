f71131924dd492fddd8e7de1757ae92152281483
Admin0705 <user@dunn.cloud>
2018-02-16 22:25:43 -0500

    update

--- scripts/menus/backup-restore/main.sh
+++ scripts/menus/backup-restore/main.sh
@@ -8,8 +8,7 @@ CHOICE=$(
 whiptail --title "Backup Menu" --menu "Make your choice" 11 25 4 \
     "1)" "Backup"  \
     "2)" "Restore"  \
-    "3)" "Legacy"  \
-    "4)" "Exit  "  3>&2 2>&1 1>&3
+    "3)" "Exit  "  3>&2 2>&1 1>&3
 )
 
 result=$(whoami)
@@ -22,12 +21,7 @@ case $CHOICE in
         bash /opt/plexguide/scripts/menus/backup-restore/restore/restore.sh
         ;;
 
-    #"3)")
-      #echo ymlprogram legacy > /tmp/program_var.txt
-      #bash /opt/plexguide/scripts/docker-no/backup-script.sh
-      #;;
-
-    "4)")
+    "3)")
         clear
         exit 0
         ;;
