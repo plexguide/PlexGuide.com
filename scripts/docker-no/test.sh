a21b6ec8d0e4faebb97b24ed17dd152af2f7eea1
Admin0705 <user@dunn.cloud>
2018-02-18 21:10:45 -0500

    update

--- scripts/docker-no/test.sh
+++ scripts/docker-no/test.sh
@@ -9,12 +9,12 @@ do
 
 ### Poll #1
 echo "On Poll 1"
-	a=$(systemctl status move | grep "GBytes" | grep "MBytes" | awk '{print $7}') && echo "$((a + 0))"
+	a=$(systemctl status move | grep "GBytes" | grep "MBytes" | awk '{print $7}') && echo "$((a + 0))" | bc -l
 
 sleep 20
 echo "On Poll 2"
 ### Poll #2	
-	b=$(systemctl status move | grep "GBytes" | grep "MBytes" | awk '{print $7}') && echo "$((b + 0))"
+	b=$(systemctl status move | grep "GBytes" | grep "MBytes" | awk '{print $7}') && echo "$((b + 0))" | bc -l
 
 done
 
