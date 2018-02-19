3e1895dc7650f622259bddc71ac7866703cf7df6
Admin0705 <user@dunn.cloud>
2018-02-18 19:45:26 -0500

    update

---
+++ scripts/docker-no/test.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+while [ $pgpoll1 -ne pgpoll2 ]
+do
+ 
+	pgpoll1=$(systemctl status move | grep "GBytes" | grep "MBytes" | awk '{print $7}') && echo $pgpoll1
+sleep 30
+	pgpoll2=$(systemctl status move | grep "GBytes" | grep "MBytes" | awk '{print $7}') && echo $pgpoll2
+done
+
+echo "Final Transfer Output"
+echo $pgpoll2
