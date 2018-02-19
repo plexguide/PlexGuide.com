3407b89718dbb862bd2bca0823eaaf48dfd469d2
Admin0705 <user@dunn.cloud>
2018-02-19 08:03:36 -0500

    update

--- scripts/docker-no/test2.sh
+++ scripts/docker-no/test2.sh
@@ -2,7 +2,7 @@
 ### bash /opt/plexguide/scripts/docker-no/test2.sh
 
 	echo "starting program"
-if [ ! -e "/opt/appdata/plexguide/date1.txt" ]
+if [ ! -e "/opt/appdata/plexguide/date1" ]
 then
 	date -d '0 hour ago' '+%d' > /opt/appdata/plexguide/date1
     date1=$(awk '{print $2}' /opt/appdata/plexguide/date1)
