12594aab53982eb9dad0711be5095f2ede202b58
Admin0705 <user@dunn.cloud>
2018-02-19 01:02:00 -0500

    sudo pgupdate

--- scripts/docker-no/test2.sh
+++ scripts/docker-no/test2.sh
@@ -3,4 +3,4 @@
 
 date1="Sat Dec 28 03:22:19 2013"
 date2="Sun Dec 29 02:22:19 2013"
-date -d @$(( $(date -d "$date2" +%s) - $(date -d "$date1" +%s) )) -u +'%H:%M:%S
+date -d @$(( $(date -d "$date2" +%s) - $(date -d "$date1" +%s) )) -u +'%H:%M:%S'
