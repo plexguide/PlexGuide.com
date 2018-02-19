9253708b5fb74c5751069b08f7bf1398d2b39d5e
Admin0705 <user@dunn.cloud>
2018-02-19 01:03:04 -0500

    update

--- scripts/docker-no/test2.sh
+++ scripts/docker-no/test2.sh
@@ -2,5 +2,5 @@
 ### bash /opt/plexguide/scripts/docker-no/test2.sh
 
 date1="Sat Dec 28 03:22:19 2013"
-date2="Sun Dec 29 02:22:19 2013"
+date2="Sun Dec 29 06:22:19 2013"
 date -d @$(( $(date -d "$date2" +%s) - $(date -d "$date1" +%s) )) -u +'%H:%M:%S'
