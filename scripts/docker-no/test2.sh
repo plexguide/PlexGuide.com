8132f21735b0a6b4ed933362b8911e314c92e5fe
Admin0705 <user@dunn.cloud>
2018-02-19 07:39:46 -0500

    update

--- scripts/docker-no/test2.sh
+++ scripts/docker-no/test2.sh
@@ -4,12 +4,9 @@
 file="/var/plexguide/vnc.yes"
 if [ -e "$file" ]
 then
-    #date -d '0 hour ago' '+%d' > /opt/appdata/plexguide/date.txt
-    #echo "flag date set"
+    date -d '0 hour ago' '+%d' > /opt/appdata/plexguide/date.txt
+    echo "flag date set"
 fi
-
-
-
 
 #if [ ! -f /opt/appdata/plexguide/date1.txt ]
 #	then
@@ -21,16 +18,13 @@ while [ 1 -lt 10000000 ]
 do
 
 	a=$(awk '{print 1}' /opt/appdata/plexguide/date.txt)
-	echo  "$b"
+	echo  "$a"
 	echo "flag1"
 	
-	if 
-	then
-	YMLDISPLAY=$(awk '/ymlprogram/{print $2}' /opt/plexguide/tmp.txt)
-	fi
-
 	sleep 10
 	$b=(date -d '0 hour ago' '+%d')
+	echo "$b"
 	echo "flag2"
+
 done
 
