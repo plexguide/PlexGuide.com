522e5ec1916e6195bfd8f84c6754086982156af5
Admin0705 <user@dunn.cloud>
2018-02-18 21:05:22 -0500

    update

--- scripts/docker-no/test.sh
+++ scripts/docker-no/test.sh
@@ -1,22 +1,22 @@
 #!/bin/bash
 ### bash /opt/plexguide/scripts/docker-no/test.sh
 
-pgpoll1="1"
-pgpoll2="2"
+pgpolla="1"
+pgpollb="2"
 
-while [ "$pgpoll1" -ne "$pgpoll2" ]
+while [ "$pgpolla" -ne "$pgpollb" ]
 do
 
 ### Poll #1
 echo "On Poll 1"
-	pgpoll1=$(systemctl status move | grep "GBytes" | grep "MBytes" | awk '{print $7}') && echo $pgpoll1
+	pgpolla=$(systemctl status move | grep "GBytes" | grep "MBytes" | awk '{print $7}') && echo "$((pgpolla" + 0))"
 
 sleep 20
 echo "On Poll 2"
 ### Poll #2	
-	pgpoll2=$(systemctl status move | grep "GBytes" | grep "MBytes" | awk '{print $7}') && echo $pgpoll2
+	pgpollb=$(systemctl status move | grep "GBytes" | grep "MBytes" | awk '{print $7}') && echo "$((pgpollb" + 0))"
 
 done
 
 echo "Final Transfer Output"
-echo $pgpoll2
+echo $pgpollb
