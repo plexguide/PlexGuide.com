efafb5c9baf9cacd4e20d8e2a5a55f149815bd9a
Admin0705 <user@dunn.cloud>
2018-02-18 20:50:42 -0500

    update

--- scripts/docker-no/test.sh
+++ scripts/docker-no/test.sh
@@ -4,10 +4,11 @@ while [ $pgpoll1 -ne $pgpoll2 ]
 do
 
 ### Poll #1
+echo "On Poll 1"
 	pgpoll1=$(systemctl status move | grep "GBytes" | grep "MBytes" | awk '{print $7}') && echo $pgpoll1
 
 sleep 20
-
+echo "On Poll 2"
 ### Poll #2	
 	pgpoll2=$(systemctl status move | grep "GBytes" | grep "MBytes" | awk '{print $7}') && echo $pgpoll2
 
