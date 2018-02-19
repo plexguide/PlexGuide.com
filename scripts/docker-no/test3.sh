01f72f14fc066d2fd7eb26abe48d41e6c433c420
Admin0705 <user@dunn.cloud>
2018-02-19 08:43:11 -0500

    update

---
+++ scripts/docker-no/test3.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+### bash /opt/plexguide/scripts/docker-no/test2.sh
+
+echo "Starting Program" 
+
+if [ ! -e "/opt/appdata/plexguide/date1" ]
+then
+	date +%s > /opt/appdata/plexguide/date1
+    date1=$(awk '{print $1}' /opt/appdata/plexguide/date1)
+    echo "a date never existed"
+    echo "$date1"
+else
+	echo "file exists already"
+	date1=$(awk '{print $1}' /opt/appdata/plexguide/date1)
+	echo "$date1"
+fi
+
+date +%s > /opt/appdata/plexguide/date2
+date2=$(awk '{print $1}' /opt/appdata/plexguide/date2)
+
+total=$((date2-date1))
+
+while [ "$total" -lt 90001 ]
+do
+	date -d '0 hour ago' '+%d' > /opt/appdata/plexguide/date2
+	date2=$(awk '{print $1}' /opt/appdata/plexguide/date2)
+	echo "$date2"
+	sleep 5
+done
+
+
+
