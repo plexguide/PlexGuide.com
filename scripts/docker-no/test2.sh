69e1779580c8922c5983adc9cf00f6c8bd0ede93
Admin0705 <user@dunn.cloud>
2018-02-19 07:28:49 -0500

    update

--- scripts/docker-no/test2.sh
+++ scripts/docker-no/test2.sh
@@ -1,6 +1,25 @@
 #!/bin/bash
 ### bash /opt/plexguide/scripts/docker-no/test2.sh
 
-date1="Sat Dec 28 03:22:19 2013"
-date2="Sun Dec 29 06:22:19 2013"
-date -d @$(( $(date -d "$date2" +%s) - $(date -d "$date1" +%s) )) -u +'%H:%M:%S'
+if [ ! -f /opt/appdata/plexguide/date1.txt ]; then
+    date -d '0 hour ago' '+%d' > /opt/appdata/plexguide/date.txt
+    echo "flag date set"
+fi
+
+while [ 1 -lt 10000000 ]
+do
+
+	a=$(awk '{print 1}' /opt/appdata/plexguide/date.txt)
+	echo  "$b"
+	echo "flag1"
+	
+	if 
+	then
+	YMLDISPLAY=$(awk '/ymlprogram/{print $2}' /opt/plexguide/tmp.txt)
+	fi
+
+	sleep 10
+	$b=(date -d '0 hour ago' '+%d')
+	echo "flag2"
+done
+
