f9f908c5f9a80ae8ee8c2486aa2b223fa0b1f2b5
Admin0705 <user@dunn.cloud>
2018-02-24 16:20:06 -0500

    update

--- scripts/docker-no/test.sh
+++ scripts/docker-no/test.sh
@@ -6,20 +6,11 @@ echo "$d"
 
 mfolder="/mnt/gdrive/plexguide/backup.old/backup-"
 mpath="$mfolder$d"
-echo "path is"
-echo $mfolder
-echo $d
-echo $mpath
 
 mkdir /mnt/gdrive/plexguide/backup.old/
 mkdir $mpath
 mv /mnt/gdrive/plexguide/backup/* $mpath
 
-timestamp # print timestamp
-# do something else...
-timestamp 
-
-s
 docker ps -a --format "{{.Names}}"  > /opt/appdata/plexguide/running
 
 while read p; do
