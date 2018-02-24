1fed69232f0f0ab1d430b930fb24dd177d451e8c
Admin0705 <user@dunn.cloud>
2018-02-24 10:43:11 -0500

    update

--- menus/main.sh
+++ menus/main.sh
@@ -1,42 +1,5 @@
 #!/bin/bash
 
-### remove exit hold
-#rm -r /var/plexguide/exit.yes 1>/dev/null 2>&1
-
-#check to see if /var/plexguide/dep exists - if not, install dependencies
-bash /opt/plexguide/scripts/docker-no/user.sh
-
-# copying rclone config to user incase bonehead is not root
-cp /root/.config/rclone/rclone.conf ~/.config/rclone/rclone.conf 1>/dev/null 2>&1
-
-# Checking to see if VNC Container is Running
-file="/var/plexguide/vnc.yes"
-if [ -e "$file" ]
-then
-whiptail --title "Warning" --msgbox "You still have the VNC Container Running! Make sure to Destroy the Container via the VNC Menu!" 9 66
-fi
-
-file="/var/plexguide/dep35.yes"
-if [ -e "$file" ]
-then
-   touch /var/plexguide/message.no
-else
-   bash /opt/plexguide/scripts/startup/dep.sh
-fi
-
-## starup Message
-
-  bash /opt/plexguide/scripts/docker-no/random.sh
-
-## Force Exit if Required
-file="/var/plexguide/exit.yes"
-if [ -e "$file" ]
-then
-   exit
-fi
-
-clear
-
 HEIGHT=16
 WIDTH=45
 CHOICE_HEIGHT=11
