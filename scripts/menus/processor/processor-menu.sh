6437d09bc83fed5912a2d51cbd24dde28296b227
Admin0705 <user@dunn.cloud>
2018-02-23 23:29:30 -0500

    update

--- scripts/menus/processor/processor-menu.sh
+++ scripts/menus/processor/processor-menu.sh
@@ -13,8 +13,7 @@ $DIALOG --title " Virtual Machine" --clear \
 
 case $? in
   0)
-    dialog --title "Example Dialog message box" \
---msgbox "\n Installation Completed on host7" 6 50
+    dialog --title "Example Dialog message box" --msgbox "\n Installation Completed on host7" 6 50
     exit
   1)
     dialog --title "User - System Is Not Virtual" \  --msgbox "\n We recommend that you select performance mode. By default, your utilizing ondemand mode. Mode does not kick in until you REBOOT!" 6 50
