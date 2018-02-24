90bec1d835a38ac6ecc2ec1e556e1b4df5ea638a
Admin0705 <user@dunn.cloud>
2018-02-23 23:00:23 -0500

    test

--- scripts/menus/processor/processor-menu.sh
+++ scripts/menus/processor/processor-menu.sh
@@ -4,7 +4,21 @@ clear
 
 sudo touch /var/plexguide/asked.processor
 ################# Virtual Machine Check
-dialog --title "Hello" --msgbox 'Hello world!' 6 20
+#dialog --title "Hello" --msgbox 'Hello world!' 6 20
+
+DIALOG=${DIALOG=dialog}
+
+$DIALOG --title " My first dialog" --clear \
+        --yesno "Hello , this is my first dialog program" 10 30
+
+case $? in
+  0)
+    echo "Yes chosen.";;
+  1)
+    echo "No chosen.";;
+  255)
+    echo "ESC pressed.";;
+esac
 
 if (whiptail --title "Virutal Machine Question" --yesno "Are You Utilizing A Virtual Machine or VPS?" 8 56) then
 
