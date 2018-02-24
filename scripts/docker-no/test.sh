bfc72915268775902badf26a8c3f6933c27e5090
Admin0705 <user@dunn.cloud>
2018-02-24 15:46:36 -0500

    update

--- scripts/docker-no/test.sh
+++ scripts/docker-no/test.sh
@@ -4,7 +4,7 @@ timestamp() {
 
 # 
 mkdir /mnt/gdrive/plexguide/backup.old/
-mkdir /mnt/drive/plexguide/backup.old/backup-{timestamp}
+mkdir /mnt/gdrive/plexguide/backup.old/backup-{timestamp}
 mv /mnt/gdrive/plexguide/backup /mnt/drive/plexguide/backup.old/backup-{timestamp}
 
 timestamp # print timestamp
