0804ec9f73306cd3e88f65e898e4fd2fcf1caaf6
Admin0705 <user@dunn.cloud>
2018-02-23 23:23:11 -0500

    update

--- scripts/menus/processor/processor-menu.sh
+++ scripts/menus/processor/processor-menu.sh
@@ -6,13 +6,6 @@ sudo touch /var/plexguide/asked.processor
 ################# Virtual Machine Check
 #dialog --title "Hello" --msgbox 'Hello world!' 6 20
 
-dialog --backtitle "OS infomration" \
---radiolist "Select OS:" 10 40 3 \
- 1 "Linux 7.2" off \
- 2 "Solaris 9" on \
- 3 "HPUX 11i" off
-
-
 DIALOG=${DIALOG=dialog}
 
 $DIALOG --title " Virtual Machine" --clear \
