38804c43742326811f7f6ab60ee97f20300c6a90
Admin0705 <user@dunn.cloud>
2018-02-24 18:09:46 -0500

    update

---
+++ scripts/backup-restore/restore.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+
+
+
+
+
+
+
+#d=$(date +%Y-%m-%d-%T)
+
+#touch /opt/appdata/plexguide/backup 1>/dev/null 2>&1
+#sudo rm -r /opt/appdata/plex/trans* 1>/dev/null 2>&1
+
+#mfolder="/mnt/gdrive/plexguide/backup.old/backup-"
+#mpath="$mfolder$d"
+
+#mkdir /mnt/gdrive/plexguide/backup.old/ 1>/dev/null 2>&1
+#mkdir $mpath
+#mv /mnt/gdrive/plexguide/backup/* $mpath 
+
+ls -la | awk '{ print $9}' | tail -n 6 > /opt/appdata/plexguide/backuplist
+
+count=0
+
+while read p; do
+  ((count++))
+      	if [ "count" = 1 ]; then
+            echo "it worked"
+        fi
+  #echo $p > /tmp/program_var
+  #ansible-playbook /opt/plexguide/ansible/plexguide.yml --tags backup
+done </opt/appdata/plexguide/backuplist
+
+#rm -r /mnt/gdrive/plexguide/backup/watchtower.tar 1>/dev/null 2>&1
+#rm -r /opt/appdata/plexguide/backup 1>/dev/null 2>&
+
+#echo ""
+#echo "Backup Complete"
+#read -n 1 -s -r -p "Press any key to continue "
+#clear
