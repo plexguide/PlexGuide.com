ed3b9e6375e57d14b22e6c45a8b3624807c89b74
Admin0705 <user@dunn.cloud>
2018-02-23 23:36:36 -0500

    update

--- scripts/docker-no/user.sh
+++ scripts/docker-no/user.sh
@@ -2,6 +2,13 @@
 
 mkdir -p /var/plexguide 1>/dev/null 2>&1
 
+file="/var/plexguide/plexguide.user"
+if [ -e "$file" ]
+then
+    echo ""
+else
+clear
+
 whiptail --title "Important" --msgbox "Welcome to the PlexGuide.com Installer.  If you have questions,
 please read the Wikis or join our #Discord Channel." 8 78
 touch /var/plexguide/plexguide.user 1>/dev/null 2>&1
