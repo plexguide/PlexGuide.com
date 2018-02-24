4dbf3b559c7aad2243a3b68d7ab3e44f3dfc4ed1
Admin0705 <user@dunn.cloud>
2018-02-24 15:15:30 -0500

    update

--- scripts/docker-no/test.sh
+++ scripts/docker-no/test.sh
@@ -2,5 +2,5 @@ docker ps -a --format "{{.Names}}"  > /opt/appdata/plexguide/running
 
 while read p; do
   echo $p > /tmp/program_var
-  ansible-playbook /opt/plexguide/ansible/plexguide.yml --tags restore
+  ansible-playbook /opt/plexguide/ansible/plexguide.yml --tags backup
 done </opt/appdata/plexguide/running
