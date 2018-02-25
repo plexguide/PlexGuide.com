88fa1d846f0044a9c40a8cc85edc76c317e2a936
Admin0705 <user@dunn.cloud>
2018-02-25 15:24:19 -0500

    update

--- menus/plexdrive/main.sh
+++ menus/plexdrive/main.sh
@@ -1,19 +1,7 @@
 #!/bin/bash
 
 ############# User Confirms They Understand
-DIALOG=${DIALOG=dialog}
-
-$DIALOG --title " My first dialog" --clear \
-        --yes "Hello , this is my first dialog program" 10 30
-
-case $? in
-  0)
-    echo "Yes chosen.";;
-  1)
-    echo "No chosen.";;
-  255)
-    echo "ESC pressed.";;
-esac
+dialog --title "Title" --msgbox "Hello World" 0 0
 
 
 ############# Menu
