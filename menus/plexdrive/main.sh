a58c198d52458b1068c650ec5940ed2653cd99eb
Admin0705 <user@dunn.cloud>
2018-02-25 15:14:35 -0500

    update

--- menus/plexdrive/main.sh
+++ menus/plexdrive/main.sh
@@ -7,8 +7,8 @@ BACKTITLE="Visit https://PlexGuide.com - Automations Made Simple"
 TITLE="PlexDrive for PG"
 MENU="Choose one of the following options:"
 
-OPTIONS=(A "PlexDrive5 Install"
-         B "PlexDrive4 (Not Ready)"
+OPTIONS=(A "PlexDrive4 (Recommended)"
+         B "PlexDrive5 "
          C "Remove PlexDrive Tokens"
          Z "Exit")
 
@@ -23,13 +23,13 @@ CHOICE=$(dialog --clear \
 clear
 case $CHOICE in
         A)
-            rm -r /opt/appdata/plexguide/pd4 1>/dev/null 2>&1
-            touch /opt/appdata/plexguide/pd5 1>/dev/null 2>&1
-            bash /opt/plexguide/scripts/docker-no/plexdrive.sh ;;
-        B)
             rm -r /opt/appdata/plexguide/pd5 1>/dev/null 2>&1
             touch /opt/appdata/plexguide/pd4 1>/dev/null 2>&1
             bash /opt/plexguide/scripts/docker-no/plexdrive4.sh ;;
+        B)
+            rm -r /opt/appdata/plexguide/pd4 1>/dev/null 2>&1
+            touch /opt/appdata/plexguide/pd5 1>/dev/null 2>&1
+            bash /opt/plexguide/scripts/docker-no/plexdrive.sh ;;
         C)
             rm -r /root/.plexdrive 1>/dev/null 2>&1
             rm -r ~/.plexdrive 1>/dev/null 2>&1
