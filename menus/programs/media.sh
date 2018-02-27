0ce53bbf21ad705f782eb66fcf86e3b5eb4170ee
Admin0705 <user@dunn.cloud>
2018-02-27 06:01:47 -0500

    update

--- menus/programs/media.sh
+++ menus/programs/media.sh
@@ -1,4 +1,21 @@
 #!/bin/bash
+#
+# [PlexGuide Menu]
+#
+# GitHub:   https://github.com/Admin9705/PlexGuide.com-The-Awesome-Plex-Server
+# Author:   Admin9705 - Detique
+# URL:      https://plexguide.com
+#
+# PlexGuide Copyright (C) 2018 PlexGuide.com
+# Licensed under GNU General Public License v3.0 GPL-3 (in short)
+#
+#   You may copy, distribute and modify the software as long as you track
+#   changes/dates in source files. Any modifications to our software
+#   including (via compiler) GPL-licensed code must also be made available
+#   under the GPL along with build & install instructions.
+#
+#################################################################################
+
 export NCURSES_NO_UTF8_ACS=1
   ## point to variable file for ipv4 and domain.com
   source <(grep '^ .*='  /opt/appdata/plexguide/var.sh)
