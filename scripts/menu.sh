ebbcde9430204c70e74cd2ca79b40808d99b1691
Admin0705 <user@dunn.cloud>
2018-02-27 19:00:45 -0500

    update

--- scripts/menu.sh
+++ scripts/menu.sh
@@ -21,7 +21,7 @@ if [ -e "$file" ]
 then
    touch /var/plexguide/message.no
 else
-   bash /opt/plexguide/scripts/baseinstaller/installer.sh
+   bash /opt/plexguide/scripts/baseinstall/main.sh
 fi
 
 ## starup Message
