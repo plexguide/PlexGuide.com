ba36855385aa0b291911e90b8b75c1da99a25cb7
Admin0705 <user@dunn.cloud>
2018-02-27 22:26:35 -0500

    update

---
+++ menus/pgverions/main.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+#
+# [PlexGuide Menu]
+#
+# GitHub:   https://github.com/Admin9705/PlexGuide.com-The-Awesome-Plex-Server
+# Author:   Admin9705 & Detique
+# URL:      https://plexguide.com
+#
+# PlexGuide Copyright (C) 2018 PlexGuide.com
+# Licensed under GNU General Public License v3.0 GPL-3 (in short)
+#
+#   You may copy, distribute and modify the software as long as you track
+#   changes/dates in source files. Any modifications to our software
+#   including (via compiler) GPL-licensed code must also be made available
+#   under the GPL along with build & install instructions.
+#
+#################################################################################
+
+export NCURSES_NO_UTF8_ACS=1
+HEIGHT=12
+WIDTH=45
+CHOICE_HEIGHT=5
+BACKTITLE="Visit https://PlexGuide.com - Automations Made Simple"
+TITLE="PG Older Versions"
+MENU="Restore to an Older PG Version:"
+
+OPTIONS=(A "Version: 5.048"
+         Z "Exit")
+
+CHOICE=$(dialog --clear \
+                --backtitle "$BACKTITLE" \
+                --title "$TITLE" \
+                --menu "$MENU" \
+                $HEIGHT $WIDTH $CHOICE_HEIGHT \
+                "${OPTIONS[@]}" \
+                2>&1 >/dev/tty)
+
+clear
+case $CHOICE in
+        A)
+            bash /opt/plexguide/scripts/menus/backup-restore/backup/backup.sh ;;
+
+        Z)
+            clear
+            exit 0
+            ;;
+esac
+
+clear
