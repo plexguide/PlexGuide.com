0a3b74e35a6de6e6a48ec6fb07c5c527213dfe29
Admin0705 <user@dunn.cloud>
2018-03-01 16:43:13 -0500

    update

--- scripts/checker/main.sh
+++ scripts/checker/main.sh
@@ -1,3 +1,3 @@
-sudo rm -r /tmp/version.sh 1>/dev/null 2>&1
-wget https://raw.githubusercontent.com/Admin9705/PlexGuide.com-The-Awesome-Plex-Server/Version-5/version.sh 1>/dev/null 2>&1
-sudo bash /tmp/version.sh 1>/dev/null 2>&1
+sudo rm -r /tmp/main.sh 1>/dev/null 2>&1
+wget https://raw.githubusercontent.com/Admin9705/PlexGuide.com-The-Awesome-Plex-Server/Version-5/menus/version/main.sh 1>/dev/null 2>&1
+sudo mv /tmp/main.sh /menus/version/main.sh 1>/dev/null 2>&1
