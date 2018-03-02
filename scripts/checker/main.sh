66793b4e23eaaf535cec9ffe83ed2da1e10b4e2e
gforce75 <user@dunn.cloud>
2018-03-01 23:06:40 -0500

    update

--- scripts/checker/main.sh
+++ scripts/checker/main.sh
@@ -1,6 +1,6 @@
-sudo rm -r /opt/plexguide/menus/version/main.sh
-sudo mkdir /opt/plexguide/menus/version -p
-sudo chmod 755 /opt/plexguide/menus/version/
-wget https://raw.githubusercontent.com/Admin9705/PlexGuide.com-The-Awesome-Plex-Server/Version-5/menus/version/main.sh -P /opt/plexguide/menus/version/
+#sudo rm -r /opt/plexguide/menus/version/main.sh
+#sudo mkdir /opt/plexguide/menus/version -p
+#sudo chmod 755 /opt/plexguide/menus/version/
+#wget https://raw.githubusercontent.com/Admin9705/PlexGuide.com-The-Awesome-Plex-Server/Version-5/menus/version/main.sh -P /opt/plexguide/menus/version/
 #sudo rm -r /opt/plexguide/menus/version/main.sh 1>/dev/null 2>&1
 #sudo mv /tmp/main.sh /opt/plexguide/menus/version/main.sh 1>/dev/null 2>&1
