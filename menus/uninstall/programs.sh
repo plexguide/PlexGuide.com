de9cf6f9efe4e6635e75a2e6524b1b039a44e144
Admin0705 <user@dunn.cloud>
2018-03-03 15:48:07 -0500

    update

--- menus/uninstall/programs.sh
+++ menus/uninstall/programs.sh
@@ -21,7 +21,7 @@ sudo rm -r /tmp/namedPipe
 mkfifo /tmp/namedPipe # this creates named pipe, aka fifo
 
 dialog --inputbox "Name of the Program You Want to Uninstall" 8 40 2> /tmp/namedPipe &
-OUTPUT="$( cat /tmp/namedPipe1 )"
+OUTPUT="$( cat /tmp/namedPipe )"
 
 
 echo  "This is the output " $OUTPUT
