b93c985a361fcb9129e246cab2adfdacfb0a96ac
Admin0705 <user@dunn.cloud>
2018-03-03 14:59:10 -0500

    testing programs uninstall

---
+++ menus/uninstall/programs.sh
@@ -0,0 +1,58 @@
+#!/bin/bash
+#
+# [PlexGuide Restore Script]
+#
+# GitHub:   https://github.com/Admin9705/PlexGuide.com-The-Awesome-Plex-Server
+# Author:   Admin9705 - deiteq
+# URL:      https://plexguide.com
+#
+# PlexGuide Copyright (C) 2018 PlexGuide.com
+# Licensed under GNU General Public License v3.0 GPL-3 (in short)
+#
+#   You may copy, distribute and modify the software as long as you track
+#   changes/dates in source files. Any modifications to our software
+#   including (via compiler) GPL-licensed code must also be made available
+#   under the GPL along with build & install instructions.
+#
+#################################################################################
+
+mfolder="/mnt/gdrive/plexguide/backup.old/backup-"
+mpath="$mfolder$d"
+
+docker ps -a --format "{{.Names}}" > /opt/appdata/plexguide/running
+
+#read -n 1 -s -r -p "Press any key to continue "
+
+HEIGHT=11
+WIDTH=32
+CHOICE_HEIGHT=5
+BACKTITLE="Visit https://PlexGuide.com - Automations Made Simple"
+TITLE="PG Version Install"
+MENU="Make a Selection"
+
+OPTIONS=(
+while read p; do
+         A "$p"
+done </opt/appdata/plexguide/running
+         Z "Exit")
+CHOICE=$(dialog --clear \
+                --backtitle "$BACKTITLE" \
+                --title "$TITLE" \
+                --menu "$MENU" \
+                $HEIGHT $WIDTH $CHOICE_HEIGHT \
+                "${OPTIONS[@]}" \
+                2>&1 >/dev/tty)
+
+clear
+case $CHOICE in
+        A)
+
+            exit 0 ;;
+        B)
+            touch /var/plexguide/ask.yes 1>/dev/null 2>&1
+            version="5.059" ;;
+        Z)
+            clear
+            exit 0
+            ;;
+esac
