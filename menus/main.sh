b48faa549f23418357d1e8cb8cb41dd0db9ed114
Admin0705 <user@dunn.cloud>
2018-03-03 18:28:41 -0500

    update

--- menus/main.sh
+++ menus/main.sh
@@ -17,9 +17,9 @@
 #################################################################################
 
 export NCURSES_NO_UTF8_ACS=1
-HEIGHT=16
+HEIGHT=17
 WIDTH=45
-CHOICE_HEIGHT=10
+CHOICE_HEIGHT=11
 BACKTITLE="Visit PlexGuide.com - Automations Made Simple"
 TITLE="PlexGuide - Version 5.060"
 
