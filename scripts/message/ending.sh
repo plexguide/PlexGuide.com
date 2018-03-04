ed9517e40a7f4d9e9776bc039656e26346280443
Admin0705 <user@dunn.cloud>
2018-03-03 22:29:18 -0500

    update

---
+++ scripts/message/ending.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+#
+# [PlexGuide Menu]
+#
+# GitHub:   https://github.com/Admin9705/PlexGuide.com-The-Awesome-Plex-Server
+# Author:   Admin9705 & Detique
+# URL:      https://plexguide.com
+#
+# PlexGuide Copyright (C) 2018 PlexGuide.com
+# Licensed under GNU General Public License v3.0 GPL-3 (in short)
+#
+#   You may copy, distribute and modify the software as long as you track
+#   changes/dates in source files. Any modifications to our software
+#   including (via compiler) GPL-licensed code must also be made available
+#   under the GPL along with build & install instructions.
+#
+#################################################################################
+clear
+        echo "1. Please STAR PG via http://github.plexguide.com"
+        echo "2. Join the PG Discord via http://discord.plexguide.com"
+        echo "3. Donate to PG via http://donate.plexguide.com"
+        echo ""
+        echo "TIP : Press Z, then [ENTER] in the Menus to Exit"
+        echo "TIP : Menu Letters Displayed are HotKeys"
+        echo ""
+        echo "NOTE: Restart the Program Anytime, type: sudo plexguide"
+        echo "NOTE: Update to latest DEV, type       : sudo pgdev"
+        echo "NOTE: Update Plexguide Anytime, type   : sudo pgupdate"
+        echo ""
