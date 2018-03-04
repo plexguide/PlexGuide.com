e0125cd9e88887c176dff72f7b10e782c6f96451
Admin0705 <user@dunn.cloud>
2018-03-04 12:45:36 -0500

    update

--- menus/backup-restore/backupmass.sh
+++ menus/backup-restore/backupmass.sh
@@ -28,7 +28,7 @@ if dialog --stdout --title "Backup Mass Confirmation" \
             clear
         fi
 
-dialog --infobox "Backup: Starting Processing" 3 30 ; sleep 1
+dialog --infobox "Backup: Starting Processing" 3 50 ; sleep 1
 
 d=$(date +%Y-%m-%d-%T) 1>/dev/null 2>&1
 
