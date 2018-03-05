2a11eba3567b0eca00da2b3594463c8323d72f0b
Admin0705 <user@dunn.cloud>
2018-03-04 19:14:57 -0500

    update

--- menus/plexdrive/rc-pd.sh
+++ menus/plexdrive/rc-pd.sh
@@ -1,7 +1,7 @@
  #!/bin/bash
 export NCURSES_NO_UTF8_ACS=1
 HEIGHT=10
-WIDTH=32
+WIDTH=28
 CHOICE_HEIGHT=4
 BACKTITLE="Visit https://PlexGuide.com - Automations Made Simple"
 TITLE="PlexDrive - RClone"
