81d23771fa783d3ec079593dd6c399690691a5a9
Deiteq <fayly80@gmail.com>
2018-03-05 22:56:38 +0100

    x2go tests

--- scripts/test/x2go/x2go.sh
+++ scripts/test/x2go/x2go.sh
@@ -3,6 +3,12 @@
 docker stop x2go
 docker rm x2go
 
-git clone https://github.com/tatsuya6502/docker-x2go
+#git clone https://github.com/tatsuya6502/docker-x2go
+#cd docker-x2go
+#./run.sh
+
+mkdir ~/docker-x2go
+cp /opt/plexguide/scripts/test/x2go/docker-x2go ~/docker-x2go
+
 cd docker-x2go
 ./run.sh
