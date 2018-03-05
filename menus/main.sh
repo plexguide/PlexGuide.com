1e895c4f4bba4bd4d2adef565ee86b405866b3d2
Admin0705 <user@dunn.cloud>
2018-03-04 20:00:06 -0500

    update

--- menus/main.sh
+++ menus/main.sh
@@ -57,6 +57,7 @@ case $CHOICE in
             bash /opt/plexguide/menus/backup-restore/main.sh ;;
         H)
             bash /opt/plexguide/scripts/upgrade/main.sh 
+            sudo bash /opt/plexguide/scripts/message/ending.sh
             exit 0 ;;
         Z)
             sudo bash /opt/plexguide/scripts/message/ending.sh
