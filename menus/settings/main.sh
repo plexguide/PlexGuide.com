b2941d4d3b0e8cbd2adc1de751e47d47938c856c
Admin0705 <user@dunn.cloud>
2018-03-07 22:02:57 -0500

    update

--- menus/settings/main.sh
+++ menus/settings/main.sh
@@ -16,9 +16,9 @@
 #
 #################################################################################
 
-HEIGHT=12
+HEIGHT=13
 WIDTH=55
-CHOICE_HEIGHT=6
+CHOICE_HEIGHT=7
 BACKTITLE="Visit https://PlexGuide.com - Automations Made Simple"
 TITLE="PG Settings"
 MENU="Make Your Selection Choice:"
