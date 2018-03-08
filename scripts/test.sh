2947184c3c24e91b11a1b9f0d44784332e146428
Admin0705 <user@dunn.cloud>
2018-03-07 19:41:58 -0500

    update

--- scripts/test.sh
+++ scripts/test.sh
@@ -4,7 +4,7 @@
 
 dialog --title "Inputbox - To take input from you" \
 --backtitle "Linux Shell Script Tutorial Example" \
---inputbox "Enter your name " 8 60 2>$OUTPUT
+--inputbox "Enter your name " 8 60 2> $OUTPUT
 
 
 
