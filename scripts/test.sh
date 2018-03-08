1370555e94ed4ef1605c56c6c1ea79d251d6f29b
Admin0705 <user@dunn.cloud>
2018-03-07 19:25:09 -0500

    update

--- scripts/test.sh
+++ scripts/test.sh
@@ -1,7 +1,6 @@
 
-dialog --title \
-"name" --inputbox \
-"Put your name:" 0 0 2> $tempfile
+dialog --title "name" --inputbox "Put your name:" 0 0 2> $tempfile
 
 clear
+echo "testfile"
 echo "$tempfile"
