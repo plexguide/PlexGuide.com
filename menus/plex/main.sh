c755249d886ccff743f83235e4a42dca4ca146fe
Admin0705 <user@dunn.cloud>
2018-03-08 18:11:52 -0500

    update

--- menus/plex/main.sh
+++ menus/plex/main.sh
@@ -10,6 +10,28 @@ display=PLEX
 program=plex
 port=32400
 
+    dialog --infobox "Pay ATTENTION: Is this Server A REMOTE SERVER (Non-Local)? If You SAY -NO- and it is, you must repeat this process!" 4 50
+    sleep 4
+
+    if dialog --stdout --title "PAY ATTENTION!" \
+      --backtitle "Visit https://PlexGuide.com - Automations Made Simple" \
+      --yesno "\nIs this Server a REMOTE SERVER (Non-Local)?" 7 50; then
+
+    dialog --title "PLEX CLAIM INFORMATION" \
+    --msgbox "\nVisit http://plex.tv/claim and PRESS the [COPY] Button (do not highlight and copy). You have 5 minutes starting NOW! [PRESS ENTER] when you are READY!" 10 50
+
+    dialog --title "Input >> PLEX CLAIM" \
+    --backtitle "Visit https://PlexGuide.com - Automations Made Simple" \
+    --inputbox "Visit http://plex.tv/claim - Token?" 8 40 2>/tmp/plextoken
+    plextoken=$(cat /tmp/plextoken)
+    dialog --infobox "Typed Tag: $token" 3 45
+    sleep 4
+    clear
+    else
+        echo "claimedalready" > /tmp/plextoken 1>/dev/null 2>&1
+    fi
+
+
 dialog --title "Plex Claim Info" \
 --msgbox "\nVisit http//:$ipv4:32400/web AFTER to complete the install. If installing Plex on a REMOTE SERVER, have your Plex Claim Token ready by heading to https://plex.tv/claim. The Claim Token is valid only for 4 minutes! If the claim process does not work, read the plex wiki for the other 3 methods to claim!" 12 50
 
@@ -64,27 +86,6 @@ case $CHOICE in
             clear
             exit 0 ;;
 esac
-
-    dialog --infobox "Pay ATTENTION: Is this Server A REMOTE SERVER (Non-Local)? If You SAY -NO- and it is, you must repeat this process!" 4 50
-    sleep 4
-
-    if dialog --stdout --title "PAY ATTENTION!" \
-      --backtitle "Visit https://PlexGuide.com - Automations Made Simple" \
-      --yesno "\nIs this Server a REMOTE SERVER (Non-Local)?" 7 50; then
-
-    dialog --title "PLEX CLAIM INFORMATION" \
-    --msgbox "\nVisit http://plex.tv/claim and PRESS the [COPY] Button (do not highlight and copy). You have 5 minutes starting NOW! [PRESS ENTER] when you are READY!" 10 50
-
-    dialog --title "Input >> PLEX CLAIM" \
-    --backtitle "Visit https://PlexGuide.com - Automations Made Simple" \
-    --inputbox "Visit http://plex.tv/claim - Token?" 8 40 2>/tmp/plextoken
-    plextoken=$(cat /tmp/plextoken)
-    dialog --infobox "Typed Tag: $token" 3 45
-    sleep 4
-    clear
-    else
-        echo "claimedalready" > /tmp/plextoken 1>/dev/null 2>&1
-    fi
 
 dialog --title "Input >> Tag Version" \
 --backtitle "Visit https://PlexGuide.com - Automations Made Simple" \
