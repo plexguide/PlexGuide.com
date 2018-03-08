1af489df09aa01b960a92bbfa7c1c95bd7110ce8
Admin0705 <user@dunn.cloud>
2018-03-07 19:33:18 -0500

    update

--- scripts/test.sh
+++ scripts/test.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+OUTPUT="/tmp/input.txt"
+
 dialog --title "Inputbox - To take input from you" \
 --backtitle "Linux Shell Script Tutorial Example" \
 --inputbox "Enter your name " 8 60 2>$OUTPUT
