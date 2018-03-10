084b88db4045ecdcb20bb3c094aea3c5a3f7558e
Deiteq <fayly80@gmail.com>
2018-03-10 19:27:45 +0100

    x2go tests

--- scripts/test/x2go/x2go.sh
+++ scripts/test/x2go/x2go.sh
@@ -1,18 +1,19 @@
 #!/bin/bash
 
-docker stop x2go
-docker rm x2go
+#docker stop x2go
+#docker rm x2go
 
 #git clone https://github.com/tatsuya6502/docker-x2go
 #cd docker-x2go
 #./run.sh
 
 #mkdir ~/docker-x2go
-mv /opt/plexguide/scripts/test/x2go/docker-x2go ~/
+#mv /opt/plexguide/scripts/test/x2go/docker-x2go ~/
 echo 1
-mv /opt/plexguide/scripts/test/x2go/run.sh ~/docker-x2go/
+#mv /opt/plexguide/scripts/test/x2go/run.sh ~/docker-x2go/
 echo 2
-cd docker-x2go
+cd /opt/plexguide/scripts/test/x2go/docker-x2go
+#cd docker-x2go
 echo 3
 #chmod +x start-sshd.sh
 #chmod +x Dockerfile
