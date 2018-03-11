17dc5d967374dc011f158bdc6f86b441ec96cd74
Admin0705 <user@dunn.cloud>
2018-03-10 22:43:44 -0500

    uppdate

--- menus/version/main.sh
+++ menus/version/main.sh
@@ -12,7 +12,7 @@
 #   You may copy, distribute and modify the software as long as you track
 #   changes/dates in source files. Any modifications to our software
 #   including (via compiler) GPL-licensed code must also be made available
-#   under the GPL along with build & install instructions.
+#   under the GPL along with build & install instructions. 
 #
 #################################################################################
 HEIGHT=14
