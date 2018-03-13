2e427b95a2f993737a9b32d518170d0c065946c3
Admin0705 <user@dunn.cloud>
2018-03-13 12:32:22 -0400

    update

--- menus/plexdrive/main.sh
+++ menus/plexdrive/main.sh
@@ -56,7 +56,7 @@ case $CHOICE in
                 chown root:root /usr/bin/plexdrive
                 chmod 755 /usr/bin/plexdrive
                 systemctl enable plexdrive
-                bash opt/plexguide/menus/plexdrive/check4.sh &>/dev/null &
+                bash /opt/plexguide/menus/plexdrive/check4.sh &>/dev/null &
                 plexdrive mount --uid=1000 --gid=1000 -v 3 --refresh-interval=1m --fuse-options=allow_other,read_only,allow_non_empty_mount --config=/root/.plexdrive /mnt/plexdrive
             else
                 dialog --title "PG Update Status" --msgbox "\nExiting - User Selected No" 0 0
@@ -76,7 +76,7 @@ case $CHOICE in
                 chown root:root /usr/bin/plexdrive
                 chmod 755 /usr/bin/plexdrive
                 systemctl enable plexdrive
-                bash opt/plexguide/menus/plexdrive/check5.sh &>/dev/null &
+                bash /opt/plexguide/menus/plexdrive/check5.sh &>/dev/null &
                 plexdrive mount --uid=1000 --gid=1000 -v 3 --refresh-interval=1m --chunk-load-threads=8 --chunk-check-threads=8 --chunk-load-ahead=4 --chunk-size=10M --max-chunks=300 --fuse-options=allow_other,read_only --config=/root/.plexdrive --cache-file=/root/.plexdrive/cache.bolt /mnt/plexdrive
             else
                 dialog --title "PG Update Status" --msgbox "\nExiting - User Selected No" 0 0
