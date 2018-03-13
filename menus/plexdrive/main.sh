48f2d93ff19d05e0ca89e4c045610818633772b7
Admin0705 <user@dunn.cloud>
2018-03-13 00:59:21 -0400

    update

--- menus/plexdrive/main.sh
+++ menus/plexdrive/main.sh
@@ -74,7 +74,7 @@ case $CHOICE in
                 chmod 755 /usr/bin/plexdrive
                 systemctl enable plexdrive
                 echo "a"
-                plexdrive mount --uid=1000 --gid=1000 -v 3 --refresh-interval=1m --chunk-load-threads=8 --chunk-check-threads=8 --chunk-load-ahead=4 --chunk-size=10M --max-chunks=300 --fuse-options=allow_other,read_only --config=/root/.plexdrive --cache-file=/root/.plexdrive/cache.bolt /mnt/plexdrive >> /opt/appdata/plexdrive.info
+                plexdrive mount --uid=1000 --gid=1000 -v 3 --refresh-interval=1m --chunk-load-threads=8 --chunk-check-threads=8 --chunk-load-ahead=4 --chunk-size=10M --max-chunks=300 --fuse-options=allow_other,read_only --config=/root/.plexdrive --cache-file=/root/.plexdrive/cache.bolt /mnt/plexdrive >> /opt/appdata/plexdrive.info 2>&1
                 echo "b"
                 PD="No"
                 while [ "$PD" != "Opening" ]; do
