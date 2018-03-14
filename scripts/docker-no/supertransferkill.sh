e003b1b35c6c8fbd3a62f0706a12739bf9f83e51
Admin0705 <user@dunn.cloud>
2018-03-14 06:19:56 -0400

    update

--- scripts/docker-no/supertransferkill.sh
+++ scripts/docker-no/supertransferkill.sh
@@ -1,3 +1,17 @@
-#!/bin/bash 
+#!/bin/bash
+#
+# GitHub:   https://github.com/Admin9705/PlexGuide.com-The-Awesome-Plex-Server
+# Author:   Flicker-Rate
+# URL:      https://plexguide.com
+#
+# PlexGuide Copyright (C) 2018 PlexGuide.com
+# Licensed under GNU General Public License v3.0 GPL-3 (in short)
+#
+#   You may copy, distribute and modify the software as long as you track
+#   changes/dates in source files. Any modifications to our software
+#   including (via compiler) GPL-licensed code must also be made available
+#   under the GPL along with build & install instructions. 
+#
+#################################################################################
 ps -ef | grep supertransfer | grep -v "grep" | awk '{print $2}' | xargs kill; 
 exit 0
