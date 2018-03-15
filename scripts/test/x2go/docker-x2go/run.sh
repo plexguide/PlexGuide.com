8da206ccdc3379a5f00fb0552ba162a677ceab88
Deiteq <fayly80@gmail.com>
2018-03-15 19:31:00 +0100

    test

--- scripts/test/x2go/docker-x2go/run.sh
+++ scripts/test/x2go/docker-x2go/run.sh
@@ -2,6 +2,8 @@
 
 docker stop x2go
 docker rm x2go
+echo creating container
 docker run -it -d -p 2222:22 --name=x2go -v=/mnt:/mnt -v=/opt:/opt quay.io/tatsuya6502/x2go:latest
+echo Please wait for ssh key
 sleep 3
 docker logs x2go
