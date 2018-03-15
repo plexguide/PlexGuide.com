05dc476f733d83b69b6fcd1d2ebbaa4de47a3ed1
Admin0705 <user@dunn.cloud>
2018-03-14 20:06:15 -0400

    update

--- menus/backup-restore/backup.sh
+++ menus/backup-restore/backup.sh
@@ -145,13 +145,13 @@ fi
     echo "true" > /tmp/alive
     while [ "$loop" = "true" ]
     do
-        dialog --infobox "Backing Up ◷" 3 22
+        dialog --infobox "Backing Up /-\| " 3 22
         sleep 0.5
-        dialog --infobox "Backing Up ◶" 3 22
+        dialog --infobox "Backing Up |/-\ " 3 22
         sleep 0.5
-        dialog --infobox "Backing Up ◵" 3 22
+        dialog --infobox "Backing Up \|/- " 3 22
         sleep 0.5
-        dialog --infobox "Backing Up ◴" 3 22
+        dialog --infobox "Backing Up -\|/ " 3 22
         sleep 0.5
         loop=$(cat /tmp/alive)
     done
