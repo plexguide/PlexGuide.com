cbcb29a07170906408483d033c6574878ef41a9e
Deiteq <fayly80@gmail.com>
2018-03-15 19:35:43 +0100

    test

--- scripts/test/x2go/docker-x2go/run.sh
+++ scripts/test/x2go/docker-x2go/run.sh
@@ -2,8 +2,14 @@
 
 docker stop x2go
 docker rm x2go
-echo creating container
+echo
+echo Creating container
+echo
 docker run -it -d -p 2222:22 --name=x2go -v=/mnt:/mnt -v=/opt:/opt quay.io/tatsuya6502/x2go:latest
+echo
 echo Please wait for ssh key
+echo
 sleep 3
+echo Copy key and save to your pc using Notepad and point X2Go Client to it!
+echo
 docker logs x2go
