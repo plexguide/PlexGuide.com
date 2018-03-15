326353206404f35fbcd78fa3867da293c1d3289d
Deiteq <fayly80@gmail.com>
2018-03-15 18:37:30 +0100

    test

--- scripts/test/x2go/docker-x2go/run.sh
+++ scripts/test/x2go/docker-x2go/run.sh
@@ -2,6 +2,6 @@
 
 docker stop x2go
 docker rm x2go
-docker run -it -d -p 2222:22 --name=x2go quay.io/tatsuya6502/x2go:latest
+docker run -it -d -p 2222:22 --name=x2go -v=/mnt:/mnt -v=/opt:/opt quay.io/tatsuya6502/x2go:latest
 sleep 3
 docker logs x2go
