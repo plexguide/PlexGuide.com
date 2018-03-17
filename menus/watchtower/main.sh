18415746c1508a98e0f9cf09b2aac170d66f6b48
Admin0705 <user@dunn.cloud>
2018-03-17 07:01:20 -0400

    update

--- menus/watchtower/main.sh
+++ menus/watchtower/main.sh
@@ -11,7 +11,7 @@ if [ -e "$file" ]
 then
    clear
 else
-   dialog --infobox "Notice: WatchTower allows your Containers to Auto-Update!\n\nPROS: You containers will always be up-to-date.\n\nCONS: If something is wrong with the newest continer, you'll have issues; rare but happens.\n\n NOTE: Typically recommend Plex & Emby to be updated manually for stability purposes!" 60 12
+   dialog --infobox "Notice: WatchTower allows your Containers to Auto-Update!\n\nPROS: You containers will always be up-to-date.\n\nCONS: If something is wrong with the newest continer, you'll have issues; rare but happens.\n\n NOTE: Typically recommend Plex & Emby to be updated manually for stability purposes!" 12 60
    sleep 10
    touch /var/plexguide/watchtower.yes
 fi
@@ -39,7 +39,7 @@ CHOICE=$(dialog --clear \
 clear
 case $CHOICE in
         A)
-            if dialog --stdout --title "System Update" \
+            if dialog --stdout --title "WatchTower Question" \
               --backtitle "Visit https://PlexGuide.com - Automations Made Simple" \
               --yesno "\nYou Want to --Update All Containers except Plex & Emby?" 7 0 0; then
               ansible-playbook /opt/plexguide/ansible/plexguide.yml --tags watchtower --skip-tags=all 1>/dev/null 2>&1
@@ -54,7 +54,7 @@ case $CHOICE in
             fi
             ;;
         B)
-            if dialog --stdout --title "System Update" \
+            if dialog --stdout --title "WatchTower Question" \
               --backtitle "Visit https://PlexGuide.com - Automations Made Simple" \
               --yesno "\nYou Want to --Update All Containers except Plex & Emby?" 7 0 0; then
               ansible-playbook /opt/plexguide/ansible/plexguide.yml --tags watchtower --skip-tags=plexemby 1>/dev/null 2>&1
@@ -64,17 +64,17 @@ case $CHOICE in
             else
               clear
               dialog --title "WatchTower Status" --msgbox "\nUser Failed to Select Yes, Going Back to the Main Menu!" 0 0
-              bash /opt/plexguide/menus/watchtower/main.sh
+              bash /opt/plexguide/menus/watchtower/main.sh 
               exit 0
             fi
             ;;
-        D)
-            if dialog --stdout --title "System Update" \
+        C)
+            if dialog --stdout --title "WatchTower Questions" \
               --backtitle "Visit https://PlexGuide.com - Automations Made Simple" \
               --yesno "\nDo You Want to Disable and/or Remove WatchTower?" 7 0 0; then
               dialog --infobox "Notice: All Your containers will Auto-Update!\n\nMade an error? Just SELECT it again!" 0 0
-              docker stop watchtower
-              docker rm watchtower
+              docker stop watchtower 1>/dev/null 2>&1
+              docker rm watchtower 1>/dev/null 2>&1
               dialog --infobox "Notice: WatchTower is not enabled or has been removed! To manually update, use this Application and rerun your selcted Application for the newest update!" 0 0
               sleep 8
             else
