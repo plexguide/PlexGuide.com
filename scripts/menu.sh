dc28c45662a3b5a9da9a2f48eb1fa0371587479a
Admin0705 <user@dunn.cloud>
2018-03-20 07:14:09 -0400

    udpate

--- scripts/menu.sh
+++ scripts/menu.sh
@@ -63,7 +63,7 @@ else
    bash /opt/plexguide/menus/notifications/main.sh
 fi
 
-file="/var/plexguide/dep43.yes"
+file="/var/plexguide/dep44.yes"
 if [ -e "$file" ]
 then
    touch /var/plexguide/message.no
