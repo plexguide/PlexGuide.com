4a4696f4db964add94950308b5af7ebfda240cdf
Admin0705 <user@dunn.cloud>
2018-03-20 23:42:22 -0400

    test
    test

--- menus/ports/main.sh
+++ menus/ports/main.sh
@@ -70,7 +70,7 @@ while read p; do
   echo $p > /tmp/program_var
   app=$( cat /tmp/program_var )
   dialog --infobox "Reconsturcting Your Container: $app" 3 50
-  ansible-playbook /opt/plexguide/ansible/plexguide.yml --tags "$app" #--skip-tags webtools #1>/dev/null 2>&1
+  ansible-playbook /opt/plexguide/ansible/plexguide.yml --tags "$app" --skip-tags webtools #1>/dev/null 2>&1
   #read -n 1 -s -r -p "Press any key to continue "
 done </opt/appdata/plexguide/running
 
