d6ecde6564c63d8e45310287179b7484cc54e491
Admin0705 <user@dunn.cloud>
2018-03-22 01:29:05 -0400

    update

--- menus/plexdrive/check4.sh
+++ menus/plexdrive/check4.sh
@@ -16,17 +16,14 @@
 #
 #################################################################################
 
-#rm -r /opt/appdata/plexdrive.info 1>/dev/null 2>&1
-#touch /opt/appdata/plexdrive.info 1>/dev/null 2>&1
 PD="Blank"
 
 while [ "$PD" != "Opening" ]; do
 sleep 3
-#systemctl status plexdrive >> /opt/appdata/plexguide/plexdrive.info
 PD=$(grep -o Opening /opt/appdata/plexguide/plexdrive.info | head -1)
 done
 
-rm -r /opt/appdata/plexdrive.info 1>/dev/null 2>&1
+rm -r /opt/appdata/plexguide/plexdrive.info 1>/dev/null 2>&1
 clear
 echo "PlexDrive4 Finished Scanning - Rebooting Your System"
-sudo reboot
+reboot
