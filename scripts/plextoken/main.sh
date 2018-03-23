d60fd33420307fa537846092dbc3beee99c7d3ca
Admin0705 <user@dunn.cloud>
2018-03-22 20:29:32 -0400

    update

--- scripts/plextoken/main.sh
+++ scripts/plextoken/main.sh
@@ -32,7 +32,7 @@ fi
  
     dialog --title "Input >> Plex Password" \
     --backtitle "Visit https://PlexGuide.com - Automations Made Simple" \
-    --inputbox "Your Plex Password" 8 45 2>/tmp/plexuser
+    --inputbox "Your Plex Password" 8 45 2>/tmp/plexpassword
     plexpassword=$(cat /tmp/plexpassword)
     dialog --infobox "Typed Password: $plexpassword" 3 45
     sleep 4
