867343fd8f135d7121c30c7c06902db7fdb8502d
Admin0705 <user@dunn.cloud>
2018-03-24 07:44:30 -0400

    update

--- menus/plex/old.sh
+++ menus/plex/old.sh
@@ -1,10 +1,23 @@
-
- #!/bin/bash
+#!/bin/bash
+#
+# [PlexGuide Menu]
+#
+# GitHub:   https://github.com/Admin9705/PlexGuide.com-The-Awesome-Plex-Server
+# Author:   Admin9705 & Deiteq
+# URL:      https://plexguide.com
+#
+# PlexGuide Copyright (C) 2018 PlexGuide.com
+# Licensed under GNU General Public License v3.0 GPL-3 (in short)
+#
+#   You may copy, distribute and modify the software as long as you track
+#   changes/dates in source files. Any modifications to our software
+#   including (via compiler) GPL-licensed code must also be made available
+#   under the GPL along with build & install instructions.
+#
+#################################################################################
 export NCURSES_NO_UTF8_ACS=1
  ## point to variable file for ipv4 and domain.com
  source <(grep '^ .*='  /opt/appdata/plexguide/var.sh)
- echo $ipv4
- echo $domain
 
  ### demo ip / comment out when done
  #ipv4=69.69.69.69
@@ -13,25 +26,16 @@ display=PLEX
 program=plex
 port=32400
 
-    dialog --infobox "Pay ATTENTION: Is this Server A REMOTE SERVER (Non-Local)?\n\nIf You SAY -NO- and it is, you must repeat this process!" 7 50
-    sleep 4
 
-    if dialog --stdout --title "PAY ATTENTION!" \
-      --backtitle "Visit https://PlexGuide.com - Automations Made Simple" \
-      --yesno "\nIs this Server a REMOTE SERVER (Non-Local)?" 7 50; then
 
-    dialog --title "PLEX CLAIM INFORMATION" \
-    --msgbox "\nVisit http://plex.tv/claim and PRESS the [COPY] Button (do not highlight and copy). You have 5 minutes starting NOW! [PRESS ENTER] when you are READY!" 10 50
+    #sleep 4
 
-    dialog --title "Input >> PLEX CLAIM" \
-    --backtitle "Visit https://PlexGuide.com - Automations Made Simple" \
-    --inputbox "Token? Windows Users - SHIFT + INSERT to PASTE" 8 50 2>/tmp/plextoken
-    plextoken=$(cat /tmp/plextoken)
-    dialog --infobox "Token: $plextoken" 3 45
-    sleep 4
-    else
-        echo "claimedalready" > /tmp/plextoken 1>/dev/null 2>&1
-    fi
+    #bash /opt/plexguide/scripts/plextoken/test.sh
+    #plextoken=$(cat /opt/appdata/plexguide/plextoken)
+    #plextoken="claim-$plextoken"
+    #echo "claim-NARF" > /opt/appdata/plexguide/plextoken
+    #echo "$plextoken" > /opt/appdata/plexguide/plextoken
+
 
 HEIGHT=10
 WIDTH=40
@@ -57,11 +61,11 @@ case $CHOICE in
         A)
                 echo "latest" > /tmp/plextag
                 dialog --infobox "Selected Tag: Latest" 3 38
-                sleep 4
+                sleep 2
 
-            dialog --infobox "Installing Plex: Please Wait" 3 45   
-            ansible-playbook /opt/plexguide/ansible/plexguide.yml --tags plex --skip-tags webtools 1>/dev/null 2>&1
-            #read -n 1 -s -r -p "Press any key to continue "
+            #dialog --infobox "Installing Plex: Please Wait" 3 45   
+            ansible-playbook /opt/plexguide/ansible/plexguide.yml --tags plex --skip-tags webtools #1>/dev/null 2>&1
+            read -n 1 -s -r -p "Press any key to continue "
             ;;
 
         B)
@@ -73,7 +77,7 @@ case $CHOICE in
                 --inputbox "Windows Users - SHIFT + INSERT to PASTE" 8 40 2>/tmp/plextag
                 plexgtag=$(cat /tmp/plextag)
                 dialog --infobox "Typed Tag: $plextag" 3 45
-                sleep 4
+                sleep 2
             
             dialog --infobox "Installing Plex: Please Wait" 3 45
             ansible-playbook /opt/plexguide/ansible/plexguide.yml --tags plex --skip-tags webtools 1>/dev/null 2>&1
@@ -81,7 +85,9 @@ case $CHOICE in
             ;;
         Z)
             clear
-            exit 0 ;;
+            exit 0 
+            failed > /tmp/plexinstall
+            ;;
 
 ########## Deploy End
 esac 
--- scripts/plextoken/create.sh
+++ scripts/plextoken/create.sh
@@ -2,3 +2,11 @@ token=$(curl -H "Content-Length: 0" -H "X-Plex-Client-Identifier: PlexInTheCloud
 
 # Grab the Plex Section ID of our new TV show library
 tvID=$(curl -H "X-Plex-Token: ${token}" http://127.0.0.1:32400/library/sections | grep "show" | grep "title=" | awk -F = '{print $6" "$7" "$8}' | sed 's/ art//g' | sed 's/title//g' | sed 's/type//g' | awk -F \" '{print "Section=\""$6"\" ID="$2}' | cut -d '"' -f2)
+
+    #sleep 4
+
+    #bash /opt/plexguide/scripts/plextoken/test.sh
+    #plextoken=$(cat /opt/appdata/plexguide/plextoken)
+    #plextoken="claim-$plextoken"
+    #echo "claim-NARF" > /opt/appdata/plexguide/plextoken
+    #echo "$plextoken" > /opt/appdata/plexguide/plextoken
