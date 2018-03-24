d002485ed7a38c62651a1cef73ce887e923d1b80
Admin0705 <user@dunn.cloud>
2018-03-24 08:34:11 -0400

    update

--- menus/plex/main.sh
+++ menus/plex/main.sh
@@ -30,14 +30,14 @@ display=PLEX
 program=plex
 port=32400
 
-    dialog --infobox "Pay ATTENTION: Is this Server A REMOTE SERVER (Non-Local)?\n\nIf You SAY -NO- and it is, you must repeat this process!" 7 50
-    sleep 4
-
     if dialog --stdout --title "PAY ATTENTION!" \
       --backtitle "Visit https://PlexGuide.com - Automations Made Simple" \
-      --yesno "\nDo you require to claim this SERVER? (No, if already done and is working fine" 7 50; then
+      --yesno "\nDo you require to claim this SERVER? Select No: IF your SERVER is already Claimed & Working" 0 0; then
       echo "claimedalready" > /tmp/plextoken 1>/dev/null 2>&1
     else
+        dialog --infobox "Pay ATTENTION: Is this Server A REMOTE SERVER (Non-Local)?\n\nIf You SAY -NO- and it is, you must repeat this process!" 7 50
+        sleep 4
+     
         if dialog --stdout --title "PAY ATTENTION!" \
           --backtitle "Visit https://PlexGuide.com - Automations Made Simple" \
           --yesno "\nIs this Server a REMOTE SERVER (Non-Local)?" 7 50; then
