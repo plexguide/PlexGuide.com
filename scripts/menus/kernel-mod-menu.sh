3188bc40d47785faffeb27eacc4acf75674b6bbd
flicker <flicker-rate@protonmail.com>
2018-03-24 16:45:08 -0600

    menu size

--- scripts/menus/kernel-mod-menu.sh
+++ scripts/menus/kernel-mod-menu.sh
@@ -5,7 +5,7 @@ clear
 ################# init message
 
 if (whiptail --title "Network Speed" --yesno "Is Your Server On at least a 500mbit line?" 8 56) then
-    whiptail --title "Network Speed - Yes" --msgbox "These tweaks work best in high-latency, low packet loss enviroments (ie: server is located in America, but you are streaming from Austrailia)." 9 66
+  echo good
 else
     whiptail --title "Network Speed - No" --msgbox "We reccomend only enabling BBR on slower networks." 9 66
 fi
@@ -13,7 +13,7 @@ fi
 while [ 1 ]
 do
 CHOICE=$(
-whiptail --title "Kernel Profiles" --menu "See Wiki For Info" 16 60 5 \
+whiptail --title "Kernel Profiles" --menu "See Wiki For Info" 10 40 6 \
     "1)" "Enable BBR TCP Congestion Control"  \
     "2)" "Klaver Kernel Tweaks + BBR"  \
     "3)" "tj007s13 Kernel Tweaks + BBR"  \
@@ -63,7 +63,7 @@ case $CHOICE in
       if cat /proc/sys/net/ipv4/tcp_available_congestion_control | grep bbr -q
       then
         ansible-playbook /opt/plexguide/ansible/roles/processor/processor.yml --tags network_tuning --skip-tags $skip_tags
-        cat /etc/sysctl.conf 
+        cat /etc/sysctl.conf
         echo ""
         read -n 1 -s -r -p "Press any key to continue "
         bash /opt/plexguide/scripts/menus/processor/reboot.sh
