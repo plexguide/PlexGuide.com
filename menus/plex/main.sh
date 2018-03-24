fba04f00187161aefe5ac6b29af160610553afa4
Admin0705 <user@dunn.cloud>
2018-03-24 08:37:13 -0400

    update

--- menus/plex/main.sh
+++ menus/plex/main.sh
@@ -33,8 +33,7 @@ port=32400
     if dialog --stdout --title "PAY ATTENTION!" \
       --backtitle "Visit https://PlexGuide.com - Automations Made Simple" \
       --yesno "\nDo you require to claim this SERVER?\n\nSelect No: IF your SERVER is already Claimed & Working" 0 0; then
-      echo "claimedalready" > /tmp/plextoken 1>/dev/null 2>&1
-    else
+
         dialog --infobox "Pay ATTENTION: Is this Server A REMOTE SERVER (Non-Local)?\n\nIf You SAY -NO- and it is, you must repeat this process!" 7 50
         sleep 4
 
@@ -54,6 +53,8 @@ port=32400
         else
             echo "claimedalready" > /tmp/plextoken 1>/dev/null 2>&1
         fi
+    else
+       echo "claimedalready" > /tmp/plextoken 1>/dev/null 2>&1
     fi
 
 HEIGHT=10
