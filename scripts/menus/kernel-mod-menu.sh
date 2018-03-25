3ef3518d7b81cb90cb073627fc3f3f9f6ee9480b
flicker <flicker-rate@protonmail.com>
2018-03-24 22:43:11 -0600

    patch

--- scripts/menus/kernel-mod-menu.sh
+++ scripts/menus/kernel-mod-menu.sh
@@ -15,11 +15,12 @@ do
 CHOICE=$(
 whiptail --title "Kernel Profiles" --menu "See Wiki For More Info + iperf Network Benchmarks" 14 50 6 \
     "1)" "Enable BBR TCP Congestion Control"  \
-    "2)" "High-Latency Network + BBR"  \
-    "3)" "Low-Latency Network + BBR"  \
-    "4)" "Install Latest Generic Kernel"  \
-    "5)" "Install Xanmod Kernel"  \
-    "6)" "Exit"  3>&2 2>&1 1>&3
+    "2)" "Klaver + BBR"  \
+    "3)" "TJ007 + BBR"  \
+    "4)" "Seedboxer + BBR"  \
+    "5)" "Install Latest Generic Kernel"  \
+    "6)" "Install Xanmod Kernel"  \
+    "7)" "Exit"  3>&2 2>&1 1>&3
 )
 
 result=$(whoami)
@@ -27,7 +28,7 @@ case $CHOICE in
     "1)")
     clear
       # check if bbr is avail
-      skip_tags='tj,klaver'
+      skip_tags='tj,klaver,seedboxer'
       if cat /proc/sys/net/ipv4/tcp_available_congestion_control | grep bbr -q
         then
         ansible-playbook /opt/plexguide/ansible/roles/plexguide.yml --tags network_tuning --skip-tags $skip_tags
@@ -43,7 +44,7 @@ case $CHOICE in
     "2)")
       clear
       # check if bbr is avail
-      skip_tags='tj'
+      skip_tags='tj,seedboxer'
       if cat /proc/sys/net/ipv4/tcp_available_congestion_control | grep bbr -q
       then
         ansible-playbook /opt/plexguide/ansible/roles/plexguide.yml --tags network_tuning --skip-tags $skip_tags
@@ -59,7 +60,7 @@ case $CHOICE in
     "3)")
       clear
       # check if bbr is avail
-      skip_tags='klaver'
+      skip_tags='klaver,seedboxer'
       if cat /proc/sys/net/ipv4/tcp_available_congestion_control | grep bbr -q
       then
         ansible-playbook /opt/plexguide/ansible/roles/plexguide.yml --tags network_tuning --skip-tags $skip_tags
@@ -75,6 +76,23 @@ case $CHOICE in
 
     "4)")
       clear
+      # check if bbr is avail
+      skip_tags='klaver,tj'
+      if cat /proc/sys/net/ipv4/tcp_available_congestion_control | grep bbr -q
+      then
+        ansible-playbook /opt/plexguide/ansible/roles/plexguide.yml --tags network_tuning --skip-tags $skip_tags
+        cat /etc/sysctl.conf
+        echo ""
+        read -n 1 -s -r -p "Press any key to continue "
+        bash /opt/plexguide/scripts/menus/processor/reboot.sh
+      else
+        whiptail --title "Unsupported Kernel" --msgbox "Your Kernel, $(uname -r) does not support BBR. Please Update Your Kernel." 9 66
+        bash /opt/plexguide/scripts/menus/kernel-mod-menu.sh
+      fi
+    ;;
+
+    "5)")
+      clear
         if (whiptail --title "Kernel Upgrade" --yesno "Are You Sure You Want To Upgrade Your Kernel? (warning: this may break drivers)" 8 56) then
           sudo apt update -y && sudo apt sudo apt install --install-recommends linux-generic-hwe-16.04
           bash /opt/plexguide/scripts/menus/processor/reboot.sh
@@ -83,7 +101,7 @@ case $CHOICE in
         fi
     ;;
 
-    "5)")
+    "6)")
       clear
         if (whiptail --title "Kernel Upgrade" --yesno "Are You Sure You Want To Install An Expirimental Kernel? (warning: this may break drivers)" 8 56) then
           echo 'deb http://deb.xanmod.org releases main' | sudo tee /etc/apt/sources.list.d/xanmod-kernel.list && wget -qO - http://deb.xanmod.org/gpg.key | sudo apt-key add -
@@ -94,7 +112,7 @@ case $CHOICE in
         fi
     ;;
 
-    "6)")
+    "7)")
       clear
       exit 0
       ;;
