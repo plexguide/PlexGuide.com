ced824283df9cb6b4330576528dffebc3684d365
Admin0705 <user@dunn.cloud>
2018-03-29 00:44:48 -0400

    test

--- menus/traefik/godaddy.sh
+++ menus/traefik/godaddy.sh
@@ -15,24 +15,17 @@
 #   under the GPL along with build & install instructions.
 #
 #################################################################################
-  
-  dialog --infobox "Configuring Traefik For: GoDaddy" 4 35
+  "godaddy" > /tmp/provider
+  provider=$(cat /tmp/provider)
+
+  dialog --infobox "Configuring Traefik For: $provider" 4 35
   sleep 2
   
-  dialog --title "Input Required Information" \
-  --backtitle "Visit https://PlexGuide.com - Automations Made Simple" \
-  --inputbox "GODADDY_API_KEY: " 8 40 2>/tmp/var1
+  display1="GODADDY_API_KEY:"
+    dialog --title "Input Required Information" \
+    --backtitle "Visit https://PlexGuide.com - Automations Made Simple" \
+    --inputbox "$display1 " 8 40 2>/tmp/var1
   var1=$(cat /tmp/var1)
+  main1=$()
 
-  dialog --infobox "GODADDY_API_KEY:\n\n $var1" 4 45
-  sleep 2
-
-  dialog --title "Input Required Information" \
-  --backtitle "Visit https://PlexGuide.com - Automations Made Simple" \
-  --inputbox "GODADDY_API_SECRET: " 8 40 2>/tmp/var2
-  var2=$(cat /tmp/var2)
-
-  dialog --infobox "GODADDY_API_SECRET:\n\n $var2" 4 45
-  sleep 2
-
-  ansible-playbook /opt/plexguide/ansible/roles/traefik2/traefik2.yml --tags godaddy
+#  ansible-playbook /opt/plexguide/ansible/roles/traefik2/traefik2.yml --tags $provider
