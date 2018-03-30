d0cc894add41be9bac28670390e1d993bdab5f37
Admin0705 <user@dunn.cloud>
2018-03-30 16:51:39 -0400

    update

--- menus/traefik/main.sh
+++ menus/traefik/main.sh
@@ -50,8 +50,8 @@ case $CHOICE in
     C)
       clear ;;
     D)
-      dialog --title "Mini FAQ: Page 1 of 2" --msgbox "\n\nVisit traefik.plexguide.com for more detailed info\n\nTraefik v2 is the new version that allows all of your subdomains (unlimited) to receive an SSL CERTIFICATE. This allows you to create custom subdomains, not limited by having a max of 20, and cuts down on the RATE ERRORS. There is some minor work on your end to make this work!" 0 0
-      dialog --title "Mini FAQ: Page 2 of 2" --msgbox "\n\nLegacy Traefik works fine, but you will not be able to create custom subdomains and more limited in the amount of subdomains you can create per week. It is easier to setup though.\n\nVisit https://traefik.plexguide.com for more information." 0 0
+      dialog --title "Mini FAQ: Page 1 of 2" --msgbox "\nVisit traefik.plexguide.com for more detailed info\n\nTraefik v2 is the new version that allows all of your subdomains (unlimited) to receive an SSL CERTIFICATE. This allows you to create custom subdomains, not limited by having a max of 20, and cuts down on the RATE ERRORS. There is some minor work on your end to make this work!" 0 0
+      dialog --title "Mini FAQ: Page 2 of 2" --msgbox "\nLegacy Traefik works fine, but you will not be able to create custom subdomains and more limited in the amount of subdomains you can create per week. It is easier to setup though.\n\nVisit https://traefik.plexguide.com for more information." 0 0
       bash /opt/plexguide/menus/traefik/main.sh
       exit
       ;;
