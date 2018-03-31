b3fb7e71617b0650cc7a75a400b41b2ac9d9a472
Admin0705 <user@dunn.cloud>
2018-03-31 15:50:15 -0400

    update

--- menus/traefik/provider.sh
+++ menus/traefik/provider.sh
@@ -44,52 +44,56 @@ CHOICE=$(dialog --backtitle "$BACKTITLE" \
 case $CHOICE in
     A)
       dialog --title "Word of Caution" --msgbox "\nCloudflare is a great way to go! It may appear to work, but you need up to 12 hours if you just setup with CloudFlare!\n\nIf not, you may see an issue regarding the nameserver in the Portainer Logs!" 0 0
-      docker stop traefik 1>/dev/null 2>&1
-      docker rm traefik 1>/dev/null 2>&1
-      docker rm -r /opt/appdata/traefik 1>/dev/null 2>&1
-      sleep 3
       echo "CLOUDFLARE_EMAIL" > /tmp/display1
       echo "CLOUDFLARE_API_KEY" > /tmp/display2
       echo "cloudflare" > /var/plexguide/provider
+      dialog --infobox "Setting Up CloudFlare" 3 45
+      sleep 1     
+      docker stop traefik 1>/dev/null 2>&1
+      docker rm traefik 1>/dev/null 2>&1
+      docker rm -r /opt/appdata/traefik 1>/dev/null 2>&1
+
 
       bash /opt/plexguide/menus/traefik/menu.sh
       bash /opt/plexguide/menus/traefik/version.sh
       bash /opt/plexguide/menus/traefik/rebuild.sh
       exit 0 ;;
     B)
+      echo "GANDI_API_KEY" > /tmp/display1
+      echo "gandiv5" > /var/plexguide/provider
+      dialog --infobox "Setting Up Gandi" 3 45
+      sleep 1
       docker stop traefik 1>/dev/null 2>&1
       docker rm traefik 1>/dev/null 2>&1
       docker rm -r /opt/appdata/traefik 1>/dev/null 2>&1
-      sleep 3
-      echo "GANDI_API_KEY" > /tmp/display1
-      echo "gandiv5" > /var/plexguide/provider
-
       bash /opt/plexguide/menus/traefik/menu.sh
       bash /opt/plexguide/menus/traefik/version.sh
       bash /opt/plexguide/menus/traefik/rebuild.sh
       exit 0 ;;
     C)
-      docker stop traefik 1>/dev/null 2>&1
-      docker rm traefik 1>/dev/null 2>&1
-      docker rm -r /opt/appdata/traefik 1>/dev/null 2>&1
-      sleep 3
       echo "GODADDY_API_KEY" > /tmp/display1
       echo "GODADDY_API_SECRET" > /tmp/display2
       echo "godaddy" > /var/plexguide/provider
-
+      dialog --infobox "Setting Up Godaddy" 3 45
+      sleep 1
+      docker stop traefik 1>/dev/null 2>&1
+      docker rm traefik 1>/dev/null 2>&1
+      docker rm -r /opt/appdata/traefik 1>/dev/null 2>&1
       bash /opt/plexguide/menus/traefik/menu.sh
       bash /opt/plexguide/menus/traefik/version.sh
       bash /opt/plexguide/menus/traefik/rebuild.sh
       exit 0 ;;
     D)
       dialog --title "Word of Caution" --msgbox "\nNameCheap requires a customer to spend at least 50 USD within the last two years to access their API!\n\nTold them the issue it causes and GoDaddy has no such requirement. If you cannot access the API, point your NameCheap Domain to CloudFlare!" 0 0
-      docker stop traefik 1>/dev/null 2>&1
-      docker rm traefik 1>/dev/null 2>&1
-      docker rm -r /opt/appdata/traefik 1>/dev/null 2>&1
-      sleep 3
       echo "NAMECHEAP_API_USER" > /tmp/display1
       echo "NAMECHEAP_API_KEY" > /tmp/display2
       echo "namecheap" > /var/plexguide/provider
+      dialog --infobox "Setting Up NameCheap" 3 45
+      sleep 1
+      docker stop traefik 1>/dev/null 2>&1
+      docker rm traefik 1>/dev/null 2>&1
+      docker rm -r /opt/appdata/traefik 1>/dev/null 2>&1
+
 
       bash /opt/plexguide/menus/traefik/menu.sh
       bash /opt/plexguide/menus/traefik/version.sh
