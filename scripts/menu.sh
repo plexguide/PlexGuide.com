996405826b0117891aed76ae307ed466e062a844
Admin9705 <24727006+Admin9705@users.noreply.github.com>
2018-03-31 18:39:18 -0400

    Update

--- scripts/menu.sh
+++ scripts/menu.sh
@@ -76,7 +76,7 @@ else
    bash /opt/plexguide/menus/notifications/main.sh
 fi
 
-file="/var/plexguide/dep46.yes"
+file="/var/plexguide/dep47.yes"
 if [ -e "$file" ]
 then
    touch /var/plexguide/message.no
