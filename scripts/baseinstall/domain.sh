ba9d915c29420ba648a9d059577b8c8ebb8c1aca
Admin0705 <user@dunn.cloud>
2018-03-31 16:35:36 -0400

    update

--- scripts/baseinstall/domain.sh
+++ scripts/baseinstall/domain.sh
@@ -24,8 +24,9 @@
         dialog --infobox "Need to Change? Change via Settings Any Time!" 4 28
         sleep 2
         
+        ##### NOTE THAT THIS A ONETIME MENU
         dialog --title "Copy & Paste Note" --msgbox "\nNote: For COPY & PASTE, write this down!\n\nWIN Users - CTRL + INSERT\nMAC Users - CMD + V" 0 0
-        bash /opt/plexguide/menus/traefik/main.sh
+        bash /opt/plexguide/scripts/baseline/traefik.sh
       else
         dialog --title "Domain - No" --msgbox "\nSet this up later anytime via settings!" 0 0
         echo "later" > /var/plexguide/server.domain
