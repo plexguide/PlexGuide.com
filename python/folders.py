b7f0a6f27f056390c221bfccb95f563b74a15d84
Admin0705 <user@dunn.cloud>
2018-04-01 11:57:59 -0400

    update

--- python/folders.py
+++ python/folders.py
@@ -1,12 +1,35 @@
+################## Built for PY3
+
 import os
 import os.path
 import shutil
 
 folders = [
-"red", 
-"green", 
-"blue", 
-"purple"
+"var/plexguide/logs", 
+"mnt/deluge/temp",
+"mnt/deluge/downloaded", 
+"mnt/deluge/torrent", 
+"mnt/rutorrents/downloads",  
+"mnt/move/tv", 
+"mnt/move/movies", 
+"mnt/move/music", 
+"mnt/nzbget/completed/movies",
+"mnt/nzbget/completed/music",
+"mnt/nzbget/completed/tv",
+"mnt/nzbget/completed/ebooks",
+"mnt/nzbget/incomplete",
+"mnt/nzbget/nzb",
+"mnt/nzbget/queue",
+"mnt/nzbget/tmp",
+"mnt/nzbget/log",
+"mnt/sab/admin",
+"mnt/sab/complete/tv",
+"mnt/sab/complete/movies",
+"mnt/sab/complete/music",
+"mnt/sab/complete/ebooks",
+"mnt/sab/incomplete",
+"mnt/sab/nzb",
+"opt/appdata/plexguide"
 ]
 for folder in folders:
     print(folder)
