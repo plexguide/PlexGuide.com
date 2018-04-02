0e5d47446f4376b5e15186d5c4a40b3fa481436c
Admin0705 <user@dunn.cloud>
2018-04-02 08:38:51 -0400

    update

--- menus/programs/manager.sh
+++ menus/programs/manager.sh
@@ -22,9 +22,9 @@ source <(grep '^ .*='  /opt/appdata/plexguide/var.sh)
 echo $ipv4
 domain=$( cat /var/plexguide/server.domain )
 
-HEIGHT=14
+HEIGHT=15
 WIDTH=38
-CHOICE_HEIGHT=9
+CHOICE_HEIGHT=10
 BACKTITLE="Visit PlexGuide.com - Automations Made Simple"
 TITLE="Applications - Manager Programs"
 
