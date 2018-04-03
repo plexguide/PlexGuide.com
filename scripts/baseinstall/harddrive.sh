46162faec78e6b4a7d457d78f3fcce7d6a420cfb
Admin0705 <user@dunn.cloud>
2018-04-03 12:20:21 -0400

    update

---
+++ scripts/baseinstall/harddrive.sh
@@ -0,0 +1,59 @@
+#!/bin/bash
+#
+# [PlexGuide Menu]
+#
+# GitHub:   https://github.com/Admin9705/PlexGuide.com-The-Awesome-Plex-Server
+# Author:   Admin9705
+# URL:      https://plexguide.com
+#
+# PlexGuide Copyright (C) 2018 PlexGuide.com
+# Licensed under GNU General Public License v3.0 GPL-3 (in short)
+#
+#   You may copy, distribute and modify the software as long as you track
+#   changes/dates in source files. Any modifications to our software
+#   including (via compiler) GPL-licensed code must also be made available
+#   under the GPL along with build & install instructions.
+#
+#################################################################################
+######## This is a ONE TIME MENU
+export NCURSES_NO_UTF8_ACS=1
+
+HEIGHT=11
+WIDTH=35
+CHOICE_HEIGHT=6
+BACKTITLE="Visit PlexGuide.com - Automations Made Simple"
+TITLE="Make A Choice - Visit Again In Settings!"
+
+OPTIONS=(A "YES: A Second HD Will Be Used! It's READY!"
+         B "YES: A Second HD Will Be Used! It's NOT READY"
+         C "NO:  Do Not Have a SECOND HD! Do NOT ASK AGAIN!"
+         D "MINI FAQ: Why this Question?")
+
+CHOICE=$(dialog --backtitle "$BACKTITLE" \
+                --title "$TITLE" \
+                --menu "$MENU" \
+                $HEIGHT $WIDTH $CHOICE_HEIGHT \
+                "${OPTIONS[@]}" \
+                2>&1 >/dev/tty)
+
+case $CHOICE in
+    A)
+      dialog --title "HD Selection" --msgbox "\nYou Selected: Yes, and I am Ready!\n\nThis you named and can access your HD! If you botch the name, visit SETTINGS and change ANYTIME!" 0 0
+      echo "yes" > /var/plexguide/server.hd
+      ;;
+    B)
+      dialog --title "HD Selection" --msgbox "\nYou Selected: Yes, but not ready!\n\nWhen your ready, visit SETTINGS for setup ANYTIME!" 0 0
+      echo "nr" > /var/plexguide/server.hd
+      ;;
+    C)
+      dialog --title "HD Selection" --msgbox "\nYou Selected: NO 2ND Harddrive for SETUP!\n\nNeed to Make Changes? Visit SETTINGS and change ANYTIME!" 0 0
+      echo "no" > /var/plexguide/server.hd
+      exit
+      ;;
+    D)
+      dialog --title "NOT READY" --msgbox "\nWill Update the Information" 0 0
+      exit
+    ;;
+esac
+
+exit
