55f2a5caf7bebb6313364d74fbdb4c8f03bc1395
Admin0705 <user@dunn.cloud>
2018-04-04 21:03:13 -0400

    update

--- menus/benchmark/main.sh
+++ menus/benchmark/main.sh
@@ -74,7 +74,7 @@ case $CHOICE in
         E)
             program=speed
             port=8223
-            dialog --infobox "Installing: SpeedTEST Server" 3 40
+            dialog --infobox "Installing: SpeedTEST Server" 3 38
             ansible-playbook /opt/plexguide/ansible/plexguide.yml --tags speedtestserver 1>/dev/null 2>&1
 
             echo "$program" > /tmp/program
