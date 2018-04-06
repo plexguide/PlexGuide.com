07ea838a5d03d119733774faacc46e7421bbbac0
Admin0705 <user@dunn.cloud>
2018-04-06 09:08:34 -0400

    update

--- menus/startup/message2.sh
+++ menus/startup/message2.sh
@@ -1,21 +1,21 @@
 #!/bin/bash
 export NCURSES_NO_UTF8_ACS=1
 
-domain=$( cat /var/plexguide/server.domain )
-hd=$( cat /var/plexguide/server.hd.path )
-ip=$( cat /var/plexguide/server.ip )
+domain=$( cat /var/plexguide/server.domain ) 1>/dev/null 2>&1
+hd=$( cat /var/plexguide/server.hd.path ) 1>/dev/null 2>&1
+ip=$( cat /var/plexguide/server.ip ) 1>/dev/null 2>&1
 
-docker=$( cat /var/plexguide/docker.version )
-docker=${docker::-1} 
+docker=$( cat /var/plexguide/docker.version ) 1>/dev/null 2>&1
+docker=${docker::-1} 1>/dev/null 2>&1
 
-cert2=$( cat /var/plexguide/status.traefik2 )
+cert2=$( cat /var/plexguide/status.traefik2 ) 1>/dev/null 2>&1
 if [ "$cert2" == "certificate" ]
 then
-	echo "\nTraefik v2: Certificate is Valid" > /var/plexguide/status.traefik.cert
-	cert2=$( cat /var/plexguide/status.traefik.cert )
+	echo "\nTraefik v2: Certificate is Valid" > /var/plexguide/status.traefik.cert 1>/dev/null 2>&1
+	cert2=$( cat /var/plexguide/status.traefik.cert ) 1>/dev/null 2>&1
 else
-	echo "\nTraefik v2: Certificate is NOT Valid" > /var/plexguide/status.traefik.cert
-	cert2=$( cat /var/plexguide/status.traefik.cert )
+	echo "\nTraefik v2: Certificate is NOT Valid" > /var/plexguide/status.traefik.cert 1>/dev/null 2>&1
+	cert2=$( cat /var/plexguide/status.traefik.cert ) 1>/dev/null 2>&1
 fi
 
 dialog --title "PG Startup Variable Page" --msgbox "\nIP:     $ip\nDomain: $domain\n$cert2\nDownload Path : $hd\nDocker Version: $docker" 0 0
