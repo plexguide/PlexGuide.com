731868ef97135e7a9376a7b426a78e129b6fa6f2
Admin0705 <user@dunn.cloud>
2018-04-06 09:33:48 -0400

    update

--- menus/main.sh
+++ menus/main.sh
@@ -23,7 +23,7 @@ HEIGHT=15
 WIDTH=38
 CHOICE_HEIGHT=10
 BACKTITLE="Visit PlexGuide.com - Automations Made Simple"
-TITLE="PlexGuide - Version 5.068 BETA 2"
+TITLE="PlexGuide - Version 5.068 DEV"
 
 OPTIONS=(A "Donation Menu"
          B "RClone & PlexDrive"
