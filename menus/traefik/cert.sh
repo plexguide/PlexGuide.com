f93e7fb1953b44f9fed3bc5a3c185baa1980af18
Admin0705 <user@dunn.cloud>
2018-04-06 01:58:15 -0400

    update

---
+++ menus/traefik/cert.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+#
+# [Rebuilding Containers]
+#
+# GitHub:   https://github.com/Admin9705/PlexGuide.com-The-Awesome-Plex-Server
+# Author:   Admin9705 - Deiteq
+# URL:      https://plexguide.com
+#
+# PlexGuide Copyright (C) 2018 PlexGuide.com
+# Licensed under GNU General Public License v3.0 GPL-3 (in short)
+#
+#   You may copy, distribute and modify the software as long as you track
+#   changes/dates in source files. Any modifications to our software
+#   including (via compiler) GPL-licensed code must also be made available
+#   under the GPL along with build & install instructions.
+#
+#################################################################################
+docker logs --tail 1 traefik2 2> /var/plexguide/status.traefik2
+dock=$( cat /var/plexguide/status.traefik2)
+dock=${dock#*responded with a} 
+dock=${dock::-1}
+path=" /$path"
+echo "$dock" > /var/plexguide/status.traefik2
