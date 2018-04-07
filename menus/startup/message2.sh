4a78c142d672a2af2c8ef73357fdade5e3fdc8b9
Admin0705 <user@dunn.cloud>
2018-04-06 22:16:05 -0400

    update

--- menus/startup/message2.sh
+++ menus/startup/message2.sh
@@ -12,7 +12,7 @@ hostname -I | awk '{print $1}' > /var/plexguide/server.ip
 ip=$( cat /var/plexguide/server.ip ) 1>/dev/null 2>&1
 
 #### GDrive or Local Edition (Local Not Working Yet)
-echo "5.068\n" > /var/plexguide/pg.version
+#cat /var/plexguide/pg.version > /var/plexguide/pg.version
 version=$( cat /var/plexguide/pg.version ) 1>/dev/null 2>&1
 
 #### PG Version (Not Working Yet)
