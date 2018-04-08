b6d9173a3404131721249c28f8c68e5164d1d864
Admin0705 <user@dunn.cloud>
2018-04-08 08:17:59 -0400

    update

--- scripts/autodelete/install.sh
+++ scripts/autodelete/install.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
-# Credit: RXWatcher
-find /mnt/nzbget/completed/ebooks/* -type d -mmin +60 -ls -exec rm -rf {} + 2>/dev/null
-find /mnt/nzbget/completed/movies/* -type d -mmin +60 -ls -exec rm -rf {} + 2>/dev/null
-find /mnt/nzbget/completed/music/*  -type d -mmin +60 -ls -exec rm -rf {} + 2>/dev/null
-find /mnt/nzbget/completed/tv/* -type d -mmin +60 -ls -exec rm -rf {} + 2>/dev/null
+
+### For NZBGET
+echo "nzbget" > /tmp/program_var
+ansible-playbook /opt/plexguide/ansible/plexguide.yml --tags autodelete
+
+### For SABNZBD
+# later
