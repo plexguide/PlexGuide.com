628d9078fb86857b9500dff679e915c76d381b85
Admin0705 <user@dunn.cloud>
2018-04-10 17:44:39 -0400

    update

--- menus/localmain.sh
+++ menus/localmain.sh
@@ -16,16 +16,16 @@
 #
 #################################################################################
 #hostname -I | awk '{print $1}' > /var/plexguide/server.ip
-
+edition=$( cat /var/plexguide/pg.edition ) 1>/dev/null 2>&1
 version=$( cat /var/plexguide/pg.version ) 1>/dev/null 2>&1
 
 export NCURSES_NO_UTF8_ACS=1
 clear
 HEIGHT=12
-WIDTH=36
+WIDTH=40
 CHOICE_HEIGHT=6
 BACKTITLE="Visit PlexGuide.com - Automations Made Simple"
-TITLE="NOT READY Local HD - $version"
+TITLE="$edition - $version"
 
 OPTIONS=(A "Donation Menu"
          B "PG Program Suite"
