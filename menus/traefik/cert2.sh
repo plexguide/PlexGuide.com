517ef0d98a7d6a2d5f24465eb46f5b154334438c
Admin0705 <user@dunn.cloud>
2018-04-11 07:42:55 -0400

    update

--- menus/traefik/cert2.sh
+++ menus/traefik/cert2.sh
@@ -15,7 +15,7 @@
 #   under the GPL along with build & install instructions.
 #
 #################################################################################
-docker logs --tail 1000 traefik2 2> /var/plexguide/status.traefik2
+docker logs --tail 50 traefik2 2> /var/plexguide/status.traefik2
 dock=$( cat /var/plexguide/status.traefik2 )
 dock=${dock#*responded with a} 
 dock=${dock::-1}
