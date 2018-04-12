ed0ede96bd2ad2de605badfb36c915d150617173
Admin0705 <user@dunn.cloud>
2018-04-12 00:03:43 -0400

    update

--- menus/drives/solodeploy.sh
+++ menus/drives/solodeploy.sh
@@ -53,6 +53,10 @@ if [ "$deploy" == "drive" ]
   systemctl disable unionfs 1>/dev/null 2>&1
   systemctl deamon-reload 1>/dev/null 2>&1
 
+  chmod 775 /mnt/unionfs
+  chmod 775 /mnt/move
+  chown 1000:1000 /mnt/unionfs
+  chown 1000:1000 /mnt/move
   ##### Creates a  Symbolic Link
   ln -s "/mnt/move/" "/mnt/unionfs" 1>/dev/null 2>&1
 echo "drive" > /var/plexguide/pg.server.deploy
