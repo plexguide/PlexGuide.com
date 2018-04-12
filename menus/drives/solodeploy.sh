f04dd74465bb5f7af06f1592c6ee6ca811cc5981
Admin0705 <user@dunn.cloud>
2018-04-12 07:23:06 -0400

    update

--- menus/drives/solodeploy.sh
+++ menus/drives/solodeploy.sh
@@ -56,8 +56,8 @@ if [ "$deploy" == "drive" ]
   #### Build Certain Paths
   chmod 775 /mnt/unionfs 1>/dev/null 2>&1
   chmod 775 /mnt/move 1>/dev/null 2>&1
-  chown -r 1000:1000 /mnt/unionfs 1>/dev/null 2>&1
-  chown -r 1000:1000 /mnt/move 1>/dev/null 2>&1
+  chown 1000:1000 /mnt/unionfs 1>/dev/null 2>&1
+  chown 1000:1000 /mnt/move 1>/dev/null 2>&1
   
   mkdir /mnt/unionfs/tv 1>/dev/null 2>&1
   mkdir /mnt/unionfs/movies 1>/dev/null 2>&1
@@ -69,10 +69,10 @@ if [ "$deploy" == "drive" ]
   chmod 775 /mnt/unionfs/ebooks 1>/dev/null 2>&1
   chmod 775 /mnt/unionfs/music 1>/dev/null 2>&1
 
-  chown -r 1000:1000 /mnt/unionfs/tv 1>/dev/null 2>&1
-  chown -r 1000:1000 /mnt/unionfs/movies 1>/dev/null 2>&1
-  chown -r 1000:1000 /mnt/unionfs/ebooks 1>/dev/null 2>&1
-  chown -r 1000:1000 /mnt/unionfs/music 1>/dev/null 2>&1
+  chown 1000:1000 /mnt/unionfs/tv 1>/dev/null 2>&1
+  chown 1000:1000 /mnt/unionfs/movies 1>/dev/null 2>&1
+  chown 1000:1000 /mnt/unionfs/ebooks 1>/dev/null 2>&1
+  chown 1000:1000 /mnt/unionfs/music 1>/dev/null 2>&1
 
   #### Creates a  Symbolic Link
   #ln -s "/mnt/move/" "/mnt/unionfs" 1>/dev/null 2>&1
