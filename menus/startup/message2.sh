f68205ff9c345592fef1e3337d0be27ec0b68771
Admin0705 <user@dunn.cloud>
2018-04-15 09:31:15 -0400

    update

--- menus/startup/message2.sh
+++ menus/startup/message2.sh
@@ -21,27 +21,49 @@ version=$( cat /var/plexguide/pg.version ) 1>/dev/null 2>&1
 #### Edition of PG
 edition=$( cat /var/plexguide/pg.edition ) 1>/dev/null 2>&1
 
-provider=$( cat /var/plexguide/provider ) 1>/dev/null 2>&1
-if [ "$provider" == "null" ]
-then
-	cert1=$( cat /var/plexguide/status.traefik1 ) 1>/dev/null 2>&1
-	if [ "$cert1" == "certificate" ]
+#### Checks to See if Either Traefik Exisxts
+docker logs traefik2 2> /var/plexguide/traefik.error2
+docker logs traefik 2> /var/plexguide/traefik.error1
+error2=$( cat /var/plexguide/traefik.error2 )
+error2=${error2::-1}
+error1=$( cat /var/plexguide/traefik.error1 )
+
+#### If neither one exist, displays message below; if does executes the stuff under else
+if [ "$error2" == "$error1" ]
+  then
+    echo "\nWARNING: Traefik is not installed!" > /var/plexguide/status.traefik.cert
+  else
+  	
+  	#### Version 1 or 2 Display
+	provider=$( cat /var/plexguide/provider ) 1>/dev/null 2>&1
+	if [ "$provider" == "null" ]
 	then
-		echo "\nTraefik v1: Certificate is Valid" > /var/plexguide/status.traefik.cert
-		cert1=$( cat /var/plexguide/status.traefik.cert ) 1>/dev/null 2>&1
+		cert1=$( cat /var/plexguide/status.traefik1 ) 1>/dev/null 2>&1
+		if [ "$cert1" == "certificate" ]
+		then
+			echo "\nTraefik v1: Certificate is Valid" > /var/plexguide/status.traefik.cert
+			cert1=$( cat /var/plexguide/status.traefik.cert ) 1>/dev/null 2>&1
+		else
+			echo "\nTraefik v1: Certificate is NOT Valid" > /var/plexguide/status.traefik.cert
+			cert1=$( cat /var/plexguide/status.traefik.cert ) 1>/dev/null 2>&1
+		fi
 	else
-		echo "\nTraefik v1: Certificate is NOT Valid" > /var/plexguide/status.traefik.cert
-		cert1=$( cat /var/plexguide/status.traefik.cert ) 1>/dev/null 2>&1
+		cert2=$( cat /var/plexguide/status.traefik2 ) 1>/dev/null 2>&1
+		if [ "$cert2" == "certificate" ]
+		then
+			echo "\nTraefik v2: Certificate is Valid" > /var/plexguide/status.traefik.cert
+			cert2=$( cat /var/plexguide/status.traefik.cert ) 1>/dev/null 2>&1
+		else
+			echo "\nTraefik v2: Certificate is NOT Valid" > /var/plexguide/status.traefik.cert
+			cert2=$( cat /var/plexguide/status.traefik.cert ) 1>/dev/null 2>&1
+		fi
 	fi
-else
-	cert2=$( cat /var/plexguide/status.traefik2 ) 1>/dev/null 2>&1
-	if [ "$cert2" == "certificate" ]
-	then
-		echo "\nTraefik v2: Certificate is Valid" > /var/plexguide/status.traefik.cert
-		cert2=$( cat /var/plexguide/status.traefik.cert ) 1>/dev/null 2>&1
-	else
-		echo "\nTraefik v2: Certificate is NOT Valid" > /var/plexguide/status.traefik.cert
-		cert2=$( cat /var/plexguide/status.traefik.cert ) 1>/dev/null 2>&1
-	fi
+
 fi
+
+
+
+
+
+
 dialog --title "PG Startup Variable Page" --msgbox "\n$edition\nPG Version: $version\n\nIP:     $ip\nDomain: $domain\n$cert1$cert2\nDocker Version: $docker\nDownload Path : $hd" 0 0
