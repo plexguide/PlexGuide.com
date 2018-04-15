f83fa1f85eba67fc387550b256272b92bb17dac9
Admin0705 <user@dunn.cloud>
2018-04-15 10:27:43 -0400

    update

--- scripts/baseinstall/main.sh
+++ scripts/baseinstall/main.sh
@@ -180,6 +180,7 @@ sleep 2
         bash /opt/plexguide/menus/donate/main.sh
     fi
 
+touch /var/plexguide/server.hd.role.yes
 cat /var/plexguide/pg.preinstall > /var/plexguide/pg.preinstall.stored
 
 echo "PG Install is Complete" > /tmp/pushover
