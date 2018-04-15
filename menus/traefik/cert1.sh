58ed69cb0072f37e47b1dd64212aa9f0796bdfd3
Admin0705 <user@dunn.cloud>
2018-04-15 08:57:41 -0400

    update

--- menus/traefik/cert1.sh
+++ menus/traefik/cert1.sh
@@ -15,15 +15,22 @@
 #   under the GPL along with build & install instructions.
 #
 #################################################################################
+#docker logs --tail 20 traefik 2> /var/plexguide/status.traefik1
+#dock=$( cat /var/plexguide/status.traefik1 )
+
+#string='*with a certificate.'
+#if [[ $string = *"with a certificate"* ]]; then
+#  echo "certificate" > /var/plexguide/status.traefik1
+#else
+#  echo "nope" > /var/plexguide/status.traefik1
+#fi
+
 docker logs --tail 20 traefik 2> /var/plexguide/status.traefik1
 dock=$( cat /var/plexguide/status.traefik1 )
-
-string='*with a certificate.'
-if [[ $string = *"with a certificate"* ]]; then
-  echo "certificate" > /var/plexguide/status.traefik1
-else
-  echo "nope" > /var/plexguide/status.traefik1
-fi
+dock=${dock#*responded with a} 
+dock=${dock::-1}
+echo $dock | head -c 1
+echo $dock > /var/plexguide/status.traefik1
 
 ######### path to call script
 #bash /opt/plexguide/menus/traefik/cert2.sh
