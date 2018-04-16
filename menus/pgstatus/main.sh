bb656a918efcc0c08c720a1eda0d40633137b478
Admin0705 <user@dunn.cloud>
2018-04-16 09:36:16 -0400

    updates

--- menus/pgstatus/main.sh
+++ menus/pgstatus/main.sh
@@ -16,7 +16,7 @@
 #
 #################################################################################
 
-  dialog --title "Flicker-Rate's PG Status" --msgbox "\nThis PROGRAM is an early BETA!\n\nOnce loaded, press CTRL+C to break out of it! We are working on a HOTKEY solution!\n\nIf you have ideas or solutions, visit us in the DISCORD or post on the Forums!\n\nThanks!\n\n@Flicker-Rate\n\nNOTE: You can type -- pgstatus -- in Termainal to access also! 0 0
+  dialog --title "Flicker-Rate's PG Status" --msgbox "\nThis PROGRAM is an early BETA!\n\nOnce loaded, press CTRL+C to break out of it! We are working on a HOTKEY solution!\n\nIf you have ideas or solutions, visit us in the DISCORD or post on the Forums!\n\nThanks!\n\n@Flicker-Rate\n\nNOTE: You can type -- pgstatus -- in Termainal to access also!" 0 0
 
 
   clear
