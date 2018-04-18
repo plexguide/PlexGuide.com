af14112560629d901b28bd6fa2a40061c02e5770
Chris Macnichol <chrismacnichol@gmail.com>
2018-04-18 13:17:34 -0400

    no message

--- ansible/roles/cadvisor/tests/test.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash -e
-ansible-playbook -t cadvisor_docker_testing ./test.yml -vv
-docker-compose -f ./output/docker-compose.yml config > /dev/null
-[[ $(docker ps --filter status=running --filter name="^/output_cadvisor_1" -q | wc -l) -eq 1 ]]
