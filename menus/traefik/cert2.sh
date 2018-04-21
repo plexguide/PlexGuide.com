6604ee4cac17ff8159ac1194fc669657ac470a0e
Admin0705 <user@dunn.cloud>
2018-04-21 10:10:31 -0400

    update

--- menus/traefik/cert2.sh
+++ menus/traefik/cert2.sh
@@ -28,7 +28,7 @@
 docker logs --tail 100 traefik2 2> /var/plexguide/status.traefik2
 dock=$( cat /var/plexguide/status.traefik2 )
 info=${dock#*responded with a}
-info=$( echo ${info:0:12} )
+info=$( echo ${info:0:12} ) 1>/dev/null 2>&1
 echo $info 1>/dev/null 2>&1 | head -c 1
 echo $info > /var/plexguide/status.traefik2
 
