1192ea10bdb10e7a4e96db004986bf034133fbfd
Admin0705 <user@dunn.cloud>
2018-04-22 17:51:47 -0400

    udpate

--- menus/nzb/main.sh
+++ menus/nzb/main.sh
@@ -15,22 +15,4 @@
 #   under the GPL along with build & install instructions.
 #
 #################################################################################
-MAXCOUNT=1
-count=1
-
-while [ "$count" -le $MAXCOUNT ]
-do
-  number=$RANDOM
-  let "count += 1"  # Increment count.
-done
-
-## so if the highest number is 10, the range must be 11 - Range = maxnumber + 1
-RANGE=20
-
-number=$RANDOM
-let "number %= $RANGE"
-
-if [ "$number" -eq "3" ]
-then
-dialog --title "PG Member NZB Specials" --msgbox "\nWe are partnered to provide our PG Members with Deep Discounts\n\n1. newshosting.plexguide.com  - Free VPN & 41% Discount! Fastest!\n2. usenetserver.plexguide.com - Free VPN & 39% Discount! Well-Known!\n3. easynews.plexguide.com     - Top Speeds & Great Alternative!\n4. pureusenet.plexguide.com   - Pay Euros & Packages as Low as 1.83!\n5. xlned.plexguide.com        - Pay Euros & Packages as Low as 2.24!\n\nIf you need the LINKS again, visit our Wiki!" 14 73
-fi
+dialog --title "PG Member NZB Specials" --msgbox "\nWe are partnered to provide our PG Members with Deep Discounts\n\n1. newshosting.plexguide.com  - Free VPN & 41% Discount! Fastest!\n2. usenetserver.plexguide.com - Free VPN & 39% Discount! Well-Known!\n3. easynews.plexguide.com     - Top Speeds & Great Alternative!\n4. pureusenet.plexguide.com   - Pay Euros & Packages as Low as 1.83!\n5. xlned.plexguide.com        - Pay Euros & Packages as Low as 2.24!\n\n* gsuite.plexguide.com -        MEMBERS Get a FREE 14 Day Trial!\n\nIf you need the LINKS again, visit our Wiki!" 14 73
