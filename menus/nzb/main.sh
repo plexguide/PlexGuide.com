e7e411e40788438b2d5fb72c29b0a70ea542e757
Admin0705 <user@dunn.cloud>
2018-04-22 17:29:26 -0400

    update

--- menus/nzb/main.sh
+++ menus/nzb/main.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #
-# [PG BaseInstall]
+# [PG Script]
 #
 # GitHub:   https://github.com/Admin9705/PlexGuide.com-The-Awesome-Plex-Server
 # Author:   FlickerRate
@@ -15,5 +15,16 @@
 #   under the GPL along with build & install instructions.
 #
 #################################################################################
+MAXCOUNT=1
+count=1
 
-dialog --title "PG Member NZB Specials" --msgbox "\nWe are partnered to provide our PG Members with Deep Discounts\n\n1. newshosting.plexguide.com  - Free VPN & 41% Discount! Fastest!\n2. usenetserver.plexguide.com - Free VPN & 39% Discount! Well-Known!\n3. easynews.plexguide.com     - Top Speeds & Great Alternative!\n4. pureusenet.plexguide.com   - Pay Euros & Packages as Low as 1.83!\n5. xlned.plexguide.com        - Pay Euros & Packages as Low as 2.24!" 12 73
+while [ "$count" -le $MAXCOUNT ]
+do
+  number=$RANDOM
+  let "count += 1"  # Increment count.
+done
+
+## so if the highest number is 10, the range must be 11 - Range = maxnumber + 1
+RANGE=7
+
+dialog --title "PG Member NZB Specials" --msgbox "\nWe are partnered to provide our PG Members with Deep Discounts\n\n1. newshosting.plexguide.com  - Free VPN & 41% Discount! Fastest!\n2. usenetserver.plexguide.com - Free VPN & 39% Discount! Well-Known!\n3. easynews.plexguide.com     - Top Speeds & Great Alternative!\n4. pureusenet.plexguide.com   - Pay Euros & Packages as Low as 1.83!\n5. xlned.plexguide.com        - Pay Euros & Packages as Low as 2.24!\n\nIf you need the LINKS again, visit our Wiki!" 14 73
