deb58e1810505db349da7d7f8c2e923857d40603
Admin0705 <user@dunn.cloud>
2018-04-23 00:09:46 -0400

    update

--- scripts/docker-no/rcache.sh
+++ scripts/docker-no/rcache.sh
@@ -43,7 +43,7 @@ cp ~/.config/rclone/rclone.conf /root/.config/rclone/ 1>/dev/null 2>&1
 ansible-playbook /opt/plexguide/ansible/plexguide.yml --tags rcache_un #--skip-tags=rclone_temp
 
 ## RClone Script
-chmod 755 /opt/appdata/plexguide/rclone.sh
+chmod 755 /opt/appdata/plexguide/gdrive.sh
 
 ## Create the Move Script
 chmod 755 /opt/appdata/plexguide/move.sh
