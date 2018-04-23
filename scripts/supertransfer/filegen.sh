0663a8b2781349866ad3edeaf2c6fca48fa1544c
flicker <flicker-rate@protonmail.com>
2018-04-22 23:58:33 -0600

    dev

---
+++ scripts/supertransfer/filegen.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+maxgb=32
+max=$(( maxgb * 1000000 ))
+while true; do
+	while [[ $(du /mnt/move -c | tail -1 | awk '{print $1}') -lt $max ]]; do
+    flag=0
+		dd if=/dev/urandom of=/mnt/move/movies/1G_$(date +%H_%M_%S) bs=64M count=32 &>/dev/null
+		echo "[INFO] 1G file created. Dir size: $(du /mnt/move/ -hc | tail -1)"
+	done
+sleep 1
+[[ $flag == 0 ]] && echo "[WAIT] Max Size hit. Dir size: $(du /mnt/move/ -hc | tail -1)" && flag=1
+done
