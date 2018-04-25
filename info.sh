f1f1654be34b8b202f797f726eae04620d2c4a07
Admin0705 <user@dunn.cloud>
2018-04-25 12:25:26 -0400

    update

--- info.sh
+++ info.sh
@@ -17,4 +17,4 @@
 #################################################################################
 
 echo "5.072 DEV" > /var/plexguide/pg.version
-echo "60" > /var/plexguide/pg.preinstall
+echo "61" > /var/plexguide/pg.preinstall
