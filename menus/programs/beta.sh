15cf3a9da0476c45dbdd0781318e2d7c7be4a99c
Migz93 <33037112+Migz93@users.noreply.github.com>
2018-04-25 21:57:58 +0100

    commit

--- menus/programs/beta.sh
+++ menus/programs/beta.sh
@@ -3,7 +3,7 @@ export NCURSES_NO_UTF8_ACS=1
 
  HEIGHT=11
  WIDTH=55
- CHOICE_HEIGHT=4
+ CHOICE_HEIGHT=5
  BACKTITLE="Visit PlexGuide.com - Automations Made Simple"
  TITLE="Applications - VPN Programs"
 
