ab72ca3240f3de791bc5c248541af55b220b30c2
Admin0705 <user@dunn.cloud>
2018-04-27 15:30:05 -0400

    update

--- menus/pgtrak/main.sh
+++ menus/pgtrak/main.sh
@@ -18,14 +18,14 @@
 export NCURSES_NO_UTF8_ACS=1
 
 ### Pull the API Keys
-info=$( cat /opt/appdata/radarr/config.xml )
-info=${info#*<ApiKey>}
-info1=$( echo ${info:0:32} )
+info=$( cat /opt/appdata/radarr/config.xml ) 1>/dev/null 2>&1
+info=${info#*<ApiKey>} 1>/dev/null 2>&1
+info1=$( echo ${info:0:32} ) 1>/dev/null 2>&1
 echo "$info1" > /var/plexguide/api.radarr
 
-info=$( cat /opt/appdata/sonarr/config.xml )
-info=${info#*<ApiKey>}
-info2=$( echo ${info:0:32} )
+info=$( cat /opt/appdata/sonarr/config.xml ) 1>/dev/null 2>&1
+info=${info#*<ApiKey>} 1>/dev/null 2>&1
+info2=$( echo ${info:0:32} ) 1>/dev/null 2>&1
 echo "$info2" > /var/plexguide/api.sonarr
 
 ### If Both are Blank, which means neither deployed, warn USER!
