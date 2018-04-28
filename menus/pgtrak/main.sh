87c7960fc5bd7ee6160358e94982b7a4254ee79e
Admin0705 <user@dunn.cloud>
2018-04-27 22:46:18 -0400

    update

--- menus/pgtrak/main.sh
+++ menus/pgtrak/main.sh
@@ -111,7 +111,7 @@ case $CHOICE in
             if dialog --stdout --title "If this IS NOT your first time, be aware that you may loose your personal configs from the config.json if you have edited it from before!\n\nTake note of what you put and edit it again! Want to redploy PGTrak?" 0 0
                 --backtitle "Visit https://PlexGuide.com - Automations Made Simple" \
                 --yesno "\n-- Deploy Warning --" 0 0; then
-                            dialog --infobox "Deploying PGTrak!" 3 26
+                dialog --infobox "Deploying PGTrak!" 3 26
             ansible-playbook /opt/plexguide/ansible/plexguide.yml --tags pgtrak 1>/dev/null 2>&1
             #read -n 1 -s -r -p "Press any key to continue"
             dialog --title "PGDupes Status" --msgbox "\nPGTrak Deployment Complete! Use the CMD pgtrak in the Command Line!" 0 0
