d1e3dc39f5900bad6e07ca68234a5c542b53ffc4
Admin0705 <user@dunn.cloud>
2018-04-29 15:50:29 -0400

    update

--- menus/watchtower/main.sh
+++ menus/watchtower/main.sh
@@ -1,18 +1,14 @@
 
- #!/bin/bash
+#!/bin/bash
 export NCURSES_NO_UTF8_ACS=1
- ## point to variable file for ipv4 and domain.com
- source <(grep '^ .*='  /opt/appdata/plexguide/var.sh)
- echo $ipv4
- echo $domain
+
 
 file="/var/plexguide/watchtower.yes"
 if [ -e "$file" ]
 then
    clear
 else
-   dialog --title "WatchTower Status" --msgbox "\nNotice: WatchTower enable your Containers to Auto-Update!\n\nPROS: You containers will always be up-to-date.\n\nCONS: If something is wrong with the newest continer, you'll have issues; rare but happens.\n\nNOTE: Typically recommend Plex & Emby to be updated manually for stability purposes!" 14 62
-   touch /var/plexguide/watchtower.yes
+   dialog --title "WatchTower Status" --msgbox "\nNotice: WatchTower enable your Containers to Auto-Update!\n\nPROS: You containers will always be up-to-date.\n\nCONS: If something is wrong with the newest continer, you'll have issues; rare but happens.\n\nNOTE: Typically recommend Plex & Emby to be updated manually. Any automatic update of images can be bugged!" 14 62
 fi
 
 HEIGHT=11
@@ -43,6 +39,7 @@ case $CHOICE in
               --yesno "\nYou Want to --Update All Containers except Plex & Emby?" 7 34; then
               ansible-playbook /opt/plexguide/ansible/plexguide.yml --tags watchtower --skip-tags=watchall &>/dev/null &
               dialog --infobox "Notice: Your containers will Auto-Update except PLEX & Emby!\n\nMade an error? Just SELECT it again!" 0 0
+              touch /var/plexguide/watchtower.yes
               sleep 9
               exit 0
             else
@@ -58,6 +55,7 @@ case $CHOICE in
               --yesno "\nYou Want to have all Your Containers Auto-Update?" 7 34; then
               ansible-playbook /opt/plexguide/ansible/plexguide.yml --tags watchtower --skip-tags=plexemby &>/dev/null &
               dialog --infobox "Notice: All Your containers will Auto-Update!\n\nMade an error? Just SELECT it again!" 0 0
+              touch /var/plexguide/watchtower.yes
               sleep 9
               exit 0
             else
@@ -74,6 +72,7 @@ case $CHOICE in
               docker stop watchtower 1>/dev/null 2>&1
               docker rm watchtower 1>/dev/null 2>&1
               dialog --infobox "Notice: WatchTower is not enabled or has been removed!\n\nUpdate manually by rerunning your targeted Application for the newest update!" 0 0
+              touch /var/plexguide/watchtower.yes
               sleep 9
               exit 0
             else
@@ -84,12 +83,19 @@ case $CHOICE in
             fi
               ;;
         D)
-              dialog --title "WatchTower Status" --msgbox "\nNotice: WatchTower allows your Containers to Auto-Update!\n\nPROS: You containers will always be up-to-date.\n\nCONS: If something is wrong with the newest continer, you'll have issues; rare but happens.\n\nNOTE: Typically recommend Plex & Emby to be updated manually for stability purposes!" 14 62
+              dialog --title "WatchTower Status" --msgbox "\nNotice: WatchTower allows your Containers to Auto-Update!\n\nPROS: You containers will always be up-to-date.\n\nCONS: If something is wrong with the newest continer, you'll have issues; rare but happens.\n\nNOTE: Typically recommend Plex & Emby to be updated manually for stability purposes!" 0 0 
               ;;
         Z)
-            clear
-            exit 0
+            file="/var/plexguide/watchtower.yes"
+            if [ -e "$file" ]
+            then
+               exit 0
+            else
+               dialog --title "Dummy Proof Alert!" --msgbox "\nFor the first time select, you must select a status! REDO!" 14 62
+            fi
             ;;
 esac
+
+
 
 bash /opt/plexguide/menus/watchtower/main.sh
