9c87c0cb955b5823d8f1aadda3b3133f90de0f50
Admin0705 <user@dunn.cloud>
2018-05-01 08:10:17 -0400

    update

--- scripts/baseinstall/main.sh
+++ scripts/baseinstall/main.sh
@@ -78,8 +78,8 @@ python3 -m pip install pip--upgrade 1>/dev/null 2>&1
 python3 -m pip install setuptools --upgrade 1>/dev/null 2>&1
 python3 -m pip install requests --upgrade 1>/dev/null 2>&1
 python3 -m pip install netaddr --upgrade 1>/dev/null 2>&1 
-pip install --upgrade pip
-sudo easy_install pip
+pip install --upgrade pip 1>/dev/null 2>&1
+sudo easy_install pip 1>/dev/null 2>&1
 
 ## Repeating until determine root cause
 yes | apt-get update 1>/dev/null 2>&1
@@ -88,16 +88,13 @@ apt-get install build-essential -y 1>/dev/null 2>&1
 apt-get install libssl-dev -y 1>/dev/null 2>&1
 apt-get install libffi-dev -y 1>/dev/null 2>&1
 apt-get install python3-dev -y 1>/dev/null 2>&1
-apt-get install python3-pip -y 1>/dev/null 2>&1
 apt-get install python-dev -y 1>/dev/null 2>&1
-apt-get install python-pip -y 1>/dev/null 2>&1
 python3 -m pip install pyOpenSSL --upgrade 1>/dev/null 2>&1
-python3 -m pip install pip--upgrade 1>/dev/null 2>&1
 python3 -m pip install setuptools --upgrade 1>/dev/null 2>&1
 python3 -m pip install requests --upgrade 1>/dev/null 2>&1
 python3 -m pip install netaddr --upgrade 1>/dev/null 2>&1 
-pip install --upgrade pip
-sudo easy_install pip
+pip install --upgrade pip 1>/dev/null 2>&1 
+sudo easy_install pip 1>/dev/null 2>&1 
 
 echo "18" | dialog --gauge "Installing: Software Properties Common" 7 50 0
 yes | apt-get install software-properties-common 1>/dev/null 2>&1
