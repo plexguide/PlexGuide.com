1a2573df65ca1b4d3c6a3ac77d72998cd11589f3
Admin0705 <user@dunn.cloud>
2018-05-01 07:25:42 -0400

    update

--- scripts/baseinstall/main.sh
+++ scripts/baseinstall/main.sh
@@ -78,6 +78,8 @@ python3 -m pip install pip--upgrade 1>/dev/null 2>&1
 python3 -m pip install setuptools --upgrade 1>/dev/null 2>&1
 python3 -m pip install requests --upgrade 1>/dev/null 2>&1
 python3 -m pip install netaddr --upgrade 1>/dev/null 2>&1 
+pip install --upgrade pip
+sudo easy_install pip
 
 echo "18" | dialog --gauge "Installing: Software Properties Common" 7 50 0
 yes | apt-get install software-properties-common 1>/dev/null 2>&1
