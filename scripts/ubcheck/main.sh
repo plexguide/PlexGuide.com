0b4c611483901f3b47df3fcc0d738fecf77903bb
Admin9705 <24727006+Admin9705@users.noreply.github.com>
2018-05-03 15:48:17 -0400

    Update main.sh

--- scripts/ubcheck/main.sh
+++ scripts/ubcheck/main.sh
@@ -22,7 +22,10 @@ ubversion=$( cat /var/plexguide/ubversion )
 if [ "$ubversion" == "16.04" ]; then
     dialog --title "Ubuntu Version" --msgbox "\nGood Choice! You Are Running Ubuntu 16.04!" 7 50
 else
-    dialog --title "Ubuntu Version" --msgbox "\nWARNING! SYSTEM IS NOT Running Ubuntu 16.04! USE AT YOUR OWN RISK! No Support Provided!" 8 50
+if [ "$ubversion" == "18.04" ]; then
+    dialog --title "Ubuntu Version" --msgbox "\n You Are Running Ubuntu 18.04! Please Report Issues!" 7 50
+else
+    dialog --title "Ubuntu Version" --msgbox "\nWARNING! SYSTEM IS NOT Running Ubuntu 16.04/18.04! USE AT YOUR OWN RISK! No Support Provided!" 8 50
 fi
-
+fi
 exit
