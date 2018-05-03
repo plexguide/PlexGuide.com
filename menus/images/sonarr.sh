10ce600f1d3bd8d77b3a1e2208932132407f8e2d
Admin0705 <user@dunn.cloud>
2018-05-03 12:23:19 -0400

    update

--- menus/images/sonarr.sh
+++ menus/images/sonarr.sh
@@ -27,7 +27,7 @@ TITLE="Select Your Image for $app"
 MENU="Make a Selection Choice:"
 OPTIONS=(A "linuxserver/sonarr: Recommended"
          B "hotio/suitarr     : Space Saver"
-         C "zachawii/sonarr    : MP4 Converter"
+         C "zachawii/sonarr   : MP4 Converter"
          D "Why Important: FAQ"
          Z "Exit")
 
