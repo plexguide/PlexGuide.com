92710421816dfd5aa603f45469a1820296079bfd
gforce75 <user@dunn.cloud>
2018-05-04 00:29:13 -0400

    udpate

--- menus/drives/multideploy.sh
+++ menus/drives/multideploy.sh
@@ -29,15 +29,15 @@ file="/usr/bin/mergerfs" 1>/dev/null 2>&1
     else
 
 dialog --infobox "Installing MergerFS!" 0 0
-wget "https://github.com/trapexit/mergerfs/releases/download/2.24.2/mergerfs_2.24.2.ubuntu-xenial_amd64.deb" 1>/dev/null 2>&1
-apt-get install g++ pkg-config git git-buildpackage pandoc debhelper libfuse-dev libattr1-dev -y 1>/dev/null 2>&1
-git clone https://github.com/trapexit/mergerfs.git 1>/dev/null 2>&1
+wget "https://github.com/trapexit/mergerfs/releases/download/2.24.2/mergerfs_2.24.2.ubuntu-xenial_amd64.deb" #1>/dev/null 2>&1sudo
+apt-get install g++ pkg-config git git-buildpackage pandoc debhelper libfuse-dev libattr1-dev -y #1>/dev/null 2>&1
+git clone https://github.com/trapexit/mergerfs.git #1>/dev/null 2>&1
 cd mergerfs
-make clean 1>/dev/null 2>&1
-make deb 1>/dev/null 2>&1
+make clean #1>/dev/null 2>&1
+make deb #1>/dev/null 2>&1
 cd .. 
-dpkg -i mergerfs*_amd64.deb 1>/dev/null 2>&1
-rm mergerfs*_amd64.deb mergerfs*_amd64.changes mergerfs*.dsc mergerfs*.tar.gz 1>/dev/null 2>&1
+dpkg -i mergerfs*_amd64.deb #1>/dev/null 2>&1
+rm mergerfs*_amd64.deb mergerfs*_amd64.changes mergerfs*.dsc mergerfs*.tar.gz #1>/dev/null 2>&1
 fi 
 
 if [ "$deploy" == "drives" ]
