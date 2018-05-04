c77959e7119ac53f0cf652af01b2497309ad90b4
gforce75 <user@dunn.cloud>
2018-05-04 00:45:24 -0400

    update

--- menus/localmain.sh
+++ menus/localmain.sh
@@ -106,8 +106,7 @@ cd ..
 dpkg -i mergerfs*_amd64.deb #1>/dev/null 2>&1
 rm mergerfs*_amd64.deb mergerfs*_amd64.changes mergerfs*.dsc mergerfs*.tar.gz #1>/dev/null 2>&1
 fi 
-
-
+fi
 
             #### Multiple Editions HD
             bash /opt/plexguide/menus/drives/hds.sh
