15ea2bb0ed6fa21c555e94e2baf9e17d58ee74fc
Admin0705 <user@dunn.cloud>
2018-05-05 11:20:13 -0400

    update

---
+++ menus/rclone/select2.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+export NCURSES_NO_UTF8_ACS=1
+
+#############
+HEIGHT=10
+WIDTH=45
+CHOICE_HEIGHT=4
+BACKTITLE="Visit https://PlexGuide.com - Automations Made Simple"
+TITLE="PlexDrive for PG"
+MENU="Choose one of the following options:"
+
+OPTIONS=(A "RClone - Unencrypted (Recommended)"
+         B "RClone - Encrypted (NOT BUILT)"
+         Z "Exit")
+
+CHOICE=$(dialog --clear \
+                --backtitle "$BACKTITLE" \
+                --title "$TITLE" \
+                --menu "$MENU" \
+                $HEIGHT $WIDTH $CHOICE_HEIGHT \
+                "${OPTIONS[@]}" \
+                2>&1 >/dev/tty)
+
+clear
+case $CHOICE in
+        A)
+            bash /opt/plexguide/scripts/docker-no/rclone-un.sh
+              echo "RClone - You Chose the Unencrypted Method" > /tmp/pushover
+              ansible-playbook /opt/plexguide/ansible/plexguide.yml --tags pushover &>/dev/null &
+              ansible-playbook /opt/plexguide/ansible/plexguide.yml --tags clean &>/dev/null &
+              ;;
+        B)
+              ;;
+        Z)
+            clear
+            exit 0 ;;
+esac
