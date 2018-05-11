275d7aa93c812fa25863c87682f97ca1dc16a3b2
Admin0705 <user@dunn.cloud>
2018-05-10 23:29:22 -0400

    UPDATE

--- menus/localmain.sh
+++ menus/localmain.sh
@@ -95,7 +95,7 @@ file="/usr/bin/mergerfs"
     else
 dialog --infobox "Installing MergerFS (Please Wait!)" 3 50
 wget "https://github.com/trapexit/mergerfs/releases/download/2.24.2/mergerfs_2.24.2.ubuntu-xenial_amd64.deb" 1>/dev/null 2>&1
-apt-get install g++ pkg-config git git-buildpackage pandoc debhelper libfuse-dev libattr1-dev -y 1>/dev/null 2>&1
+apt-get install g++ pkg-config git git-buildpackage pandoc debhelper libfuse-dev libattr1-dev -y
 git clone https://github.com/trapexit/mergerfs.git 1>/dev/null 2>&1
 cd mergerfs
 make clean 1>/dev/null 2>&1
