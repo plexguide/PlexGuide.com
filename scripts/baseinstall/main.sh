c20aac50707b8d715639d77894e557e21352a597
Admin0705 <user@dunn.cloud>
2018-05-12 07:11:29 -0400

    update

--- scripts/baseinstall/main.sh
+++ scripts/baseinstall/main.sh
@@ -70,12 +70,10 @@ yes | apt-get update 1>/dev/null 2>&1
 
 echo "12" | dialog --gauge "Installing: Software Properties Common" 7 50 0
 yes | apt-get install software-properties-common 1>/dev/null 2>&1
-sleep 1
 
 echo "18" | dialog --gauge "Enabling System Health Monitoring" 7 50 0
 yes | apt-get install sysstat nmon 1>/dev/null 2>&1
 sed -i 's/false/true/g' /etc/default/sysstat 1>/dev/null 2>&1
-sleep 1
 
 ############################################################ Enables Use of ROLES AfterWards
 echo "22" | dialog --gauge "Installing: Ansible Playbook" 7 50 0
@@ -119,7 +117,7 @@ docker_var=$( cat /var/plexguide/docker.version )
 if [ "$docker_var" == "18.03.1-ce," ]
 then
   echo "50" | dialog --gauge "Docker Is Already Installed" 7 50 0
-  sleep 2
+  sleep 1
   #read -n 1 -s -r -p "Press any key to continue "
 else
 
@@ -157,15 +155,15 @@ file="/usr/bin/docker" 1>/dev/null 2>&1
 echo "75" | dialog --gauge "Installing: RClone & Services" 7 50 0
 bash /opt/plexguide/scripts/startup/rclone-preinstall.sh &>/dev/null &
 touch /var/plexguide/basics.yes &>/dev/null &
-sleep 1
+#sleep 1
 
 echo "77" | dialog --gauge "Installing: Auto-Delete" 7 50 0
 bash /opt/plexguide/scripts/autodelete/install.sh &>/dev/null &
-sleep 1
+#sleep 1
 
 echo "79" | dialog --gauge "Installing: Portainer" 7 50 0
 ansible-playbook /opt/plexguide/ansible/plexguide.yml --tags portainer &>/dev/null &
-sleep 1
+#sleep 1
 echo "Portainer Installed - Goto Port 9000 and Set Your Password!" > /tmp/pushover
 ansible-playbook /opt/plexguide/ansible/plexguide.yml --tags pushover &>/dev/null &
 
@@ -177,20 +175,19 @@ ansible-playbook /opt/plexguide/ansible/plexguide.yml --tags dockerfix 1>/dev/nu
 echo "85" | dialog --gauge "Forcing Reboot of Existing Containers!" 7 50 0
 bash /opt/plexguide/scripts/containers/reboot.sh &>/dev/null &
 #read -n 1 -s -r -p "Press any key to continue "
-sleep 2
+#sleep 2
 
 echo "88" | dialog --gauge "Installing: WatchTower" 7 50 0
-##### Traefik Process
 file="/var/plexguide/watchtower.yes"
 if [ -e "$file" ]
     then
       ansible-playbook /opt/plexguide/ansible/plexguide.yml --tags watchtower &>/dev/null &
-      sleep 2
+      #sleep 1
     else
       bash /opt/plexguide/menus/watchtower/main.sh
 fi
 
-echo "91" | dialog --gauge "Installing: Python Support" 7 50 0
+echo "94" | dialog --gauge "Installing: Python Support" 7 50 0
 bash /opt/plexguide/scripts/baseinstall/python.sh 1>/dev/null 2>&1
 
 ##### Traefik Process
@@ -204,21 +201,20 @@ if [ -e "$file" ]
       touch /var/plexguide/base.domain
 fi
 
-file="/var/plexguide/base.hd"
-if [ -e "$file" ]
-    then
-      echo "" 1>/dev/null 2>&1
-    else
-      dialog --title "2nd Harddrive - One Time Message" --msgbox "\nYou will be asked if you want to setup a second harddrive.\n\nOnly SETUP if you have your harddrive is formatted and ready to go! If you have one and it's not ready, you can visit the SETTINGS to set this up anytime!" 0 0
-      echo "94" | dialog --gauge "Setting Up: 2nd Hard Drive Question" 7 50 0
-      ansible-playbook /opt/plexguide/ansible/plexguide.yml --tags watchtower &>/dev/null &
-      sleep 2
-      bash /opt/plexguide/scripts/baseinstall/harddrive.sh
-      echo "null" > /var/plexguide/base.hd
-fi
+#file="/var/plexguide/base.hd"
+#if [ -e "$file" ]
+#    then
+#      echo "" 1>/dev/null 2>&1
+#    else
+#      dialog --title "2nd Harddrive - One Time Message" --msgbox "\nYou will be asked if you want to setup a second harddrive.\n\nOnly SETUP if you have your harddrive is formatted and ready to go! If you have one and it's not ready, you can visit the SETTINGS to set this up anytime!" 0 0
+#      echo "94" | dialog --gauge "Setting Up: 2nd Hard Drive Question" 7 50 0
+#      sleep 2
+#      bash /opt/plexguide/scripts/baseinstall/harddrive.sh
+#      echo "null" > /var/plexguide/base.hd
+#fi
 
 echo "99" | dialog --gauge "Donation Question" 7 50 0
-sleep 2
+sleep 1
 
   file="/var/plexguide/donation.yes"
   if [ -e "$file" ]
