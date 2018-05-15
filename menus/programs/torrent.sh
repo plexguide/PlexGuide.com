09111bd9bbd52ed37a3615881f9c4ae5e6eb85d7
The Creator <nb3slayer@gmail.com>
2018-05-15 21:46:32 +0200

    g

--- menus/programs/torrent.sh
+++ menus/programs/torrent.sh
@@ -17,9 +17,9 @@
 #################################################################################
 export NCURSES_NO_UTF8_ACS=1
 
- HEIGHT=11
+ HEIGHT=12
  WIDTH=38
- CHOICE_HEIGHT=5
+ CHOICE_HEIGHT=6
  BACKTITLE="Visit PlexGuide.com - Automations Made Simple"
  TITLE="Applications - Torrent Programs"
 
