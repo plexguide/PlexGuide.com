8e05edde0b69cb1fef83aeb6adce2a577d9b81c3
Admin0705 <user@dunn.cloud>
2018-05-23 06:58:43 -0400

    update

--- menus/gce.sh
+++ menus/gce.sh
@@ -22,21 +22,22 @@ bash /opt/plexguide/menus/gce/gcechecker.sh
 edition=$( cat /var/plexguide/pg.edition ) 1>/dev/null 2>&1
 version=$( cat /var/plexguide/pg.version ) 1>/dev/null 2>&1
 
-HEIGHT=16
+HEIGHT=17
 WIDTH=40
-CHOICE_HEIGHT=10
+CHOICE_HEIGHT=11
 BACKTITLE="Visit PlexGuide.com - Automations Made Simple"
 TITLE="$edition - $version"
 
-OPTIONS=(A "PG RClone Cache"
-         B "PG Programs"
+OPTIONS=(A "Install RClone"
+         B "Deploy PG Drive"
          C "PG SuperTransfer2"
-         D "PG Server NET Benchmarks"
-         E "PG Trek"
-         F "PG Troubleshooting Actions"
-         G "PG Backup & Restore"
-         H "PG Updates"
-         I "PG Edition Switch"
+         D "PG Programs"
+         E "PG Server NET Benchmarks"
+         F "PG Trek"
+         G "PG Troubleshooting Actions"
+         H "PG Backup & Restore"
+         I "PG Updates"
+         J "PG Edition Switch"
          Z "Exit")
 
 CHOICE=$(dialog --backtitle "$BACKTITLE" \
@@ -47,29 +48,65 @@ CHOICE=$(dialog --backtitle "$BACKTITLE" \
                 2>&1 >/dev/tty)
 case $CHOICE in
         A)
-            bash /opt/plexguide/menus/rclone/old-or-new.sh ;;
+clear
+curl https://rclone.org/install.sh | sudo bash -s beta
+sleep 1
+dialog --title "RClone Status" --msgbox "\nThe LATEST RCLONE Beta is now Installed!" 0 0
+
+# allows others to access fuse
+tee "/etc/fuse.conf" > /dev/null <<EOF
+# /etc/fuse.conf - Configuration file for Filesystem in Userspace (FUSE)
+# Set the maximum number of FUSE mounts allowed to non-root users.
+# The default is 1000.
+#mount_max = 1000
+# Allow non-root users to specify the allow_other or allow_root mount options.
+user_allow_other
+EOF
+
+            chown 1000:1000 /usr/bin/rclone 1>/dev/null 2>&1
+            chmod 755 /usr/bin/rclone 1>/dev/null 2>&1
+            clear
+            rclone config
+            touch /mnt/gdrive/plexguide/
+            ;;
         B)
-            bash /opt/plexguide/menus/gce/programs.sh ;;
-        C)
+            clear
+            #### RClone Missing Warning
+            file="/usr/bin/rclone" 1>/dev/null 2>&1
+              if [ -e "$file" ]
+                then
+                  echo "" 1>/dev/null 2>&1
+                else
+                  dialog --title "WARNING!" --msgbox "\nYou Need to Install RClone First!" 0 0
+                  bash /opt/plexguide/menus/pgdrive/main.sh
+                  exit
+              fi
+
+            ansible-playbook /opt/plexguide/ansible/plexguide.yml --tags gce #1>/dev/null 2>&1
+            dialog --title "NOTE" --msgbox "\nPG Drive Deployed!!" 0 0
+            ;;
+        D)    
         clear
         bash /opt/plexguide/scripts/supertransfer/config.sh
         ansible-playbook /opt/plexguide/ansible/plexguide.yml --tags supertransfer2
         journalctl -f -u supertransfer2
             ;;
-        D)
-            bash /opt/plexguide/menus/benchmark/main.sh ;;
+        C)
+
         E)
-            bash /opt/plexguide/menus/info-tshoot/info.sh ;;
+            bash /opt/plexguide/menus/benchmark/main.sh ;;
         F)
+            bash /opt/plexguide/menus/info-tshoot/info.sh ;;
+        G)
             bash /opt/plexguide/menus/info-tshoot/tshoot.sh ;;
 
-        G)
-            bash /opt/plexguide/menus/backup-restore/main.sh ;;
         H)
+            bash /opt/plexguide/menus/backup-restore/main.sh ;;
+        I)
             bash /opt/plexguide/scripts/upgrade/main.sh
             bash /opt/plexguide/scripts/message/ending.sh
             exit 0 ;;
-        I)
+        J)
             bash /opt/plexguide/scripts/baseinstall/edition.sh
             ;;
         Z)
