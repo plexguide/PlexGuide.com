c2a16ff542f2fed4389f815b80cd5c20b86e34c8
Admin0705 <user@dunn.cloud>
2018-05-26 12:23:42 -0400

    update

--- menus/mount/unencrypted.sh
+++ menus/mount/unencrypted.sh
@@ -176,7 +176,7 @@ EOF
               journalctl -f -u supertransfer2       
             fi
             #### DEPLOY a TRANSFER SYSTEM - END
-            bash /opt/plexguide/menus/mount/unencrypted.sh
+
             ;;
 
         E)
