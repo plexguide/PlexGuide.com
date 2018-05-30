2621c1829e10ec0074832b885d1194519e7e7a3b
omg <omg@orionsbelt.space>
2018-05-30 10:43:39 -0800

    Formatting

--- scripts/supertransfer/auto-rename-my-keys.sh
+++ scripts/supertransfer/auto-rename-my-keys.sh
@@ -13,27 +13,29 @@
 #
 # cd /opt/appdata/plexguide/supertransfer
 # ./auto-rename-my-keys.sh
+_Main() {
+  [[ ! $(ls | egrep .\.json$) ]] && echo no json keys found in this dir && exit 1
 
-[[ ! $(ls | egrep .\.json$ ) ]] && echo no json keys found in this dir && exit 1
+  read -p 'would you like to do a dry run?' answer
+  [[ $answer =~ [y|Y] ]] && dryrun=1
 
-read -p 'would you like to do a dry run?' answer
-[[ $answer =~ [y|Y] ]] && dryrun=1
+  mkdir processed_keys
+  for file in *.json; do
+    rename=$(egrep client_email $file | cut -f4 -d'"' | cut -c1-6 | sed 's/-//g')
+    if [[ -n $dryrun ]]; then
+      echo $file will be renamed to ${rename}.json
+    else
+      if [[ $(pwd) == '/opt/appdata/plexguide/supertransfer/' ]]; then
+        mv $file ${rename}.json -v
+      else
+        mv $file processed_keys/${rename}.json -v
+        tar -cvf processed_keys.tar processed_keys
+        echo "Processed keys ready. To extract: (optional)"
+        echo "tar -xvf processed_keys.tar "
+      fi
+    fi
+  done
 
-mkdir processed_keys
-for file in *.json ; do
- rename=$(egrep client_email $file | cut -f4 -d'"' | cut -c1-6 | sed 's/-//g')
- if [[ -n $dryrun ]]; then
-    echo $file will be renamed to ${rename}.json
- else
-   if [[ $(pwd) == '/opt/appdata/plexguide/supertransfer/' ]]; then
-    mv $file ${rename}.json -v
-   else
-    mv $file processed_keys/${rename}.json -v
-    tar -cvf processed_keys.tar processed_keys
-    echo "Processed keys ready. To extract: (optional)"
-    echo "tar -xvf processed_keys.tar "
-  fi
- fi
-done
-
-exit 0
+  exit 0
+}
+_Main
