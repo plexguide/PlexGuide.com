8d349ce8412dfa961e3924d23cff7d3306c7edba
Admin9705 <user@dunn.cloud>
2018-05-31 07:02:28 -0400

    update

--- menus/version/main.sh
+++ menus/version/main.sh
@@ -26,7 +26,8 @@ TITLE="Select A PlexGuide Version"
 MENU="Make a Selection:"
 
 OPTIONS=(A "Developer: 5.082"
-         B "Patch 1  : 5.081"
+         B "Beta 1   : 5.082"
+         B "Stable P1: 5.081"
          C "Stable   : 5.080"
          D "Stable   : 5.079"
          E "Stable   : 5.078"
@@ -75,14 +76,17 @@ case $CHOICE in
             exit 0 ;;
         B)
             touch /var/plexguide/ask.yes 1>/dev/null 2>&1
-            version="5.081p1" ;; 
+            version="5.082b1" ;; 
         C)
             touch /var/plexguide/ask.yes 1>/dev/null 2>&1
-            version="5.080" ;;
+            version="5.081p1" ;; 
         D)
             touch /var/plexguide/ask.yes 1>/dev/null 2>&1
-            version="5.079" ;; 
+            version="5.080" ;;
         E)
+            touch /var/plexguide/ask.yes 1>/dev/null 2>&1
+            version="5.079" ;; 
+        F)
             touch /var/plexguide/ask.yes 1>/dev/null 2>&1
             version="5.078" ;;         
         Z)
