90e73adea5f8749f8052b44195ee433e3c6cd1e7
Jacob Nielsen <jnillern@gmail.com>
2018-06-05 23:01:32 +0200

    Update rcloneupload.sh

--- scripts/supertransfer/rcloneupload.sh
+++ scripts/supertransfer/rcloneupload.sh
@@ -61,11 +61,14 @@ rclone_upload() {
   local secs=$(( $(date +%s) - $t1 ))
   if [[ $rclone_fin_flag == 1 ]]; then
     printf " [ OK ] $gdsaLeast\tFinished: "${localFile#"$localDir"}" in %dh:%dm:%ds\n" $(($secs/3600)) $(($secs%3600/60)) $(($secs%60))
-    sleep 5
-    rclone rc vfs/forget file=/${localFile#"$localDir"} &>/dev/null
-    printf " [ OK ] Refreshed Cache for "${localFile#"$localDir"}"\n"
     sleep 10
-    [[ -n $(ls "${localFile}") ]] && sleep 45  # sleep so files are deleted off disk before resuming; good for TV episodes
+    if [ -d "${localFile}" ]; then
+      sleep 45  # sleep so files are deleted off disk before resuming; good for TV episodes
+    else
+      rclone rc vfs/forget file=/${localFile#"$localDir"} &>/dev/null
+      printf " [ OK ] Refreshed Cache for "${localFile}"\n"
+      sleep 5
+    fi
   else
     printf " [FAIL] $gdsaLeast\tUPLOAD FAILED: "${localFile}" in %dh:%dm:%ds\n" $(($secs/3600)) $(($secs%3600/60)) $(($secs%60))
     cat $logfile >> /tmp/rclonefail.log
