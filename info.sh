9496da43a400e167b7215a4ad6be4b787d2282f1
Admin0705 <user@dunn.cloud>
2018-06-05 21:27:44 -0400

    update

--- info.sh
+++ info.sh
@@ -19,8 +19,8 @@ echo "5.084" > /var/plexguide/pg.version
 echo "72" > /var/plexguide/pg.preinstall
 
 #### docker
-echo "18.03.0" > /var/plexguide/pg.docker16
-
+echo "18.03.1" > /var/plexguide/pg.docker16
+echo "18.03.1~ce~3-0~ubuntu" > /var/plexguide/pg.docker18.full
 
 echo "18.05.0" > /var/plexguide/pg.docker18
 echo "18.05.0~ce~3-0~ubuntu" > /var/plexguide/pg.docker18.full
