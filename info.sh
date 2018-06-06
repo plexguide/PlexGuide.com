fa7ef4959477098d3caaf9845fc554a05f821d44
Admin0705 <user@dunn.cloud>
2018-06-06 00:10:46 -0400

    update

--- info.sh
+++ info.sh
@@ -28,3 +28,5 @@ echo "18.05.0" > /var/plexguide/pg.docker18
 echo "18.05.0~ce~3-0~ubuntu" > /var/plexguide/pg.docker18.full
 echo "edge" > /var/plexguide/pg.docker18.edition
 #### DOCKER END
+
+echo "1" > /var/plexguide/pg.ansible
