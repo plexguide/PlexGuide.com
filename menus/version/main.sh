38a395f7995798d399a0bfdcba43de8e33b8f1e5
Admin0705 <user@dunn.cloud>
2018-06-07 08:35:19 -0400

    update

--- menus/version/main.sh
+++ menus/version/main.sh
@@ -78,7 +78,7 @@ case $CHOICE in
             exit 0 ;;
         B)
             touch /var/plexguide/ask.yes 1>/dev/null 2>&1
-            version="5.086b1" ;; 
+            version="5.086b2" ;; 
         C)
             touch /var/plexguide/ask.yes 1>/dev/null 2>&1
             version="5.085u1" ;; 
