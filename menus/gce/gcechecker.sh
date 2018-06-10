e065c707e5a86812d7bafa447c1ad6b0863a4607
Admin0705 <user@dunn.cloud>
2018-06-09 21:33:44 -0400

    update

--- menus/gce/gcechecker.sh
+++ menus/gce/gcechecker.sh
@@ -44,11 +44,13 @@ if [ "$deploy" == "yes" ] && [ "$drop" != "yes" ]
       chmod a+w /mnt 1>/dev/null 2>&1
       echo UUID=`blkid | grep nvme0n1 | cut -f2 -d'"'` /mnt ext4 discard,defaults,nobarrier,nofail 0 2 | tee -a /etc/fstab
 
-      mv /mnt/move /nvme/move 1>/dev/null 2>&1
-      ln -s /nvme/move /mnt 1>/dev/null 2>&1
+      mkdir /mnt/move 1>/dev/null 2>&1
+      chmod 0755 /mnt/move 1>/dev/null 2>&1
+      #mv /mnt/move /nvme/move 1>/dev/null 2>&1
+      #ln -s /nvme/move /mnt 1>/dev/null 2>&1
 
       chown -R 1000:1000 /mnt 1>/dev/null 2>&1
-      chown -R 1000:1000 /nvme2 1>/dev/null 2>&1
+      chown -R 1000:1000 /mnt/move 1>/dev/null 2>&1
 
       echo "10" | dialog --gauge "Deploying Sonarr" 7 50 0
       echo "linuxserver/sonarr" > /var/plexguide/image.sonarr
