152ffa62c17d0773c62cad87ef177b2c9f7296df
Admin9705 <24727006+Admin9705@users.noreply.github.com>
2018-06-12 04:54:54 -0700

    update

--- menus/backup-restore/backup.sh
+++ menus/backup-restore/backup.sh
@@ -15,7 +15,7 @@
 #   under the GPL along with build & install instructions.
 #
 #################################################################################
-
+echo "INFO - Backup Menu Selected" > /var/plexguide/pg.log && bash /opt/plexguide/scripts/log.sh
 export NCURSES_NO_UTF8_ACS=1
 
 HEIGHT=19
@@ -121,6 +121,7 @@ case $CHOICE in
         28)
             echo "qbittorrent" > /tmp/program_var ;;
         Z)
+            echo "INFO - Backup Menu Exited" > /var/plexguide/pg.log && bash /opt/plexguide/scripts/log.sh
             clear
             exit 0 ;;
 
