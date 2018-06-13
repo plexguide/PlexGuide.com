d06e963d6514ce965c5169c094f2ac6aaa133531
Admin0705 <user@dunn.cloud>
2018-06-13 12:34:58 -0400

    updates

--- info.sh
+++ info.sh
@@ -16,7 +16,7 @@
 #
 #################################################################################
 #### PG VARIBLES
-echo "5.090 BETA 1" > /var/plexguide/pg.version
+echo "5.090 BETA 2" > /var/plexguide/pg.version
 echo "81" > /var/plexguide/pg.preinstall
 
 #### DOCKER START
