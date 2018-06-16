8b2ec37d008e0f310ed2b9b25e630a6b7a48897e
Admin0705 <user@dunn.cloud>
2018-06-16 09:22:58 -0400

    update

--- ansible/roles/pgdrive/templates/ufsmonitor.sh
+++ ansible/roles/pgdrive/templates/ufsmonitor.sh
@@ -23,43 +23,43 @@ sleep 3
 while true
 do
 
-file="/mnt/unionfs/plexguide/pgchecker.bin"
-  if [ -e "$file" ]
-    then
-  echo "" 1>/dev/null 2>&1
-  echo 'INFO - UNIONFS: Up & Running' > /var/plexguide/pg.log && bash /opt/plexguide/scripts/log.sh
-    else
-    echo 'FAILURE - UNIONFS FAILED - PGChecker.bin missing!' > /var/plexguide/pg.log && bash /opt/plexguide/scripts/log.sh
-    docker stop plex &>/dev/null &
-    docker stop sonarr &>/dev/null &
-    docker stop radarr &>/dev/null &
-    docker stop sonarr4k &>/dev/null &
-    docker stop radarr4k&>/dev/null &
-    echo 'WARNING - Plex, Radarr & Sonarr were shutdown' > /var/plexguide/pg.log && bash /opt/plexguide/scripts/log.sh
-    echo 'INFO - Redeploying PGDrive Service To Assist' > /var/plexguide/pg.log && bash /opt/plexguide/scripts/log.sh
-    ansible-playbook /opt/plexguide/ansible/plexguide.yml --tags gdrive 1>/dev/null 2>&1
-    ansible-playbook /opt/plexguide/ansible/plexguide.yml --tags tdrive 1>/dev/null 2>&1
-    ansible-playbook /opt/plexguide/ansible/plexguide.yml --tags unionfs 1>/dev/null 2>&1
-    echo 'INFO - PGDrive ReDeployment Complete' > /var/plexguide/pg.log && bash /opt/plexguide/scripts/log.sh
-    echo 'INFO - Plex, Raddar & Sonarr will be Redeployed in 30 seconds' > /var/plexguide/pg.log && bash /opt/plexguide/scripts/log.sh
-    sleep 30
-    #### checking for pgchecker.bin again; if fails, exit script and warn user in PGLog; requires restart or T-Shoot / Warn User in STARTUP
-		file="/mnt/unionfs/plexguide/pgchecker.bin" 1>/dev/null 2>&1
-		  if [ -e "$file" ]
-		    then
-			    docker restart plex &>/dev/null &
-			    docker restart sonarr &>/dev/null &
-			    docker restart radarr &>/dev/null &
-			    docker restart sonarr4k &>/dev/null &
-			    docker restart radarr4k&>/dev/null &
-			    echo 'INFO - Plex, Raddar & Sonarr were Deployed!' > /var/plexguide/pg.log && bash /opt/plexguide/scripts/log.sh
-			  else 
-             	echo 'FAILURE - UNIONFS FAILED! You must restart and T-Shoot Your System' > /var/plexguide/pg.log && bash /opt/plexguide/scripts/log.sh
-             	sleep 10
-             	exit
-		  fi
+  file="/mnt/unionfs/plexguide/pgchecker.bin"
+      if [ -e "$file" ]
+        then
+      echo "" 1>/dev/null 2>&1
+      echo 'INFO - UNIONFS: Up & Running' > /var/plexguide/pg.log && bash /opt/plexguide/scripts/log.sh
+        else
+        echo 'FAILURE - UNIONFS FAILED - PGChecker.bin missing!' > /var/plexguide/pg.log && bash /opt/plexguide/scripts/log.sh
+        docker stop plex &>/dev/null &
+        docker stop sonarr &>/dev/null &
+        docker stop radarr &>/dev/null &
+        docker stop sonarr4k &>/dev/null &
+        docker stop radarr4k&>/dev/null &
+        echo 'WARNING - Plex, Radarr & Sonarr were shutdown' > /var/plexguide/pg.log && bash /opt/plexguide/scripts/log.sh
+        echo 'INFO - Redeploying PGDrive Service To Assist' > /var/plexguide/pg.log && bash /opt/plexguide/scripts/log.sh
+        ansible-playbook /opt/plexguide/ansible/plexguide.yml --tags gdrive 1>/dev/null 2>&1
+        ansible-playbook /opt/plexguide/ansible/plexguide.yml --tags tdrive 1>/dev/null 2>&1
+        ansible-playbook /opt/plexguide/ansible/plexguide.yml --tags unionfs 1>/dev/null 2>&1
+        echo 'INFO - PGDrive ReDeployment Complete' > /var/plexguide/pg.log && bash /opt/plexguide/scripts/log.sh
+        echo 'INFO - Plex, Raddar & Sonarr will be Redeployed in 30 seconds' > /var/plexguide/pg.log && bash /opt/plexguide/scripts/log.sh
+        sleep 30
+        #### checking for pgchecker.bin again; if fails, exit script and warn user in PGLog; requires restart or T-Shoot / Warn User in STARTUP
+  		  file="/mnt/unionfs/plexguide/pgchecker.bin" 1>/dev/null 2>&1
+  		  if [ -e "$file" ]
+  		    then
+  			    docker restart plex &>/dev/null &
+  			    docker restart sonarr &>/dev/null &
+  			    docker restart radarr &>/dev/null &
+  			    docker restart sonarr4k &>/dev/null &
+  			    docker restart radarr4k&>/dev/null &
+  			    echo 'INFO - Plex, Raddar & Sonarr were Deployed!' > /var/plexguide/pg.log && bash /opt/plexguide/scripts/log.sh
+  			  else 
+            echo 'FAILURE - UNIONFS FAILED! You must restart and T-Shoot Your System' > /var/plexguide/pg.log && bash /opt/plexguide/scripts/log.sh
+            sleep 10
+            exit
+  		  fi
 
-    fi
+      fi
 
 ### For While Loop
 done
