60c41c18e84ab57864ce96f3219130b32ed8d3c6
Admin9705 <24727006+Admin9705@users.noreply.github.com>
2018-06-15 21:58:45 -0400

    update

--- info.sh
+++ info.sh
@@ -31,6 +31,7 @@ echo "edge" > /var/plexguide/pg.docker18.edition
 
 #### Insatller
 echo "1" > /var/plexguide/pg.ansible
+echo "1" > /var/plexguide/pg.commands
 echo "1" > /var/plexguide/pg.python
 echo "1" > /var/plexguide/pg.docstart
 echo "1" > /var/plexguide/pg.label
