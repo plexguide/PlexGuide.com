62302e8adecedfca5ede5e775639a9c5144eca71
Admin0705 <user@dunn.cloud>
2018-06-17 00:02:27 -0400

    update

--- scripts/baseinstall/main.sh
+++ scripts/baseinstall/main.sh
@@ -199,7 +199,7 @@ docver=$( cat /var/plexguide/ubversion )
 
   if [ "$docver" == "16.04" ]
     then
-  echo "40" | dialog --gauge "Installing: UB16 - Docker $version_recall (Please Be Patient)" 7 58 0
+  echo "40" | dialog --gauge "Installing: UB16 - Docker $version_recall16 (Please Be Patient)" 7 58 0
   echo "INFO - Installing Docker for UB16" > /var/plexguide/pg.log && bash /opt/plexguide/scripts/log.sh
   sleep 2
   clear
@@ -222,7 +222,7 @@ docver=$( cat /var/plexguide/ubversion )
 
   if [ "$docver" == "18.04" ]
     then
-  echo "40" | dialog --gauge "Installing: UB18 - $version_recall (Please Be Patient)" 7 58 0
+  echo "40" | dialog --gauge "Installing: UB18 - $version_recall18 (Please Be Patient)" 7 58 0
   echo "INFO - Installing Docker for UB18" > /var/plexguide/pg.log && bash /opt/plexguide/scripts/log.sh
   sleep 2 
   clear
