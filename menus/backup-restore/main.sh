68c81a7fc5be73d76ec36852f26eb2d31a6bbc3d
Admin0705 <user@dunn.cloud>
2018-06-18 07:04:55 -0400

    update

--- menus/backup-restore/main.sh
+++ menus/backup-restore/main.sh
@@ -20,11 +20,11 @@ recovery=$( cat /var/plexguide/restore.id )
 
 export NCURSES_NO_UTF8_ACS=1
 HEIGHT=13
-WIDTH=60
+WIDTH=52
 CHOICE_HEIGHT=6
 BACKTITLE="Visit https://PlexGuide.com - Automations Made Simple"
 TITLE="Backup & Restore Menu"
-MENU=">>> Current Recovery ID Server: $recovery"
+MENU="Current Recovery ID Server: $recovery"
 
 OPTIONS=(A "Solo Backup"
          B "Solo Restore"
