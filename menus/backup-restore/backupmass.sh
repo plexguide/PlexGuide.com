8c1f9c023c09692b53be8945660d5d4c390882bc
Admin0705 <user@dunn.cloud>
2018-06-18 18:56:46 -0400

    update

--- menus/backup-restore/backupmass.sh
+++ menus/backup-restore/backupmass.sh
@@ -37,14 +37,10 @@ d=$(date +%Y-%m-%d-%T) 1>/dev/null 2>&1
 touch /opt/appdata/plexguide/backup 1>/dev/null 2>&1
 sudo rm -r /opt/appdata/plex/trans* 1>/dev/null 2>&1
 
-mfolder="/mnt/gdrive/plexguide/backup.old/$server/backup-"
-mpath="$mfolder$d"
+mfolder="/mnt/gdrive/plexguide/backup.old/$server/backup-$d"
 
-
-
-mkdir -p /mnt/gdrive/plexguide/backup.old/$server/ 1>/dev/null 2>&1
-mkdir -p $mpath
-mv /mnt/gdrive/plexguide/backup/$server/* $mpath 1>/dev/null 2>&1
+mkdir -p $mfolder 1>/dev/null 2>&1
+mv /mnt/gdrive/plexguide/backup/$server/* $mfolder 1>/dev/null 2>&1
 
 docker ps -a --format "{{.Names}}"  > /opt/appdata/plexguide/running
 sed -i -e "/watchtower/d" /opt/appdata/plexguide/running 1>/dev/null 2>&1
