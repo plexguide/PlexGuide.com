989dbdba0696968462d93ab3f84661c5d4e2392a
Admin0705 <user@dunn.cloud>
2018-06-18 22:53:32 -0400

    update

--- menus/settings/main.sh
+++ menus/settings/main.sh
@@ -71,7 +71,7 @@ echo 'INFO - Selected: 2nd HD Interface' > /var/plexguide/pg.log && bash /opt/pl
         bash /opt/plexguide/menus/tld/main.sh ;;
     I)
 echo 'INFO - Selected: Change Server ID' > /var/plexguide/pg.log && bash /opt/plexguide/scripts/log.sh
-        bash /opt/plexguide/menus/backup-restore/server.sh
+        bash /opt/plexguide/menus/backup-restore/server.sh ;;
     Z)
         clear
         exit 0
