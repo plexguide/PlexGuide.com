4649b96ec3f6554a21ad9ef6b86f11acdf1d9680
Deiteq <fayly80@gmail.com>
2018-06-20 19:12:35 +0200

    rclone edit

--- menus/rclone/select.sh
+++ menus/rclone/select.sh
@@ -30,7 +30,7 @@ case $CHOICE in
         B)
             # remove all rclone related services
             ansible-playbook /opt/plexguide/scripts/test/check-remove/tasks/main-pd.yml 
-            bash /opt/plexguide/scripts/docker-no/rclone-en.sh &>/dev/null &
+            bash /opt/plexguide/scripts/docker-no/rclone-en.sh 
 #            bash /opt/plexguide/scripts/docker-no/rclone-en2.sh
  #             ansible-playbook /opt/plexguide/ansible/plexguide.yml --tags clean-encrypt &>/dev/null &
               ;;
