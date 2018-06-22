0f3924c814f976a7ff8672fc8d6f92782242fff0
Admin0705 <user@dunn.cloud>
2018-06-22 05:05:46 -0400

    update

--- menus/wordpress/deploywp.sh
+++ menus/wordpress/deploywp.sh
@@ -68,7 +68,8 @@ fi
       exit
   fi
 
-if [ "$port" -ge 100 -a "$port" -le 999 ]; then
+if [ "$port" -ge 100 -a "$port" -le 999 ]
+  then
 else
   dialog --title "Server ID Choice" --msgbox "\nYou Failed to Enter a Value between 100-999!\n\nExiting!" 0 0
 exit
