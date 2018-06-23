0ad8d526ba232b7490dee7f0cab7432ff7aff03b
Admin0705 <user@dunn.cloud>
2018-06-23 00:30:06 -0400

    update

--- menus/version/main.sh
+++ menus/version/main.sh
@@ -93,7 +93,7 @@ echo 'INFO - Selected: Exit Upgrade Menu' > /var/plexguide/pg.log && bash /opt/p
             ;;
         01)
             touch /var/plexguide/ask.yes 1>/dev/null 2>&1
-            version="5.098b1" ;;
+            version="5.098b2" ;;
         02)
             touch /var/plexguide/ask.yes 1>/dev/null 2>&1
             version="5.097" ;;
