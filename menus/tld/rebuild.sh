32adf62aac3a196b3caf323a40b672c5e588f910
Admin0705 <user@dunn.cloud>
2018-06-23 12:35:49 -0400

    update

--- menus/tld/rebuild.sh
+++ menus/tld/rebuild.sh
@@ -137,7 +137,7 @@ while read p; do
   #read -n 1 -s -r -p "Press any key to continue "
 done </tmp/wp.running
   echo 'INFO - Finished Rebuilding Containers' > /var/plexguide/pg.log && bash /opt/plexguide/scripts/log.sh
-fi
-
+else
 dialog --msgbox "Containers are Rebuilding!\n\nCheck the Top Level Domain in 1 - 3 Minutes\n\nNote: This only REBUILDS the App. If you never deployed it, make sure to do so!" 0 0
-dialog --msgbox "The Top Supported Domain:\n\n$choice!" 0 0
+fi
+dialog --msgbox "The Top Supported Domain:\n\n$choice" 0 0
