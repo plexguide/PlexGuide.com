1e9ec92a899e50a08bcf678a87e890c793db5316
Admin0705 <user@dunn.cloud>
2018-06-23 00:26:29 -0400

    update

--- menus/version/main.sh
+++ menus/version/main.sh
@@ -29,7 +29,7 @@ MENU="Make a Selection:"
 
 OPTIONS=(00 "Developer    ~ 5.098"
          Z "----- Exit Menu -----"
-         01 "Beta 1       ~ 5.098"
+         01 "Beta 2       ~ 5.098"
          02 "Stable       ~ 5.097"
          03 "Stable       ~ 5.096" 
          04 "Stable       ~ 5.093"            
