5b5cc9956367fa93c49d89509220793d6493d446
Admin0705 <user@dunn.cloud>
2018-06-23 23:29:18 -0400

    update

--- menus/wordpress/main.sh
+++ menus/wordpress/main.sh
@@ -38,7 +38,6 @@ MENU="Last Deployed WP Server: $wp"
 OPTIONS=(A "Deploy New WP Server"
          B "Backup a WP Server"
          C "Restore a WP Server"
-         D "(NOT READY) Top Level Domain Options"
          Z "Exit")
 
 CHOICE=$(dialog --clear \
@@ -61,10 +60,6 @@ echo 'INFO - Selected: Backup a WP Server' > /var/plexguide/pg.log && bash /opt/
         C)
 echo 'INFO - Selected: Restore a WP Server' > /var/plexguide/pg.log && bash /opt/plexguide/scripts/log.sh
             bash /opt/plexguide/menus/wordpress/restore.sh 
-            ;;
-        D)
-echo 'INFO - Selected: Top Level Domain Options' > /var/plexguide/pg.log && bash /opt/plexguide/scripts/log.sh
-            bash /opt/plexguide/menus/backup-restore/restoremass.sh
             ;;
         Z)
             clear
