7733d69d8f57285830ff8f7d751eaa478557018c
Admin0705 <user@dunn.cloud>
2018-06-26 00:25:15 -0400

    update

--- roles/alias/scripts/baseline.sh
+++ roles/alias/scripts/baseline.sh
@@ -12,21 +12,30 @@
 #   including (via compiler) GPL-licensed code must also be made available
 #   under the GPL along with build & install instructions.
 #
-# Purpose: Part of the Baseline Script & Install Alias Commands
+# Purpose: Install $keyword1 Commands for Baseline Install
+# Execution: bash /opt/plexguide/roles/alias/scripts/baseline.sh
 #
 #################################################################################
+$keyword1=Alias
+
+echo "INFO - Baseline Install: Start Execution of $keyword1 Script" > /var/plexguide/pg.log && bash /opt/plexguide/scripts/log.sh
+
 pg_alias=$( cat /var/plexguide/pg.alias )
 pg_alias_stored=$( cat /var/plexguide/pg.alias.stored )
 
 if [ "$pg_alias" == "$pg_alias_stored" ]
     then
-      echo "65" | dialog --gauge "Alias File Is Already Installed" 7 50 0
+      echo "65" | dialog --gauge "$keyword1 File Is Already Installed" 7 50 0
+      echo "INFO - Baseline Install: $keyword1 is Already Installed" > /var/plexguide/pg.log && bash /opt/plexguide/scripts/log.sh
       sleep 2
     else 
-      echo "65" | dialog --gauge "Installing: Alias File" 7 50 0
+      echo "65" | dialog --gauge "Installing: $keyword1 File" 7 50 0
+      echo "INFO - Baseline Install: $keyword1 is Installing" > /var/plexguide/pg.log && bash /opt/plexguide/scripts/log.sh
       sleep 2
       clear
       ansible-playbook /opt/plexguide/pg.yml --tags alias
       cat /var/plexguide/pg.alias > /var/plexguide/pg.alias.stored
       sleep 2
 fi 
+
+echo "INFO - Baseline Install: Exiting $keyword1 Script" > /var/plexguide/pg.log && bash /opt/plexguide/scripts/log.sh
