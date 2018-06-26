227df1f7f9930db519612886c82be5416e3fe0d4
Admin0705 <user@dunn.cloud>
2018-06-26 15:37:58 -0400

    uipdate

--- roles/portainer/scripts/baseline.sh
+++ roles/portainer/scripts/baseline.sh
@@ -17,7 +17,7 @@ echo "INFO - $keyword1: Start Execution of $keyword2 Script" > /var/plexguide/pg
 
 ### MAIN SCRIPT #######################################################
 echo "85" | dialog --gauge "Installing: Portainer" 7 50 0
-sleep 1
+sleep 2
 ansible-playbook /opt/plexguide/pg.yml --tags portainer &>/dev/null &
 
 ### ENDING: DECLARED VARIABLES 
