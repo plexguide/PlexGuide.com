84f7fb206e886ac5a9fcbbe830b149ea25eb02df
Admin0705 <user@dunn.cloud>
2018-06-26 01:06:10 -0400

    update

--- roles/alias/scripts/baseline.sh
+++ roles/alias/scripts/baseline.sh
@@ -1,12 +1,4 @@
 #!/bin/bash
-### Starting Declared Variables #################################################
-keyword1="Baseline Install"
-keyword2="Alias"
-pg_alias=$( cat /var/plexguide/pg.alias )
-pg_alias_stored=$( cat /var/plexguide/pg.alias.stored )
-### Starting Log ################################################################
-echo "INFO - $keyword1: Start Execution of $keyword2 Script" > /var/plexguide/pg.log && bash /opt/plexguide/scripts/log.sh
-#################################################################################
 # GitHub:   https://github.com/Admin9705/PlexGuide.com-The-Awesome-Plex-Server
 # Author:   Admin9705 & Deiteq
 # URL:      https://plexguide.com
@@ -15,7 +7,17 @@ echo "INFO - $keyword1: Start Execution of $keyword2 Script" > /var/plexguide/pg
 # Licensed under GNU General Public License v3.0 GPL-3 (Read License in File)
 #
 # Execution: bash /opt/plexguide/roles/alias/scripts/baseline.sh
-#################################################################################
+
+### STARTING DECLARED VARIABLES #######################################
+keyword1="Baseline Install"
+keyword2="Alias"
+pg_alias=$( cat /var/plexguide/pg.alias )
+pg_alias_stored=$( cat /var/plexguide/pg.alias.stored )
+
+### STARTING LOG ######################################################
+echo "INFO - $keyword1: Start Execution of $keyword2 Script" > /var/plexguide/pg.log && bash /opt/plexguide/scripts/log.sh
+
+### MAIN SCRIPT #######################################################
 if [ "$pg_alias" == "$pg_alias_stored" ]
     then
       echo "65" | dialog --gauge "$keyword2 File Is Already Installed" 7 50 0
@@ -27,9 +29,11 @@ if [ "$pg_alias" == "$pg_alias_stored" ]
       sleep 2
       clear
       ansible-playbook /opt/plexguide/pg.yml --tags alias
-      cat /var/plexguide/pg.alias > /var/plexguide/pg.alias.stored
       sleep 2
 fi 
 
-### Exiting Log Log ################################################################
+### ENDING: DECLARED VARIABLES 
+      cat /var/plexguide/pg.alias > /var/plexguide/pg.alias.stored
+
+### ENDING: FINAL LOG ##################################################
 echo "INFO - $keyword1: Exiting $keyword2 Script" > /var/plexguide/pg.log && bash /opt/plexguide/scripts/log.sh
