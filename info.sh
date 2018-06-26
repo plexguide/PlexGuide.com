9c9878ee1a34f8d5282ddc0f7eba113a0a0e4c7e
Admin9705 <24727006+Admin9705@users.noreply.github.com>
2018-06-26 10:09:19 -0400

    Update info.sh

--- info.sh
+++ info.sh
@@ -16,7 +16,7 @@
 #
 #################################################################################
 #### PG VARIBLES
-echo "5.1" > /var/plexguide/pg.version
+echo "6.00 DEV" > /var/plexguide/pg.version
 echo "89" > /var/plexguide/pg.preinstall
 
 #### DOCKER START
