5887db05178672d203bf992d0713a23627395335
Admin0705 <user@dunn.cloud>
2018-06-26 07:58:43 -0400

    update

--- menus/version/main.sh
+++ menus/version/main.sh
@@ -29,18 +29,14 @@ MENU="Make a Selection:"
 
 OPTIONS=(00 "Developer    ~ 6.000"
          Z "----- Exit Menu -----"
-         01 "Stable U3    ~ 5.099"
-         02 "Stable       ~ 5.096" 
-         03 "Stable       ~ 5.093"            
-         04 "Stable       ~ 5.087"
-         05 "Stable       ~ 5.086"
-         06 "Depreciated  ~ 5.083"
-         07 "Depreciated  ~ 5.080"
-         08 "Depreciated  ~ 5.079"
-         09 "Depreciated  ~ 5.078"
-         10 "Historical   ~ 5.048"
-         11 "Historical   ~ 5.013"
-         12 "Historical   ~ 5.003")
+         01 "Stable       ~ 5.1"
+         02 "Stable       ~ 5.099"
+         03 "Stable       ~ 5.096"             
+         04 "Depreciated  ~ 5.087"
+         05 "Historical   ~ 5.079"
+         06 "Historical   ~ 5.048"
+         07 "Historical   ~ 5.013"
+         08 "Historical   ~ 5.003")
 
 CHOICE=$(dialog --clear \
                 --backtitle "$BACKTITLE" \
@@ -92,38 +88,26 @@ echo 'INFO - Selected: Exit Upgrade Menu' > /var/plexguide/pg.log && bash /opt/p
             ;;
         01)
             touch /var/plexguide/ask.yes 1>/dev/null 2>&1
-            version="5.099" ;;
+            version="5.1" ;;
         02)
             touch /var/plexguide/ask.yes 1>/dev/null 2>&1
-            version="5.096u1" ;;
+            version="5.099" ;;
         03)
             touch /var/plexguide/ask.yes 1>/dev/null 2>&1
-            version="5.093" ;;
+            version="5.096u1" ;;
         04)
             touch /var/plexguide/ask.yes 1>/dev/null 2>&1
             version="5.087" ;;
         05)
             touch /var/plexguide/ask.yes 1>/dev/null 2>&1
-            version="5.086b2" ;;
+            version="5.079" ;;
         06)
             touch /var/plexguide/ask.yes 1>/dev/null 2>&1
-            version="5.083" ;;
+            version="5.048" ;;
         07)
             touch /var/plexguide/ask.yes 1>/dev/null 2>&1
-            version="5.080" ;;
-        08)
-            touch /var/plexguide/ask.yes 1>/dev/null 2>&1
-            version="5.079" ;;
-        09)
-            touch /var/plexguide/ask.yes 1>/dev/null 2>&1
-            version="5.078" ;;
-        10)
-            touch /var/plexguide/ask.yes 1>/dev/null 2>&1
-            version="5.048" ;;
-        11)
-            touch /var/plexguide/ask.yes 1>/dev/null 2>&1
             version="5.013" ;;
-        12)
+        08)
             touch /var/plexguide/ask.yes 1>/dev/null 2>&1
             version="5.003" ;;
 esac
