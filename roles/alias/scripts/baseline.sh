960398d270d658258c43871c3e5c4e7bfd734951
Admin0705 <user@dunn.cloud>
2018-06-26 00:30:49 -0400

    update

--- roles/alias/scripts/baseline.sh
+++ roles/alias/scripts/baseline.sh
@@ -1,36 +1,32 @@
 #!/bin/bash
-#
+### Declared Variables ##########################################################
+
+$keyword1="Baseline Install"
+$keyword2="Alias"
+
+### Starting Log ################################################################
+echo "INFO - $keyword1: Start Execution of $keyword2 Script" > /var/plexguide/pg.log && bash /opt/plexguide/scripts/log.sh
+
+#################################################################################
 # GitHub:   https://github.com/Admin9705/PlexGuide.com-The-Awesome-Plex-Server
-# Author:   Admin9705 & Deiteq & FlickerRate
+# Author:   Admin9705 & Deiteq
 # URL:      https://plexguide.com
 #
 # PlexGuide Copyright (C) 2018 PlexGuide.com
-# Licensed under GNU General Public License v3.0 GPL-3 (in short)
-#
-#   You may copy, distribute and modify the software as long as you track
-#   changes/dates in source files. Any modifications to our software
-#   including (via compiler) GPL-licensed code must also be made available
-#   under the GPL along with build & install instructions.
-#
-# Purpose: Install $keyword1 Commands for Baseline Install
-# Execution: bash /opt/plexguide/roles/alias/scripts/baseline.sh
+# Licensed under GNU General Public License v3.0 GPL-3 (Read License in File)
 #
 #################################################################################
-$keyword1=Alias
-
-echo "INFO - Baseline Install: Start Execution of $keyword1 Script" > /var/plexguide/pg.log && bash /opt/plexguide/scripts/log.sh
-
 pg_alias=$( cat /var/plexguide/pg.alias )
 pg_alias_stored=$( cat /var/plexguide/pg.alias.stored )
 
 if [ "$pg_alias" == "$pg_alias_stored" ]
     then
-      echo "65" | dialog --gauge "$keyword1 File Is Already Installed" 7 50 0
-      echo "INFO - Baseline Install: $keyword1 is Already Installed" > /var/plexguide/pg.log && bash /opt/plexguide/scripts/log.sh
+      echo "65" | dialog --gauge "$keyword2 File Is Already Installed" 7 50 0
+      echo "INFO - $keyword1: $keyword2 is Already Installed" > /var/plexguide/pg.log && bash /opt/plexguide/scripts/log.sh
       sleep 2
     else 
-      echo "65" | dialog --gauge "Installing: $keyword1 File" 7 50 0
-      echo "INFO - Baseline Install: $keyword1 is Installing" > /var/plexguide/pg.log && bash /opt/plexguide/scripts/log.sh
+      echo "65" | dialog --gauge "Installing: $keyword2 File" 7 50 0
+      echo "INFO - $keyword1: $keyword2 is Installing" > /var/plexguide/pg.log && bash /opt/plexguide/scripts/log.sh
       sleep 2
       clear
       ansible-playbook /opt/plexguide/pg.yml --tags alias
@@ -38,4 +34,5 @@ if [ "$pg_alias" == "$pg_alias_stored" ]
       sleep 2
 fi 
 
-echo "INFO - Baseline Install: Exiting $keyword1 Script" > /var/plexguide/pg.log && bash /opt/plexguide/scripts/log.sh
+### Exiting Log Log ################################################################
+echo "INFO - $keyword1: Exiting $keyword2 Script" > /var/plexguide/pg.log && bash /opt/plexguide/scripts/log.sh
