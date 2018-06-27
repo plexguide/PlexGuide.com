bc1c42874d2eae9bcfea76aa8898beec1d949910
Admin0705 <user@dunn.cloud>
2018-06-27 03:26:54 -0400

    update

--- scripts/menu.sh
+++ scripts/menu.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 export NCURSES_NO_UTF8_ACS=1
 
+### GEN STARTED
+bash /opt/plexguide/roles/baseline/scripts/gen.sh &>/dev/null &
+
 ###################### FOR VARIABLS ROLE SO DOESNT CREATE RED - START
 file="/var/plexguide"
 if [ -e "$file" ]
