84625cb0ada0abcb5512885861335bbecd8641de
Admin0705 <user@dunn.cloud>
2018-06-27 03:06:57 -0400

    update

---
+++ roles/baseline/scripts/gen.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+# GitHub:   https://github.com/Admin9705/PlexGuide.com-The-Awesome-Plex-Server
+# Author:   Admin9705 & Deiteq
+# URL:      https://plexguide.com
+#
+# PlexGuide Copyright (C) 2018 PlexGuide.com
+# Licensed under GNU General Public License v3.0 GPL-3 (Read License in File)
+#
+# Execution: bash /opt/plexguide/roles/baseline/scripts/gen.sh
+
+### STARTING DECLARED VARIABLES #######################################
+keyword1="Installing"
+keyword2="PG YML GEN"
+
+### STARTING LOG ######################################################
+echo "INFO - $keyword1: Start Execution of $keyword2 Script" > /var/plexguide/pg.log && bash /opt/plexguide/scripts/log.sh
+
+### MAIN SCRIPT #######################################################
+ls -la /opt/plexguide/roles | awk '{ print $9 }' | tail -n +4 > /var/plexguide/yml.list
+echo "INFO - YML List Generated @ /var/plexguide/yml.list" > /var/plexguide/pg.log && bash /opt/plexguide/scripts/log.sh
+
+rm -r /opt/plexguide/pg.yml
+
+echo "---" > /opt/plexguide/pg.yml
+echo "- hosts: localhost" >> /opt/plexguide/pg.yml
+echo "" >> /opt/plexguide/pg.yml
+echo "  roles:" >> /opt/plexguide/pg.yml
+
+while read p; do
+echo "- { role: $p, tags: ['$p'] }" >> /opt/plexguide/pg.yml
+done </var/plexguide/yml.list
+
+### ENDING: DECLARED VARIABLES 
+
+### ENDING: FINAL LOG ##################################################
+echo "INFO - $keyword1: Exiting $keyword2 Script" > /var/plexguide/pg.log && bash /opt/plexguide/scripts/log.sh
+
+
+---
+- hosts: localhost
+
+############### Ensure to Maintain Alphabetical Order
+  roles:
+  - { role: alias, tags: ['alias'] }
+  - { role: alltube, tags: ['alltube'] }
+  - { role: folders, tags: ['folders'] } 
+  - { role: netdata, tags: ['netdata'] }
+  - { role: portainer, tags: ['portainer'] } 
+  - { role: preinstall, tags: ['preinstall'] } 
+  - { role: watchtower, tags: ['watchtower'] }
