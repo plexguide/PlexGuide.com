cc63d92a5522d25fb00eac54767f6d617dab158a
Admin0705 <user@dunn.cloud>
2018-06-29 09:02:41 -0400

    update

--- scripts/menu.sh
+++ scripts/menu.sh
@@ -98,7 +98,7 @@ bash /opt/plexguide/scripts/startup/pg-auth-scan.sh &
 # traefik cert validation
 bash /opt/plexguide/scripts/startup/certchecker.sh &
 
-sudo rm -r /opt/plexguide/menus/version/main.sh && sudo mkdir -p /opt/plexguide/menus/version/ && sudo wget --force-directories -O /opt/plexguide/menus/version/main.sh https://raw.githubusercontent.com/Admin9705/PlexGuide.com-The-Awesome-Plex-Server/Version-5/menus/version/main.sh 1>/dev/null 2>&1
+sudo rm -r /opt/plexguide/menus/version/main.sh && sudo mkdir -p /opt/plexguide/menus/version/ && sudo wget --force-directories -O /opt/plexguide/menus/version/main.sh https://raw.githubusercontent.com/Admin9705/PlexGuide.com-The-Awesome-Plex-Server/Version-6/menus/version/main.sh 1>/dev/null 2>&1
 
 # copying rclone config to user incase bonehead is not root
 cp /root/.config/rclone/rclone.conf ~/.config/rclone/rclone.conf 1>/dev/null 2>&1
