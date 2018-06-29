bffe9a509f1c98ffae4c5d7c44e0bd36c64c3589
Admin0705 <user@dunn.cloud>
2018-06-29 17:11:53 -0400

    update

--- scripts/baseinstall/main.sh
+++ scripts/baseinstall/main.sh
@@ -135,8 +135,6 @@ if [ "$pg_ansible" == "$pg_ansible_stored" ]
       echo "INFO - Installing: Support" > /var/plexguide/pg.log && bash /opt/plexguide/scripts/log.sh
       clear
       sleep 2
-      #bash /opt/plexguide/scripts/baseinstall/python.sh
-      sleep 2
       clear
       sudo add-apt-repository ppa:ansible/ansible-2.5 -y
       apt-get update -y 
@@ -295,6 +293,21 @@ bash /opt/plexguide/scripts/containers/reboot.sh &>/dev/null &
 
 #### Install WatchTower Command - 95 Percent
 bash /opt/plexguide/roles/baseline/scripts/watchtower.sh
+
+########### Python Support
+pg_python=$( cat /var/plexguide/pg.python )
+pg_python_stored=$( cat /var/plexguide/pg.python.stored )
+
+if [ "$pg_python" == "$pg_python_stored" ]
+    then
+      echo "99" | dialog --gauge "Python Support Is Already Installed" 7 50 0
+      sleep 2
+    else 
+      echo "99" | dialog --gauge "Installing: Python Support" 7 50 0
+      bash /opt/plexguide/scripts/baseinstall/python.sh &>/dev/null &
+      cat /var/plexguide/pg.python > /var/plexguide/pg.python.stored
+      sleep 2
+fi
 
 ############################# Editions
 if [ "$edition" == "PG Edition: GCE Feed" ]
