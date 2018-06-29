c6b10326c4375f4664089d4835930b55b8f04afe
Admin0705 <user@dunn.cloud>
2018-06-29 14:02:32 -0400

    update

--- roles/baseline/scripts/gen.sh
+++ roles/baseline/scripts/gen.sh
@@ -30,15 +30,15 @@ while read p; do
 echo "  - { role: $p, tags: ['$p'] }" >> /opt/plexguide/pg.yml
 done </var/plexguide/yml.list
 
-mkdir -p /etc/ansible/inventories/ 1>/dev/null 2>&1
-echo "[local]" > /etc/ansible/inventories/local
-echo "localhost ansible_connection=local" >> /etc/ansible/inventories/local
+#mkdir -p /etc/ansible/inventories/ 1>/dev/null 2>&1
+#echo "[local]" > /etc/ansible/inventories/local
+#echo "localhost ansible_connection=local" >> /etc/ansible/inventories/local
 
 ### Reference: https://docs.ansible.com/ansible/2.4/intro_configuration.html
-echo "[defaults]" > /etc/ansible/ansible.cfg
-echo "command_warnings = False" >> /etc/ansible/ansible.cfg
-echo "callback_whitelist = profile_tasks" >> /etc/ansible/ansible.cfg
-echo "inventory = inventories/local" >> /etc/ansible/ansible.cfg
+#echo "[defaults]" > /etc/ansible/ansible.cfg
+#echo "command_warnings = False" >> /etc/ansible/ansible.cfg
+#echo "callback_whitelist = profile_tasks" >> /etc/ansible/ansible.cfg
+#echo "inventory = inventories/local" >> /etc/ansible/ansible.cfg
 ### ENDING: DECLARED VARIABLES 
 
 ### ENDING: FINAL LOG ##################################################
