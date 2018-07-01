4812c14f74108a358b7974bcf84264211d134d0b
Admin0705 <user@dunn.cloud>
2018-07-01 12:49:35 -0400

    update

--- roles/traefikdeploy/scripts/rebuild.sh
+++ roles/traefikdeploy/scripts/rebuild.sh
@@ -28,7 +28,7 @@ count=$(wc -l < /var/plexguide/container.running)
 ((count++))
 ((count--))
 
-for ((i=1; i<$count+1; i++)); do
+for ((i=1; i<$count-1; i++)); do
 	app=$(sed "${i}q;d" /var/plexguide/container.running)
 	ansible-playbook /opt/plexguide/pg.yml --tags $app --skip-tags cron
 done
