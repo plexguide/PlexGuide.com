d55797852db3751fc07c137bd25fbafb25ea7929
Admin0705 <user@dunn.cloud>
2018-07-03 08:30:54 -0400

    Add -- backup -- command if user needs to backup, but cannot normally

--- roles/info.sh
+++ roles/info.sh
@@ -17,7 +17,7 @@
 #################################################################################
 #### PG VARIBLES
 echo "6.003 EDGE" > /var/plexguide/pg.version
-echo "106" > /var/plexguide/pg.preinstall
+echo "107" > /var/plexguide/pg.preinstall
 
 #### DOCKER START
 echo "18.03.1" > /var/plexguide/pg.docker16
@@ -35,5 +35,5 @@ echo "1" > /var/plexguide/pg.python
 echo "1" > /var/plexguide/pg.docstart
 echo "2" > /var/plexguide/pg.watchtower
 echo "1" > /var/plexguide/pg.label
-echo "9" > /var/plexguide/pg.alias
+echo "10" > /var/plexguide/pg.alias
 echo "1" > /var/plexguide/pg.dep ## dependencies
