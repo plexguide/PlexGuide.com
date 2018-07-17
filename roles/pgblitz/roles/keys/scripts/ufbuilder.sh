7dca58860753e72ccb5c809c8a7c861ee83b877e
Admin0705 <user@dunn.cloud>
2018-07-17 00:46:31 -0500

    update

---
+++ roles/pgblitz/roles/keys/scripts/ufbuilder.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+#
+# [PlexGuide Menu]
+#
+# GitHub:   https://github.com/Admin9705/PlexGuide.com-The-Awesome-Plex-Server
+# Author:   Admin9705 & FlickerRate
+# URL:      https://plexguide.com
+#
+# PlexGuide Copyright (C) 2018 PlexGuide.com
+# Licensed under GNU General Public License v3.0 GPL-3 (in short)
+#
+#   You may copy, distribute and modify the software as long as you track
+#   changes/dates in source files. Any modifications to our software
+#   including (via compiler) GPL-licensed code must also be made available
+#   under the GPL along with build & install instructions.
+#
+#################################################################################
+
+#build a service out of this
+#Remake another for PGBlitz
+#clear && ansible-playbook /opt/plexguide/pg.yml --tags cloudst2 --skip-tags cron
+#build unionfs additon build script
+
+path=/opt/appdata/pgblitz/keys
+rpath=/root/.config/rclone/rclone.conf
+
+ls -la $path/processed | awk '{ print $9}' | tail -n +4 > /tmp/pg.gdsa.ufs
+
+while read p; do
+echo "/mnt/pgblitz/$p=RO;"> /tmp/pg.gdsa.build
+done </tmp/pg.gdsa.ufs
+cat /tmp/pg.gdsa.build
