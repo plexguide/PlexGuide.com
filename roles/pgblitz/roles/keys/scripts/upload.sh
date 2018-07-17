08322beb6dd5e0d36a8ca5567af9d9f981d58fa4
Admin0705 <user@dunn.cloud>
2018-07-16 23:58:34 -0500

    update

--- roles/pgblitz/roles/keys/scripts/upload.sh
+++ roles/pgblitz/roles/keys/scripts/upload.sh
@@ -25,11 +25,12 @@ rpath=/root/.config/rclone/rclone.conf
 ls -la $path/processed | awk '{ print $9}' | tail -n +4 > /tmp/pg.gdsalist
 
 while read p; do
+  echo 'INFO - PGBlitz: Using GDSA $p for transfer' > /var/plexguide/pg.log && bash /opt/plexguide/roles/log/log.sh
 
   mkdir -p /mnt/pgblitz/$p
-  touch /mnt/pgblitz/$p/dog.txt
   mv /mnt/move/* /mnt/pgblitz/$p
-
+  echo 'INFO - PGBlitz: Moved Items /mnt/move to /mnt/pgblitz/$p' > /var/plexguide/pg.log && bash /opt/plexguide/roles/log/log.sh
+  echo 'INFO - PGBlitz: Starting PGBlitz Transfer Using $p' > /var/plexguide/pg.log && bash /opt/plexguide/roles/log/log.sh
   ls -la /mnt/pgblitz/$p
   echo "sleep 3"
   rclone move --tpslimit 6 --checkers=20 \
@@ -39,8 +40,8 @@ while read p; do
     --exclude=".unionfs-fuse/**" --exclude=".unionfs/**" \
     --drive-chunk-size=32M \
     /mnt/pgblitz/$p $p:
-
+    rm -r /mnt/pgblitz/$p
     echo "$p - GDSA"
-    echo ""
+    echo 'INFO - PGBlitz: $p - Transfer Complete' > /var/plexguide/pg.log && bash /opt/plexguide/roles/log/log.sh
     sleep 10
 done </tmp/pg.gdsa
