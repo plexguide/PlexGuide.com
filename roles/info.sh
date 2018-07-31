7398f95b7bba787804bd9af023cb9bd33100c348
Admin0705 <user@dunn.cloud>
2018-07-31 08:27:59 -0400

    update

--- roles/info.sh
+++ roles/info.sh
@@ -16,7 +16,7 @@
 #
 #################################################################################
 #### PG VARIBLES
-echo "6.023" > /var/plexguide/pg.version
+echo "6.023 Update 2" > /var/plexguide/pg.version
 echo "133" > /var/plexguide/pg.preinstall
 
 #### Installer
