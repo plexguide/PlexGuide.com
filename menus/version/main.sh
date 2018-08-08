9a074684fcd34335a8f498f28f76d9c3874c35fa
Admin0705 <user@dunn.cloud>
2018-08-08 07:56:01 -0400

    update

--- menus/version/main.sh
+++ menus/version/main.sh
@@ -28,12 +28,12 @@ MENU="Make a Selection:"
 
 OPTIONS=(Z "----- Exit Menu -----"
          01 "EDGE         ~ 6.028"
-         01 "Release      ~ 6.027"
-         02 "Release      ~ 6.026"
-         03 "Release      ~ 6.023"
-         04 "Release      ~ 6.016"
-         05 "Release      ~ 6.015"
-         06 "Internal Bug Test")
+         02 "Release      ~ 6.027"
+         03 "Release      ~ 6.026"
+         04 "Release      ~ 6.023"
+         05 "Release      ~ 6.016"
+         06 "Release      ~ 6.015"
+         07 "Internal Bug Test")
 
 CHOICE=$(dialog --clear \
                 --backtitle "$BACKTITLE" \
@@ -70,11 +70,14 @@ case $CHOICE in
     exit ;;
   02)
     touch /var/plexguide/ask.yes 1>/dev/null 2>&1
-    version="6.026" ;;
+    version="6.027" ;;
   03)
     touch /var/plexguide/ask.yes 1>/dev/null 2>&1
-    version="6.023" ;;
+    version="6.026" ;;
   04)
+    touch /var/plexguide/ask.yes 1>/dev/null 2>&1
+    version="6.023" ;;
+  05)
     touch /var/plexguide/ask.yes 1>/dev/null 2>&1
     version="6.016" ;;
   05)
