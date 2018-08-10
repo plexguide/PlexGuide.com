09ae0a3ff8124a008468de793f3dd1e6f906a490
Admin0705 <user@dunn.cloud>
2018-08-10 09:05:30 -0400

    update

--- roles/backup-restore-nav/main.sh
+++ roles/backup-restore-nav/main.sh
@@ -44,7 +44,7 @@ CHOICE=$(dialog --clear \
 clear
 case $CHOICE in
   A)
-    bash /opt/plexguide/menus/backup/solo.sh ;;
+    bash /opt/plexguide/menus/backup/scripts/solo.sh ;;
   B)
     bash /opt/plexguide/menus/backup-restore/restore.sh ;;
   C)
