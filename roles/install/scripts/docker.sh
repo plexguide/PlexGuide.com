db11e1b36e8c4b155c267a64bd670c0db040ef77
Admin0705 <user@dunn.cloud>
2018-08-12 17:10:07 -0400

    update

--- roles/install/scripts/docker.sh
+++ roles/install/scripts/docker.sh
@@ -30,6 +30,37 @@ if [ "$pg_docker" == "$pg_docker_stored" ]; then
       sleep 2
       echo ""
       ansible-playbook /opt/plexguide/pg.yml --tags docker
+
+      #### Checks if Docker Is Present; Fails attempts to Install Old School
+      file="/usr/bin/docker" 1>/dev/null 2>&1
+        if [ -e "$file" ]
+          then
+              echo "" 1>/dev/null 2>&1
+          else
+            ##### Install Docker the Emergency Way
+            clear
+            echo "Installing Docker the Old School Way - (Please Be Patient)"
+            sleep 2
+            clear
+            curl -fsSL get.docker.com -o get-docker.sh
+            sh get-docker.sh
+            echo ""
+            echo "Starting Docker (Please Be Patient)"
+            sleep 2
+            systemctl start docker
+            sleep 2
+
+            ##### Checking Again, if fails again; warns user
+            file="/usr/bin/docker" 1>/dev/null 2>&1
+              if [ -e "$file" ]
+                then
+                echo "INFO - SUCCESS: Docker Failsafe Resulted in a Succesful Docker Install" > /var/plexguide/pg.log && bash /opt/plexguide/roles/log/log.sh
+              else
+                echo "INFO - FAILED: Docker Failsafe Resulted in a Failed Docker Install" > /var/plexguide/pg.log && bash /opt/plexguide/roles/log/log.sh
+                  exit
+              fi
+         fi
+
       cat /var/plexguide/pg.docker > /var/plexguide/pg.docker.stored
   fi
 ######################################################## END: Main Script
