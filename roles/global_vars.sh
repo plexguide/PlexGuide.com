73d62a2e58138cf3fe505c6b32850d7cde58d7bc
Admin0705 <user@dunn.cloud>
2018-08-12 20:40:48 -0400

    update

--- roles/global_vars.sh
+++ roles/global_vars.sh
@@ -1,6 +1,5 @@
 ## GLOBAL_VARS.SH ####################################################(START)
 ###### BE CAREFUL - WRONG FORMATTING CAN BREAK PG ###  - opt/plexguide/pg.yml
-  gather_facts: no
   vars:
     skipend: "blank"
     quescheck: "off"
