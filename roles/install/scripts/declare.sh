013d0594f291e720377224f78e8e7305a0cb79e2
Admin0705 <user@dunn.cloud>
2018-08-12 23:23:45 -0400

    update

--- roles/install/scripts/declare.sh
+++ roles/install/scripts/declare.sh
@@ -29,6 +29,13 @@ file="/var/plexguide/pg.alias.stored"
       echo "0" > $file
     fi
 
+    file="/var/plexguide/pg.edition.stored"
+      if [ -e "$file" ]; then
+        echo "" 1>/dev/null 2>&1
+      else
+        echo "0" > $file
+      fi
+
   file="/var/plexguide/pg.dependency.stored"
     if [ -e "$file" ]; then
       echo "" 1>/dev/null 2>&1
