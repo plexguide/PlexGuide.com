bc8c46f3e056b11693eee74cb5ca1d389f9080f4
Admin9705 <24727006+Admin9705@users.noreply.github.com>
2018-08-13 01:57:23 -0400

    Update pg.sh

--- pg.sh
+++ pg.sh
@@ -22,18 +22,18 @@ mkdir -p /var/plexguide
 # PG Version
 echo "6.031 BETA 1" > /var/plexguide/pg.version
 # Force Common Things To Execute Such as Folders
-echo "136" > /var/plexguide/pg.preinstall
+echo "137" > /var/plexguide/pg.preinstall
 # Changing Number Results in Forcing Portions of PreInstaller to Execute
-echo "5" > /var/plexguide/pg.ansible
-echo "2" > /var/plexguide/pg.rclone
-echo "2" > /var/plexguide/pg.python
-echo "1" > /var/plexguide/pg.docker
-echo "1" > /var/plexguide/pg.id
-echo "1" > /var/plexguide/pg.dependency
-echo "1" > /var/plexguide/pg.docstart
+echo "10" > /var/plexguide/pg.ansible
+echo "10" > /var/plexguide/pg.rclone
+echo "10" > /var/plexguide/pg.python
+echo "10" > /var/plexguide/pg.docker
+echo "10" > /var/plexguide/pg.id
+echo "10" > /var/plexguide/pg.dependency
+echo "10" > /var/plexguide/pg.docstart
 echo "2" > /var/plexguide/pg.watchtower
 echo "1" > /var/plexguide/pg.motd
-echo "31" > /var/plexguide/pg.alias
+echo "35" > /var/plexguide/pg.alias
 echo "1" > /var/plexguide/pg.dep
 echo "1" > /var/plexguide/pg.cleaner
 
