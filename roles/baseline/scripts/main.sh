c65def7733b64e9826220ba55114e1d1f5c9e87b
Admin0705 <user@dunn.cloud>
2018-08-12 20:29:15 -0400

    update

--- roles/baseline/scripts/main.sh
+++ roles/baseline/scripts/main.sh
@@ -1,20 +1,3 @@
-#!/bin/bash
-#
-# [PG BaseInstall]
-#
-# GitHub:   https://github.com/Admin9705/PlexGuide.com-The-Awesome-Plex-Server
-# Author:   Admin9705 & Deiteq & FlickerRate
-# URL:      https://plexguide.com
-#
-# PlexGuide Copyright (C) 2018 PlexGuide.com
-# Licensed under GNU General Public License v3.0 GPL-3 (in short)
-#
-#   You may copy, distribute and modify the software as long as you track
-#   changes/dates in source files. Any modifications to our software
-#   including (via compiler) GPL-licensed code must also be made available
-#   under the GPL along with build & install instructions.
-#
-#################################################################################
 echo "INFO - BaseInstall Started" > /var/plexguide/pg.log && bash /opt/plexguide/roles/log/log.sh
 edition=$( cat /var/plexguide/pg.edition )
 ############################################################ Basic Menu
@@ -57,41 +40,6 @@ if [ "$edition" == "PG Edition: GCE Feed" ]
 fi
 
 ############################################################ Starting Install Processing
-
-echo "75" | dialog --gauge "Installing: RClone & Services" 7 50 0
-sleep 2
-clear
-
-pg_rclone=$( cat /var/plexguide/pg.rclone )
-pg_rclone_stored=$( cat /var/plexguide/pg.rclone.stored )
-
-if [ "$pg_rclone" == "$pg_rclone_stored" ]; then
-      echo "75" | dialog --gauge "RClone 1.42 Is Already Installed" 7 50 0
-      sleep 2
-    else
-      echo "75" | dialog --gauge "Installing: RClone" 7 50 0
-      sleep 2
-      clear
-      ansible-playbook /opt/plexguide/pg.yml --tags rcloneinstall
-      sleep 2
-#### Alignment Note #### Have to Have It Left Aligned
-tee "/etc/fuse.conf" > /dev/null <<EOF
-# /etc/fuse.conf - Configuration file for Filesystem in Userspace (FUSE)
-# Set the maximum number of FUSE mounts allowed to non-root users.
-# The default is 1000.
-#mount_max = 1000
-# Allow non-root users to specify the allow_other or allow_root mount options.
-user_allow_other
-EOF
-
-chown 1000:1000 /usr/bin/rclone 1>/dev/null 2>&1
-chmod 755 /usr/bin/rclone 1>/dev/null 2>&1
-
-      #read -n 1 -s -r -p "Press any key to continue "
-      cat /var/plexguide/pg.rclone > /var/plexguide/pg.rclone.stored
-fi
-sleep 2
-#sleep 1
 
 echo "80" | dialog --gauge "Installing: AutoDelete & Cleaner" 7 50 0
 ansible-playbook /opt/plexguide/pg.yml --tags autodelete &>/dev/null &
