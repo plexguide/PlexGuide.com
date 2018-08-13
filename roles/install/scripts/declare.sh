8c3f74cbda958bbbdce791cfcdc088ec87a97d0f
Admin0705 <user@dunn.cloud>
2018-08-13 00:30:33 -0400

    update

--- roles/install/scripts/declare.sh
+++ roles/install/scripts/declare.sh
@@ -113,4 +113,22 @@ file="/var/plexguide/pg.python.stored"
     echo "0" > $file
   fi
 
+file="/var/plexguide/server.ports"
+  if [ -e "$file" ]
+    then
+  echo "" 1>/dev/null 2>&1
+    else
+  touch /var/plexguide/server.ports
+  echo "[OPEN]" > /var/plexguide/server.ports.status
+  fi
+
+  file="/var/plexguide/server.appguard"
+    if [ -e "$file" ]
+      then
+    echo "" 1>/dev/null 2>&1
+      else
+    touch /var/plexguide/server.appguard 1>/dev/null 2>&1
+    echo "[OFF]" > /var/plexguide/server.appguard
+    fi
+
 rm -r /var/plexguide/pg.exit 1>/dev/null 2>&1
