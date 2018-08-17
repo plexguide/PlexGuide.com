6ffa0db69dbecc81fea3c71742f4972fa494fa69
Admin0705 <user@dunn.cloud>
2018-08-16 20:02:05 -0400

    update

--- roles/versions/main.sh
+++ roles/versions/main.sh
@@ -15,52 +15,12 @@
 #   under the GPL along with build & install instructions.
 #
 #################################################################################
-one="abc"
-two="abc"
-
-while [ "$one" == "$two" ]; do
-ansible-playbook /opt/plexguide/basics.yml --tags versions
 program=$(cat /tmp/program_selection)
+running=($program)
+
+while [ "$running" != "$program" ]; do
+ansible-playbook /opt/plexguide/basics.yml --tags versions
 running=$(cat /opt/plexguide/roles/versions/scripts/ver.list | grep $program -oP )
-
-if [ "$program" == "edge" ]; then
-  ansible-playbook /opt/plexguide/pg.yml --tags pgedge
-  clear
-  exit
-fi
-
-  if [ "$program" == "edge" ]; then
-    ansible-playbook /opt/plexguide/pg.yml --tags pgedge
-    touch /var/plexguide/ask.yes 1>/dev/null 2>&1
-    echo "INFO - Selected: Upgrade to PG EDGE" > /var/plexguide/pg.log && bash /opt/plexguide/roles/log/log.sh
-    echo ""
-    bash /opt/plexguide/roles/ending/ending.sh
-    one="xyz"
-    dialog --title "--- NOTE ---" --msgbox "\nPG $program Deployed!\n\nProcess Complete!" 0 0
-    exit 0
-  else
-    touch /var/plexguide/ask.yes 1>/dev/null 2>&1
-    version="$program"
-
-    if [ "$program" == "$running" ]; then
-
-        if dialog --stdout --title "Version User Confirmation" \
-           --backtitle "Visit https://PlexGuide.com - Automations Made Simple" \
-           --yesno "\nDo Want to Install: Version - $version?" 7 50; then
-
-           touch /var/plexguide/ask.yes 1>/dev/null 2>&1
-        else
-          dialog --title "PG Update Status" --msgbox "\nExiting! User selected to NOT Install!" 0 0
-          clear
-          echo 'INFO - Selected Not To Upgrade PG' > /var/plexguide/pg.log && bash /opt/plexguide/roles/log/log.sh
-          sudo bash /opt/plexguide/roles/ending/ending.sh
-          one="xyz"
-          exit 0
-        fi
-    else
-      clear
-    fi
-fi
 
 if [ "$menu" == "update" ]; then
   echo 'INFO - Selected: PG Upgrades Menu Interface' > /var/plexguide/pg.log && bash /opt/plexguide/roles/log/log.sh
