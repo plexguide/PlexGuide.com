0dd42b9e148a118686263916e4094551093cb0c3
Admin0705 <user@dunn.cloud>
2018-08-16 21:32:42 -0400

    updaste

--- roles/versions/main.sh
+++ roles/versions/main.sh
@@ -24,6 +24,7 @@ ansible-playbook /opt/plexguide/basics.yml --tags versions
 running=$(cat /opt/plexguide/roles/versions/scripts/ver.list | grep $program -oP )
 
 if [ "$program" == "exit" ] && [ "$menu" == "on" ]; then
+dialog --title "--- NOTE ---" --msgbox "\nUser Exited PlexGuide!\n\nNo Changes Have Been Made!" 0 0
 exit
 fi
 
