7dcf6f09e5850b7d2d24b91f40942d2401452084
Admin0705 <user@dunn.cloud>
2018-08-18 13:30:52 -0400

    update

--- roles/test/test.py
+++ roles/test/test.py
@@ -1,2 +1,25 @@
-person = input('Enter your name: ')
-print('Hello', person)
+import os
+
+### clear screen
+os.system('cls')
+
+print("Welcome to the PG Version Deployment System!\n")
+
+def readFile(filename):
+    filehandle = open(filename)
+    print filehandle.read()
+    filehandle.close()
+
+fileDir = os.path.dirname(os.path.realpath('__file__'))
+
+#For accessing the file in the same folder
+filename = os.path.join(fileDir, '/opt/plexguide/roles/versions/scripts/ver.list')
+readFile(filename)
+
+print("TO Quit, type >>> exit")
+text = raw_input('Type the [PG Version] for Deployment! (All LowerCase): ')
+
+#Actually Writing It
+saveFile = open("/var/plexguide/pg.number", 'w')
+saveFile.write(text)
+saveFile.close()
