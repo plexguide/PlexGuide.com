fc6539286fd91ec4a72f80367f4d4caf45e784ed
Admin0705 <user@dunn.cloud>
2018-08-19 17:33:35 -0400

    update

--- pg.sh
+++ pg.sh
@@ -28,7 +28,7 @@ echo "10" > /var/plexguide/pg.rclone
 echo "11" > /var/plexguide/pg.python
 echo "10" > /var/plexguide/pg.docker
 echo "10" > /var/plexguide/pg.id
-echo "12" > /var/plexguide/pg.dependency
+echo "13" > /var/plexguide/pg.dependency
 echo "10" > /var/plexguide/pg.docstart
 echo "2" > /var/plexguide/pg.watchtower
 echo "1" > /var/plexguide/pg.motd
