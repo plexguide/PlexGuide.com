f1fcb1a0ad32747202ef40b0b821d92f8280d650
Admin0705 <user@dunn.cloud>
2018-08-21 18:32:35 -0400

    update

--- roles/b-srestore/scripts/solo.sh
+++ roles/b-srestore/scripts/solo.sh
@@ -17,14 +17,14 @@
 #################################################################################
 #### Recall Download Point
 mnt=$(cat /var/plexguide/server.hd.path)
-serverid=$(cat /var/plexguide/server.id)
+restoreid=$(cat /var/plexguide/restore.id)
 
 clear
 rm -r $mnt/pgops/restore.build 1>/dev/null 2>&1
 touch $mnt/pgops/restore.build 1>/dev/null 2>&1
 
 #### Recalls List for restore Operations
-ls -la /mnt/gdrive/plexguide/backup/$serverid | awk '{ print $9 }' | tail -n +2 > $mnt/pgops/restore.list
+ls -la /mnt/gdrive/plexguide/backup/$restoreid | awk '{ print $9 }' | tail -n +2 > $mnt/pgops/restore.list
 
 #### Combine for Simplicity
 path=$(echo $mnt/pgops/restore.list)
