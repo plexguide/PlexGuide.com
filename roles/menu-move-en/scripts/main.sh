f3a82c07bb3608f4deae1ad4eea7650c797913e8
Admin0705 <user@dunn.cloud>
2018-08-21 16:53:45 -0400

    update

--- roles/menu-move-en/scripts/main.sh
+++ roles/menu-move-en/scripts/main.sh
@@ -161,8 +161,8 @@ if [ "$menu" == "move" ]; then
         exit
   fi
   #### DEPLOY Move Transfer System
-  ansible-playbook /opt/plexguide/roles/templates-remove/remove-service.yml
-  ansible-playbook /opt/plexguide/roles/templates-remove/move.yml
+  ansible-playbook /opt/plexguide/roles/menu-move-en/remove-service.yml
+  ansible-playbook /opt/plexguide/roles/menu-move-en/move.yml
   echo 'SUCCESS - PGMove is now running!' > /var/plexguide/pg.log && bash /opt/plexguide/roles/log/log.sh
   echo ""
   read -n 1 -s -r -p "PG Move Deployed! Press [Any Key] to continue"
