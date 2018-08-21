35adc5971764287ef8fc4d5d0da56c999067245a
Admin0705 <user@dunn.cloud>
2018-08-21 18:12:36 -0400

    update

--- pg.sh
+++ pg.sh
@@ -23,7 +23,7 @@ mkdir -p /var/plexguide
  # Force Common Things To Execute Such as Folders
 echo "142" > /var/plexguide/pg.preinstall
 # Changing Number Results in Forcing Portions of PreInstaller to Execute
-echo "11" > /var/plexguide/pg.ansible
+echo "10" > /var/plexguide/pg.ansible
 echo "10" > /var/plexguide/pg.rclone
 echo "10" > /var/plexguide/pg.python
 echo "10" > /var/plexguide/pg.docker
@@ -32,7 +32,7 @@ echo "14" > /var/plexguide/pg.dependency
 echo "10" > /var/plexguide/pg.docstart
 echo "2" > /var/plexguide/pg.watchtower
 echo "1" > /var/plexguide/pg.motd
-echo "45" > /var/plexguide/pg.alias
+echo "46" > /var/plexguide/pg.alias
 echo "1" > /var/plexguide/pg.dep
 echo "1" > /var/plexguide/pg.cleaner
 
