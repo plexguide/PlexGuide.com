4e986f20b2491644de881e741c6f6d93e8b650bd
Admin0705 <user@dunn.cloud>
2018-08-21 16:32:55 -0400

    update

--- roles/menu-move/scripts/main.sh
+++ roles/menu-move/scripts/main.sh
@@ -32,7 +32,7 @@ fi
 #### exit # 1
 while [ "$menu" != "break" ]; do
 menu=$(cat /var/plexguide/move.menu)
-ansible-playbook /opt/plexguide/basics.yml --tags menu-move
+ansible-playbook /opt/plexguide/roles/menu-move/main.yml
 menu=$(cat /var/plexguide/move.menu)
 
 #### rclone # 2
