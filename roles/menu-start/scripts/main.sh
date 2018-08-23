91f5927e4afbbf5f0b86e01519b7224661cf4e65
Admin0705 <user@dunn.cloud>
2018-08-23 11:17:15 -0400

    update

--- roles/menu-start/scripts/main.sh
+++ roles/menu-start/scripts/main.sh
@@ -68,11 +68,6 @@ if [ "$menu" == "settings" ]; then
   bash /opt/plexguide/menus/settings/main.sh
 fi
 
-if [ "$menu" == "update" ]; then
-  echo 'INFO - Selected: PG Upgrades Menu Interface' > /var/plexguide/pg.log && bash /opt/plexguide/roles/log/log.sh
-  bash /opt/plexguide/roles/versions/main.sh
-fi
-
 if [ "$menu" == "wckd" ]; then
   echo 'INFO - Selected: WCKD Authentication' > /var/plexguide/pg.log && bash /opt/plexguide/roles/log/log.sh
   ansible-playbook /opt/plexguide/pg.yml --tags authclient --extra-vars "quescheck=on cron=off display=on"
