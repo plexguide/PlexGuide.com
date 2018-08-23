8460c413197cbd37594a9064176df0f735ac41e1
Admin0705 <user@dunn.cloud>
2018-08-23 09:20:03 -0400

    update

--- roles/menu-network/scripts/main.sh
+++ roles/menu-network/scripts/main.sh
@@ -15,24 +15,22 @@
 #   under the GPL along with build & install instructions.
 #
 #################################################################################
-echo "on" > /var/plexguide/ports.menu
+echo "on" > /var/plexguide/network.menu
 menu=$(echo "on")
 
 while [ "$menu" != "break" ]; do
-menu=$(cat /var/plexguide/ports.menu)
-ansible-playbook /opt/plexguide/roles/menu-ports/main.yml
-menu=$(cat /var/plexguide/ports.menu)
+menu=$(cat /var/plexguide/network.menu)
+ansible-playbook /opt/plexguide/roles/menu-network/main.yml
+menu=$(cat /var/plexguide/network.menu)
 
 if [ "$menu" == "open" ]; then
-  echo "" > /var/plexguide/server.ports
   echo 'INFO - Selected: Open Server Ports' > /var/plexguide/pg.log && bash /opt/plexguide/roles/log/log.sh
-  bash /opt/plexguide/roles/menu-ports/scripts/rebuild.sh
+  bash /opt/plexguide/roles/menu-network/scripts/rebuild.sh
 fi
 
 if [ "$menu" == "closed" ]; then
-  echo "127.0.0.1:" > /var/plexguide/server.ports
   echo 'INFO - Selected: Closed Server Ports' > /var/plexguide/pg.log && bash /opt/plexguide/roles/log/log.sh
-  bash /opt/plexguide/roles/menu-ports/scripts/rebuild.sh
+  bash /opt/plexguide/roles/menu-network/scripts/rebuild.sh
 fi
 
 echo 'INFO - Looping: Transport System Select Interface' > /var/plexguide/pg.log && bash /opt/plexguide/roles/log/log.sh
