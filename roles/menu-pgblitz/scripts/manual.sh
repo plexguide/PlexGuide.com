9ce154c736890c78a7dad4b8441ae1f80ad14f73
Admin0705 <user@dunn.cloud>
2018-08-31 08:35:46 -0400

    update

--- roles/menu-pgblitz/scripts/manual.sh
+++ roles/menu-pgblitz/scripts/manual.sh
@@ -154,9 +154,114 @@ if [ "$menu" == "email" ]; then
 
 fi
 
-if [ "$menu" == "enmove" ]; then
+if [ "$menu" == "process" ]; then
+  gdsa=`ls -la /opt/appdata/pgblitz/keys/unprocessed | awk '{print $9}' | grep GDSA | wc -l`;
+  if [ $gdsa > 2 ]; then
+    if [ "$encryption" == "on" ]; then
+      dialog --title "SET ENCRYPTION PASSWORD" \
+            --inputbox "Password: " 8 52 2>/opt/appdata/pgblitz/vars/password
+      dialog --title "SET ENCRYPTION SALT" \
+            --inputbox "Salt: " 8 52 2>/opt/appdata/pgblitz/vars/salt
+    fi
+    bash /opt/plexguide/roles/pgblitz/scripts/validator.sh
+  else
+    dialog --title "WARNING!" --msgbox "\nIt seems like you have no JSON files :(" 0 0
+  fi
+
+fi
+
+if [ "$menu" == "deploy" ]; then
+  ### Checkers
+  if [ "$final" == "gdrive" ]; then
+    echo 'FAILURE - Must Configure gdrive for RCLONE' > /var/plexguide/pg.log && bash /opt/plexguide/roles/log/log.sh
+    dialog --title "WARNING!" --msgbox "\nGDrive for RClone Must be Configured for PG Blitz!\n\nThis is required to BackUp/Restore any PG Data!" 0 0
+    bash /opt/plexguide/roles/pgblitz/scripts/main.sh
+    exit
+  fi
+
+  if [ "$final" == "tdrive" ]; then
+    echo 'FAILURE - Must Configure tdrive for RCLONE' > /var/plexguide/pg.log && bash /opt/plexguide/roles/log/log.sh
+    dialog --title "WARNING!" --msgbox "\nTDrive for RClone Must be Configured for PG Blitz!\n\nThis is required for TeamDrives to Work!!" 0 0
+    bash /opt/plexguide/roles/pgblitz/scripts/main.sh
+    exit
+  fi
+  if [ "$final" == "tcrypt" ] || [ "$final" == "gcrypt" ]; then
+    echo 'FAILURE - Must Configure $final for RCLONE for Encrypted Edition' > /var/plexguide/pg.log && bash /opt/plexguide/roles/log/log.sh
+    dialog --title "WARNING!" --msgbox "\n$final for RClone Must be Configured for PG Blitz!\n\nThis is required for the Encrypted Edition!!" 0 0
+    bash /opt/plexguide/roles/pgblitz/scripts/main.sh
+    exit
+  fi
+
+  #### BLANK OUT PATH - This Builds For UnionFS
+  rm -r /var/plexguide/unionfs.pgpath 1>/dev/null 2>&1
+  touch /var/plexguide/unionfs.pgpath 1>/dev/null 2>&1
+
+  ### Build UnionFS Paths Based on Version
+  if [ "$final" == "unencrypted" ];then
+    echo -n "/mnt/gdrive=RO:/mnt/tdrive=RO:" >> /var/plexguide/unionfs.pgpath
+  elif [ "$final" == "encrypted" ];then
+    echo -n "/mnt/gcrypt=RO:/mnt/tcrypt=RO:" >> /var/plexguide/unionfs.pgpath
+  fi
+
+  ### Add GDSA Paths for UnionFS
+  bash /opt/plexguide/roles/pgblitz/scripts/ufbuilder.sh
+  temp=$( cat /tmp/pg.gdsa.build )
+  echo -n "$temp" >> /var/plexguide/unionfs.pgpath
+
+  ### Execute Playbook Based on Version
+  if [ "$final" == "unencrypted" ];then
+    ansible-playbook /opt/plexguide/pg.yml --tags pgblitz --skip-tags encrypted
+  elif [ "$final" == "encrypted" ];then
+    ansible-playbook /opt/plexguide/pg.yml --tags pgblitz
+  fi
+  echo ""
+  read -n 1 -s -r -p "Press any key to continue"
+  dialog --title "PGBLitz WebGUI" \
+        --yesno "Would you like to deploy the new PGBlitz WebGUI?" 7 60
+    response=$?
+    case $response in
+        0)
+          echo 'INFO - DEPLOYING PGBLITZ WEBGUI' > /var/plexguide/pg.log && bash /opt/plexguide/roles/log/log.sh
+          ansible-playbook /opt/plexguide/pg.yml --tags blitzui
+          ;;
+        1)
+          echo 'INFO - NOT DEPLOYING PGBLITZ WEBGUI' > /var/plexguide/pg.log && bash /opt/plexguide/roles/log/log.sh
+          ;;
+    esac
+  dialog --title "NOTE" --msgbox "\nPG Drive & PG Blitz Deployed!!" 0 0
+fi
+
+if [ "$menu" == "path" ]; then
+  bash /opt/plexguide/scripts/baseinstall/harddrive.sh
+fi
+
+if [ "$menu" == "bad" ]; then
   echo 'INFO - Selected: PG Move - PG Drive' > /var/plexguide/pg.log && bash /opt/plexguide/roles/log/log.sh
-  bash /opt/plexguide/roles/menu-move-en/scripts/main.sh
+  dialog --infobox "Reprocessing BAD JSONs (Please Wait)" 3 40
+  sleep 2
+  clear
+  mv /opt/appdata/pgblitz/keys/badjson/* /opt/appdata/pgblitz/keys/unprocessed/ 1>/dev/null 2>&1
+  bash /opt/plexguide/roles/pgblitz/scripts/validator.sh
+fi
+
+if [ "$menu" == "baseline" ]; then
+  echo 'INFO - Selected: PG Move - PG Drive' > /var/plexguide/pg.log && bash /opt/plexguide/roles/log/log.sh
+  dialog --infobox "Baselining PGBlitz (Please Wait)" 3 40
+  sleep 2
+  systemctl stop pgblitz 1>/dev/null 2>&1
+  systemctl disable pgblitz 1>/dev/null 2>&1
+  rm -r /root/.config/rclone/rclone.conf 1>/dev/null 2>&1
+  rm -r /opt/appdata/pgblitz/keys/unprocessed/* 1>/dev/null 2>&1
+  rm -r /opt/appdata/pgblitz/keys/processed/* 1>/dev/null 2>&1
+  rm -r /opt/appdata/pgblitz/keys/badjson/* 1>/dev/null 2>&1
+  dialog --title "NOTE" --msgbox "\nKeys Cleared!\n\nYou must reconfigure RClone and Repeat the Process Again!" 0 0
+fi
+
+if [ "$menu" == "disable" ]; then
+  echo 'INFO - Selected: PG Move - PG Drive' > /var/plexguide/pg.log && bash /opt/plexguide/roles/log/log.sh
+  sudo systemctl stop pgblitz 1>/dev/null 2>&1
+  sudo systemctl rm pgblitz 1>/dev/null 2>&1
+  dialog --title "NOTE" --msgbox "\nPG Blitz is Disabled!\n\nYou must rerun PGDrives & PGBlitz to Enable Again!" 0 0
 fi
 
 echo 'INFO - Looping: Transport System Select Interface' > /var/plexguide/pg.log && bash /opt/plexguide/roles/log/log.sh
