ba69dcf055977f335d34cc09a502465265065a9f
Admin0705 <user@dunn.cloud>
2018-08-31 08:04:38 -0400

    update

--- roles/menu-pgblitz/scripts/manual.sh
+++ roles/menu-pgblitz/scripts/manual.sh
@@ -104,18 +104,20 @@ if [ "$menu" == "jsons" ]; then
   fi
       echo 'INFO - DEPLOYING CLOUDBLITZ' > /var/plexguide/pg.log && bash /opt/plexguide/roles/log/log.sh
       #### Deploy CloudBlitz
-      clear && ansible-playbook /opt/plexguide/pg.yml --tags cloudblitz --extra-vars "skipend="yes --skip-tags cron
+      ansible-playbook /opt/plexguide/pg.yml --tags cloudblitz --extra-vars "skipend="yes --skip-tags cron
       #### Note How to Create Json files
+      echo "Visit Port 7997 and Upload your JSON files | User: plex & Paswword: guide"
+      echo "NOTE: Keys are Stored below for Processing: /opt/appdata/pgblitz/keys/unprocessed/"
       echo ""
-      read -n 1 -s -r -p "Visit Port 7997 and Upload your JSON files\n\nKeys are Stored below for Processing:\n/opt/appdata/pgblitz/keys/unprocessed/\n\nUser - PW: plex / guide\n\nWhen Finished, Press [ENTER] to Continue!"
+      read -n 1 -s -r -p "When Finished, Press [ANY KEY] to Continue!"
       echo ""
       echo "Please Wait"
       #dialog --title "NOTE" --msgbox "\nVisit Port 7997 and Upload your JSON files\n\nKeys are Stored below for Processing:\n/opt/appdata/pgblitz/keys/unprocessed/\n\nUser - PW: plex / guide\n\nWhen Finished, Press [ENTER] to Continue!" 0 0
       #dialog --infobox "Please Wait" 3 22
       docker stop cloudblitz 1>/dev/null 2>&1
       docker rm cloudblitz 1>/dev/null 2>&1
-      echo ""
-      read -n 1 -s -r -p "If you havn't already you need to use Option C\nand add you email's to your tdrive\nThen use option D to validate your JSON files"
+      echo "WARNING: Make Sure to Use the E-Mail and Validation Processes!"
+      read -n 1 -s -r -p "Press [ANY KEY] to Continue"
 fi
 
 if [ "$menu" == "blitzmanual" ]; then
