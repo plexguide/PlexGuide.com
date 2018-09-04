57288eee99d2e8c468d7e08ad330ee658f3378bd
Admin9705 <user@dunn.cloud>
2018-09-04 17:03:04 -0400

    update

--- roles/menu-pgblitz/scripts/manual.sh
+++ roles/menu-pgblitz/scripts/manual.sh
@@ -130,7 +130,7 @@ if [ "$menu" == "process" ]; then
 
   mkdir -p
   gdsa="ls -la /opt/appdata/pgblitz/keys/unprocessed | awk '{print $9}' | tail -n +4 | wc -l"
-  if [ "$gdsa" != 0 ]; then
+  if [ "$gdsa" -ne "0" ]; then
     if [ "$encryption" == "on" ]; then
       dialog --title "SET ENCRYPTION PASSWORD" \
             --inputbox "Password: " 8 52 2>/opt/appdata/pgblitz/vars/password
