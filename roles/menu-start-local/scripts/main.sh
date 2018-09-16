c3d6a08238738c0491331e70ca74189eab09288e
Admin9705 <test@dunn.cloud>
2018-09-16 12:25:58 -0400

    update

---
+++ roles/menu-start-local/scripts/main.sh
@@ -0,0 +1,60 @@
+#!/bin/bash
+#
+# [Ansible Role]
+#
+# GitHub:   https://github.com/Admin9705/PlexGuide.com-The-Awesome-Plex-Server
+# Author:   Admin9705 & Deiteq
+# URL:      https://plexguide.com
+#
+# PlexGuide Copyright (C) 2018 PlexGuide.com
+# Licensed under GNU General Public License v3.0 GPL-3 (in short)
+#
+#   You may copy, distribute and modify the software as long as you track
+#   changes/dates in source files. Any modifications to our software
+#   including (via compiler) GPL-licensed code must also be made available
+#   under the GPL along with build & install instructions.
+#
+#################################################################################
+echo "on" > /var/plexguide/main.menu
+menu=$(echo "on")
+
+while [ "$menu" != "break" ]; do
+menu=$(cat /var/plexguide/main.menu)
+ansible-playbook /opt/plexguide/roles/menu-start-local/main.yml
+menu=$(cat /var/plexguide/main.menu)
+
+if [ "$menu" == "mount" ]; then
+  echo 'INFO - Selected: Deploy a Mount System' > /var/plexguide/pg.log && bash /opt/plexguide/roles/log/log.sh
+  bash /opt/plexguide/roles/menu-transport/scripts/main.sh
+fi
+
+if [ "$menu" == "programs" ]; then
+  echo 'INFO - Selected: PG Program Suite' > /var/plexguide/pg.log && bash /opt/plexguide/roles/log/log.sh
+  bash /opt/plexguide/roles/programs/main.sh
+fi
+
+if [ "$menu" == "plextools" ]; then
+  echo 'INFO - Selected: PLEX Enhancements' > /var/plexguide/pg.log && bash /opt/plexguide/roles/log/log.sh
+  bash /opt/plexguide/roles/menu-plexaddons/scripts/main.sh
+fi
+
+if [ "$menu" == "tshoot" ]; then
+  echo 'INFO - Selected: PG Server Information' > /var/plexguide/pg.log && bash /opt/plexguide/roles/log/log.sh
+  bash /opt/plexguide/roles/info-tshoot/tshoot.sh
+fi
+
+if [ "$menu" == "auditor" ]; then
+  echo 'INFO - Selected: Auditor' > /var/plexguide/pg.log && bash /opt/plexguide/roles/log/log.sh
+  bash /opt/plexguide/roles/menu-network/scripts/main.sh
+fi
+
+if [ "$menu" == "settings" ]; then
+  echo 'INFO - Selected: Settings' > /var/plexguide/pg.log && bash /opt/plexguide/roles/log/log.sh
+  bash /opt/plexguide/menus/settings/main.sh
+fi
+
+echo 'INFO - Looping: Main GDrive Interface Menu' > /var/plexguide/pg.log && bash /opt/plexguide/roles/log/log.sh
+done
+
+echo 'INFO - Selected: Exiting PlexGuide' > /var/plexguide/pg.log && bash /opt/plexguide/roles/log/log.sh
+bash /opt/plexguide/roles/ending/ending.sh
