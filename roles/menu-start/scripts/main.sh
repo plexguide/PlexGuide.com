486e25cb65d9ab2879c9306ea9e2c1909f633c8c
Admin9705 <user@dunn.cloud>
2018-09-17 11:41:08 -0400

    update

--- roles/menu-start/scripts/main.sh
+++ roles/menu-start/scripts/main.sh
@@ -44,7 +44,19 @@ fi
 
 if [ "$menu" == "traefik" ]; then
   echo 'INFO - Selected: Traefik & TLD' > /var/plexguide/pg.log && bash /opt/plexguide/roles/log/log.sh
-  bash /opt/plexguide/roles/menu-tld/scripts/submenu.sh
+
+  ### Affects Only Multi-HD and No Mount Is Deployed!
+  multi=$(cat /var/plexguide/multi.unionfs)
+  edition=$(cat /var/plexguide/pg.edition.stored)
+
+  if [ "$edition" == "PG Edition - HD Multi" ] && [ "$multi" == "" ]; then
+    echo ""
+    echo "WARNING: You cannot proceed! Deploy one mount with UNIONFS first!"
+    read -n 1 -s -r -p "Press [ANY] Key to Continue"
+  else
+    bash bash /opt/plexguide/roles/menu-tld/scripts/submenu.sh
+  fi
+
 fi
 
 if [ "$menu" == "programs" ]; then
