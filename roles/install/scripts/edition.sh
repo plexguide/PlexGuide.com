d627308fe40d6e78eb48074fc699f8c9a720447c
Admin9705 <test@dunn.cloud>
2018-09-16 23:26:26 -0400

    update

--- roles/install/scripts/edition.sh
+++ roles/install/scripts/edition.sh
@@ -25,58 +25,10 @@ sudo echo "INFO - Start of Script: $sname" > /var/plexguide/pg.log
 sudo bash /opt/plexguide/roles/log/log.sh
 ######################################################## START: Main Script
 if [ "$pg_edition" == "$pg_edition_stored" ]; then
-      echo "" 1>/dev/null 2>&1
-    else
-      dialog --infobox "WARNING! Setting Your PlexGuide Edition! You Can Only Set the Edition One Time!\n\nChoose Carefully!" 0 0
-      sleep 5
-      clear
-
-echo 'INFO - Visited PG Edition Menu' > /var/plexguide/pg.log && bash /opt/plexguide/roles/log/log.sh
-
-HEIGHT=10
-WIDTH=31
-CHOICE_HEIGHT=4
-BACKTITLE="Visit PlexGuide.com - Automations Made Simple"
-TITLE="Select Your Edition!"
-
-OPTIONS=(A "GDrive Edition"
-         B "HD Solo Edition"
-         C "HD Multi Edition"
-         D "GCE Feeder Edition")
-
-CHOICE=$(dialog --backtitle "$BACKTITLE" \
-                --title "$TITLE" \
-                --menu "$MENU" \
-                $HEIGHT $WIDTH $CHOICE_HEIGHT \
-                "${OPTIONS[@]}" \
-                2>&1 >/dev/tty)
-
-case $CHOICE in
-    A)
-      echo 'INFO - Select PG Edition: GDrive' > /var/plexguide/pg.log && bash /opt/plexguide/roles/log/log.sh
-      echo "PG Edition: GDrive" > /var/plexguide/pg.edition
-      echo "gdrive" > /var/plexguide/pg.server.deploy
-      ;;
-    B)
-      echo 'INFO - Select PG Edition: HD Solo' > /var/plexguide/pg.log && bash /opt/plexguide/roles/log/log.sh
-      ansible-playbook /opt/plexguide/pg.yml --tags folders_solo &>/dev/null &
-      echo "PG Edition: HD Solo" > /var/plexguide/pg.edition
-      echo "drive" > /var/plexguide/pg.server.deploy
-      exit
-      ;;
-    C)
-      echo 'INFO - Select PG Edition: HD Multi' > /var/plexguide/pg.log && bash /opt/plexguide/roles/log/log.sh
-      echo "PG Edition: HD Multi" > /var/plexguide/pg.edition
-      echo "drives" > /var/plexguide/pg.server.deploy
-      ;;
-    D)
-      echo 'INFO - Select PG Edition: GCE Feeder' > /var/plexguide/pg.log && bash /opt/plexguide/roles/log/log.sh
-      echo "PG Edition: GCE Feed" > /var/plexguide/pg.edition
-      echo "feeder" > /var/plexguide/pg.server.deploy
-      ;;
-esac
-      cat /var/plexguide/pg.edition > /var/plexguide/pg.edition.stored
-  fi
+  echo "" 1>/dev/null 2>&1
+else
+  bash /opt/plexguide/roles/menu-install-editions/scripts/main.sh
+fi
 ######################################################## END: Main Script
 #
 #
