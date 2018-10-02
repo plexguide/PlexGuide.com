63b8025f29ef6a8f6fe030dc98b38ca45415aa2c
Admin9705 <user@dunn.cloud>
2018-10-01 21:27:12 -0400

    update

--- roles/menu-appguard/scripts/main.sh
+++ roles/menu-appguard/scripts/main.sh
@@ -26,7 +26,7 @@ menu=$(cat /var/plexguide/.menu)
 file=/var/plexguide/auth.lock
 if [ -e "$file" ]; then
 bash /opt/plexguide/roles/menu-appguard/scripts/rebuild.sh
-rm -r touch /var/plexguide/auth.lock
+rm -r /var/plexguide/auth.lock
 fi
 
 echo 'INFO - Looping: PG Authentication Interface' > /var/plexguide/pg.log && bash /opt/plexguide/roles/log/log.sh
