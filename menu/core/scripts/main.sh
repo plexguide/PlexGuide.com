ee94a95eba677472e132244757241a544b4ce0e8
Admin9705 <user@dunn.cloud>
2018-10-03 00:35:01 -0400

    update

--- menu/core/scripts/main.sh
+++ menu/core/scripts/main.sh
@@ -23,7 +23,7 @@ menu=$(echo "on")
 
 while [ "$menu" != "break" ]; do
 menu=$(cat /var/plexguide/appguard.menu)
-ansible-playbook /opt/plexguide/menu/interface/$program/file.sh
+bash /opt/plexguide/menu/interface/$program/file.sh
 menu=$(cat /var/plexguide/appguard.menu)
 
 finalchoice=$(cat /var/plexguide/final.choice)
