ef27860dad0ad6dbe762389f6ad05c806c2ef6b9
Admin9705 <user@dunn.cloud>
2018-10-03 15:59:39 -0400

    update

--- menu/core/scripts/main.sh
+++ menu/core/scripts/main.sh
@@ -15,29 +15,21 @@
 #   under the GPL along with build & install instructions.
 #
 #################################################################################
-echo "on" > /var/plexguide/final.choice
+echo "dummy" > /var/plexguide/final.choice
 echo "removal" > /var/plexguide/type.choice ## temp
 program=$(cat /var/plexguide/type.choice)
 
 menu=$(echo "on")
 
 while [ "$menu" != "break" ]; do
-menu=$(cat /var/plexguide/appguard.menu)
+menu=$(cat /var/plexguide/final.choice)
+
+### Loads Key Variables
+bash /opt/plexguide/menu/interface/$program/var.sh
+### Loads Key Execution
+ansible-playbook /opt/plexguide/menu/core/selection.yml
+### Executes Actions
 bash /opt/plexguide/menu/interface/$program/file.sh
-menu=$(cat /var/plexguide/appguard.menu)
-
-finalchoice=$(cat /var/plexguide/final.choice)
-if [ "$menu" == "2" ]; then
-  bash /opt/plexguide/menu/interface/$program/file.sh
-fi
-
-if [ "$menu" == "3" ]; then
-  bash /opt/plexguide/menu/interface/$program/file.sh
-fi
-
-if [ "$menu" == "4" ]; then
-  bash /opt/plexguide/menu/interface/$program/file.sh
-fi
 
 echo 'INFO - Looping: PG Authentication Interface' > /var/plexguide/pg.log && bash /opt/plexguide/roles/log/log.sh
 done
