0343373f268a2aee9dfc917e680e8b4e66cd9292
Admin9705 <user@dunn.cloud>
2018-10-05 14:24:23 -0400

    update

--- roles/install/scripts/gcloud.sh
+++ roles/install/scripts/gcloud.sh
@@ -32,6 +32,7 @@ if [ "$pg_cloud" == "$pg_cloud_stored" ]; then
       echo "deb http://packages.cloud.google.com/apt $CLOUD_SDK_REPO main" | sudo tee -a /etc/apt/sources.list.d/google-cloud-sdk.list
       curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add
       sudo apt-get update && sudo apt-get install google-cloud-sdk
+      cat /var/plexguide/pg.gcloud > /var/plexguide/pg.gcloud.stored
   fi
 ######################################################## END: Main Script
 #
