95cb732d3abe639f767656c759a19ef8fcb90056
Admin9705 <user@dunn.cloud>
2018-10-05 19:24:56 -0400

    update

--- menu/interface/gcloud/file.sh
+++ menu/interface/gcloud/file.sh
@@ -193,11 +193,13 @@ elif [ "$typed" == "6" ]; then echo "Creating 50 Keys - Daily Upload Limit Set t
     gcloud iam service-accounts keys create /opt/appdata/pgblitz/keys/processed/gdsa0$count --iam-account gdsa0$count@$project.iam.gserviceaccount.com --key-file-type="json"
     echo "gdsa0$count" > /var/plexguide/json.tempbuild
     bash /opt/plexguide/roles/menu-pgblitz/scripts/gdsa.sh
+    echo ""
   else
     gcloud iam service-accounts create gdsa$count --display-name “gdsa$count”
     gcloud iam service-accounts keys create /opt/appdata/pgblitz/keys/processed/gdsa$count --iam-account gdsa$count@$project.iam.gserviceaccount.com --key-file-type="json"
     echo "gdsa$count" > /var/plexguide/json.tempbuild
     bash /opt/plexguide/roles/menu-pgblitz/scripts/gdsa.sh
+    echo ""
   fi
   done
 
