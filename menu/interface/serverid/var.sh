572ab6b64299a6118f34228925d9f8143204c960
Admin9705 <user@dunn.cloud>
2018-10-11 09:52:02 -0400

    udpate

--- menu/interface/serverid/var.sh
+++ menu/interface/serverid/var.sh
@@ -18,8 +18,6 @@
 echo 2 > /var/plexguide/menu.number
 
 file="/var/plexguide/server.id"
-  if [ -e "$file" ]; then
-    null=null2
-  else
+  if [ ! -e "$file" ]; then
     echo NOT-SET > /var/plexguide/server.id
   fi
