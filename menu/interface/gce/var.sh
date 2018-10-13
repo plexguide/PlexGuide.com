23787d645986153b181681d1cc5301f995192269
Admin9705 <user@dunn.cloud>
2018-10-12 22:46:52 -0400

    update

--- menu/interface/gce/var.sh
+++ menu/interface/gce/var.sh
@@ -25,18 +25,3 @@ file="/var/plexguide/project.final"
   else
     echo "[NOT SET]" > /var/plexguide/project.final
   fi
-
-file="/var/plexguide/project.keycount"
-  if [ -e "$file" ]; then
-    echo "" 1>/dev/null 2>&1
-  else
-    echo "0" > /var/plexguide/project.keycount
-  fi
-
-### For PGBlitz - Ensure Not Deployed Start
-  file="/var/plexguide/project.deployed"
-    if [ -e "$file" ]; then
-      echo "" 1>/dev/null 2>&1
-    else
-      echo "no" > /var/plexguide/project.deployed
-    fi
