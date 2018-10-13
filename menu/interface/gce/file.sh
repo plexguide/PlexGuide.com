fd16970c727414541cfe4813fbab20721156ede4
Admin9705 <user@dunn.cloud>
2018-10-13 13:13:28 -0400

    update

--- menu/interface/gce/file.sh
+++ menu/interface/gce/file.sh
@@ -272,10 +272,10 @@ echo "NOTE: Please Standby"
 echo ""
 
   break=off
-  while [ $break == off]; do
+  while [ $break == off ]; do
   ipdelete=$(gcloud compute addresses list | grep pg-gce | tail -n +2)
 
-    if [ $ipdelete != ""]; then
+    if [ $ipdelete != "" ]; then
     regdelete=$(gcloud compute addresses list | grep pg-gce | tail -n +2 | awk '{print $2}')
     addprint=$(gcloud compute addresses list | grep pg-gce | tail -n +2 | awk '{print $3}')
     gcloud compute addresses delete pg-gce --region=us-east4 --quiet
