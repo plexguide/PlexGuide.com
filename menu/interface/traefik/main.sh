9f99811f8c81f68a652d259d47b51907673dbcbd
Admin9705 <user@dunn.cloud>
2018-10-21 23:30:34 -0400

    update

--- menu/interface/traefik/main.sh
+++ menu/interface/traefik/main.sh
@@ -521,8 +521,6 @@ EOF
 else
   typed="1"
   echo ""
-  echo 'INFO - Exiting PlexGuide & Display Ending Menu' > /var/plexguide/pg.log && bash /opt/plexguide/roles/log/log.sh
-  bash /opt/plexguide/roles/ending/ending.sh
   exit
 fi
 
