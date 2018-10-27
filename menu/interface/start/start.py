4beae6f65b0be7aadc94d29044e6e83def76c8a1
Admin9705 <user@dunn.cloud>
2018-10-27 04:25:40 -0400

    update

---
+++ menu/interface/start/start.py
@@ -0,0 +1,53 @@
+#!/bin/bash
+#
+# [Ansible Role]
+#
+# GitHub:   https://github.com/Admin9705/PlexGuide.com-The-Awesome-Plex-Server
+# Author:   Admin9705
+# URL:      https://plexguide.com
+#
+# PlexGuide Copyright (C) 2018 PlexGuide.com
+# Licensed under GNU General Public License v3.0 GPL-3 (in short)
+#
+#   You may copy, distribute and modify the software as long as you track
+#   changes/dates in source files. Any modifications to our software
+#   including (via compiler) GPL-licensed code must also be made available
+#   under the GPL along with build & install instructions.
+#
+#################################################################################
+# https://pypi.org/project/console-menu/
+#
+
+# Import the necessary packages
+from consolemenu import *
+from consolemenu.items import *
+
+# Create the menu
+menu = ConsoleMenu("PlexGuide","Welcome to PlexGuide")
+
+# Create some items
+
+# MenuItem is the base class for all items, it doesn't do anything when selected
+menu_item = MenuItem("Menu Item")
+
+# A FunctionItem runs a Python function when selected
+function_item = FunctionItem("Call a Python function", input, ["Enter an input"])
+
+# A CommandItem runs a console command
+command_item = CommandItem("Run a console command",  "bash /opt/plexguide/menu/interface/traefik/main.sh")
+
+# A SelectionMenu constructs a menu from a list of strings
+selection_menu = SelectionMenu(["item1", "item2", "item3"])
+
+# A SubmenuItem lets you add a menu (the selection_menu above, for example)
+# as a submenu of another menu
+submenu_item = SubmenuItem("Submenu item", selection_menu, menu)
+
+# Once we're done creating them, we just add the items to the menu
+menu.append_item(menu_item)
+menu.append_item(function_item)
+menu.append_item(command_item)
+menu.append_item(submenu_item)
+
+# Finally, we call show to show the menu and allow the user to interact
+menu.show()
