2714f133cc5669d622edeb331f7d6bd094bf75c3
Admin9705 <user@dunn.cloud>
2018-10-28 00:54:45 -0400

    update

--- menu/interface/start/tools.py
+++ menu/interface/start/tools.py
@@ -26,7 +26,7 @@ with open('/var/plexguide/pg.edition', 'r') as myfile:
 
 def main():
     # Change some menu formatting
-    menu_format = MenuFormatBuilder().set_border_style_type(MenuBorderStyleType.LIGHT_BORDER)\
+    menu_format = MenuFormatBuilder().set_border_style_type(MenuBorderStyleType.DOUBLE_LINE_BORDER)\
         .set_prompt("SELECT>")\
         .set_title_align('left')\
         .set_subtitle_align('left')\
