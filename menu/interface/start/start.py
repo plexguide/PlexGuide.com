22ae4dfb62fca43ed354ec30710f3adc59729ea3
Admin9705 <user@dunn.cloud>
2018-10-28 05:29:27 -0400

    update

--- menu/interface/start/start.py
+++ menu/interface/start/start.py
@@ -96,10 +96,10 @@ with open('/var/plexguide/traefik.deployed', 'r') as myfile:
 
     command_item2 = CommandItem("Traefik & TLD Deployment       " + traefik, "bash /opt/plexguide/menu/interface/traefik/main.sh")
     command_item3 = CommandItem("Server Port Guard              " + ports, "bash /opt/plexguide/roles/menu-ports/scripts/main.sh && python3 /opt/plexguide/menu/interface/start/start.py")
-    command2_item = CommandItem("Applicaiton Guard              " + appguard , "bash /opt/plexguide/roles/menu-appguard/scripts/main.sh && python3 /opt/plexguide/menu/interface/start/start.py" )
+    command2_item = Command2Item("Applicaiton Guard              " + appguard , "bash /opt/plexguide/roles/menu-appguard/scripts/main.sh && python3 /opt/plexguide/menu/interface/start/start.py" )
     command_item4 = CommandItem("Program Suite Installer", "bash /opt/plexguide/menu/interface/apps/main.sh")
     command_item5 = CommandItem("PG Tools & Services", "python3 /opt/plexguide/menu/interface/start/tools.py")
-    command_item6 = Command2Item("Settings", "echo 'settings' > /var/plexguide/type.choice && bash /opt/plexguide/menu/core/scripts/main.sh")
+    command_item6 = CommandItem("Settings", "echo 'settings' > /var/plexguide/type.choice && bash /opt/plexguide/menu/core/scripts/main.sh")
 
     # Once we're done creating them, we just add the items to the menu
     menu.append_item(command_item1)
