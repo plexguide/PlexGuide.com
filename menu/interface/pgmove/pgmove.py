a5789dd7b4c2098ac0a7871a87f194bac90f0e76
Admin9705 <user@dunn.cloud>
2018-10-28 20:22:49 -0400

    update

--- menu/interface/pgmove/pgmove.py
+++ menu/interface/pgmove/pgmove.py
@@ -62,7 +62,7 @@ menu.formatter = menu_format
 item1 = MenuItem("Item 1", menu)
 
 # A CommandItem runs a console command
-rollover_item1 = RolloverItem("Configure RClone: " + configure,"echo && rclone config && mkdir -p /root/.config/rclone/ && chown -R 1000:1000 /root/.config/rclone/ && cp ~/.config/rclone/rclone.conf /root/.config/rclone/ 1>/dev/null 2>&1 && python3 /opt/plexguide/menu/interface/pgmove/pgmove.py")
+rollover_item1 = RolloverItem("Configure RClone: " + configure,"rclone config && mkdir -p /root/.config/rclone/ && chown -R 1000:1000 /root/.config/rclone/ && cp ~/.config/rclone/rclone.conf /root/.config/rclone/ && python3 /opt/plexguide/menu/interface/pgmove/pgmove.py")
 rollover_item2 = RolloverItem("Upload BW Limit : " + bwlimit + " MB", "python3 /opt/plexguide/menu/interface/pgmove/speeds.py && python3 /opt/plexguide/menu/interface/pgmove/pgmove.py")
 
 if configure == '[Encrypted]':
