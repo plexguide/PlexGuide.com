865ac536425919e8b94ff4b80d35ff17c1229721
Admin9705 <user@dunn.cloud>
2018-10-28 20:29:06 -0400

    update

--- menu/interface/pgmove/rclone.sh
+++ menu/interface/pgmove/rclone.sh
@@ -16,5 +16,5 @@
 rclone config
 mkdir -p /root/.config/rclone/
 chown -R 1000:1000 /root/.config/rclone/
-cp ~/.config/rclone/rclone.conf /root/.config/rclone/
+cp ~/.config/rclone/rclone.conf /root/.config/rclone/ 1>/dev/null 2>&1
 python3 /opt/plexguide/menu/interface/pgmove/pgmove.py
