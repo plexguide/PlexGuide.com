49e97af676e306c85c6e7613f58a5d660b5d7199
Admin9705 <user@dunn.cloud>
2018-10-28 21:05:38 -0400

    update

--- menu/interface/start/start.py
+++ menu/interface/start/start.py
@@ -90,7 +90,7 @@ with open('/var/plexguide/traefik.deployed', 'r') as myfile:
         .show_header_bottom_border(True)
 
     menu = ConsoleMenu(edition + " - " + pgversion + " | Ansible: " + ansible, "Domain: " + domain + " | Server ID: " + serverid,
-                       prologue_text=("Welcome to PlexGuide & for Being Part of the Team! NOTE: Thank you to those who Donate & Support Others!"))
+                       prologue_text=("Welcome to PlexGuide! Sign up for NewsHosting @ https://newshosting.plexguide.com! Amazingly fast for your NZB Needs!"))
     menu.formatter = menu_format
     item1 = MenuItem("Item 1", menu)
 
