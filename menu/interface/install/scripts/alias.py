972c6c77880e87f6f5026a4969d0fc97285f1ca9
Admin9705 <user@dunn.cloud>
2018-10-29 21:38:52 -0400

    update

--- menu/interface/install/scripts/alias.py
+++ menu/interface/install/scripts/alias.py
@@ -31,14 +31,20 @@ menu_format = MenuFormatBuilder().set_border_style_type(MenuBorderStyleType.HEAV
     .set_subtitle_align('left')\
     .set_left_margin(2)\
     .set_right_margin(2)\
+    .show_header_bottom_border(True)
 
-menu = ConsoleMenu("Installing: PG Alias ",
+menu = ConsoleMenu("Installing: PG Alias"",
                    prologue_text=("Alias are custom commands such as plexguide, pgfork, pgedge that install on your box!"))
 menu.formatter = menu_format
 item1 = MenuItem("Item 1", menu)
 
 # A CommandItem runs a console command
+rollover_item1 = RolloverItem("NOTE: PLEASE STANDBY!", "echo")
+# Once we're done creating them, we just add the items to the menu
+menu.append_item(rollover_item)
+
 # Finally, we call show to show the menu and allow the user to interact
 menu.start()
 
-time.sleep(4)
+# Sleep 5 Seconds
+time.sleep(5)
