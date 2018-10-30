6e5844fba969f023e2585b017e8e7e065d04365e
Admin9705 <user@dunn.cloud>
2018-10-29 21:59:04 -0400

    update

--- menu/interface/install/scripts/alias.py
+++ menu/interface/install/scripts/alias.py
@@ -38,20 +38,20 @@ if starter == stored:
 # Menu Start
 
     # Change some menu formatting
-menu_format = MenuFormatBuilder().set_border_style_type(MenuBorderStyleType.HEAVY_BORDER)\
-    .set_prompt("")\
-    .set_title_align('center')\
-    .set_subtitle_align('left')\
-    .set_left_margin(2)\
-    .set_right_margin(2)\
-    .show_header_bottom_border(True)
+    menu_format = MenuFormatBuilder().set_border_style_type(MenuBorderStyleType.HEAVY_BORDER)\
+        .set_prompt("")\
+        .set_title_align('center')\
+        .set_subtitle_align('left')\
+        .set_left_margin(2)\
+        .set_right_margin(2)\
+        .show_header_bottom_border(True)
 
-menu = ConsoleMenu("INSTALLING: PG Alias",
-                   prologue_text=("Alias are custom commands such as plexguide, pgfork, pgedge that install on your box! PLEASE STANDBY!"))
-menu.formatter = menu_format
-item1 = MenuItem("Item 1", menu)
-# Finally, we call show to show the menu and allow the user to interact
-menu.start()
+    menu = ConsoleMenu("INSTALLING: PG Alias",
+                       prologue_text=("Alias are custom commands such as plexguide, pgfork, pgedge that install on your box! PLEASE STANDBY!"))
+    menu.formatter = menu_format
+    item1 = MenuItem("Item 1", menu)
+    # Finally, we call show to show the menu and allow the user to interact
+    menu.start()
 
-# Sleep 5 Seconds
-time.sleep(5)
+    # Sleep 5 Seconds
+    time.sleep(5)
