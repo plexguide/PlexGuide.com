23a58dec6234b517aa16c8c2b86d9be493f42b04
Admin9705 <user@dunn.cloud>
2018-10-29 21:10:00 -0400

    update

---
+++ menu/interface/install/scripts/alias.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python3
+#
+# GitHub:   https://github.com/Admin9705/PlexGuide.com-The-Awesome-Plex-Server
+# Author:   Admin9705
+# URL:      https://plexguide.com
+#
+# PlexGuide Copyright (C) 2018 PlexGuide.com
+# Licensed under GNU General Public License v3.0 GPL-3 (in short)
+#
+#   You may copy, distribute and modify the software as long as you track
+#   changes/dates in source files. Any modifications to our software
+#   including (via compiler) GPL-licensed code must also be made available
+#   under the GPL along with build & install instructions.
+#
+#################################################################################
+# Import for Bash Ending
+from subprocess import call
+
+# Import for Menu Interface
+from consolemenu import *
+from consolemenu.format import *
+from consolemenu.items import *
+# Menu Start
+
+    # Change some menu formatting
+menu_format = MenuFormatBuilder().set_border_style_type(MenuBorderStyleType.HEAVY_BORDER)\
+    .set_prompt("SELECT>")\
+    .set_title_align('left')\
+    .set_subtitle_align('left')\
+    .set_left_margin(2)\
+    .set_right_margin(2)\
+    .show_header_bottom_border(True)
+
+menu = ConsoleMenu("Welcome to PG Move!",
+                   prologue_text=("PG Move is a simple uploader. It is highly recommend that you keep the BW @ 9MB; equaling 750GB per day over a period of 24 horus! If uploading less than 750GB per day, user can increase the speeds! Making Changes? Redeploy PG Move!"))
+menu.formatter = menu_format
+item1 = MenuItem("Item 1", menu)
+
+menu.start()
