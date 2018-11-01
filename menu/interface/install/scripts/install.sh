05634b58d0835594dbe239f0b0ea8c9718cb6294
Admin9705 <user@dunn.cloud>
2018-10-31 23:56:56 -0400

    update

---
+++ menu/interface/install/scripts/install.sh
@@ -0,0 +1,63 @@
+#!/bin/sh
+
+# Disable IPv6
+if [ -f /etc/sysctl.d/99-sysctl.conf ]; then
+    grep -q -F 'net.ipv6.conf.all.disable_ipv6 = 1' /etc/sysctl.d/99-sysctl.conf || echo 'net.ipv6.conf.all.disable_ipv6 = 1' >> /etc/sysctl.d/99-sysctl.conf
+    grep -q -F 'net.ipv6.conf.default.disable_ipv6 = 1' /etc/sysctl.d/99-sysctl.conf || echo 'net.ipv6.conf.default.disable_ipv6 = 1' >> /etc/sysctl.d/99-sysctl.conf
+    grep -q -F 'net.ipv6.conf.lo.disable_ipv6 = 1' /etc/sysctl.d/99-sysctl.conf || echo 'net.ipv6.conf.lo.disable_ipv6 = 1' >> /etc/sysctl.d/99-sysctl.conf
+    sysctl -p
+fi
+
+# Add APT repos
+add-apt-repository main
+add-apt-repository universe
+add-apt-repository restricted
+add-apt-repository multiverse
+
+# Upgrade
+apt-get update
+apt-get upgrade
+apt-get full-upgrade
+
+# Install Dependencies
+apt-get install -y --reinstall \
+    nano \
+    git \
+    build-essential \
+    libssl-dev \
+    libffi-dev \
+    python3-dev \
+    python3-pip \
+    python-dev \
+    python-pip
+python3 -m pip install --disable-pip-version-check --upgrade --force-reinstall pip==18.1
+python3 -m pip install --disable-pip-version-check --upgrade --force-reinstall setuptools
+python3 -m pip install --disable-pip-version-check --upgrade --force-reinstall \
+    pyOpenSSL \
+    requests \
+    netaddr
+python -m pip install --disable-pip-version-check --upgrade --force-reinstall pip==18.1
+python -m pip install --disable-pip-version-check --upgrade --force-reinstall setuptools
+python -m pip install --disable-pip-version-check --upgrade --force-reinstall \
+    pyOpenSSL \
+    requests \
+    netaddr \
+    ansible==${1-2.5.11}
+
+## Copy pip to /usr/bin
+cp /usr/local/bin/pip /usr/bin/pip
+cp /usr/local/bin/pip3 /usr/bin/pip3
+
+apt-get install dialog -y
+git clone https://github.com/Admin9705/PlexGuide.com-The-Awesome-Plex-Server.git /opt/plexguide && cp /opt/plexguide/menu/interface/alias/templates/plexguide /bin/
+cp /opt/plexguide/menu/interface/alias/templates/plexguide /bin/plexguide
+
+chmod 755 /bin/plexguide
+chown 1000:1000 /bin/plexguide
+
+python3 /opt/plexguide/menu/interface/install/scripts/pgconsole.py
+
+clear
+
+echo "Execute PlexGuide Anytime By Typing: plexguide"
+echo
--- menu/interface/install/scripts/pgconsole.py
+++ menu/interface/install/scripts/pgconsole.py
@@ -13,14 +13,8 @@
 #   under the GPL along with build & install instructions.
 #
 #################################################################################
-
 # Import for Bash Ending
 from subprocess import call
-# Install PlexGuide Console if Missing
-rc = call("pip list --format columns --disable-pip-version-check | grep plexguide-menu > /var/plexguide/apip.check", shell=True)
 
-with open('/var/plexguide/apip.check', 'r') as myfile:
-    apip=myfile.read().replace('\n', '')
-
-    if apip == '':
-        rc = call("echo && echo 'Standby - Installing: PG Interface v0.0.3' && sleep 4 && echo && pip install git+git://github.com/Admin9705/plexguide-menu.git --disable-pip-version-check", shell=True)
+# Installing PlexGuide Console
+rc = call("echo && echo 'Standby - Installing: PG Interface v0.0.3' && sleep 4 && echo && pip install git+git://github.com/Admin9705/plexguide-menu.git --disable-pip-version-check", shell=True)
