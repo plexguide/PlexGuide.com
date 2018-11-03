5214583cb61610888c879d6cfb9ab755564c9e12
Admin9705 <user@dunn.cloud>
2018-11-02 21:49:08 -0400

    update

---
+++ install/yesno.sh
@@ -0,0 +1,40 @@
+#!/usr/bin/env python3
+#
+# GitHub:   https://github.com/Admin9705/PlexGuide.com-The-Awesome-Plex-Server
+# Author:   Admin9705
+# URL:      https://plexguide.com
+#
+# PlexGuide Copyright (C) 2018 PlexGuide.com
+# Licensed under GNU General Public License v3.0 GPL-3 (in short)
+#
+#   You may copy, distribute and modify the software as long as you track
+#   changes/dates in source files. Any modifications to our software
+#   including (via compiler) GPL-licensed code must also be made available
+#   under the GPL along with build & install instructions.
+#
+#################################################################################
+tee <<-EOF
+
+┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓
+┃ 🌎 AGREEMENT: Install PlexGuide                                     ┃
+┣━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┫
+┃                                                                     ┃
+┃ Do you agree to install PlexGuide in accordance with the GNUv3      ┃
+┃ License?                                                            ┃
+┃                                                                     ┃
+┃ PLEASE STANDBY!                                                     ┃
+┃                                                                     ┃
+┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛
+
+EOF
+
+# Ask User
+read -p "SELECT (y/n)? " -n 1 -r
+
+# Actions Based on Response
+if [[ ! $REPLY =~ ^[Yy]$ ]]; then
+  bash /opt/plexguide/roles/ending/ending.sh
+tee <<-EOF
+User Failed to Select [y]!
+To start again, type >>> bash install.sh
+EOF
