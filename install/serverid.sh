7494d975df5cbb8ab40c65f2993dbbb64e2653ca
Admin9705 <user@dunn.cloud>
2018-11-02 22:32:02 -0400

    update

---
+++ install/serverid.sh
@@ -0,0 +1,60 @@
+#!/usr/bin/env python3
+#
+# GitHub:   https://github.com/Admin9705/PlexGuide.com-The-Awesome-Plex-Server
+# Author:   Admin9705
+# URL:      https://plexguide.com
+#
+# PlexGuide Copyright (C) 2018 PlexGuide.com
+# Licensed under GNU General Public License v3.0 GPL-3 (in short)
+#
+#   You may copy, distribute and modify the software as long as you track
+#   changes/dates in source files. Any modifications to our software
+#   including (via compiler) GPL-licensed code must also be made available
+#   under the GPL along with build & install instructions.
+#
+#################################################################################
+touch /var/plexguide/pg.preinstall.stored
+start=$( cat /var/plexguide/pg.preinstall )
+stored=$( cat /var/plexguide/pg.preinstall.stored )
+
+if [ "$start" != "$stored" ]; then
+
+tee <<-EOF
+
+┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓
+┃ ⌛ ESTABLISHING: Server ID                                          ┃
+┣━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┫
+┃                                                                     ┃
+┃ To establish the identification of the server and for identity of   ┃
+┃ of backin up the server.                                            ┃
+┃                                                                     ┃
+┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛
+EOF
+
+# Standby
+read -p 'TYPE a Server ID Name & Press [ENTER] : ' typed < /dev/tty
+
+  if [ "$typed" == "" ]; then
+tee <<-EOF
+
+┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓
+┃ ⛔️ WARNING! WARNING! WARNING!                                       ┃
+┣━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┫
+┃                                                                     ┃
+┃ The Server ID Cannot Be Blank!                                      ┃
+┃                                                                     ┃
+┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛
+EOF
+sleep 3
+bash /opt/plexguide/install/serverid.sh
+exit
+else
+echo "$typed" > /var/plexguide/server.id
+tee <<-EOF
+
+┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓
+┃ ✅️ PASS: ServerID Set                                               ┃
+┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛
+EOF
+sleep 3
+fi
