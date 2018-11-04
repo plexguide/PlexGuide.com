0ecd230fc220ffbde6e39f3d0606082dbb786175
Admin9705 <user@dunn.cloud>
2018-11-04 14:30:41 -0500

    update

--- menu/data/data.sh
+++ menu/data/data.sh
@@ -58,7 +58,7 @@ elif [ "$typed" == "4" ]; then
 elif [ "$typed" == "5" ]; then
   bash /opt/plexguide/
 elif [ "$typed" == "6" ]; then
-  bash /opt/plexguide/menu/data/restore.id
+  bash /opt/plexguide/menu/data/restoreid.sh
 elif [ "$typed" == "7" ]; then
   exit
 else
