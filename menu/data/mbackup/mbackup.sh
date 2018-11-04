d978699d1740be74bcec75b0476f9bf8ecf90bdf
Admin9705 <user@dunn.cloud>
2018-11-04 09:24:40 -0500

    update

--- menu/data/mbackup/mbackup.sh
+++ menu/data/mbackup/mbackup.sh
@@ -15,7 +15,7 @@
 #################################################################################
 
 # Recalls List for Backup Operations
-ls -la /opt/appdata | awk '{ print $9}' | tail -n +4 > /tmp/backup.list
+ls -la /opt/appdata | awk '{ print $9 }' | tail -n +4 > /tmp/backup.list
 echo > /tmp/backup.build
 # Remove Items fromt the List
 
