9ed00c677f06881a7a3d807acd23de3130116e2d
Admin9705 <user@dunn.cloud>
2018-11-06 21:45:34 -0500

    update

--- menu/apps/apps.sh
+++ menu/apps/apps.sh
@@ -39,7 +39,7 @@ EOF
 cat /var/plexguide/programs.temp
 echo && echo
 # Standby
-echo "↘️  TO EXIT - type >>> exit" && echo
+echo "↘️   TO EXIT - type >>> exit" && echo
 read -p '🌎  TYPE the App Name & Press [ENTER]: ' typed < /dev/tty
 
   if [ "$typed" == "" ]; then
