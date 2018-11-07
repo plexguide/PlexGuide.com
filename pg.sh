24313101d582bbe27c5950df6820ac489b694015
Admin9705 <user@dunn.cloud>
2018-11-06 23:46:44 -0500

    update

--- pg.sh
+++ pg.sh
@@ -83,7 +83,7 @@ bash /opt/plexguide/install/serverid.sh
 bash /opt/plexguide/install/dependency.sh
 bash /opt/plexguide/install/folders.sh
 bash /opt/plexguide/menu/interface/install/scripts/docker.sh
-bash /opt/plexguide/menu/interface/install/scripts/docstart.sh ### Good
+bash /opt/plexguide/menu/interface/install/scripts/docstart.sh 
 
 echo "portainer" > /tmp/program_selection && ansible-playbook /opt/plexguide/programs/core/main.yml --extra-vars "quescheck=off cron=off display=off" &>/dev/null &
 
