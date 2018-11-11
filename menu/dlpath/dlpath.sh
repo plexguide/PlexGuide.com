649f9740db556d6a8348c1f76579dbeafc1394df
Admin9705 <user@dunn.cloud>
2018-11-11 09:14:07 -0500

    update

---
+++ menu/dlpath/dlpath.sh
@@ -0,0 +1,246 @@
+#!/bin/bash
+#
+# [Ansible Role]
+#
+# GitHub:   https://github.com/Admin9705/PlexGuide.com-The-Awesome-Plex-Server
+# Author:   Admin9705
+# URL:      https://plexguide.com
+#
+# PlexGuide Copyright (C) 2018 PlexGuide.com
+# Licensed under GNU General Public License v3.0 GPL-3 (in short)
+#
+#   You may copy, distribute and modify the software as long as you track
+#   changes/dates in source files. Any modifications to our software
+#   including (via compiler) GPL-licensed code must also be made available
+#   under the GPL along with build & install instructions.
+#
+#################################################################################
+pgpath=$(cat /var/plexguide/server.hd.path)
+
+break=no
+while [ "$break" == "no" ]; do
+
+tee <<-EOF
+
+---------------------------------------------------------------------------
+PlexGuide Download/Processing Selection Interface
+---------------------------------------------------------------------------
+
+NOTE: PG does not format harddrives. User is responsible to format and mount
+their secondary drives! You may change this later in the PG Settings!
+
+PURPOSE: Allow DOWNLOADS to process on a SECONDARY DRIVE (which is good for
+small or slow primary drives). Like Windows, you can have your stuff download
+and process on a (D): Drive instead of the (C): Drive.
+
+WARNING: Don't know what to do? Leave Select - NO -
+
+Default Path: /mnt
+Current Path: $pgpath
+
+EOF
+read -p "Change the Current Download/Processing Path? (y/n): " -n 1 -r
+echo    # move cursor to a new line
+if [[ ! $REPLY =~ ^[Yy]$ ]]
+then
+  echo ""
+  echo "---------------------------------------------------"
+  echo "SYSTEM MESSAGE: [Y] Key was NOT Selected - Exiting!"
+  echo "---------------------------------------------------"
+  echo ""
+  read -n 1 -s -r -p "Press [ANY KEY] to Continue "
+  echo ""
+  exit 1;
+fi
+
+tee <<-EOF
+
+---------------------------------------------------------------------------
+SYSTEM MESSAGE: User Selected to Change the Path!
+---------------------------------------------------------------------------
+
+Current Path: $pgpath
+
+NOTE: When typing your path following the examples as shown below! We will
+then attempt to check to see if your path exists! If not, we will let you know!
+
+Examples:
+/mnt/mymedia
+/secondhd/media
+/myhd/storage/media
+
+echo "To QUIT, type >>> exit"
+EOF
+
+read -p 'Type the NEW PATH (follow example above): ' typed
+
+storage=$(grep $typed /var/plexguide/ver.temp)
+
+  if [ "$typed" == "exit" ]; then
+tee <<-EOF
+
+---------------------------------------------------------------------------
+SYSTEM MESSAGE: Exiting the Downloading/Processing Selection Interface
+---------------------------------------------------------------------------
+
+EOF
+  read -n 1 -s -r -p "Press [ANY KEY] to Continue "
+  echo ""
+  exit
+else
+tee <<-EOF
+
+---------------------------------------------------------------------------
+SYSTEM MESSAGE: Checking Path: $typed
+---------------------------------------------------------------------------
+EOF
+sleep 1.5
+
+##################################################### TYPED CHECKERS - START
+  typed2=$typed
+  bonehead=no
+  ##### If BONEHEAD forgot to add a / in the beginning, we fix for them
+  initial="$(echo $typed | head -c 1)"
+  if [ "$initial" != "/" ]; then
+    typed="/$typed"
+    bonehead=yes
+  fi
+  ##### If BONEHEAD added a / at the end, we fix for them
+  initial="${typed: -1}"
+  if [ "$initial" == "/" ]; then
+    typed=${typed::-1}
+    bonehead=yes
+  fi
+
+  ##### Notify User is a Bonehead
+  if [ "$bonehead" == "yes" ]; then
+tee <<-EOF
+
+---------------------------------------------------------------------------
+ALERT: We Fixed Your Typos (pay attention to the example next time)
+---------------------------------------------------------------------------
+
+You Typed : $typed2
+Changed To: $typed
+
+EOF
+
+read -n 1 -s -r -p "Press [ANY KEY] to Continue "
+else
+tee <<-EOF
+
+---------------------------------------------------------------------------
+SYSTEM MESSAGE: Input is Valid
+---------------------------------------------------------------------------
+EOF
+  fi
+##################################################### TYPED CHECKERS - START
+tee <<-EOF
+
+---------------------------------------------------------------------------
+SYSTEM MESSAGE: Checking if the Location is Valid
+---------------------------------------------------------------------------
+EOF
+sleep 1.5
+
+mkdir $typed/test 1>/dev/null 2>&1
+
+file="$typed/test"
+  if [ -e "$file" ]; then
+
+tee <<-EOF
+
+---------------------------------------------------------------------------
+SYSTEM MESSAGE: The Path Exists! Review the Amount of Space You Have!
+---------------------------------------------------------------------------
+
+Your Current Space for $typed:
+
+EOF
+df -h $typed
+echo ""
+echo "Pay Attention to How Much Space You Have!"
+echo ""
+
+read -p "Continue to Set $typed? (y/n): " -n 1 -r
+echo    # move cursor to a new line
+if [[ ! $REPLY =~ ^[Yy]$ ]]
+then
+  echo ""
+  echo "---------------------------------------------------------------------------"
+  echo "SYSTEM MESSAGE: [Y] Key was NOT Selected - Restarting!"
+  echo "---------------------------------------------------------------------------"
+  echo ""
+  read -n 1 -s -r -p "Press [ANY KEY] to Continue "
+  echo ""
+  bash /opt/plexguide/menu/interface/dlpath/main.sh
+  exit
+fi
+
+tee <<-EOF
+
+---------------------------------------------------------------------------
+SYSTEM MESSAGE: CHMODing & CHOWNing: $typed
+---------------------------------------------------------------------------
+
+Note: Please Standby
+EOF
+sleep 2
+
+    chown 1000:1000 "$typed"
+    chmod 0775 "$typed"
+    rm -rf "$typed/test"
+    echo $typed > /var/plexguide/server.hd.path
+    break=off
+
+tee <<-EOF
+
+---------------------------------------------------------------------------
+SYSTEM MESSAGE: Rewriting Folders! STANDBY!
+---------------------------------------------------------------------------
+
+EOF
+sleep 2
+ansible-playbook /opt/plexguide/menu/interface/folders/main.yml
+tee <<-EOF
+
+---------------------------------------------------------------------------
+SYSTEM MESSAGE: Rebuilding Containers! STANDBY!
+---------------------------------------------------------------------------
+
+EOF
+sleep 2
+
+bash /opt/plexguide/menu/interface/dlpath/rebuild.sh
+
+tee <<-EOF
+
+---------------------------------------------------------------------------
+SYSTEM MESSAGE: Process Complete!
+---------------------------------------------------------------------------
+
+EOF
+read -n 1 -s -r -p "Press [ANY KEY] to Continue "
+echo ""
+  else
+tee <<-EOF
+
+---------------------------------------------------------------------------
+SYSTEM MESSAGE: $typed DOES NOT Exist!
+---------------------------------------------------------------------------
+
+Note: Restarting the Process! Remember, you have to format your secondary
+location (if another harddrive). You must ensure that linux is able to READ
+your location.
+
+Advice: Exit PG and (Test) Type >>> mkdir $typed/testfolder
+
+EOF
+read -n 1 -s -r -p "Press [ANY KEY] to Continue "
+echo ""
+  fi
+
+### Final FI
+fi
+
+done
---
+++ menu/dlpath/dlpath2.sh
@@ -0,0 +1,87 @@
+#!/bin/bash
+#
+# GitHub:   https://github.com/Admin9705/PlexGuide.com-The-Awesome-Plex-Server
+# Author:   Admin9705
+# URL:      https://plexguide.com
+#
+# PlexGuide Copyright (C) 2018 PlexGuide.com
+# Licensed under GNU General Public License v3.0 GPL-3 (in short)
+#
+#   You may copy, distribute and modify the software as long as you track
+#   changes/dates in source files. Any modifications to our software
+#   including (via compiler) GPL-licensed code must also be made available
+#   under the GPL along with build & install instructions.
+#
+#################################################################################
+
+# Create Variables (If New) & Recall
+variable () {
+  file="$1"
+  if [ ! -e "$file" ]; then echo "$2" > $1; fi
+}
+
+variable /var/plexguide/server.hd.path "/mnt"
+pgpath=$(cat /var/plexguide/server.hd.path)
+###################### FOR VARIABLS ROLE SO DOESNT CREATE RED - START
+
+# Menu Interface
+tee <<-EOF
+
+━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
+🌎  PG Processing Disk Interface
+━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
+
+🌵  Processing Space: $used of $capacity | $percentage Used Capacity
+    Processing Disk : $pgpath
+
+⚠️  NOTE: PG does not format your second disk, nor mount it! We can
+only assist by changing the location path!
+
+Purpose: Enables PG System to process items on a SECONDARY Drive rather
+than tax the PRIMARY DRIVE. Like Windows, you can have your items
+process on a (D): Drive instead of on a (C): Drive.
+
+Do You Want To Change the Processing Disk?
+[1] No
+[2] Yes
+
+━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
+EOF
+# Standby
+read -p 'Type a Number | Press [ENTER]: ' typed < /dev/tty
+
+  if [ "$typed" == "1" ]; then
+
+    if [ "$edition" == "PG Edition - GDrive" ]; then bash /opt/plexguide/menu/transport/transport.sh
+    elif [ "$edition" == "PG Edition - GCE Feed" ]; then bash /opt/plexguide/menu/transport/transport.sh
+    elif [ "$edition" == "PG Edition - HD Multi" ]; then bash /opt/plexguide/menu/multihd/scripts/main.sh
+    elif [ "$edition" == "PG Edition - HD Solo" ]; then
+tee <<-EOF
+
+━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
+⛔️ WARNING! - Using Solo HD Edition! You Cannot Set Mounts! Restarting!
+━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
+EOF
+sleep 3
+     fi
+
+elif [ "$typed" == "2" ]; then
+  bash /opt/plexguide/menu/traefik/traefik.sh
+elif [ "$typed" == "3" ]; then
+  bash /opt/plexguide/menu/apps/apps.sh
+elif [ "$typed" == "4" ]; then
+  bash /opt/plexguide/menu/removal/removal.sh
+elif [ "$typed" == "5" ]; then
+  bash /opt/plexguide/menu/tools/tools.sh
+elif [ "$typed" == "6" ]; then
+  bash /opt/plexguide/menu/settings/settings.sh
+elif [ "$typed" == "7" ]; then
+  bash /opt/plexguide/roles/ending/ending.sh
+  exit
+else
+  bash /opt/plexguide/menu/start/start.sh
+  exit
+fi
+
+bash /opt/plexguide/menu/start/start.sh
+exit
