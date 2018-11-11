da297a9dc7ff52afa187bad51600e3fd04929bb7
Admin9705 <user@dunn.cloud>
2018-11-11 09:34:27 -0500

    update

--- menu/dlpath/dlpath.sh
+++ menu/dlpath/dlpath.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 #
-# [Ansible Role]
-#
 # GitHub:   https://github.com/Admin9705/PlexGuide.com-The-Awesome-Plex-Server
 # Author:   Admin9705
 # URL:      https://plexguide.com
@@ -15,232 +13,106 @@
 #   under the GPL along with build & install instructions.
 #
 #################################################################################
+
+# Create Variables (If New) & Recall
+variable () {
+  file="$1"
+  if [ ! -e "$file" ]; then echo "$2" > $1; fi
+}
+
+# For ZipLocations
+
+variable /var/plexguide/server.hd.path "/mnt"
 pgpath=$(cat /var/plexguide/server.hd.path)
 
-break=no
-while [ "$break" == "no" ]; do
+used=$(df -h $pgpath | tail -n +2 | awk '{print $3}')
+capacity=$(df -h $pgpath | tail -n +2 | awk '{print $2}')
+percentage=$(df -h $pgpath | tail -n +2 | awk '{print $5}')
+###################### FOR VARIABLS ROLE SO DOESNT CREATE RED - START
 
+# Menu Interface
 tee <<-EOF
 
----------------------------------------------------------------------------
-PlexGuide Download/Processing Selection Interface
----------------------------------------------------------------------------
+━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
+🌎  PG Processing Disk Interface
+━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
 
-NOTE: PG does not format harddrives. User is responsible to format and mount
-their secondary drives! You may change this later in the PG Settings!
+🌵  Processing Disk : $pgpath
+    Processing Space: $used of $capacity | $percentage Used Capacity
 
-PURPOSE: Allow DOWNLOADS to process on a SECONDARY DRIVE (which is good for
-small or slow primary drives). Like Windows, you can have your stuff download
-and process on a (D): Drive instead of the (C): Drive.
+NOTE: PG does not format your second disk, nor mount it! We can
+only assist by changing the location path!
 
-WARNING: Don't know what to do? Leave Select - NO -
+Purpose: Enables PG System to process items on a SECONDARY Drive rather
+than tax the PRIMARY DRIVE. Like Windows, you can have your items
+process on a (D): Drive instead of on a (C): Drive.
 
-Default Path: /mnt
-Current Path: $pgpath
+Do You Want To Change the Processing Disk?
 
+[1] No
+[2] Yes
+
+━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
 EOF
-read -p "Change the Current Download/Processing Path? (y/n): " -n 1 -r
-echo    # move cursor to a new line
-if [[ ! $REPLY =~ ^[Yy]$ ]]
-then
-  echo ""
-  echo "---------------------------------------------------"
-  echo "SYSTEM MESSAGE: [Y] Key was NOT Selected - Exiting!"
-  echo "---------------------------------------------------"
-  echo ""
-  read -n 1 -s -r -p "Press [ANY KEY] to Continue "
-  echo ""
-  exit 1;
-fi
+# Standby
+read -p '↘️   Type a Number | Press [ENTER]: ' typed < /dev/tty
 
+  if [ "$typed" == "1" ]; then
+    exit
+elif [ "$typed" == "2" ]; then
 tee <<-EOF
 
----------------------------------------------------------------------------
-SYSTEM MESSAGE: User Selected to Change the Path!
----------------------------------------------------------------------------
+━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
+🍖   NOM NOM: Selected to Change the Processing Path
+━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
 
-Current Path: $pgpath
+🌵  Current Processing Disk : $pgpath
 
-NOTE: When typing your path following the examples as shown below! We will
-then attempt to check to see if your path exists! If not, we will let you know!
+NOTE: Type the path as show in the examples below! PG will then attempt
+to see if your path exists!
 
 Examples:
-/mnt/mymedia
-/secondhd/media
-/myhd/storage/media
+(1) /mnt/mymedia   (2) /secondhd/media   (3) /myhd/storage/media
 
-echo "To QUIT, type >>> exit"
+Do You Want To Continue to Change the Processing Disk?
+
+[1] No
+[2] Yes
+
+━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
 EOF
 
-read -p 'Type the NEW PATH (follow example above): ' typed
-
-storage=$(grep $typed /var/plexguide/ver.temp)
-
-  if [ "$typed" == "exit" ]; then
-tee <<-EOF
-
----------------------------------------------------------------------------
-SYSTEM MESSAGE: Exiting the Downloading/Processing Selection Interface
----------------------------------------------------------------------------
-
-EOF
-  read -n 1 -s -r -p "Press [ANY KEY] to Continue "
-  echo ""
-  exit
-else
-tee <<-EOF
-
----------------------------------------------------------------------------
-SYSTEM MESSAGE: Checking Path: $typed
----------------------------------------------------------------------------
-EOF
-sleep 1.5
-
-##################################################### TYPED CHECKERS - START
-  typed2=$typed
-  bonehead=no
-  ##### If BONEHEAD forgot to add a / in the beginning, we fix for them
-  initial="$(echo $typed | head -c 1)"
-  if [ "$initial" != "/" ]; then
-    typed="/$typed"
-    bonehead=yes
-  fi
-  ##### If BONEHEAD added a / at the end, we fix for them
-  initial="${typed: -1}"
-  if [ "$initial" == "/" ]; then
-    typed=${typed::-1}
-    bonehead=yes
-  fi
-
-  ##### Notify User is a Bonehead
-  if [ "$bonehead" == "yes" ]; then
-tee <<-EOF
-
----------------------------------------------------------------------------
-ALERT: We Fixed Your Typos (pay attention to the example next time)
----------------------------------------------------------------------------
-
-You Typed : $typed2
-Changed To: $typed
-
-EOF
-
-read -n 1 -s -r -p "Press [ANY KEY] to Continue "
-else
-tee <<-EOF
-
----------------------------------------------------------------------------
-SYSTEM MESSAGE: Input is Valid
----------------------------------------------------------------------------
-EOF
-  fi
-##################################################### TYPED CHECKERS - START
-tee <<-EOF
-
----------------------------------------------------------------------------
-SYSTEM MESSAGE: Checking if the Location is Valid
----------------------------------------------------------------------------
-EOF
-sleep 1.5
-
-mkdir $typed/test 1>/dev/null 2>&1
-
-file="$typed/test"
-  if [ -e "$file" ]; then
-
-tee <<-EOF
-
----------------------------------------------------------------------------
-SYSTEM MESSAGE: The Path Exists! Review the Amount of Space You Have!
----------------------------------------------------------------------------
-
-Your Current Space for $typed:
-
-EOF
-df -h $typed
-echo ""
-echo "Pay Attention to How Much Space You Have!"
-echo ""
-
-read -p "Continue to Set $typed? (y/n): " -n 1 -r
-echo    # move cursor to a new line
-if [[ ! $REPLY =~ ^[Yy]$ ]]
-then
-  echo ""
-  echo "---------------------------------------------------------------------------"
-  echo "SYSTEM MESSAGE: [Y] Key was NOT Selected - Restarting!"
-  echo "---------------------------------------------------------------------------"
-  echo ""
-  read -n 1 -s -r -p "Press [ANY KEY] to Continue "
-  echo ""
-  bash /opt/plexguide/menu/interface/dlpath/main.sh
-  exit
-fi
-
-tee <<-EOF
-
----------------------------------------------------------------------------
-SYSTEM MESSAGE: CHMODing & CHOWNing: $typed
----------------------------------------------------------------------------
-
-Note: Please Standby
-EOF
-sleep 2
-
-    chown 1000:1000 "$typed"
-    chmod 0775 "$typed"
-    rm -rf "$typed/test"
-    echo $typed > /var/plexguide/server.hd.path
-    break=off
-
-tee <<-EOF
-
----------------------------------------------------------------------------
-SYSTEM MESSAGE: Rewriting Folders! STANDBY!
----------------------------------------------------------------------------
-
-EOF
-sleep 2
-ansible-playbook /opt/plexguide/menu/interface/folders/main.yml
-tee <<-EOF
-
----------------------------------------------------------------------------
-SYSTEM MESSAGE: Rebuilding Containers! STANDBY!
----------------------------------------------------------------------------
-
-EOF
-sleep 2
-
-bash /opt/plexguide/menu/interface/dlpath/rebuild.sh
-
-tee <<-EOF
-
----------------------------------------------------------------------------
-SYSTEM MESSAGE: Process Complete!
----------------------------------------------------------------------------
-
-EOF
-read -n 1 -s -r -p "Press [ANY KEY] to Continue "
-echo ""
+# SubQuestion About Continuing
+  if [ "$typed" == "1" ]; then
+    exit
+  elif [ "$typed" == "2" ]; then
+    a=b
   else
 tee <<-EOF
 
----------------------------------------------------------------------------
-SYSTEM MESSAGE: $typed DOES NOT Exist!
----------------------------------------------------------------------------
-
-Note: Restarting the Process! Remember, you have to format your secondary
-location (if another harddrive). You must ensure that linux is able to READ
-your location.
-
-Advice: Exit PG and (Test) Type >>> mkdir $typed/testfolder
-
+━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
+🍖   NOM NOM: Failed to Make a Valid Selection! Restarting the Process!
+━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
 EOF
-read -n 1 -s -r -p "Press [ANY KEY] to Continue "
-echo ""
+sleep 3
+bash /opt/plexguide/menu/dlpath/dlpath.sh
   fi
 
-### Final FI
+# Standby
+read -p '↘️   Type a Number | Press [ENTER]: ' typed < /dev/tty
+
+
+else
+# Repeats If User Fails to Answer Primary Question Correctly
+tee <<-EOF
+
+━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
+🍖   NOM NOM: Failed to Make a Valid Selection! Restarting the Process!
+━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
+EOF
+sleep 3
+bash /opt/plexguide/menu/dlpath/dlpath.sh
 fi
 
-done
+bash /opt/plexguide/menu/dlpath/dlpath.sh
+exit
