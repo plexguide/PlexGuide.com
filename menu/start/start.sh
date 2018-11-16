98f1d75df922bdcef42cdeb509e429a84a1492b7
Admin9705 <user@dunn.cloud>
2018-11-15 22:28:54 -0500

    update

--- menu/start/start.sh
+++ menu/start/start.sh
@@ -196,6 +196,8 @@ elif [ "$typed" == "3" ]; then
   bash /opt/plexguide/menu/apps/apps.sh
 elif [ "$typed" == "4" ]; then
   bash /opt/plexguide/menu/removal/removal.sh
+elif [ "$typed" == "5" ]; then
+  bash /opt/plexguide/menu/cloudselect/cloudselect.sh
 elif [ "$typed" == "6" ]; then
   bash /opt/plexguide/menu/tools/tools.sh
 elif [ "$typed" == "7" ]; then
