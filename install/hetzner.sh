3fe8bca3ffac71c867030e6fab7a49a58c0f474d
Admin9705 <24727006+Admin9705@users.noreply.github.com>
2018-11-18 16:32:43 -0500

    update

--- install/hetzner.sh
+++ install/hetzner.sh
@@ -49,12 +49,12 @@ if [ -e "$file" ]; then rm -rf /bin/hcloud; fi
 
 #mkdir -p /"$path"/"$tarminus"/
 wget -P /$path "https://github.com/hetznercloud/cli/releases/download/$version/$tar"
-tar -xvf $tar
+tar -xvf /$path/$tar
 echo $tarminus
 mv /$path/$tarminus/bin/hcloud /bin/
 sleep 500
-rm -rf /$path/$tarminus 1>/dev/null 2>&1
-rm -rf /$path/$tar 1>/dev/null 2>&1
+#rm -rf /$path/$tarminus 1>/dev/null 2>&1
+#rm -rf /$path/$tar 1>/dev/null 2>&1
 
 # Prevents From Repeating
 cat /var/plexguide/pg.hetzner > /var/plexguide/pg.hetzner.stored
