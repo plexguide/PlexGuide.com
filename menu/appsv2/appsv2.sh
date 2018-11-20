d50d18ee43cfab57bd4e294dbf1eb6eab23193b1
Admin9705 <user@dunn.cloud>
2018-11-20 11:47:21 -0500

    update

--- menu/appsv2/appsv2.sh
+++ menu/appsv2/appsv2.sh
@@ -26,7 +26,7 @@ while read p; do
     echo " " >> /var/plexguide/programs.temp
   fi
 
-done </opt/plexguide/menu/appsv2/appsv2.list
+done </opt/plexguide/menu/containers/_apps.list
 
 tee <<-EOF
 
