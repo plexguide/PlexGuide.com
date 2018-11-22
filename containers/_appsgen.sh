d6ed438b8d40727936d2494b6aff4c568d01838c
Admin0705 <user@dunn.cloud>
2018-11-22 07:40:08 -0500

    update

--- containers/_appsgen.sh
+++ containers/_appsgen.sh
@@ -1,11 +1,7 @@
 ls -la /opt/plexguide/containers/ | sed -e 's/.yml//g' \
 | awk '{print $9}' | tail -n +4  > /var/plexguide/app.list
 
-sed -i -e "/traefik/d" /var/plexguide/container.running
-sed -i -e "/watchtower/d" /var/plexguide/container.running
-sed -i -e "/word*/d" /var/plexguide/container.running
-sed -i -e "/plex/d" /var/plexguide/container.running
-sed -i -e "/x2go*/d" /var/plexguide/container.running
-sed -i -e "/bitwarden/d" /var/plexguide/container.running
-sed -i -e "/ombi/d" /var/plexguide/container.running
-sed -i -e "/portainer/d" /var/plexguide/container.running
+sed -i -e "/traefik/d" /var/plexguide/app.list
+sed -i -e "/_appsgen.sh/d" /var/plexguide/app.list
+sed -i -e "/_c*/d" /var/plexguide/app.list
+sed -i -e "/_a*/d" /var/plexguide/app.list
