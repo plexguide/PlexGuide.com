6a53ac3302f0d2e47ea736e73cdaeec01f910b39
Admin0705 <user@dunn.cloud>
2018-11-22 08:38:24 -0500

    update

--- containers/_appsgen.sh
+++ containers/_appsgen.sh
@@ -24,4 +24,4 @@ sed -i -e "/_appsgen.sh/d" /var/plexguide/app.list
 sed -i -e "/_c*/d" /var/plexguide/app.list
 sed -i -e "/_a*/d" /var/plexguide/app.list
 sed -i -e "/templates/d" /var/plexguide/app.list
-sed -i -e "/*.retry/d" /var/plexguide/app.list
+sed -i -e "/retry/d" /var/plexguide/app.list
