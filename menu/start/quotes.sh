be2cb2a5d33c2219ca7e56977892887fc4b90dff
Admin0705 <user@dunn.cloud>
2018-11-24 09:53:46 -0500

    update

--- menu/start/quotes.sh
+++ menu/start/quotes.sh
@@ -1,13 +1,15 @@
 quote1 () {
-################################################################################
 echo "Manbearpig is in there and we all have to kill him while we all have the
 chance, I'm cereal!" > /var/plexguide/startup.quote
 echo "                                                       Al Gore ~ SouthPark" > /var/plexguide/startup.source
 }
 
-
-
+quote2 () {
+echo "Stan, don’t you know the first law of physics? Anything that’s fun costs
+at least eight dollars" > /var/plexguide/startup.quote
+echo "                                                       Cartman ~ SouthPark" > /var/plexguide/startup.source
+}
 
 
 # END FUNCTIONS ################################################################
-quote1
+quote2
