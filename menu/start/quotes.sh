e63c0535b517531f9ede535a4dc575603c3f2b84
Admin0705 <user@dunn.cloud>
2018-11-24 10:17:03 -0500

    update

--- menu/start/quotes.sh
+++ menu/start/quotes.sh
@@ -14,6 +14,23 @@ echo "Don't do drugs kids. There is a time and place for everything. It's called
 college." > /var/plexguide/startup.quote
 echo "                                                          Chef ~ SouthPark" > /var/plexguide/startup.source
 }
+
+quote4 () {
+echo "I left you plenty of food. It's at the supermarket." > /var/plexguide/startup.quote
+echo "                                       Peggy Bundy ~ Married With Children" > /var/plexguide/startup.source
+}
+
+quote5 () {
+echo "Well, because I just realized that everything I've been doing up to now,
+the bathing, the tooth brushing, changing of the socks, being nice to people,
+trying to succeed…it's all for nothing. All those things are designed to 
+attract. Why should I be attractive? I'm married...with children." > /var/plexguide/startup.quote
+echo "                                         Al Bundy ~ Married With Children" > /var/plexguide/startup.source
+}
+
+
+
+
 # END FUNCTIONS ################################################################
-num=$( echo $(($RANDOM % 4)) )
+num=$( echo $(($RANDOM % 5)) )
 quote$num
