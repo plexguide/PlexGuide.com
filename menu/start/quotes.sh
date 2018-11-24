b3d806bfa8b3b6debd257c77444fd9fbb3d5289b
Admin0705 <user@dunn.cloud>
2018-11-24 09:35:39 -0500

    update

--- menu/start/quotes.sh
+++ menu/start/quotes.sh
@@ -9,7 +9,12 @@
 quote1 () {
 echo
 read -p '⛔️ ERROR - BAD INPUT! | PRESS [ENTER] ' typed < /dev/tty
-question2
+
+echo "Manbearpig is in there and we all have to kill him while we all have the
+chance, I'm cereal!" > /var/plexguide/startup.quote
+echo "                                                       Al Gore ~ SouthPark" > /var/plexguide/startup.source
 }
 
 # END FUNCTIONS ################################################################
+
+quote1
