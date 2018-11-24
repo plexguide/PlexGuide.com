b8185ebbe6952d71c97c470f6e0b0f3beb5db1b3
Admin0705 <user@dunn.cloud>
2018-11-24 09:49:21 -0500

    update

--- menu/start/quotes.sh
+++ menu/start/quotes.sh
@@ -2,7 +2,8 @@ quote1 () {
 ################################################################################
 echo "Manbearpig is in there and we all have to kill him while we all have the
 chance, I'm cereal!" > /var/plexguide/startup.quote
-echo "                                                       Al Gore ~ SouthPark" > /var/plexguide/startup.source}
+echo "                                                       Al Gore ~ SouthPark" > /var/plexguide/startup.source
+}
 
 
 
