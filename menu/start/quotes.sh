9df6b41602aa99dd544b8f876c98ee8adbde3b81
Admin0705 <user@dunn.cloud>
2018-11-24 09:28:10 -0500

    update

---
+++ menu/start/quotes.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#
+# Title:      PlexGuide (Reference Title File)
+# Author(s):  Admin9705
+# URL:        https://plexguide.com - http://github.plexguide.com
+# GNU:        General Public License v3.0
+################################################################################
+
+badinput2 () {
+echo
+read -p '⛔️ ERROR - BAD INPUT! | PRESS [ENTER] ' typed < /dev/tty
+question2
+}
