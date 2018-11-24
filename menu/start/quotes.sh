5b8419896a80950a83d7cefbd1645516d1808dfc
Admin0705 <user@dunn.cloud>
2018-11-24 09:48:16 -0500

    update

--- menu/start/quotes.sh
+++ menu/start/quotes.sh
@@ -1,24 +1,12 @@
-#!/bin/bash
-#
-# Title:      PlexGuide (Reference Title File)
-# Author(s):  Admin9705
-# URL:        https://plexguide.com - http://github.plexguide.com
-# GNU:        General Public License v3.0
-################################################################################
-
 quote1 () {
-##########################################################################
-echo \n
-"Manbearpig is in there and we all have to kill him while we all have the
-chance, I'm cereal!
-\n" > /var/plexguide/startup.quote
-echo "\n
-                                                       Al Gore ~ SouthPark
-\n" > /var/plexguide/startup.source
-##########################################################################
+################################################################################
+echo "Manbearpig is in there and we all have to kill him while we all have the
+chance, I'm cereal!" > /var/plexguide/startup.quote
+echo "                                                       Al Gore ~ SouthPark" > /var/plexguide/startup.source}
 
-}
 
-# END FUNCTIONS
 
+
+
+# END FUNCTIONS ################################################################
 quote1
