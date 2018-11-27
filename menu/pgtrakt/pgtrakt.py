8355670b15187c0987472c7af2b1ef528fb81c1e
Admin9705 <24727006+Admin9705@users.noreply.github.com>
2018-11-27 12:28:20 -0500

    update

--- menu/pgtrakt/pgtrakt.py
+++ menu/pgtrakt/pgtrakt.py
@@ -19,7 +19,7 @@ notify = None
 
 # Click
 @click.group(help='Add new shows & movies to Sonarr/Radarr from Trakt.')
-@click.version_option('1.2.3', prog_name='traktarr')
+@click.version_option('1.2.3', prog_name='pgtrakt')
 @click.option(
     '--config',
     envvar='TRAKTARR_CONFIG',
