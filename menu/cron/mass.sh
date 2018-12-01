26bd0f945fc2e9191b98172b44d091ae3277142b
Admin9705 <24727006+Admin9705@users.noreply.github.com>
2018-11-30 23:24:46 -0500

    update

--- menu/cron/mass.sh
+++ menu/cron/mass.sh
@@ -13,6 +13,22 @@ mkdir -p /opt/appdata/plexguide/cron
 # FUNCTIONS START ##############################################################
 source /opt/plexguide/menu/functions/functions.sh
 
+weekleyrandom () {
+  echo $(($RANDOM % 23)) > /var/plexguide/cron/cron.hour
+  echo $(($RANDOM % 59)) > /var/plexguide/cron/cron.minute
+  echo $(($RANDOM % 6))> /var/plexguide/cron/$program.cron.day
+
+  ansible-playbook /opt/plexguide/menu/cron/cron.yml
+}
+
+dailyrandom () {
+  echo $(($RANDOM % 23)) > /var/plexguide/cron/cron.hour
+  echo $(($RANDOM % 59)) > /var/plexguide/cron/cron.minute
+  echo "*/1" > /var/plexguide/cron/$program.cron.day
+
+  ansible-playbook /opt/plexguide/menu/cron/cron.yml
+}
+
 # FIRST QUESTION
 question1 () {
 space=$(cat /var/plexguide/data.location)
@@ -33,75 +49,9 @@ tee <<-EOF
 EOF
 
   read -p '↘️  Type Number | Press [ENTER]: ' typed < /dev/tty
-  if [ "$typed" == "1" ]; then ansible-playbook /opt/plexguide/menu/cron/remove.yml && exit;
-elif [ "$typed" == "2" ]; then break="on";
-elif [ "$typed" == "3" ]; then bash /opt/plexguide/menu/data/location.sh && question1;
-else badinput; fi
+  if [ "$typed" == "1" ]; exit;
+elif [ "$typed" == "2" ]; then ansible-playbook /opt/plexguide/menu/cron/remove.yml && exit;
+elif [ "$typed" == "3" ]; then dailyrandom && ansible-playbook /opt/plexguide/menu/cron/cron.yml;
+elif [ "$typed" == "4" ]; then weeklyrandom && ansible-playbook /opt/plexguide/menu/cron/cron.yml;
+else badinput1; fi
 }
-
-# SECOND QUESTION
-question2 () {
-tee <<-EOF
-
-━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
-⌛ PG Cron - Backup How Often?
-━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
-
-WEEKLEY
-0 - Sunday
-1 - Monday
-2 - Tuesday
-3 - Wednesday
-4 - Thursday
-5 - Friday
-6 - Saturday
-
-DAILY
-7 - Daily
-
-━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
-EOF
-
-  read -p '↘️  Type Number | Press [ENTER]: ' typed < /dev/tty
-  if [[ "$typed" -ge "0" && "$typed" -le "7" ]]; then echo $typed > /var/plexguide/cron/cron.day && break=1;
-elif [ "$typed" == "8" ]; then echo "*/1" > /var/plexguide/cron/$program.cron.day && break=1;
-else badinput; fi
-}
-
-# THIRD QUESTION
-question3 () {
-tee <<-EOF
-
-━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
-⌛ PG Cron - Hour of the Day?
-━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
-
-Type an HOUR from [0 to 23]
-
-0  = 00:00 | 12AM
-12 = 12:00 | 12PM
-18 = 18:00 | 6 PM
-
-━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
-EOF
-
-  read -p '↘️  Type a Number | Press [ENTER]: ' typed < /dev/tty
-  if [[ "$typed" -ge "0" && "$typed" -le "23" ]]; then echo $typed > /var/plexguide/cron/cron.hour && break=1;
-else badinput; fi
-}
-
-# FUNCTIONS END ##############################################################
-
-break=off && while [ "$break" == "off" ]; do question1; done
-break=off && while [ "$break" == "off" ]; do question2; done
-break=off && while [ "$break" == "off" ]; do question3; done
-
-echo $(($RANDOM % 59)) > /var/plexguide/cron/cron.minute
-ansible-playbook /opt/plexguide/menu/cron/cron.yml
-
-#serverip=$(cat /opt/appdata/plexguide/server.info | tail -n +3 | head -n 1 | cut -d " " -f2-)
-#initialpw=$(cat /opt/appdata/plexguide/server.info | tail -n +4 | cut -d " " -f3-)
-#check=$(hcloud server list | grep "\<$sshin\>" | cut -d " " -f2- | cut -d " " -f2- | cut -d " " -f2-)
-#ipcheck=$(echo $check | awk '{ print $3 }')
-#⛔️  WARNING! - Must Configure RClone First /w >>> gdrive
-# read -n 1 -s -r -p "Press [ANY] Key to Continue "
