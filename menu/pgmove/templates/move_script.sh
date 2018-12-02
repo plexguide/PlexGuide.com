b46d435fc9cdf005e35342a31f232f44bcbca817
Admin9705 <user@dunn.cloud>
2018-12-02 10:27:45 -0500

    update

--- menu/pgmove/templates/move_script.sh
+++ menu/pgmove/templates/move_script.sh
@@ -9,7 +9,7 @@ if pidof -o %PPID -x "$0"; then
    exit 1
 fi
 
-sleep 30
+sleep 15
 while true
 do
 ## Sync, Sleep 2 Minutes, Repeat. BWLIMIT 9 Prevents Google 750GB Google Upload Ban
@@ -17,7 +17,7 @@ rclone move --config /opt/appdata/plexguide/rclone.conf --bwlimit {{bandwidth.st
   --tpslimit 6 --exclude='**partial~' --exclude="**_HIDDEN~" --exclude=".unionfs/**" \
   --exclude=".unionfs-fuse/**" --checkers=16 --max-size 99G --log-file=/opt/appdata/plexguide/rclone
   --log-level INFO --stats 5s /mnt/move {{ver.stdout}}:/
-sleep 60
+sleep 30
 # Remove empty directories (MrWednesday)
 find "/mnt/move/" -mindepth 2 -type d -empty -delete
 done
