1cce8601746fde1fbc5e0e667cd7b1518941bfd0
Admin9705 <user@dunn.cloud>
2018-12-05 09:44:24 -0500

    update

---
+++ menu/pgclone/gdrive.sh
@@ -0,0 +1,125 @@
+#!/bin/bash
+#
+# Title:      PGClone (A 100% PG Product)
+# Author(s):  Admin9705
+# URL:        https://plexguide.com - http://github.plexguide.com
+# GNU:        General Public License v3.0
+################################################################################
+
+timepull () {
+  accesstoken=$(cat /tmp/corn.txt | grep access_token | awk '{print $2}')
+  refreshtoken=$(cat /tmp/corn.txt | grep refresh_token | awk '{print $2}')
+  rcdate=$(date +'%Y-%m-%d')
+  rctime=$(date +"%H:%M:%S%7N" --date="$givenDate 60 minutes")
+  rczone=$(date +"%:z")
+  final=$(echo "${rcdate}T${rctime}${rczone}")
+}
+
+inputphase () {
+  read -p 'Client | PRESS [ENTER] ' public < /dev/tty
+  read -p 'Secret | PRESS [ENTER] ' secret < /dev/tty
+
+tee <<-EOF
+
+Copy & Paste Url into the Browser & Login with the CORRECT Account!
+
+https://accounts.google.com/o/oauth2/auth?client_id=$public&redirect_uri=urn:ietf:wg:oauth:2.0:oob&scope=https://www.googleapis.com/auth/drive&response_type=code
+
+EOF
+  read -p 'Token | PRESS [ENTER] ' token < /dev/tty
+
+  curl --request POST --data "code=$token&client_id=$public&client_secret=$secret&redirect_uri=urn:ietf:wg:oauth:2.0:oob&grant_type=authorization_code" https://accounts.google.com/o/oauth2/token > /opt/appdata/
+}
+
+testphase () {
+  echo "" > /opt/appdata/plexguide/test.conf
+  echo "[gdrive]" >> /opt/appdata/plexguide/test.conf
+  echo "client_id = " && echo -n "$public"
+  echo "client_secret =" && echo -n "$secret"
+  echo "type = drive" >> /opt/appdata/plexguide/test.conf
+  echo -n "token = {\"access_token\":${accesstoken}\"token_type\":\"Bearer\",\"refresh_token\":${refreshtoken}\"expiry\":\"${final}\"}" >> /opt/appdata/plexguide/test.conf
+
+tee <<-EOF
+
+━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
+🚀 System Messasge: Conducting GDrive Validation Checks
+━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
+EOF
+  sleep 1
+tee <<-EOF
+
+━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
+🚀 System Messasge: Creating Test Directory - gdrive:/plexguide
+━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
+EOF
+  sleep 1
+  rclone mkdir --config /opt/appdata/plexguide/test.conf gdrive:/plexguide
+tee <<-EOF
+
+━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
+🚀 System Messasge: Checking Existance of gdsa01:/plexguide
+━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
+
+EOF
+  rcheck=$(rclone lsd --config /opt/appdata/plexguide/test.conf gdrive: | grep -oP plexguide | head -n1)
+
+  if [ "$rcheck" != "plexguide" ];then
+tee <<-EOF
+
+━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
+🚀 System Messasge: Validation Checks Failed
+━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
+
+TIPS:
+1. Did you copy your username and password correctly?
+2. When you created the credentials, did you select "Other"?
+3. Did you enable your API?
+
+EOF
+    echo "Not Active" > /var/plexguide/gdrive.pgclone
+    read -p '🌍 Acknowledge Info | Press [ENTER] ' typed2 < /dev/tty
+    question1
+else
+tee <<-EOF
+
+━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
+🚀 System Messasge: Validation Checks Passed - GDrive
+━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
+EOF
+
+fi
+
+echo "Active" > /var/plexguide/gdrive.pgclone
+read -p '🌍 Acknowledge Info | Press [ENTER] ' typed2 < /dev/tty
+question1
+
+EOF
+}
+
+gdriveconfig () {
+  variable /var/plexguide/move.bw "10"
+  speed=$(cat /var/plexguide/move.bw)
+}
+
+
+question1 () {
+  touch /opt/appdata/plexguide/rclone.conf
+tee <<-EOF
+
+━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
+🌎 Welcome to PGClone ~ GDrive
+━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
+
+NOTE: PGClone modifies only the gdrive & tdrive for you (95% case use).
+Adding encryption requires using option #3 for gcrypt and/or tcrypt.
+
+1 - Configure: gdrive [not connected]
+2 - Configure: tdrive [not connected]
+3 - Configure: rclone file
+Z - Exit
+
+━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
+EOF
+}
+
+question1
