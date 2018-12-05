f54ae50feeabe340e7523e64e38f8bc51bee4853
Admin9705 <user@dunn.cloud>
2018-12-05 13:53:36 -0500

    update

---
+++ menu/functions/pgclone.sh
@@ -0,0 +1,108 @@
+#!/bin/bash
+#
+# Title:      PlexGuide (Reference Title File)
+# Author(s):  Admin9705
+# URL:        https://plexguide.com - http://github.plexguide.com
+# GNU:        General Public License v3.0
+################################################################################
+
+# BAD INPUT
+changeproject () {
+  read -p '🌍 Set/Change Project ID? (y/n)| Press [ENTER] ' typed < /dev/tty
+  if [[ "$typed" = "n" || "$typed" == "N" ]]; then question1
+elif [[ "$typed" = "y" || "$typed" == "Y" ]]; then a=b
+else badinput
+  echo ""
+  changeproject; fi
+}
+
+projectidset () {
+tee <<-EOF
+━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
+🚀 System Message: Type the Project Name to Utilize
+━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
+
+EOF
+  cat /var/plexguide/projects.list | cut -d' ' -f1 | tail -n +2
+  echo ""
+  read -p '🌍 Type Project Name | Press [ENTER]: ' typed < /dev/tty
+  echo ""
+  list=$(cat /var/plexguide/project.cut | grep $typed)
+
+  if [ "$typed" != "$list" ]; then
+tee <<-EOF
+━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
+🚀 System Message: Error! Type the Exact Project Name Listed!
+━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
+
+EOF
+  read -p '🌍 Acknowledge Info | Press [ENTER] ' typed < /dev/tty
+  projectidset
+  fi
+}
+
+testphase () {
+  echo "" > /opt/appdata/plexguide/test.conf
+  echo "[gdrive]" >> /opt/appdata/plexguide/test.conf
+  echo "client_id = $public" >> /opt/appdata/plexguide/test.conf
+  echo "client_secret = $secret" >> /opt/appdata/plexguide/test.conf
+  echo "type = drive" >> /opt/appdata/plexguide/test.conf
+  echo -n "token = {\"access_token\":${accesstoken}\"token_type\":\"Bearer\",\"refresh_token\":${refreshtoken}\"expiry\":\"${final}\"}" >> /opt/appdata/plexguide/test.conf
+  if [ "$type" == "tdrive" ]; then echo "team_drive = $teamdrive" >> /opt/appdata/plexguide/test.conf; fi
+  echo ""
+tee <<-EOF
+
+━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
+🚀 System Messasge: Conducting GDrive Validation Checks
+━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
+EOF
+  sleep 1
+tee <<-EOF
+
+━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
+🚀 System Messasge: Creating Test Directory - gdrive:/plexguide
+━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
+EOF
+  sleep 1
+  rclone mkdir --config /opt/appdata/plexguide/test.conf gdrive:/plexguide
+tee <<-EOF
+
+━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
+🚀 System Messasge: Checking Existance of gdsa01:/plexguide
+━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
+
+EOF
+  rcheck=$(rclone lsd --config /opt/appdata/plexguide/test.conf gdrive: | grep -oP plexguide | head -n1)
+
+  if [ "$rcheck" != "plexguide" ];then
+tee <<-EOF
+
+━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
+🚀 System Messasge: Validation Checks Failed
+━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
+
+TIPS:
+1. Did you copy your username and password correctly?
+2. When you created the credentials, did you select "Other"?
+3. Did you enable your API?
+
+EOF
+    echo "Not Active" > /var/plexguide/gdrive.pgclone
+    read -p '🌍 Acknowledge Info | Press [ENTER] ' typed2 < /dev/tty
+    question1
+else
+tee <<-EOF
+━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
+🚀 System Messasge: Validation Checks Passed - GDrive
+━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
+EOF
+
+fi
+
+echo "Active" > /var/plexguide/gdrive.pgclone
+echo ""
+read -p '🌍 Acknowledge Info | Press [ENTER] ' typed2 < /dev/tty
+question1
+
+EOF
+}
--- menu/pgclone/gdrive.sh
+++ menu/pgclone/gdrive.sh
@@ -6,112 +6,16 @@
 # GNU:        General Public License v3.0
 ################################################################################
 source /opt/plexguide/menu/functions/functions.sh
+source /opt/plexguide/menu/functions/pgclone.sh
 
-changeproject () {
-  read -p '🌍 Set/Change Project ID? (y/n)| Press [ENTER] ' typed < /dev/tty
-  if [[ "$typed" = "n" || "$typed" == "N" ]]; then question1
-elif [[ "$typed" = "y" || "$typed" == "Y" ]]; then a=b
-else badinput
-  echo ""
-  changeproject; fi
-}
 
-projectidset () {
-tee <<-EOF
-━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
-🚀 System Message: Type the Project Name to Utilize
-━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
-
-EOF
-  cat /var/plexguide/projects.list | cut -d' ' -f1 | tail -n +2
-  echo ""
-  read -p '🌍 Type Project Name | Press [ENTER]: ' typed < /dev/tty
-  echo ""
-  list=$(cat /var/plexguide/project.cut | grep $typed)
-
-  if [ "$typed" != "$list" ]; then
-tee <<-EOF
-━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
-🚀 System Message: Error! Type the Exact Project Name Listed!
-━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
-
-EOF
-  read -p '🌍 Acknowledge Info | Press [ENTER] ' typed < /dev/tty
-  projectidset
-  fi
-}
-
-testphase () {
-  echo "" > /opt/appdata/plexguide/test.conf
-  echo "[gdrive]" >> /opt/appdata/plexguide/test.conf
-  echo "client_id = $public" >> /opt/appdata/plexguide/test.conf
-  echo "client_secret = $secret" >> /opt/appdata/plexguide/test.conf
-  echo "type = drive" >> /opt/appdata/plexguide/test.conf
-  echo -n "token = {\"access_token\":${accesstoken}\"token_type\":\"Bearer\",\"refresh_token\":${refreshtoken}\"expiry\":\"${final}\"}" >> /opt/appdata/plexguide/test.conf
-  if [ "$type" == "tdrive" ]; then echo "team_drive = $teamdrive" >> /opt/appdata/plexguide/test.conf; fi
-  echo ""
-tee <<-EOF
-
-━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
-🚀 System Messasge: Conducting GDrive Validation Checks
-━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
-EOF
-  sleep 1
-tee <<-EOF
-
-━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
-🚀 System Messasge: Creating Test Directory - gdrive:/plexguide
-━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
-EOF
-  sleep 1
-  rclone mkdir --config /opt/appdata/plexguide/test.conf gdrive:/plexguide
-tee <<-EOF
-
-━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
-🚀 System Messasge: Checking Existance of gdsa01:/plexguide
-━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
-
-EOF
-  rcheck=$(rclone lsd --config /opt/appdata/plexguide/test.conf gdrive: | grep -oP plexguide | head -n1)
-
-  if [ "$rcheck" != "plexguide" ];then
-tee <<-EOF
-
-━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
-🚀 System Messasge: Validation Checks Failed
-━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
-
-TIPS:
-1. Did you copy your username and password correctly?
-2. When you created the credentials, did you select "Other"?
-3. Did you enable your API?
-
-EOF
-    echo "Not Active" > /var/plexguide/gdrive.pgclone
-    read -p '🌍 Acknowledge Info | Press [ENTER] ' typed2 < /dev/tty
-    question1
-else
-tee <<-EOF
-━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
-🚀 System Messasge: Validation Checks Passed - GDrive
-━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
-EOF
-
-fi
-
-echo "Active" > /var/plexguide/gdrive.pgclone
-echo ""
-read -p '🌍 Acknowledge Info | Press [ENTER] ' typed2 < /dev/tty
-question1
-
-EOF
-}
 
 ################################################################################
 question1 () {
   touch /opt/appdata/plexguide/rclone.conf
   account=$(cat /var/plexguide/project.account)
   project=$(cat /var/plexguide/pgclone.project)
+  project=$(cat /var/plexguide/pgclone.tr)
   gstatus=$(cat /var/plexguide/gdrive.pgclone)
   tstatus=$(cat /var/plexguide/tdrive.pgclone)
 
@@ -120,11 +24,13 @@ tee <<-EOF
 🌎 Welcome to PGClone                     reference:pgclone.plexguide.com
 ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
 
-1 - Login to Google Account: [$account]
-2 - Establish a Project    : [$project]
-3 - Configure: gdrive [$gstatus]
-4 - Configure: tdrive [$tstatus]
-5 - Encryption Setup
+1 - Data Transport Mode    : [$transport]
+2 - Login to Google Account: [$account]
+3 - Establish a Project    : [$project]
+4 - Configure - gdrive     : [$gstatus]
+5 - Configure - tdrive     : [$tstatus]
+6 - Encryption Setup
+7 - Deploy [NAME]
 Z - Exit
 A - Build a New project
 B - Delete a Project
@@ -266,6 +172,7 @@ EOF
 
 variable /var/plexguide/project.account "NOT-SET"
 variable /var/plexguide/pgclone.project "NOT-SET"
+variable /var/plexguide/pgclone.transport "NOT-SET"
 variable /var/plexguide/gdrive.pgclone "Not Active"
 variable /var/plexguide/tdrive.pgclone "Not Active"
 
