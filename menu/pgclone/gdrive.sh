e0fe6d96656bff7890d64921a98ea1843b9134b7
Admin9705 <user@dunn.cloud>
2018-12-05 18:09:35 -0500

    update

--- menu/pgclone/gdrive.sh
+++ menu/pgclone/gdrive.sh
@@ -26,10 +26,9 @@ question1 () {
 if [[ "$transport" == "PG Move /w No Encryption" || "$transport" == "PG Move /w Encryption" ]]; then menufix=1; fi
 
 if [ "$menufix" == "1" ]; then
-display1=
-"6 - Deploy ~ $transport"
+display1="6 - Deploy ~ $transport"
 else
-"5 - Key Management      : [keysdeployed]
+display="5 - Key Management      : [keysdeployed]
 6 - Deploy ~ $transport" ; fi
 
 tee <<-EOF
