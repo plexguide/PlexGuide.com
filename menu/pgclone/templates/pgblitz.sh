31a52070e0d7e75bfcfe52db1cfe1175951581e9
Admin9705 <user@dunn.cloud>
2018-12-20 07:53:18 -0500

    update

--- menu/pgclone/templates/pgblitz.sh
+++ menu/pgclone/templates/pgblitz.sh
@@ -66,7 +66,7 @@ do
                             # Record next GDSA in case of crash/reboot
                             echo "$GDSAUSE" > /opt/appdata/pgblitz/vars/lastGDSA
                         fi
-                        log "${GDSAARRAY[$GDSAUSE]} is now `echo "$GDSAAMOUNT/1024/1024*100" | bc -l`"
+                        log "${GDSAARRAY[$GDSAUSE]} is now `echo "$GDSAAMOUNT/1024/1024" | bc` MB"
                         # Record GDSA transfered in case of crash/reboot
                         echo "$GDSAAMOUNT" > /opt/appdata/pgblitz/vars/gdsaAmount
                     else
@@ -84,7 +84,7 @@ do
     else
         log "Nothing to upload, sleeping 5 secs"
         log "Running Blitz Finder for files not uploaded"
-        log "${GDSAARRAY[$GDSAUSE]} is now `echo "$GDSAAMOUNT/1024/1024*100" | bc -l`"
+        log "${GDSAARRAY[$GDSAUSE]} is now `echo "$GDSAAMOUNT/1024/1024" | bc` MB"
         finder
     fi
     sleep 5
