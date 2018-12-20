1903eab21828693b79146dbcd57f9d1cf6c030d0
Admin9705 <user@dunn.cloud>
2018-12-19 19:54:44 -0500

    update

--- menu/roles/autodelete/scripts/nzbget.sh
+++ menu/roles/autodelete/scripts/nzbget.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-# Credit: RXWatcher
-find /mnt/nzbget/completed/ebooks/* -type d -mmin +240 -ls -exec rm -rf {} + 2>/dev/null
-find /mnt/nzbget/completed/movies/* -type d -mmin +240 -ls -exec rm -rf {} + 2>/dev/null
-find /mnt/nzbget/completed/music/*  -type d -mmin +240 -ls -exec rm -rf {} + 2>/dev/null
-find /mnt/nzbget/completed/tv/* -type d -mmin +240 -ls -exec rm -rf {} + 2>/dev/null
+
+find /mnt/nzbget/downloads/completed/tv/* -maxdepth 1 -mmin +240 -type f -exec rm -fv {} 2>/dev/null \;
+find /mnt/nzbget/downloads/completed/music/* -maxdepth 1 -mmin +240 -type f -exec rm -fv {} 2>/dev/null \;
+find /mnt/nzbget/downloads/completed/movies/* -maxdepth 1 -mmin +240 -type f -exec rm -fv {} 2>/dev/null \;
+find /mnt/nzbget/downloads/completed/ebooks/* -maxdepth 1 -mmin +240 -type f -exec rm -fv {} 2>/dev/null \;
--- menu/roles/autodelete/scripts/sabnzbd.sh
+++ menu/roles/autodelete/scripts/sabnzbd.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# Credit: RXWatcher
+
 find /mnt/sab/complete/ebooks/* -type d -mmin +240 -ls -exec rm -rf {} + 2>/dev/null
 find /mnt/sab/complete/movies/* -type d -mmin +240 -ls -exec rm -rf {} + 2>/dev/null
 find /mnt/sab/complete/music/*  -type d -mmin +240 -ls -exec rm -rf {} + 2>/dev/null
