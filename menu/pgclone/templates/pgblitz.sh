21666da658c8b077863b6ce1135fcc8454f4336f
Admin9705 <user@dunn.cloud>
2018-12-20 05:41:10 -0500

    update

--- menu/pgclone/templates/pgblitz.sh
+++ menu/pgclone/templates/pgblitz.sh
@@ -7,64 +7,9 @@
 # GNU:        General Public License v3.0
 ################################################################################
 source /opt/plexguide/menu/functions/pgblitz.sh
-# Logging Function
-function log()
-{
-    echo "[PGBlitz] $@" > /var/plexguide/pg.log && bash /opt/plexguide/roles/log/log.sh
-    echo "[PGBlitz] $@"
-}
 
-#Make sure all the folders we need are created
-downloadpath=$(cat /var/plexguide/server.hd.path)
-path=/opt/appdata/pgblitz/keys
-mkdir -p /opt/appdata/pgblitz/pid/
-mkdir -p /opt/appdata/pgblitz/json/
-mkdir -p /opt/appdata/pgblitz/logs/
-mkdir -p /opt/appdata/pgblitz/vars/
-
-#Header
-log "PGBlitz v2.1 Started"
-log "Started for the First Time - Cleaning up if from reboot"
-
-# Remove left over webui and transfer files
-rm -f /opt/appdata/pgblitz/pid/*
-rm -f /opt/appdata/pgblitz/json/*
-rm -f /opt/appdata/pgblitz/logs/*
-
-#copy any files that failed to upload back to /mnt/move
-for i in `find /mnt/pgblitz/ -maxdepth 1 -mindepth 1 -type d`; do
-    cp -r ${i}/* $downloadpath/move
-    rm -fr ${i}/*
-done
-log "Cleaned up - Sleeping 10 secs"
-sleep 10
-
-#### Generates the GDSA List from the Processed Keys
-
-GDSAARRAY=(`ls -la $path/processed | awk '{print $9}' | grep gdsa`)
-GDSACOUNT=`expr ${#GDSAARRAY[@]} - 1`
-
-# Check to see if we have any keys
-if [ $GDSACOUNT -lt 1 ]; then
-    log "No accounts found to upload with, Exit"
-    exit 1
-fi
-
-# Check if BC is installed
-if [ `echo "10 + 10" | bc` == "20" ]; then
-    log "BC Found! All good :)"
-else
-    log "BC Not installed, Exit"
-    exit 2
-fi
-# Grabs vars from files
-if [ -e /opt/appdata/pgblitz/vars/lastGDSA ]; then
-    GDSAUSE=`cat /opt/appdata/pgblitz/vars/lastGDSA`
-    GDSAAMOUNT=`cat /opt/appdata/pgblitz/vars/gdsaAmount`
-else
-    GDSAUSE=0
-    GDSAAMOUNT=0
-fi
+starter
+stasks
 
 # Run Loop
 while [ 1 ]
