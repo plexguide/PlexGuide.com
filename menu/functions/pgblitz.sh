e1cb3b62f2cbecd888de27ae76d392499bb39922
Admin9705 <user@dunn.cloud>
2018-12-20 20:40:43 -0500

    udpate

--- menu/functions/pgblitz.sh
+++ menu/functions/pgblitz.sh
@@ -68,9 +68,9 @@ stasks () {
 
 finder () {
 downloadpath=$(cat /var/plexguide/server.hd.path)
-find $downloadpath/pgblitz/* -mmin +30 -maxdepth 1 -type f > /var/plexguide/leftover
+find $downloadpath/pgblitz/* -mmin +5 -maxdepth 1 -type f > /var/plexguide/leftover
 
-for i in `find $downloadpath/pgblitz/* -maxdepth 1 -mindepth 1 -type d`; do
+for i in `find $downloadpath/pgblitz/* -maxdepth 1 -mmin +5 -mindepth 1 -type d`; do
     log "Found Stuck Folders/Files ~ ${i}"
     log "Moving ${i} back to /mnt/move for processing"
 
