9344872ee9739e749aeb85716c42730712c47e3e
admin9705 <user@dunn.cloud>
2018-12-24 14:15:42 -0800

    update

--- menu/interface/version/version.sh
+++ menu/interface/version/version.sh
@@ -1,11 +1,10 @@
 WARNING: No More Updates Provided! Must rerun the v2 Installer. Please
-read the post on the front page in regards to 7.6
+read the post on the front page in regards to 7.6 - After 15 Jan 2019,
+this message will no longer appear and will error out. Update will fix it!
 
-✅  [CURRENT]
+✅  [Versions]
 7.5.13
 7.5.4
-
-💊  [DEPRECIATED]
 7.5.3
 7.4.13
 7.2.4
