9999031e7cface93265732ce5d16db5da4c69278
admin9705 <user@dunn.cloud>
2018-12-24 11:24:16 -0800

    update

---
+++ menu/version/file.sh
@@ -0,0 +1,63 @@
+#!/bin/bash
+#
+# Title:      PlexGuide (Reference Title File)
+# Author(s):  Admin9705 - Deiteq
+# URL:        https://plexguide.com - http://github.plexguide.com
+# GNU:        General Public License v3.0
+################################################################################
+rm -rf /var/plexguide/ver.temp 1>/dev/null 2>&1
+touch /var/plexguide/ver.temp
+
+sleep 1
+## Builds Version List for Display
+while read p; do
+  echo $p >> /var/plexguide/ver.temp
+done </opt/plexguide/menu/interface/version/version.sh
+
+tee <<-EOF
+
+━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
+📂  PG Update Interface Menu
+━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
+
+EOF
+
+cat /var/plexguide/ver.temp
+echo ""
+echo "To QUIT, type >>> exit"
+break=no
+while [ "$break" == "no" ]; do
+read -p '↘️  Type [PG Version] | PRESS ENTER: ' typed
+storage=$(grep $typed /var/plexguide/ver.temp)
+
+if [ "$typed" == "exit" ]; then
+  echo ""
+  touch /var/plexguide/exited.upgrade
+  exit
+fi
+
+if [ "$storage" != "" ]; then
+  break=yes
+  echo $storage > /var/plexguide/pg.number
+  ansible-playbook /opt/plexguide/menu/interface/version/choice.yml
+
+tee <<-EOF
+
+━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
+✅️  SYSTEM MESSAGE: Installed Verison - $storage - Standby!
+━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
+EOF
+sleep 4
+else
+tee <<-EOF
+
+━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
+⛔️  SYSTEM MESSAGE: Version $storage does not exist! - Standby!
+━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
+EOF
+  sleep 4
+  cat /var/plexguide/ver.temp
+  echo ""
+fi
+
+done
---
+++ menu/version/version.sh
@@ -0,0 +1,10 @@
+✅  [CURRENT]
+7.3.8
+
+💍  [BETA] - Avoid Using Edge! For Testing & No Support!
+7.4.5
+7.4edge
+
+💊  [DEPRECIATED]
+7.2.4
+7.1.10
