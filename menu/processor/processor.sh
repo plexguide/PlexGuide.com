cb2138ba3f467fe2ae8db49282a616ac7673d17f
admin9705 <user@dunn.cloud>
2018-12-25 10:22:44 -0800

    update

---
+++ menu/processor/processor.sh
@@ -0,0 +1,66 @@
+#!/bin/bash
+#
+# Title:      PlexGuide (Reference Title File)
+# Author(s):  Admin9705 - Deiteq
+# URL:        https://plexguide.com - http://github.plexguide.com
+# GNU:        General Public License v3.0
+################################################################################
+pro1() {
+  ansible-playbook /opt/plexguide/menu/processor/processor.yml  --tags performance
+  rebootpro
+}
+pro2() {
+  ansible-playbook /opt/plexguide/roles/menu/processor.yml  --tags ondemand
+  rebootpro
+}
+pro3() {
+  ansible-playbook /opt/plexguide/roles/menu/processor.yml  --tags conservative
+  rebootpro
+}
+pro4() {
+  echo ""
+  cpufreq-info
+  echo ""
+}
+rebootpro() {
+  bash /opt/plexguide/menu/processor/scripts/reboot.sh
+}
+exit() {
+  exit
+}
+
+question1 () {
+tee <<-EOF
+
+━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
+🌎 Processer Policy Interface       ⚡ Reference: processor.plexguide.com
+━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
+
+1. Performance Mode
+2. OnDemand Mode
+3. Conservative Mode
+4. View Processor Policy
+Z. EXIT
+
+━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
+EOF
+
+read -p 'Type a Number | Press [ENTER]: ' typed < /dev/tty
+
+case $typed in
+    1 )
+        pro1 ;;
+    2 )
+        pro2 ;;
+    3 )
+        pro3 ;;
+    4 )
+        pro4 ;;
+    z )
+        exit ;;
+    * )
+        question1 ;;
+esac
+}
+
+question1
