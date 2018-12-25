a47dae39e780b6b312753d2f9eed23d7c37bb826
admin9705 <user@dunn.cloud>
2018-12-24 22:46:03 -0800

    update

--- menu/interface/version/version.sh
+++ menu/interface/version/version.sh
@@ -1,10 +1,12 @@
-WARNING: No More Updates Provided! Must rerun the v2 Installer. Please
+WARNING: No More Updates Provided! Must run the v2 Installer. Please
 read the post on the front page in regards to 7.6 - After 15 Jan 2019,
 this message will no longer appear and will error out. Update will fix it!
+
+Visit install page or run  >>>
+curl -s https://plexguide.com/installv2.sh | sudo -H sh
 
 ✅  [Versions]
 7.5.13
 7.5.4
-7.5.3
 7.4.13
 7.2.4
