277515367a46a8589581508b518e215e57fa0a80
admin9705 <user@dunn.cloud>
2018-12-25 13:04:59 -0800

    update

---
+++ menu/kernel/processor.sh
@@ -0,0 +1,57 @@
+#!/bin/bash
+#
+# Title:      PlexGuide (Reference Title File)
+# Author(s):  Admin9705 - Deiteq
+# URL:        https://plexguide.com - http://github.plexguide.com
+# GNU:        General Public License v3.0
+################################################################################
+question1 () {
+tee <<-EOF
+
+━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
+🌎  Processer Policy Interface      ⚡ Reference: processor.plexguide.com
+━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
+
+💬  Works only on Dedicated Servers! (No VPS, ESXI, VMs, and etc)
+
+1. Performance Mode
+2. OnDemand Mode
+3. Conservative Mode
+4. View Processor Policy
+Z. EXIT
+
+━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
+EOF
+
+read -p 'Type a Number | Press [ENTER]: ' typed < /dev/tty
+
+case $typed in
+    1 )
+        ansible-playbook /opt/plexguide/menu/processor/processor.yml  --tags performance
+        rebootpro ;;
+    2 )
+        ansible-playbook /opt/plexguide/roles/menu/processor.yml  --tags ondemand
+        rebootpro ;;
+    3 )
+        ansible-playbook /opt/plexguide/roles/menu/processor.yml  --tags conservative
+        rebootpro ;;
+    4 )
+        echo ""
+        cpufreq-info
+        echo ""
+        read -p '🌍  Done? | Press [ENTER] ' typed < /dev/tty
+        ;;
+    z )
+        exit ;;
+    Z )
+        exit ;;
+    * )
+        question1 ;;
+esac
+}
+
+rebootpro() {
+  bash /opt/plexguide/menu/processor/scripts/reboot.sh
+}
+
+question1
