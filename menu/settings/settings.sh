515785e6210133a49e305ac67f6085c48c19b783
admin9705 <user@dunn.cloud>
2018-12-25 13:48:51 -0800

    update

--- menu/settings/settings.sh
+++ menu/settings/settings.sh
@@ -16,9 +16,8 @@ tee <<-EOF
 
 [1] Download Path:  Change the Processing Location
 [2] Processor    :  Enhance the CPU Processing Power
-[3] Kernel Modes :  Enhance Network Throughput
-[4] WatchTower   :  Auto-Update Application Manager
-[5] Change Time  :  Change the Server Time
+[3] WatchTower   :  Auto-Update Application Manager
+[4] Change Time  :  Change the Server Time
 [Z] Exit
 
 ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
@@ -32,10 +31,8 @@ read -p 'Type a Number | Press [ENTER]: ' typed < /dev/tty
 elif [ "$typed" == "2" ]; then
   bash /opt/plexguide/menu/processor/processor.sh
 elif [ "$typed" == "3" ]; then
-  bash /opt/plexguide/menu/scripts/menus/kernel-mod-menu.sh
-elif [ "$typed" == "4" ]; then
   watchtower
-elif [ "$typed" == "5" ]; then
+elif [ "$typed" == "4" ]; then
   dpkg-reconfigure tzdata
 elif [[ "$typed" == "z" || "$typed" == "Z" ]]; then
   exit
