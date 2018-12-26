d30c5c0efc78f262552e7e75c46a14b5637b71fe
admin9705 <user@dunn.cloud>
2018-12-25 18:00:56 -0800

    update

--- menu/endbanner/endbanner.sh
+++ menu/endbanner/endbanner.sh
@@ -5,19 +5,19 @@
 # URL:        https://plexguide.com - http://github.plexguide.com
 # GNU:        General Public License v3.0
 ################################################################################
-if [ "$program" == "plex" ]; then extra="/web"; else extra=""; fi
-
 program=$(cat /tmp/program_var)
 domain=$(cat /var/plexguide/server.domain)
 port=$(cat /tmp/program_port)
 ip=$(cat /var/plexguide/server.ip)
+
+if [ "$program" == "plex" ]; then extra="/web"; else extra=""; fi
 
 tee <<-EOF
 
 ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
 💎 APPLICATION: Access Information | $program
 ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
-▫ $ip:$port
+▫ $ip:${port}${extra}
 ▫ $domain:${port}${extra}
-▫ $program.$domaina
+▫ $program.$domain${extra}
 EOF
