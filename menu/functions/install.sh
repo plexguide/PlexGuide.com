643e97f876c42b2e7e01c6a895e724ce607ce92a
Admin9705 <24727006+Admin9705@users.noreply.github.com>
2018-12-29 20:56:43 -0600

    update

--- menu/functions/install.sh
+++ menu/functions/install.sh
@@ -55,7 +55,7 @@ updateprime() {
   echo "10" > ${abc}/pg.docstart
   echo "2" > ${abc}/pg.watchtower
   echo "1" > ${abc}/pg.motd
-  echo "91" > ${abc}/pg.alias
+  echo "92" > ${abc}/pg.alias
   echo "2" > ${abc}/pg.dep
   echo "2" > ${abc}/pg.cleaner
   echo "3" > ${abc}/pg.gcloud
