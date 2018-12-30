b112f019d11d73da43dd4e0827b839cf69eb6287
Admin9705 <24727006+Admin9705@users.noreply.github.com>
2018-12-30 00:02:43 -0600

    update

--- menu/portguard/rebuild.sh
+++ menu/portguard/rebuild.sh
@@ -24,6 +24,7 @@ sed -i -e "/plex/d" /var/plexguide/container.running
 sed -i -e "/x2go*/d" /var/plexguide/container.running
 sed -i -e "/authclient/d" /var/plexguide/container.running
 sed -i -e "/dockergc/d" /var/plexguide/container.running
+sed -i -e "/oauth/d" /var/plexguide/container.running
 
 count=$(wc -l < /var/plexguide/container.running)
 ((count++))
