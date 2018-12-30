c04b2b52846f45abf55f35452f814b2231818a4e
Admin9705 <24727006+Admin9705@users.noreply.github.com>
2018-12-29 23:57:56 -0600

    update

---
+++ menu/pgshield/rebuild.sh
@@ -0,0 +1,60 @@
+#!/bin/bash
+#
+# [Rebuilding Containers]
+#
+# GitHub:   https://github.com/Admin9705/PlexGuide.com-The-Awesome-Plex-Server
+# Author:   Admin9705 - Deiteq
+# URL:      https://plexguide.com
+#
+# PlexGuide Copyright (C) 2018 PlexGuide.com
+# Licensed under GNU General Public License v3.0 GPL-3 (in short)
+#
+#   You may copy, distribute and modify the software as long as you track
+#   changes/dates in source files. Any modifications to our software
+#   including (via compiler) GPL-licensed code must also be made available
+#   under the GPL along with build & install instructions.
+#
+#################################################################################
+docker ps -a --format "{{.Names}}"  > /var/plexguide/container.running
+
+sed -i -e "/traefik/d" /var/plexguide/container.running
+sed -i -e "/watchtower/d" /var/plexguide/container.running
+sed -i -e "/word*/d" /var/plexguide/container.running
+sed -i -e "/plex/d" /var/plexguide/container.running
+sed -i -e "/x2go*/d" /var/plexguide/container.running
+sed -i -e "/authclient/d" /var/plexguide/container.running
+sed -i -e "/dockergc/d" /var/plexguide/container.running
+
+count=$(wc -l < /var/plexguide/container.running)
+((count++))
+((count--))
+
+tee <<-EOF
+
+━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
+⚠️  PG Shield - Rebuilding Containers!
+━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
+EOF
+sleep 1.5
+for ((i=1; i<$count+1; i++)); do
+	app=$(sed "${i}q;d" /var/plexguide/container.running)
+
+tee <<-EOF
+
+━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
+↘️  PG Shield - Rebuilding [$app]
+━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
+
+EOF
+	sleep 1.5
+	ansible-playbook /opt/plexguide/containers/$app.yml
+done
+
+echo ""
+tee <<-EOF
+━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
+✅️  PG Shield - All Containers Rebuilt!
+━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
+
+EOF
+read -p 'Continue? | Press [ENTER] ' name < /dev/tty
