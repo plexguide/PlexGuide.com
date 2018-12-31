4eb4a486b5b51ee5e23f012fb8ebd83357cef9fe
Robert Baker <se7enalive@gmail.com>
2018-12-30 19:31:47 -0700

    container refactoring
    Added LL configurations, changed messaging, formatted files

--- menu/roles/autodelete/scripts/nzbget.sh
+++ menu/roles/autodelete/scripts/nzbget.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-
-find /mnt/nzbget/downloads/completed/tv/* -maxdepth 1 -mmin +240 -type f -exec rm -fv {} 2>/dev/null \;
-find /mnt/nzbget/downloads/completed/music/* -maxdepth 1 -mmin +240 -type f -exec rm -fv {} 2>/dev/null \;
-find /mnt/nzbget/downloads/completed/movies/* -maxdepth 1 -mmin +240 -type f -exec rm -fv {} 2>/dev/null \;
-find /mnt/nzbget/downloads/completed/ebooks/* -maxdepth 1 -mmin +240 -type f -exec rm -fv {} 2>/dev/null \;
+find /mnt/downloads/nzbget/tv/* -maxdepth 1 -mmin +240 -type f -exec rm -fv {} 2>/dev/null \;
+find /mnt/downloads/nzbget/music/* -maxdepth 1 -mmin +240 -type f -exec rm -fv {} 2>/dev/null \;
+find /mnt/downloads/nzbget/movies/* -maxdepth 1 -mmin +240 -type f -exec rm -fv {} 2>/dev/null \;
+find /mnt/downloads/nzbget/ebooks/* -maxdepth 1 -mmin +240 -type f -exec rm -fv {} 2>/dev/null \;
+find /mnt/downloads/nzbget/abooks/* -maxdepth 1 -mmin +240 -type f -exec rm -fv {} 2>/dev/null \;
