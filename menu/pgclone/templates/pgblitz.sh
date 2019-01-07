d7ce4017a74ea670fdc94fa9b4b6fcd1014a1994
Admin9705 <24727006+Admin9705@users.noreply.github.com>
2019-01-06 22:47:15 -0500

    Update pgblitz.sh

--- menu/pgclone/templates/pgblitz.sh
+++ menu/pgclone/templates/pgblitz.sh
@@ -36,7 +36,52 @@ while [ 1 ]; do
   if [ "$encheck" == "eblitz" ]; then keyuse=${keyuse}C; fi
 
   echo "Upload Test - Using $keyuse"
-  rclone moveto --tpslimit 12 --checkers=20 \
+  rclone moveto --tpslimit 12 --checkers=20 #!/bin/bash
+#
+# Title:      PlexGuide (Reference Title File)
+# Author(s):  Admin9705 & PhysK
+# URL:        https://plexguide.com - http://github.plexguide.com
+# GNU:        General Public License v3.0
+################################################################################
+source /opt/plexguide/menu/functions/pgblitz.sh
+
+#starter
+#stasks
+
+# Outside Variables
+dlpath=$(cat /var/plexguide/server.hd.path)
+
+# Starting Actions
+mkdir -p /$dlpath/pgblitz/upload
+
+# Inside Variables
+ls -la /opt/appdata/pgblitz/keys/processed | awk '{print $9}' | grep gdsa > /opt/appdata/plexguide/key.list
+keytotal=$(wc -l /opt/appdata/plexguide/key.list | awk '{ print $1 }')
+
+keyfirst=$(cat /opt/appdata/plexguide/key.list | head -n1)
+keylast=$(cat /opt/appdata/plexguide/key.list | tail -n1)
+
+keycurrent=0
+
+while [ 1 ]; do
+
+  if [ "$keylast" == "$keyuse" ]; then keycurrent=0; fi
+
+  let "keycurrent++"
+  keyuse=$(sed -n ''$keycurrent'p' < /opt/appdata/plexguide/key.list)
+
+  encheck=$(cat /var/plexguide/pgclone.transport)
+  if [ "$encheck" == "eblitz" ]; then keyuse=${keyuse}C; fi
+
+  echo "Upload Test - Using $keyuse"
+  rclone moveto --tpslimit 12 --checkers=20 --min-age=2m \
+        --config /opt/appdata/plexguide/rclone.conf \
+        --transfers=16 \
+        --drive-chunk-size=128M \
+        "/mnt/move/" "$keyuse:/"
+
+done
+ \
         --config /opt/appdata/plexguide/rclone.conf \
         --transfers=16 \
         --drive-chunk-size=128M \
