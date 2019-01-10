6c4f8e52f8856b1cbe07145912609822a6010c40
esportagency <46482749+esportagency@users.noreply.github.com>
2019-01-10 04:26:19 +0100

    Update pgblitz.sh
    "keyuse=${keyuse}C;" results in service pgblitz status returning 
    Jan 10 01:55:57 pgvm bash[4828]: Upload Test - Using C 
    
    and blitz to return following error message:
    2019/01/10 02:12:49 Failed to create file system for destination "C:/": didn't find section in config file 
    
    which stops pgblitz from further uploading operations.
    
    keyuse=${keyuse}; results in service pgblitz status to return correct service worker keys and correct upload behaviour again.

--- menu/pgclone/templates/pgblitz.sh
+++ menu/pgclone/templates/pgblitz.sh
@@ -37,7 +37,7 @@ while [ 1 ]; do
   keyuse=$(sed -n ''$keycurrent'p' < /opt/appdata/plexguide/key.list)
 
   encheck=$(cat /var/plexguide/pgclone.transport)
-  if [ "$encheck" == "eblitz" ]; then keyuse=${keyuse}C; fi
+  if [ "$encheck" == "eblitz" ]; then keyuse=${keyuse}; fi
 
   rclone moveto --min-age=2m \
         --config /opt/appdata/plexguide/rclone.conf \
