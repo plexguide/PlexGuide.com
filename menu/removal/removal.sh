f34d1cd70475176a59c5bbc77834382f683f8f6f
Admin9705 <user@dunn.cloud>
2019-01-10 23:07:58 -0500

    update

--- menu/removal/removal.sh
+++ menu/removal/removal.sh
@@ -115,6 +115,13 @@ tee <<-EOF
 EOF
 sleep 1
 rm -rf /opt/appdata/$typed
+
+file="/opt/plexguide/containers/$typed.yml"
+if [ ! -e "$file" ]; then
+  check=$(cat /opt/plexguide/containers/$typed.yml | grep '##PG-Community')
+    if [ "$check" == "##PG-Community" ]; then rm -r /opt/plexguide/containers/$typed.yml; fi
+fi
+  
 sleep 1.5
 
 tee <<-EOF
