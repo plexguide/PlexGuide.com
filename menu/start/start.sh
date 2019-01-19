f048d800970e3219e779afc182574fc19f76bca6
Admin9705 <user@dunn.cloud>
2019-01-18 23:25:09 -0500

    update

--- menu/start/start.sh
+++ menu/start/start.sh
@@ -209,8 +209,8 @@ elif [ "$typed" == "1" ]; then
 elif [ "$typed" == "2" ]; then
   bash /opt/plexguide/menu/pgshield/pgshield.sh
 elif [ "$typed" == "4" ]; then
-  #bash /opt/plexguide/menu/pgbox/pgboxcore.sh
-  #bash /opt/plexguide/menu/pgbox/pgboxcommunity.sh
+  bash /opt/plexguide/menu/pgbox/pgboxcore.sh
+  bash /opt/plexguide/menu/pgbox/pgboxcommunity.sh
 elif [ "$typed" == "5" ]; then
   bash /opt/plexguide/menu/pgpress/pgpress.sh
   bash /opt/pgpress/pressmain.sh
