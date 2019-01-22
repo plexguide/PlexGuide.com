9a7c84228fde5a30224d9cf8da1cf30ed34efaa8
Admin9705 <user@dunn.cloud>
2019-01-21 22:08:58 -0500

    update

--- menu/pgclone/pgclone.sh
+++ menu/pgclone/pgclone.sh
@@ -24,6 +24,21 @@ initial () {
   done
 }
 
+custom () {
+  rolename='pgclone'
+  mkdir -p "/opt/$rolename"
+  ansible-playbook "/opt/plexguide/menu/$rolename/${rolename}_personal.yml"
+
+  echo ""
+  echo "💬  Pulling Update Files - Please Wait"
+  file="/opt/pgclone/place.holder"
+  waitvar=0
+  while [ "$waitvar" == "0" ]; do
+  	sleep .5
+  	if [ -e "$file" ]; then waitvar=1; fi
+  done
+}
+
 mainbanner () {
 tee <<-EOF
 
@@ -104,7 +119,7 @@ EOF
         if [ "$existcheck" == "" ]; then echo;
         read -p '💬 Exiting! Forked Version Does Not Exist! | Press [ENTER]: ' typed < /dev/tty
         mainbanner; fi
-        initial ;;
+        custom ;;
     z )
         exit ;;
     Z )
