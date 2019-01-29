419f35d2eda7930978fdd39da9a0cea181bb8ed1
Admin9705 <24727006+Admin9705@users.noreply.github.com>
2019-01-28 23:58:16 -0500

    update

--- containers/templates/sonarr/scripts/cleanup-sonarr.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-if [ -d "$sonarr_episodefile_sourcefolder" ] && [ "$(basename $sonarr_episodefile_sourcefolder)" = "deluge_extracted" ] ; then
-    /bin/rm -rf $sonarr_episodefile_sourcefolder
-fi
