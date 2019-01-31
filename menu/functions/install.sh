93f934c5b80fe3866b37d32d9ce865808c92b28d
Robert Baker <se7enalive@gmail.com>
2019-01-30 22:07:45 -0700

    remove unneeded pkgs

--- menu/functions/install.sh
+++ menu/functions/install.sh
@@ -180,27 +180,23 @@ mergerinstall () {
     activated=true
     echo "ub16" > /var/plexguide/mergerfs.version
     wget "https://github.com/trapexit/mergerfs/releases/download/2.25.1/mergerfs_2.25.1.ubuntu-xenial_amd64.deb"
-    apt-get install g++ pkg-config git git-buildpackage pandoc debhelper libfuse-dev libattr1-dev -y
     fi
 
     if [ "$ub18check" != "" ]; then
     activated=true
     echo "ub18" > /var/plexguide/mergerfs.version
     wget "https://github.com/trapexit/mergerfs/releases/download/2.25.1/mergerfs_2.25.1.ubuntu-bionic_amd64.deb"
-    apt-get install g++ pkg-config git git-buildpackage pandoc debhelper libfuse-dev libattr1-dev -y
     fi
 
     if [ "$deb9check" != "" ]; then
     activated=true
     echo "deb9" > /var/plexguide/mergerfs.version
     wget "https://github.com/trapexit/mergerfs/releases/download/2.25.1/mergerfs_2.25.1.debian-stretch_amd64.deb"
-    apt-get install g++ pkg-config git git-buildpackage pandoc debhelper libfuse-dev libattr1-dev -y
     fi
 
     if [ "$activated" != "true" ]; then
     activated=true && echo "ub18 - but didn't detect correctly" > /var/plexguide/mergerfs.version
     wget "https://github.com/trapexit/mergerfs/releases/download/2.25.1/mergerfs_2.25.1.ubuntu-bionic_amd64.deb"
-    apt-get install g++ pkg-config git git-buildpackage pandoc debhelper libfuse-dev libattr1-dev -y
     fi
 
     git clone https://github.com/trapexit/mergerfs.git
