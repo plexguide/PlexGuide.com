2cdc73a5005a5af6f5a5d29e138cbbe3f60ad6f4
Admin9705 <24727006+Admin9705@users.noreply.github.com>
2019-02-03 20:48:43 -0500

    update

---
+++ menu/pgcloner/pggce.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+#
+# Title:      PlexGuide (Reference Title File)
+# Author(s):  Admin9705
+# URL:        https://plexguide.com - http://github.plexguide.com
+# GNU:        General Public License v3.0
+################################################################################
+
+### FILL OUT THIS AREA ###
+echo 'pggce' > /var/plexguide/pgcloner.rolename
+echo 'PGGCE' > /var/plexguide/pgcloner.roleproper
+echo 'PlexGuide-GCE' > /var/plexguide/pgcloner.projectname
+echo 'v8.4' > /var/plexguide/pgcloner.projectversion
+
+#━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
+echo "💬 PG Google GCE scripts are setup so that users can deploy any
+Google Cloud Edition container to act as as feeder between two to
+three months!" > /var/plexguide/pgcloner.info
+#━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
+
+### START PROCESS
+bash /opt/plexguide/menu/pgcloner/core/main.sh
