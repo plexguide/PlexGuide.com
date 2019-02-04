3866adc63ab7dfef287b21cf05e641b716a2d224
Admin9705 <24727006+Admin9705@users.noreply.github.com>
2019-02-03 20:58:57 -0500

    update

--- menu/cloudselect/cloudselect.sh
+++ menu/cloudselect/cloudselect.sh
@@ -23,12 +23,11 @@ EOF
 read -p 'Type a Number | Press [ENTER]: ' typed < /dev/tty
 
   if [ "$typed" == "1" ]; then
-    echo gce > /var/plexguide/type.choice && bash /opt/plexguide/menu/core/scripts/main.sh
+    echo gce > /var/plexguide/type.choice
+    bash /opt/plexguide/menu/pgcloner/pggce.sh
+    bash /opt/plexguide/menu/core/scripts/main.sh
 elif [ "$typed" == "2" ]; then
     bash /opt/plexguide/menu/hetzner/hetzner.sh
-#elif [ "$typed" == "3" ]; then
-#    bash /opt/plexguide/menu/amazonaws/amazonaws.sh
-#3 - Cloud Instance: Amazon AWS (NOT WORKING YET)
 elif [ "$typed" == "Z" ] || [ "$typed" == "z" ]; then
     exit
 else
