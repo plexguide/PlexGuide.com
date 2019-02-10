e7f1f33382fc6f494eb54f1570740cc2a8ed07f8
Admin9705 <user@dunn.cloud>
2019-02-10 16:24:24 -0500

    update

--- menu/pgui/mountcheck.sh
+++ menu/pgui/mountcheck.sh
@@ -5,7 +5,9 @@
 # URL:        https://plexguide.com - http://github.plexguide.com
 # GNU:        General Public License v3.0
 ################################################################################
+mkdir -p /opt/appdata/plexguide/emergency
 sleep 15
+diskspace27=0
 
 while true
 do
@@ -31,7 +33,36 @@ if [[ $(rclone lsd --config /opt/appdata/plexguide/rclone.conf pgunion: | grep "
 if [[ $(ls -la /mnt/unionfs | grep "plexguide") == "" ]]; then
   echo "Not Operational ⚠️"> /var/plexguide/pg.umount; else echo "Operational ✅ " > /var/plexguide/pg.umount; fi
 
+# Disk Calculations - 4000000 = 4GB
 
+leftover=$(cat df /opt/appdata/plexguide | tail -n +2 | awk '{print $4}')
+  #used=$(df -h /opt/appdata/plexguide | tail -n +2 | awk '{print $3}')
+  #capacity=$(df -h /opt/appdata/plexguide | tail -n +2 | awk '{print $2}')
+  #percentage=$(df -h /opt/appdata/plexguide | tail -n +2 | awk '{print $5}')
 
-sleep 10
+if [[ "$leftover" -lt "3000000" ]]; then
+  diskspace27=1
+  echo "Emergency: Primary DiskSpace Under 3GB - Stopped Media Programs & Downloading Programs (i.e. Plex, NZBGET, RuTorrent)" > /opt/appdata/plexguide/emergency/message.1
+  docker stop plex 1>/dev/null 2>&1
+  docker stop emby 1>/dev/null 2>&1
+  docker stop jellyfin 1>/dev/null 2>&1
+  docker stop nzbget 1>/dev/null 2>&1
+  docker stop sabnzbd 1>/dev/null 2>&1
+  docker stop rutorrent 1>/dev/null 2>&1
+  docker stop deluge 1>/dev/null 2>&1
+  docker stop qbitorrent 1>/dev/null 2>&1
+elif [[ "$leftover" -gt "3000000" && "$diskspace27" == "1" ]]; then
+  docker start plex 1>/dev/null 2>&1
+  docker start emby 1>/dev/null 2>&1
+  docker start jellyfin 1>/dev/null 2>&1
+  docker start nzbget 1>/dev/null 2>&1
+  docker start sabnzbd 1>/dev/null 2>&1
+  docker start rutorrent 1>/dev/null 2>&1
+  docker start deluge 1>/dev/null 2>&1
+  docker start qbitorrent 1>/dev/null 2>&1
+  rm -rf /opt/appdata/plexguide/emergency/message.1
+  diskspace27=0
+fi
+
+sleep 5
 done
