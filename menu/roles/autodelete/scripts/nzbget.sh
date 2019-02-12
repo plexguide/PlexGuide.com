018100e7d7084f2b60ecf67f63bb52c56ea343bd
Robert Baker <se7enalive@gmail.com>
2019-02-11 17:33:17 -0700

    drop outdated scripts. Let's let blitz/movie/cleanup handle it

--- menu/roles/autodelete/scripts/nzbget.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-find /mnt/downloads/nzbget/tv/* -maxdepth 1 -mmin +240 -type f -exec rm -fv {} 2>/dev/null \;
-find /mnt/downloads/nzbget/music/* -maxdepth 1 -mmin +240 -type f -exec rm -fv {} 2>/dev/null \;
-find /mnt/downloads/nzbget/movies/* -maxdepth 1 -mmin +240 -type f -exec rm -fv {} 2>/dev/null \;
-find /mnt/downloads/nzbget/ebooks/* -maxdepth 1 -mmin +240 -type f -exec rm -fv {} 2>/dev/null \;
-find /mnt/downloads/nzbget/abooks/* -maxdepth 1 -mmin +240 -type f -exec rm -fv {} 2>/dev/null \;
--- menu/roles/autodelete/scripts/sabnzbd.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-find /mnt/downloads/sabnzbd/abooks/* -type d -mmin +240 -ls -exec rm -rf {} + 2>/dev/null
-find /mnt/downloads/sabnzbd/ebooks/* -type d -mmin +240 -ls -exec rm -rf {} + 2>/dev/null
-find /mnt/downloads/sabnzbd/movies/* -type d -mmin +240 -ls -exec rm -rf {} + 2>/dev/null
-find /mnt/downloads/sabnzbd/music/*  -type d -mmin +240 -ls -exec rm -rf {} + 2>/dev/null
-find /mnt/downloads/sabnzbd/tv/* -type d -mmin +240 -ls -exec rm -rf {} + 2>/dev/null
