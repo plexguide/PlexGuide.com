e87a60ec0a5b1c99b0b2b206cf654792534c1a3b
Robert Baker <se7enalive@gmail.com>
2019-02-20 15:22:50 -0700

    fix

--- menu/dlpath/rebuild.sh
+++ menu/dlpath/rebuild.sh
@@ -24,7 +24,8 @@ count=$(wc -l < /var/plexguide/container.running)
 
 for ((i=1; i<$count+1; i++)); do
 	app=$(sed "${i}q;d" /var/plexguide/container.running)
-	ansible-playbook /opt/coreapps/apps/$app.yml
+	if [ -e "/opt/coreapps/apps/$app.yml" ]; then ansible-playbook /opt/coreapps/apps/$app.yml; fi
+	if [ -e "/opt/coreapps/communityapps/$app.yml" ]; then ansible-playbook /opt/communityapps/apps/$app.yml; fi
 done
 
 echo ""
