04161b12a5aa46de9962cf80170ef5c4703ffd27
Admin9705 <user@dunn.cloud>
2019-02-24 18:33:26 -0500

    update

--- menu/functions/install.sh
+++ menu/functions/install.sh
@@ -79,7 +79,7 @@ pginstall () {
 
 
 touch /var/plexguide/install.roles
-rolenumber=2
+rolenumber=3
   # Roles Ensure that PG Replicates and has once if missing; important for startup, cron and etc
 if [[ $(cat /var/plexguide/install.roles) != "$rolenumber" ]]; then
   rm -rf /opt/communityapps
