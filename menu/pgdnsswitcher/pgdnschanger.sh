5ca7a2a925b15aaa740fad6a87dbf0b4e0096bf3
MrDoobPG <46342172+MrDoobPG@users.noreply.github.com>
2019-03-10 17:58:45 +0100

    line fix 51
    from if to elif | n00b fail ^^

--- menu/pgdnsswitcher/pgdnschanger.sh
+++ menu/pgdnsswitcher/pgdnschanger.sh
@@ -42,52 +42,45 @@ EOF
 # Standby
 read -p 'Type a Number | Press [ENTER]: ' typed < /dev/tty
 
-if [ "$typed" == "1" ]; then		
+if [ "$typed" == "1" ]; then
 	sed -i '/nameserver/s/^#*/#/g' /etc/resolv.conf
 	sed -i '/nameserver/s/^#*/#/g' /etc/resolv.conf
 	sed -i '$a\nameserver 8.8.8.8\' /etc/resolv.conf
 	sed -i '$a\nameserver 8.8.4.4\' /etc/resolv.conf
-	
-if [ "$typed" == "2" ]; then		
+elif [ "$typed" == "2" ]; then
 	sed -i '/nameserver/s/^#*/#/g' /etc/resolv.conf
 	sed -i '/nameserver/s/^#*/#/g' /etc/resolv.conf
 	sed -i '$a\nameserver 8.8.8.8\' /etc/resolv.conf
 	sed -i '$a\nameserver 8.8.4.4\' /etc/resolv.conf
 	sed -i '$a\nameserver2001:4860:4860::8844\' /etc/resolv.conf
 	sed -i '$a\nameserver2001:4860:4860::8888\' /etc/resolv.conf
-	
-elif [ "$typed" == "3" ]; then		
+elif [ "$typed" == "3" ]; then
 	sed -i '/nameserver/s/^#*/#/g' /etc/resolv.conf
 	sed -i '/nameserver/s/^#*/#/g' /etc/resolv.conf
 	sed -i '$a\nameserver 1.1.1.1\' /etc/resolv.conf
 	sed -i '$a\nameserver 1.0.0.1\' /etc/resolv.conf
-	
-elif [ "$typed" == "4" ]; then		
+elif [ "$typed" == "4" ]; then
 	sed -i '/nameserver/s/^#*/#/g' /etc/resolv.conf
 	sed -i '/nameserver/s/^#*/#/g' /etc/resolv.conf
 	sed -i '$a\nameserver 1.1.1.1\' /etc/resolv.conf
 	sed -i '$a\nameserver 1.0.0.1\' /etc/resolv.conf
 	sed -i '$a\nameserver 2606:4700:4700::1111\' /etc/resolv.conf
 	sed -i '$a\nameserver 2606:4700:4700::1001\' /etc/resolv.conf	
-
-elif [ "$typed" == "5" ]; then		
+elif [ "$typed" == "5" ]; then
 	sed -i '/nameserver/s/^#*/#/g' /etc/resolv.conf
 	sed -i '/nameserver/s/^#*/#/g' /etc/resolv.conf
 	sed -i '$a\nameserver 208.67.222.222\' /etc/resolv.conf
 	sed -i '$a\nameserver 208.67.220.220\' /etc/resolv.conf
-
 elif [ "$typed" == "6" ]; then		
 	sed -i '/nameserver/s/^#*/#/g' /etc/resolv.conf
 	sed -i '/nameserver/s/^#*/#/g' /etc/resolv.conf
 	sed -i '$a\nameserver 8.26.56.26\' /etc/resolv.conf
-	sed -i '$a\nameserver 8.20.247.20\' /etc/resolv.conf	
-		
+	sed -i '$a\nameserver 8.20.247.20\' /etc/resolv.conf		
 elif [ "$typed" == "Z" ] || [ "$typed" == "z" ]; then
   exit
 else
   bash /opt/plexguide/menu/tools/tools.sh
   exit
 fi
-
 bash /opt/plexguide/menu/tools/tools.sh
-exit			
+exit
