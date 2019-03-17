bcaf104b08771b6d15600fbd18b3e3a0e0246157
Robert Baker <se7enalive@gmail.com>
2019-03-17 16:44:44 -0700

    fix

--- menu/pgcloner/pgclone.sh
+++ menu/pgcloner/pgclone.sh
@@ -10,7 +10,7 @@
 echo 'pgclone' > /var/plexguide/pgcloner.rolename
 echo 'PG Clone' > /var/plexguide/pgcloner.roleproper
 echo 'PGClone' > /var/plexguide/pgcloner.projectname
-echo '8.6test6' > /var/plexguide/pgcloner.projectversion
+echo 'v8.6' > /var/plexguide/pgcloner.projectversion
 echo 'pgclone.sh' > /var/plexguide/pgcloner.startlink
 
 #━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
