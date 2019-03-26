a196385d7dc0a918f4ef9312b9be0bf52147a600
Admin9705 <user@dunn.cloud>
2019-03-26 11:48:17 -0400

    update

--- menu/pgcloner/pgclone.sh
+++ menu/pgcloner/pgclone.sh
@@ -10,7 +10,7 @@
 echo 'pgclone' > /var/plexguide/pgcloner.rolename
 echo 'PG Clone' > /var/plexguide/pgcloner.roleproper
 echo 'PGClone' > /var/plexguide/pgcloner.projectname
-echo 'v8.6' > /var/plexguide/pgcloner.projectversion
+echo '8.6beta3' > /var/plexguide/pgcloner.projectversion
 echo 'pgclone.sh' > /var/plexguide/pgcloner.startlink
 
 #━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
