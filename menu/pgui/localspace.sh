fd308fdf42b840d04d0f1f23fd76385da574e58f
MrDoobPG <46342172+MrDoobPG@users.noreply.github.com>
2019-06-22 12:04:11 +0200

    Update localspace.sh

--- menu/pgui/localspace.sh
+++ menu/pgui/localspace.sh
@@ -9,15 +9,17 @@
 while [ 1 ]; do
 
  rm -rf /var/plexguide/spaceused.log
+ rm -rf /var/plexguide/trafficused.log
+ rm -rf /var/plexguide/incomplete-used.log
 
 	 du -sh /mnt/move | awk '{print $1}' >> /var/plexguide/spaceused.log
 	 du -sh /mnt/downloads | awk '{print $1}' >> /var/plexguide/spaceused.log
 
-echo "Used Traffic | last 3 days"  >> /var/plexguide/logs/spaceused.log
+echo "Used Traffic | last 3 days"  >> /var/plexguide/trafficused.log
          
-         vnstat -d | tail -n 5 | head -n 3 >> /var/plexguide/logs/trafficused.log
+         vnstat -d | tail -n 5 | head -n 3 >> /var/plexguide/trafficused.log
 #used space of incomplete        
-         du -sh /mnt/incomplete | awk '{print $1}' >> /var/plexguide/logs/incomplete-used.log
+         du -sh /mnt/incomplete | awk '{print $1}' >> /var/plexguide/incomplete-used.log
 
 sleep 30
 
