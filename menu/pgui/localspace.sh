c3fc3abe9cf9f1ba01aaec83a9d74e4c9b51fa47
MrDoobPG <46342172+MrDoobPG@users.noreply.github.com>
2019-06-23 21:51:05 +0200

    Update localspace.sh
    ifconfig added for automatic catching the network card ; autoinstall for vnstat added ..
    
    cheesball added for loop running

--- menu/pgui/localspace.sh
+++ menu/pgui/localspace.sh
@@ -2,10 +2,23 @@
 #
 # Title:      PGBlitz (local used space)
 # Author(s):  Admin9705
-# Coder: 	    MrDoob
+# Coder:      MrDoob
 # URL:        https://pgblitz.com - http://github.pgblitz.com
 # GNU:        General Public License v3.0
 ################################################################################
+
+# Starting Actions
+#networktools | vnstat | vnstati install | please wait"
+        apt-get install ethtool vnstat vnstati -yqq 2>&1 >> /dev/null
+        network=$(ifconfig | grep -E 'eno1|enp|ens5' | awk '{print $1}' | sed -e 's/://g' )
+                sed -i 's/eth0/'$network'/g' /etc/vnstat.conf
+                sed -i 's/UnitMode 0/UnitMode 1/g' /etc/vnstat.conf
+                sed -i 's/RateUnit 1/RateUnit 0/g' /etc/vnstat.conf
+                sed -i 's/Locale "-"/Locale "LC_ALL=en_US.UTF-8"/g' /etc/vnstat.conf
+        /etc/init.d/vnstat restart 2>&1 >> /dev/null
+
+startscript () {
+
 while [ 1 ]; do
 
  rm -rf /var/plexguide/spaceused.log
@@ -14,17 +27,23 @@ while [ 1 ]; do
 
 # move and downloads for the UI
 
-	 du -sh /mnt/move | awk '{print $1}' >> /var/plexguide/spaceused.log
-	 du -sh /mnt/downloads | awk '{print $1}' >> /var/plexguide/spaceused.log
+         du -sh /mnt/move | awk '{print $1}' >> /var/plexguide/spaceused.log
+         du -sh /mnt/downloads | awk '{print $1}' >> /var/plexguide/spaceused.log
 
-echo "Used Traffic | last 3 days"  >> /var/plexguide/logs/trafficused.log         
+echo "Used Traffic | last 3 days"  >> /var/plexguide/logs/trafficused.log
 
          vnstat -d | tail -n 5 | head -n 3 >> /var/plexguide/logs/trafficused.log
 
-#used space of incomplete        
- 
+#used space of incomplete
+
         du -sh /mnt/incomplete | awk '{print $1}' >> /var/plexguide/logs/incomplete-used.log
 
-sleep 30
+sleep 60
 
 done
+
+}
+
+# keeps the function in a loop
+cheeseballs=0
+while [[ "$cheeseballs" == "0" ]]; do startscript; done
