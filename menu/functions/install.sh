c8bcbd61fb7a6def20f94cb2d80b2da9f230aefa
deva5610 <40216384+deva5610@users.noreply.github.com>
2019-07-09 00:23:36 +0800

    Update install.sh

--- menu/functions/install.sh
+++ menu/functions/install.sh
@@ -208,7 +208,7 @@ gcloud () {
   export CLOUD_SDK_REPO="cloud-sdk-$(lsb_release -c -s)"
   echo "deb http://packages.cloud.google.com/apt $CLOUD_SDK_REPO main" | sudo tee -a /etc/apt/sources.list.d/google-cloud-sdk.list
   curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add
-  sudo apt-get update && sudo apt-get install google-cloud-sdk -y
+  sudo apt-get update && sudo apt-get install google-cloud-sdk
 }
 
 mergerinstall () {
