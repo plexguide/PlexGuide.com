4473071e16a6c3ed46b84079b3d2e0e1f2113685
Admin0705 <user@dunn.cloud>
2019-11-09 09:45:23 -0500

    update

---
+++ install/README.md
@@ -0,0 +1,23 @@
+##### WANT TO HELP? CLICK THE ★ (STAR LOGO) in the Upper-Right! 
+<p align="center">
+  <a href="https://pgblitz.com/forums" target="_blank" /a><img src="https://pgblitz.com/wikipics/logo-forums.png" width="160"/>   
+  <a href="https://github.com/PGBlitz/PGBlitz.com/wiki" target="_blank" /a><img src="https://pgblitz.com/wikipics/logo-wiki.png" width="160"/>
+  <a href="https://pgblitz.com/threads/plexguide-install-instructions.243/" target="_blank" /a><img src="https://pgblitz.com/wikipics/logo-pg-install.png" width="160"/> 
+  <a href="https://pgblitz.com/account/upgrades" target="_blank" /a><img src="https://pgblitz.com/wikipics/logo-donate.png" width="160"/>
+</p>
+
+* 📂 [**[Click Here]**](https://goo.gl/7NR3Da) - Google G-Suite (Unlimited Hard Drive Space & Storage)
+* 📂 [**[Click Here]**](https://controlpanel.newshosting.com/signup/index.php?promo=partners&a_aid=5a65169240efd&a_bid=5ecfe99b) - Top Performance NewsHost! - Blitz Members Receive a 58% Discount
+----
+### **Reference Shortcut -** http://wiki.pgblitz.com | Discord ( !wiki )
+----
+
+## 1. PG YouTube
+
+<p align="center"><kbd><a href="https://youtu.be/joqL_zjl0pE" /a><img src="https://github.com/PGBlitz/Assets/blob/master/ycovers/mainintro.png" width="400"></kbd></p>
+<p align="center"><b>PGBlitz Introduction Video</b></p>
+
+<p align="center"><kbd><a href="https://youtu.be/8lotdbpsrUE" /a><img src="https://github.com/PGBlitz/Assets/blob/master/ycovers/introv10.png" width="400"></kbd></p>
+<p align="center"><b>PGBlitz Installation Video</b></p>
+
+[**[Click Here]**](https://pgblitz.com/threads/plexguide-install-instructions.243/) for installation instructions to start the process
---
+++ install/install.sh
@@ -0,0 +1,162 @@
+#!/bin/bash
+#
+# Title:      PGBlitz (Reference Title File)
+# Author(s):  Admin9705 - Deiteq
+# URL:        https://pgblitz.com - http://github.pgblitz.com
+# GNU:        General Public License v3.0
+################################################################################
+package="curl wget software-properties-common git nano htop mc lshw zip unzip dialog sudo"
+
+tee <<-EOF
+━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
+🌎  NOTICE: PGBlitz Version 9 - Installer
+━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
+By installing, you agreeing to the terms and conditions of the GNUv3 License!
+Thanks To: You, the Linux Noobs, Sponsors, Supporters, Community & You!
+━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
+Please Standby!
+EOF
+#sleep 4
+
+# Delete If it Exist for Cloning
+if [ -e "/pg/blitz" ]; then rm -rf /pg/blitz; fi
+if [ -e "/pg/stage" ]; then rm -rf /pg/stage; fi
+rm -rf /pg/stage/place.holder 1>/dev/null 2>&1
+
+# Make Critical Folders
+mkdir -p /pg /pg/logs /pg/data /pg/stage /pg/logs /pg/tmp /pg/var/install
+chmod 775 /pg /pg/logs /pg/data /pg/stage /pg/logs /pg/tmp /pg/var/install
+chown 1000:1000 /pg /pg/logs /pg/data /pg/stage /pg/logs /pg/tmp /pg/var/install
+rm -rf /pg/var/first.update 1>/dev/null 2>&1
+
+# Clone the Program to Stage for Installation
+git clone -b v1 --single-branch https://github.com/PGBlitz/Stage.git /pg/stage
+
+# Checking to See if the Installer ever Installed Python; if so... skip
+var37="/pg/var/python.firstime"
+if [ ! -e "${var37}" ]; then
+  bash /pg/stage/pyansible.sh
+  touch /pg/var/python.firstime
+fi
+
+tee <<-EOF
+
+━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
+⌛  Executing a Base Install - Please Standby
+━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
+EOF
+apt-get install lsb-release -yqq 2>&1 >> /dev/null
+	export DEBIAN_FRONTEND=noninteractive
+apt-get install software-properties-common -yqq 2>&1 >> /dev/null
+	export DEBIAN_FRONTEND=noninteractive
+
+tee <<-EOF
+
+━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
+⌛  Updating the Server - Please Standby (Can Take 1-2 Minutes)
+━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
+EOF
+
+fullrel=$(lsb_release -sd)
+osname=$(lsb_release -si)
+relno=$(lsb_release -sr)
+relno=$(printf "%.0f\n" "$relno")
+hostname=$(hostname -I | awk '{print $1}')
+# add repo
+
+if echo $osname "Debian" &>/dev/null; then
+	add-apt-repository main 2>&1 >> /dev/null
+	add-apt-repository non-free 2>&1 >> /dev/null
+	add-apt-repository contrib 2>&1 >> /dev/null
+elif echo $osname "Ubuntu" &>/dev/null; then
+	add-apt-repository main 2>&1 >> /dev/null
+	add-apt-repository universe 2>&1 >> /dev/null
+	add-apt-repository restricted 2>&1 >> /dev/null
+	add-apt-repository multiverse 2>&1 >> /dev/null
+elif echo $osname "Rasbian" "Fedora" "CentOS"; then
+tee <<-EOF
+
+━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
+⛔ System Warning!
+━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
+Supported: UB 16/18.04 ~ LTS/SERVER and Debian 9+
+This server may not be supported due to having the incorrect OS detected!
+
+For more information, read:
+https://pgblitz.com/threads/pg-install-instructions.243/
+EOF
+  sleep 10
+fi
+
+apt-get update -yqq 2>&1 >> /dev/null
+	export DEBIAN_FRONTEND=noninteractive
+apt-get upgrade -yqq 2>&1 >> /dev/null
+	export DEBIAN_FRONTEND=noninteractive
+apt-get dist-upgrade -yqq 2>&1 >> /dev/null
+	export DEBIAN_FRONTEND=noninteractive
+apt-get autoremove -yqq 2>&1 >> /dev/null
+	export DEBIAN_FRONTEND=noninteractive
+apt-get install $package -yqq 2>&1 >> /dev/null
+	export DEBIAN_FRONTEND=noninteractive
+
+tee <<-EOF
+
+━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
+✅ Finished - Basic Updates
+━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
+EOF
+
+ansible-playbook /pg/stage/clone.yml
+bash /pg/stage/pgcloner/solo/update.sh
+
+# Copy Starting Commands for PGBlitz
+path="/pg/stage/alias"
+cp -t /bin $path/plexguide $path/pg $path/pgblitz
+
+# Verifying the Commands Installed
+tee <<-EOF
+
+━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
+⌛  Verifiying Started Commands Installed via @ /bin
+━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
+EOF
+
+# Installation fails if the pgblitz command is not in the correct location
+file="/bin/pgblitz"
+if [ ! -e "$file" ]; then
+
+tee <<-EOF
+
+━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
+⛔️  WARNING! The PGBlitz Installer Failed ~ http://pgblitz.wiki
+━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
+Please Reinstall PGBlitz by running the Command Again! This step is to
+ensure that everything is working prior to the install!
+Ensure that you utilizing the correct versions of linux as described on
+the installation page!
+━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
+EXITING!!!
+EOF
+exit
+fi
+
+# If nothing failed, notify the user that installation passed!
+tee <<-EOF
+
+━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
+✅️  PASSED! The PGBlitz Command Installed! ~ http://pgblitz.wiki
+━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
+EOF
+
+# creates a blank var; if this executes; pgblitz will go through the install process
+touch /pg/var/new.install
+
+chmod 775 /bin/plexguide /bin/pgblitz /bin/pg
+chown 1000:1000 /bin/plexguide /bin/pgblitz /bin/pg
+
+tee <<-EOF
+
+━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
+↘️  Start AnyTime By Typing >>> pgblitz [or] plexguide [or] pg
+━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
+EOF
