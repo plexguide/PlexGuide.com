d2ab5e36b90aeccc2425484d7ddaa2d1478f0781
Admin9705 <24727006+Admin9705@users.noreply.github.com>
2020-03-22 23:05:35 -0400

    update

--- mods/install/main.sh
+++ mods/install/main.sh
@@ -44,6 +44,12 @@ install_folders
 common_header "⌛ INSTALLING: Docker"; sleep 2
 ansible-playbook /pg/mods/ymls/pg.yml --tags docker
 
+common_header "⌛ INSTALLING: RClone"; sleep 2
+ansible-playbook /pg/mods/ymls/pg.yml --tags rcloneinstall
+
+common_header "⌛ INSTALLING: Google's Cloud SDK"; sleep 2
+ansible-playbook /pg/mods/ymls/pg.yml --tags google_sdk
+
 echo "HALTED - Install Check  "
 
 exit
