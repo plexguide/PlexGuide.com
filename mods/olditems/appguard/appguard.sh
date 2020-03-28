5eadd42433e2027ef5b3d75cdaee5c856dc5c65a
Admin9705 <24727006+Admin9705@users.noreply.github.com>
2020-03-28 15:21:36 -0400

    update

--- mods/olditems/appguard/appguard.sh
+++ mods/olditems/appguard/appguard.sh
@@ -1,7 +1,4 @@
 #!/bin/bash
-#
-# Title:      PlexGuide (Reference Title File)
-# Author(s):  Admin9705 - Deiteq
 # URL:        https://plexguide.com - http://github.pgblitz.com
 # GNU:        General Public License v3.0
 ################################################################################
