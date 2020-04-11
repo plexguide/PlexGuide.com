0457f5ff5822ba94c0454ed356b7a55f5447609a
Admin9705 <24727006+Admin9705@users.noreply.github.com>
2020-04-11 09:35:03 -0400

    update

--- mods/functions/.create.sh
+++ mods/functions/.create.sh
@@ -3,29 +3,30 @@
 # GNU:        General Public License v3.0
 ################################################################################
 
-## if DOESNT EXIST ## avoid replicating # note
+## remove old master file if it exist
+rm -rf "$pgfunctions"/.master.sh ##
 
+## paths
 pgfunctions="/pg/mods/functions"
 pgprimary="/pg/mods/containers/primary"
 pgcommunity="/pg/mods/containers/community"
 
 ## reads functions and stores to a temporary file
-ls "$pgfunctions" > "$pgfunctions"/.files.sh
-ls "$pgprimary" >> "$pgfunctions"/.files.sh
-ls "$pgfunction" >> "$pgfunctions"/.files.sh
-
-## remove old master file if it exist
-rm -rf "$pgfunctions"/.master.sh ##
-
-cat <<- EOF > "$pgfunctions/.master.sh"
-#!/bin/bash
-# URL:        PlexGuide.com / PGBlitz.com
-# GNU:        General Public License v3.0
-################################################################################
-
-EOF
+ls "$pgfunctions" > "$pgfunctions"/.functions.sh
+ls "$pgprimary" >> "$pgfunctions"/.primary.sh
+ls "$pgfunction" >> "$pgfunctions"/.community.sh
 
 ## adds tempory information to complete master functions file
 while read p; do
   echo "source $pgfunctions/$p" >> "$pgfunctions"/.master.sh
-done </"$pgfunctions"/.files.sh
+done </"$pgfunctions"/.functions.sh
+
+## adds tempory information to complete master functions file
+while read p; do
+  echo "source $pgprimary/$p" >> "$pgfunctions"/.master.sh
+done </"$pgprimary"/.primary.sh
+
+## adds tempory information to complete master functions file
+while read p; do
+  echo "source $pgcommunity/$p" >> "$pgfunctions"/.master.sh
+done </"$pgcommunity"/.community.sh
