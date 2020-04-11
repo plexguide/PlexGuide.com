ccaac178fa0e4f53403113d291c297e3015f3881
Admin9705 <24727006+Admin9705@users.noreply.github.com>
2020-04-10 20:26:25 -0400

    update

--- mods/apps/netdata.sh
+++ mods/apps/netdata.sh
@@ -21,8 +21,8 @@ cat <<- EOF > "/pg/tmp/$pgrole.yml"
           # traefik.frontend.auth.forward.address: '{{gauth}}'
           traefik.enable: 'true'
           traefik.port: '$port_inside'
-          traefik.frontend.rule: 'Host:$pgrole.{{domain.stdout}},{{tldset}}'
-          traefik.frontend.headers.SSLHost: '{{domain.stdout}}'
+          traefik.frontend.rule: 'Host:$pgrole.$domain,{{tldset}}'
+          traefik.frontend.headers.SSLHost: '$domain'
           traefik.frontend.headers.SSLRedirect: 'true'
           traefik.frontend.headers.STSIncludeSubdomains: 'true'
           traefik.frontend.headers.STSPreload: 'true'
@@ -31,13 +31,6 @@ cat <<- EOF > "/pg/tmp/$pgrole.yml"
           traefik.frontend.headers.contentTypeNosniff: 'true'
           traefik.frontend.headers.customResponseHeaders: 'X-Robots-Tag:noindex,nofollow,nosnippet,noarchive,notranslate,noimageindex'
           traefik.frontend.headers.forceSTSHeader: 'true'
-
-    - name: 'Setting PG Volumes'
-      set_fact:
-        pg_volumes:
-          - '/sys:/host/sys:ro'
-          - '/proc:/host/proc:ro'
-          - '/var/run/docker.sock:/var/run/docker.sock'
 
     - name: 'Setting PG ENV'
       set_fact:
@@ -55,15 +48,20 @@ cat <<- EOF > "/pg/tmp/$pgrole.yml"
           - SYS_PTRACE
         published_ports:
           - 127.0.0.1:$port_inside01:$port_outside01'
-        volumes: '{{pg_volumes}}'
+        volumes:
+          - '/sys:/host/sys:ro'
+          - '/proc:/host/proc:ro'
+          - '/var/run/docker.sock:/var/run/docker.sock'
         env: '{{pg_env}}'
         restart_policy: unless-stopped
         networks:
           - name: plexguide
             aliases:
-              - '{{pgrole}}'
+              - '$pgrole'
         security_opts:
           - apparmor:unconfined
         state: started
-        labels: '{{pg_labels}}'
+        labels:
+
+
 EOF
