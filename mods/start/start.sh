c8cdebde79d60bbe3c5494c9209678415138f552
Admin9705 <24727006+Admin9705@users.noreply.github.com>
2020-04-16 09:21:19 -0400

    update

--- mods/start/start.sh
+++ mods/start/start.sh
@@ -1,10 +1,5 @@
 #!/bin/bash
-#
-# Title:      PlexGuide (Reference Title File)
-# Author(s):  Admin9705
-# URL:        https://plexguide.com - http://github.plexguide.com
-# GNU:        General Public License v3.0
-################################################################################
+############################################################## PlexGuide.com ###
 source /pg/mods/functions/.master.sh
 
 variable () {
