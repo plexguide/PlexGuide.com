f63f683b6328dc408d2eaebae9fbd485d8aed897
Admin9705 <24727006+Admin9705@users.noreply.github.com>
2020-04-18 13:59:31 -0400

    update

--- mods/functions/.create.sh
+++ mods/functions/.create.sh
@@ -1,27 +1,29 @@
 #!/bin/bash
 ############# https://github.com/plexguide/PlexGuide.com/graphs/contributors ###
 
-## paths
+# paths
 pgprimary="/pg/mods/containers/primary"
 pgcommunity="/pg/mods/containers/community"
 fpath="/pg/mods/functions"
 
-## remove old master file if it exist
+# remove old master file if it exist
 rm -rf "$fpath"/.master.sh ##
 
-## reads functions and stores to a temporary file
+# reads functions and stores to a temporary file
 ls "$fpath" > "$fpath"/.functions.sh
 ls "$pgprimary" > "$fpath"/.primary.sh
 ls "$pgcommunity" > "$fpath"/.community.sh
 
-## adds tempory information to complete master functions file
+# adds tempory information to complete master functions file
 while read p; do
   echo "source $fpath/$p" >> "$fpath"/.master.sh
 done </"$fpath"/.functions.sh
 
-## adds tempory information to complete master functions file
+# adds tempory information to complete master functions file
 echo "source $fpath/.master.sh" > "$fpath"/.primary_apps.sh
+echo "source $fpath/.master.sh" > "$fpath"/.community_apps.sh
 
+# builds apps for primary apps
 while read p; do
   echo "source $pgprimary/$p" >> "$fpath"/.master.sh
   echo "source $pgprimary/$p" >> "$fpath"/.primary_apps.sh
@@ -29,6 +31,7 @@ while read p; do
 done </"$fpath"/.primary.sh
   bash "$fpath"/.primary_apps.sh
 
+# builds apps for community apps
 while read p; do
   echo "source $pgcommunity/$p" >> "$fpath"/.master.sh
   echo "source $pgcommunity/$p" >> "$fpath"/.community_apps.sh
