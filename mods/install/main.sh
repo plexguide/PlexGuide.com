ead1fb1b42bc4e08dc4030178cb0a4f206aacd64
Admin9705 <24727006+Admin9705@users.noreply.github.com>
2020-04-19 00:24:14 -0400

    update

--- mods/install/main.sh
+++ mods/install/main.sh
@@ -4,7 +4,7 @@ mkdir -p /pg/var/
 if [[ -e "/pg/var/pg.noinstall" ]]; then pgcmd=true; else pgcmd=false; fi
 rm -rf /pg/var/pg.noinstall
 ################################################################################
-if [[ ! -e "/usr/bin/docker" ]]; then echo "" > /bin/docker else rm -rf /bin/docker; fi
+if [[ ! -e "/usr/bin/docker" ]]; then echo "" > /bin/docker; else rm -rf /bin/docker; fi
 if [[ -e "/bin/docker" ]]; then chmod 0755 /bin/docker && chown 1000:1000 /bin/docker; fi
 ################################################################################
 if [[ "$pgcmd" != "true" ]]; then
