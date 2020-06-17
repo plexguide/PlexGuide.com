d261bf01bd140c46433bffd4452f1f539fc5285e
Admin9705 <24727006+Admin9705@users.noreply.github.com>
2020-06-16 21:35:04 -0400

    update

--- mods/containers/community/.pendingremake/templates/dokuwiki/users.auth.php
+++
@@ -1,1 +0,0 @@
-admin:$1$4fd0ad31$.cId7p1uxI4a.RcrH81On0:DokuWiki Administrator:mail@host.com:admin,user
--- mods/containers/primary/.pendingremake/templates/radarr/scripts/cleanup-radarr.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-if [ -d "$radarr_moviefile_sourcefolder" ] && [ "$(basename $radarr_moviefile_sourcefolder)" = "deluge_extracted" ] ; then
-    /bin/rm -rf $radarr_moviefile_sourcefolder
-fi
